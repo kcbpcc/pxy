c3f7f09f05ea7806ba31f6103d9aaa7974529d85
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-28 14:08:12 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -493,7 +493,7 @@ try:
                         row['qty'] > 0 and
                         row['product'] == 'CNC' and
                         row['PnL%'] > 1.4 and
-                        ((row['PnL%'] < ((row['PXY'])) and row['PnL%_H'] > ((row['PXY']))) or (row['PnL%'] > TIMPXY))
+                        ((row['PnL%'] < ((row['pxy'])) and row['PnL%_H'] > ((row['pxy']))) or (row['PnL%'] > TIMPXY))
                         #or ((row['high'] > row['close'] and row['ltp'] < row['close']) if nse_action == "SuperBear" else False)
 
                    
