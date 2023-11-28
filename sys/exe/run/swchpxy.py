b218addcf7fba1a8070d161858934acd7c885cd7
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-28 08:25:22 +0530

    Update swchpxy.py

--- sys/exe/run/swchpxy.py
+++ sys/exe/run/swchpxy.py
@@ -1,16 +1,35 @@
 import yfinance as yf
-from datetime import datetime
+import os
+import sys
 
-def analyze_stock(symbol):
-    # Get today's date
-    today_date = datetime.now().strftime('%Y-%m-%d')
+def get_nse_action():
+    # Define the stock symbol (NSEI for Nifty 50)
+    def analyze_stock(symbol):
 
-    # Fetch historical stock data for today
-    stock_data = yf.download(symbol, start=today_date, end=today_date)
+    try:
+        # Redirect standard output to os.devnull to suppress messages
+        sys.stdout = open(os.devnull, 'w')
 
+        # Download today's data
+        data = yf.download(stock_symbol, period="5d")
+    except Exception as e:
+        print(f"Error during data download: {e}")
+        return "Error", None
+    finally:
+        # Restore standard output
+        sys.stdout.close()
+        sys.stdout = sys.__stdout__
+
+    # Extract today's open, yesterday's close, and current price
+    today_open = data['Open'].iloc[-1]
+    today_high = data['High'].iloc[-1]
+    today_low = data['Low'].iloc[-1]
+    yesterday_close = data['Close'].iloc[-2]
+    current_price = data['Close'].iloc[-1]
+    
     # Check if the conditions are met
-    below_open_high_above_open = (stock_data['Low'] < stock_data['Open']) & (stock_data['High'] > stock_data['Open'])
-    above_open_low_below_open = (stock_data['High'] > stock_data['Open']) & (stock_data['Low'] < stock_data['Open'])
+    below_open_high_above_open = (today_low < today_open) & (today_high > today_open)
+    above_open_low_below_open = (today_high > today_open) & (today_low < today_open)
 
     if below_open_high_above_open.any() or above_open_low_below_open.any():
         return 'yes'
