f8b45a098e9e53b87006455bfcf12d30f7f2e721
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-29 07:02:40 +0530

    Update dngrpxy.py

--- sys/exe/run/dngrpxy.py
+++ sys/exe/run/dngrpxy.py
@@ -1,36 +1,23 @@
 import yfinance as yf
-import os
-import sys
-
-# Define the stock symbol (NSEI for Nifty 50)
 
 def dangerbear(symbol):
     try:
-        # Redirect standard output to os.devnull to suppress messages
-        sys.stdout = open(os.devnull, 'w')
-
         # Download today's data
         data = yf.download(symbol, period="5d")
     except Exception as e:
         print(f"Error during data download: {e}")
-        return "Error", None
-    finally:
-        # Restore standard output
-        #sys.stdout = sys.__stdout__
+        return 'no'  # Assume 'no' in case of an error
 
-    # Extract today's open, yesterday's close, and current price
-    yesterday_open = today_open = data['Open'].iloc[-2]
+    # Extract relevant values
+    yesterday_open = data['Open'].iloc[-2]
     today_open = data['Open'].iloc[-1]
-    today_high = data['High'].iloc[-1]
-    today_low = data['Low'].iloc[-1]
     yesterday_close = data['Close'].iloc[-2]
     current_price = data['Close'].iloc[-1]
 
     # Check if the conditions are met
-    dangerbear = (yesterday_close < yesterday_open) & (current_price < today_open) & (current_price < yesterday_close) 
-  
+    danger_bear = (yesterday_close < yesterday_open) and (current_price < today_open) and (current_price < yesterday_close)
 
-    if DangerBear.any() :
+    if danger_bear:
         return 'yes'
     else:
         return 'no'
