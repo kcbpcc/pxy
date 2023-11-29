993e92a934445230aeae26871bc4bf3fc6e2fa88
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-30 05:12:06 +0530

    Update buypxy.py

--- sys/exe/run/buypxy.py
+++ sys/exe/run/buypxy.py
@@ -2,7 +2,7 @@ from toolkit.logger import Logger
 from toolkit.currency import round_to_paise
 from toolkit.utilities import Utilities
 from login_get_kite import get_kite
-from constants import dir_path, fileutils, buff, max_target
+from constants import dir_path, fileutils
 from holdings import get
 from trendlyne import Trendlyne
 import pandas as pd
