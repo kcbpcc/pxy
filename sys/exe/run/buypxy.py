4b19bf69ed3380846a84ff96d6779f22916bc6a4
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-29 13:35:43 +0530

    Update buypxy.py

--- sys/exe/run/buypxy.py
+++ sys/exe/run/buypxy.py
@@ -17,6 +17,7 @@ import asyncio
 logging = Logger(10)
 holdings = dir_path + "holdings.csv"
 black_file = dir_path + "blacklist.txt"
+
 try:
     sys.stdout = open('output.txt', 'w')
     broker = get_kite(api="bypass", sec_dir=dir_path)
@@ -70,26 +71,26 @@ if decision == "YES":
             # get lists from positions and orders
             lst_dct_positions = broker.positions
             lst_dct_orders = [order for order in broker.orders if order.get('status') == 'OPEN']
-            
+
             if lst_dct_positions and any(lst_dct_positions):
                 symbols_positions = [dct['symbol'] for dct in lst_dct_positions]
             else:
                 symbols_positions = []
-            
+
             if lst_dct_orders and any(lst_dct_orders):
                 symbols_orders = [dct['symbol'] for dct in lst_dct_orders]
             else:
                 symbols_orders = []
-            
+
             # Combine symbols from positions and orders
             all_symbols = symbols_positions + symbols_orders
-            
+
             # Assuming lst_tlyne is defined somewhere before this block
             lst_tlyne = lst_tlyne if lst_tlyne else []  # Initialize lst_tlyne if not defined
-            
+
             # Filter lst_tlyne based on combined symbols
             lst_tlyne = [x for x in lst_tlyne if x not in all_symbols]
-            
+
             logging.info(f"filtered from positions and orders ...{lst_tlyne}")
 
     except Exception as e:
@@ -111,15 +112,15 @@ if decision == "YES":
                 if resp and isinstance(resp, dict):
                     ltp = resp[key]['last_price']
                 return ltp
-    
+
             ltp_nse = get_ltp('NSE')
             logging.info(f"NSE LTP for {dct['tradingsymbol']} is {ltp_nse}")
-    
+
             if ltp_nse <= 0:
                 # If NSE price is not available, try BSE
                 ltp_bse = get_ltp('BSE')
                 logging.info(f"BSE LTP for {dct['tradingsymbol']} is {ltp_bse}")
-    
+
                 if ltp_bse > 0:
                     exchange = 'BSE'
                     ltp = ltp_bse
@@ -129,7 +130,7 @@ if decision == "YES":
             else:
                 exchange = 'NSE'
                 ltp = ltp_nse
-    
+
             if decision == "YES":
                 order_id_buy = broker.order_place(
                     tradingsymbol=dct['tradingsymbol'],
@@ -139,13 +140,13 @@ if decision == "YES":
                     order_type='LIMIT',
                     product='CNC',
                     variety='regular',
-                    price=round_to_paise(ltp,+0.3)
+                    price=round_to_paise(ltp, +0.3)
                 )
-    
+
             if order_id_buy:
                 logging.info(
                     f"BUY {order_id_buy} placed for {dct['tradingsymbol']} successfully")
-    
+
         except Exception as e:
             print(traceback.format_exc())
             logging.error(f"{str(e)} while placing order")
@@ -161,11 +162,14 @@ if decision == "YES":
             lst_failed_symbols = [line.strip() for line in file.readlines()]
         logging.info(f"ignored symbols: {lst_failed_symbols}")
         lst_orders = [d for d in lst_all_orders if d['tradingsymbol']
-                    not in lst_failed_symbols]
+                      not in lst_failed_symbols]
         if decision == "YES":
             try:
+                # ... (previous code remains unchanged)
+
                 for d in lst_orders:
                     # Check available funds before placing each order
+
                     if decision == "YES":
                         # Sufficient funds available, proceed with the order
                         failed_symbol = transact(d)
@@ -177,10 +181,10 @@ if decision == "YES":
                         logging.warning(f"Insufficient funds for {d['tradingsymbol']}. Skipping order.")
                         with open(black_file, 'a') as file:
                             file.write(d['tradingsymbol'] + '\n')
+
             except Exception as e:
-                print(traceback.format_exc())
+                # Handle exceptions here
 
 elif decision == "NO":
     # Perform actions for "NO"
-    print("\033[91mNo Funds Available \033[0m")
-    # Add your code for the "NO" case here
+    print("\033[91mNo Funds Avalable \033[0m")
