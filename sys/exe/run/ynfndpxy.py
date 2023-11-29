ebcf42502f80fbb058bb362e627f019a53da28f6
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-29 11:22:38 +0530

    Update ynfndpxy.py

--- sys/exe/run/ynfndpxy.py
+++ sys/exe/run/ynfndpxy.py
@@ -30,7 +30,7 @@ def calculate_decision():
 
         return decision
         return available_cash
-        print(available_cash)
+    print(available_cash)
     
     except Exception as e:
         remove_token(dir_path)
