6f27804c0eb5be23e27d5a072a216d133f998e47
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-11-30 06:31:09 +0530

    Update buypxy.py

--- sys/exe/run/buypxy.py
+++ sys/exe/run/buypxy.py
@@ -2,177 +2,178 @@ from toolkit.logger import Logger
 from toolkit.currency import round_to_paise
 from toolkit.utilities import Utilities
 from login_get_kite import get_kite
-from cnstpxy import dir_path
+from cnstpxy import dir_path, fileutils, buybuff, max_target
 from byhopxy import get
 from pluspxy import Trendlyne
 import pandas as pd
 import traceback
 import sys
 import os
+import ynfndpxy
+from ynfndpxy import calculate_decision
+from mktpxy import mktpxy
+import asyncio
 
-logging = Logger(30)
-file_path = "fileHPdf.csv"
+logging = Logger(10)
+holdings = dir_path + "holdings.csv"
 black_file = dir_path + "blacklist.txt"
-
 try:
+    sys.stdout = open('output.txt', 'w')
     broker = get_kite(api="bypass", sec_dir=dir_path)
+##    sys.stdout.close()
+##    sys.stdout = sys.__stdout__
+    if fileutils.is_file_not_2day(holdings):
+        logging.debug("getting holdings for the day ...")
+        resp = broker.kite.holdings()
+        if resp and any(resp):
+            df = get(resp)
+            logging.debug(f"writing to csv ... {holdings}")
+            df.to_csv(holdings, index=False)
+        with open(black_file, 'w+') as bf:
+            pass
 except Exception as e:
     print(traceback.format_exc())
     logging.error(f"{str(e)} unable to get holdings")
     sys.exit(1)
 
-try:
-    lst = []
+# Call the calculate_decision function to get the decision
+decision = calculate_decision()
 
-    # Read from fileHPdf
-    file_size_in_bytes = os.path.getsize(file_path)
-    logging.debug(f"fileHPdf file size: {file_size_in_bytes} bytes")
-    if file_size_in_bytes > 50:
-        logging.debug(f"reading from csv ...{file_path}")
-        df_holdings = pd.read_csv(file_path)
-        if not df_holdings.empty:
-            # Extract trading symbols directly from the single column
-            lst = df_holdings['tradingsymbol'].tolist()
-
-    # Get list from Trendlyne
-    lst_tlyne = []
-    lst_dct_tlyne = Trendlyne().entry()
-    if lst_dct_tlyne and any(lst_dct_tlyne):
-        print(pd.DataFrame(
-            lst_dct_tlyne).set_index('tradingsymbol').rename_axis('Trendlyne'), "\n")
-        lst_tlyne = [dct['tradingsymbol'] for dct in lst_dct_tlyne]
-
-    # Fetch orders from Zerodha Kite
-    orders = broker.kite.orders()
-
-    # Extract trading symbols from Zerodha orders
-    lst_kite = [order['tradingsymbol'] for order in orders]
-
-    # Combine lists from fileHPdf, Trendlyne, and Zerodha Kite
-    lst = list(set(lst + lst_tlyne + lst_kite))
-
-except Exception as e:
-    print(traceback.format_exc())
-    logging.error(f"{str(e)} unable to read fileHPdf, Trendlyne calls, or fetch orders from Zerodha Kite")
-    sys.exit(1)
-
-
-
-try:
-    # Your existing code to get data from Trendlyne
-    if any(lst_tlyne):
-        logging.info(f"reading trendlyne ...{lst_tlyne}")
-        lst_tlyne = [x for x in lst_tlyne if x not in lst]
-        logging.info(f"filtered from holdings: {lst}")
-
-        # get list from positions
-        lst_dct_positions = broker.positions
-        if lst_dct_positions and any(lst_dct_positions):
-            lst_positions = [dct['symbol'] for dct in lst_dct_positions]
-            lst_tlyne = [x for x in lst_tlyne if x not in lst_positions]
-            logging.info(f"filtered from positions ...{lst_positions}")
-
-    # Fetch orders from Zerodha Kite
-    lst_dct_orders = broker.kite.orders()
-    if lst_dct_orders and any(lst_dct_orders):
-        lst_orders = [order['tradingsymbol'] for order in lst_dct_orders]
-
-        # Combine lists from Trendlyne, positions, and Zerodha Kite orders
-        lst_tlyne = list(set(lst_tlyne + lst_positions + lst_orders))
-
-except Exception as e:
-    print(traceback.format_exc())
-    logging.error(f"{str(e)} unable to read positions or fetch orders from Zerodha Kite")
-    sys.exit(1)
-
-
-def calc_target(ltp, perc):
-    # Assuming that max_target is not being used
-    resistance = round_to_paise(ltp, perc)
-    return resistance
-
-def transact(stock_data):
+if decision == "YES":
     try:
-        # Get margins information
-        response = broker.kite.margins()
-        available_cash = response["equity"]["available"]["live_balance"]
+        lst = []
+        file_size_in_bytes = os.path.getsize(holdings)
+        logging.debug(f"holdings file size: {file_size_in_bytes} bytes")
+        if file_size_in_bytes > 50:
+            logging.debug(f"reading from csv ...{holdings}")
+            df_holdings = pd.read_csv(holdings)
+            if not df_holdings.empty:
+                lst = df_holdings['tradingsymbol'].to_list()
 
-        # Check if available_cash is greater than 10000
-        if available_cash > 10000:
-            def get_ltp():
-                ltp_nse, ltp_bse = -1, -1
-                key_nse = "NSE:" + stock_data['tradingsymbol']
-                key_bse = "BSE:" + stock_data['tradingsymbol']
+        # get list from Trendlyne
+        lst_tlyne = []
+        lst_dct_tlyne = Trendlyne().entry()
+        if lst_dct_tlyne and any(lst_dct_tlyne):
+            print(pd.DataFrame(
+                lst_dct_tlyne).set_index('tradingsymbol').rename_axis('Trendlyne'), "\n")
+            lst_tlyne = [dct['tradingsymbol'] for dct in lst_dct_tlyne]
+    except Exception as e:
+        print(traceback.format_exc())
+        logging.error(f"{str(e)} unable to read holdings or Trendlyne calls")
+        sys.exit(1)
 
-                resp_nse = broker.kite.ltp(key_nse)
-                resp_bse = broker.kite.ltp(key_bse)
+    try:
+        if any(lst_tlyne):
+            logging.info(f"reading trendlyne ...{lst_tlyne}")
+            lst_tlyne = [
+                x for x in lst_tlyne if x not in lst]
+            logging.info(f"filtered from holdings: {lst}")
 
-                if resp_nse and isinstance(resp_nse, dict):
-                    ltp_nse = resp_nse[key_nse]['last_price']
-
-                if resp_bse and isinstance(resp_bse, dict):
-                    ltp_bse = resp_bse[key_bse]['last_price']
-
-                return ltp_nse, ltp_bse
-
-            ltp_nse, ltp_bse = get_ltp()
-            logging.info(f"LTP for {stock_data['tradingsymbol']} (NSE): {ltp_nse}")
-            logging.info(f"LTP for {stock_data['tradingsymbol']} (BSE): {ltp_bse}")
-
-            if 0 < ltp_nse < ltp_bse:
-                # Place the order on NSE
-                exchange = 'NSE'
-                ltp = ltp_nse
-            elif 0 < ltp_bse < ltp_nse:
-                # Place the order on BSE
-                exchange = 'BSE'
-                ltp = ltp_bse
+            # get lists from positions and orders
+            lst_dct_positions = broker.positions
+            lst_dct_orders = [order for order in broker.orders if order.get('status') == 'OPEN']
+            
+            if lst_dct_positions and any(lst_dct_positions):
+                symbols_positions = [dct['symbol'] for dct in lst_dct_positions]
             else:
-                # No valid LTP, return the symbol
-                return stock_data['tradingsymbol']
-
-            # Rest of the code to place the order using 'exchange' and 'ltp'
-            order_id = broker.order_place(
-                tradingsymbol=stock_data['tradingsymbol'],
-                exchange=exchange,
-                transaction_type='BUY',
-                quantity=int(float(stock_data['QTY'])),
-                order_type='LIMIT',
-                product='CNC',
-                variety='regular',
-                price=round_to_paise(ltp, +0.2)
-            )
-
-            if order_id:
-                logging.info(
-                    f"BUY {order_id} placed for {stock_data['tradingsymbol']} successfully on {exchange} exchange")
+                symbols_positions = []
+            
+            if lst_dct_orders and any(lst_dct_orders):
+                symbols_orders = [dct['symbol'] for dct in lst_dct_orders]
+            else:
+                symbols_orders = []
+            
+            # Combine symbols from positions and orders
+            all_symbols = symbols_positions + symbols_orders
+            
+            # Assuming lst_tlyne is defined somewhere before this block
+            lst_tlyne = lst_tlyne if lst_tlyne else []  # Initialize lst_tlyne if not defined
+            
+            # Filter lst_tlyne based on combined symbols
+            lst_tlyne = [x for x in lst_tlyne if x not in all_symbols]
+            
+            logging.info(f"filtered from positions and orders ...{lst_tlyne}")
 
     except Exception as e:
         print(traceback.format_exc())
-        logging.error(f"{str(e)} while placing order")
-        return stock_data['tradingsymbol']
+        logging.error(f"{str(e)} unable to read positions")
+        sys.exit(1)
 
-if any(lst_tlyne):
-    new_list = []
-    # Filter the original list based on the subset of 'tradingsymbol' values
-    lst_all_orders = [d for d in lst_dct_tlyne if d['tradingsymbol'] in lst_tlyne]
+    def calc_target(ltp, perc):
+        resistance = round_to_paise(ltp, perc)
+        target = round_to_paise(ltp, max_target)
+        return max(resistance, target)
 
-    # Read the list of previously failed symbols from the file
-    with open(black_file, 'r') as file:
-        lst_failed_symbols = [line.strip() for line in file.readlines()]
-    logging.info(f"ignored symbols: {lst_failed_symbols}")
-    lst_orders = [d for d in lst_all_orders if d['tradingsymbol'] not in lst_failed_symbols]
+    def transact(dct):
+        try:
+            def get_ltp(exchange):
+                ltp = -1
+                key = f"{exchange}:{dct['tradingsymbol']}"
+                resp = broker.kite.ltp(key)
+                if resp and isinstance(resp, dict):
+                    ltp = resp[key]['last_price']
+                return ltp
+    
+            ltp_nse = get_ltp('NSE')
+            logging.info(f"NSE LTP for {dct['tradingsymbol']} is {ltp_nse}")
+    
+            if ltp_nse <= 0:
+                # If NSE price is not available, try BSE
+                ltp_bse = get_ltp('BSE')
+                logging.info(f"BSE LTP for {dct['tradingsymbol']} is {ltp_bse}")
+    
+                if ltp_bse > 0:
+                    exchange = 'BSE'
+                    ltp = ltp_bse
+                else:
+                    # If both NSE and BSE prices are not available, return
+                    return dct['tradingsymbol']
+            else:
+                exchange = 'NSE'
+                ltp = ltp_nse
+    
+            if decision == "YES":
+                order_id_buy = broker.order_place(
+                    tradingsymbol=dct['tradingsymbol'],
+                    exchange=exchange,
+                    transaction_type='BUY',
+                    quantity=int(float(dct['QTY'].replace(',', ''))),
+                    order_type='LIMIT',
+                    product='CNC',
+                    variety='regular',
+                    price=round_to_paise(ltp)
+                )
+    
+            if order_id_buy:
+                logging.info(
+                    f"BUY {order_id_buy} placed for {dct['tradingsymbol']} successfully")
+    
+        except Exception as e:
+            print(traceback.format_exc())
+            logging.error(f"{str(e)} while placing order")
+            return dct['tradingsymbol']
 
-    # place trades for symbol
-    for stock_data in lst_orders:
-        failed_symbol = transact(stock_data)
-        if failed_symbol:
-            new_list.append(failed_symbol)
-        Utilities().slp_til_nxt_sec()
-
-    # write the failed symbols to file, so we don't repeat them again
-    if any(new_list):
-        with open(black_file, 'w') as file:
-            for symbol in new_list:
-                file.write(symbol + '\n')
+    if any(lst_tlyne):
+        new_list = []
+        # Filter the original list based on the subset of 'tradingsymbol' values
+        lst_all_orders = [
+            d for d in lst_dct_tlyne if d['tradingsymbol'] in lst_tlyne]
+        # Read the list of previously failed symbols from the file
+        with open(black_file, 'r') as file:
+            lst_failed_symbols = [line.strip() for line in file.readlines()]
+        logging.info(f"ignored symbols: {lst_failed_symbols}")
+        lst_orders = [d for d in lst_all_orders if d['tradingsymbol']
+                    not in lst_failed_symbols]
+        for d in lst_orders:
+            failed_symbol = transact(d)
+            if failed_symbol:
+                new_list.append(failed_symbol)
+            Utilities().slp_til_nxt_sec()
+        if any(new_list):
+            with open(black_file, 'w') as file:
+                for symbol in new_list:
+                    file.write(symbol + '\n')
+elif decision == "NO":
+    # Perform actions for "NO"
+    print("\033[91mNo Funds Avalable \033[0m") 
 
