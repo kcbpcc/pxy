e8f6562d4bcf6596787ff3d3af4a8773e3ec989e
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-01 21:36:30 +0530

    Update pluspxy.py

--- sys/exe/run/pluspxy.py
+++ sys/exe/run/pluspxy.py
@@ -6,8 +6,8 @@ import traceback
 class Trendlyne:
 
     base_url = "https://trendlyne.com/"
-    entry_url = base_url + "fundamentals/v1/stock-screener/425413/pluspxy/"
-#https://trendlyne.com/fundamentals/v1/stock-screener/425413/pluspxy/
+    entry_url = base_url + "fundamentals/stock-screener/425413/pluspxy/index/NIFTY500/nifty-500/"
+#https://trendlyne.com/fundamentals/stock-screener/425413/pluspxy/index/NIFTY500/nifty-500/
 
 
     def __init__(self):
