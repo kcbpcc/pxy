c7be6edd2a5b2a35974eae7532878b8a47772e18
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-03 18:00:01 +0530

    Update smbpxy.py

--- sys/exe/run/smbpxy.py
+++ sys/exe/run/smbpxy.py
@@ -25,7 +25,7 @@ def calculate_last_three_heikin_ashi_colors(symbol, interval, period='5d'):
         return current_color, last_closed_color, second_last_closed_color
 
     except Exception as e:
-        console.print(f"[red]Error calculating Heikin-Ashi colors: {e}[/red]")
+        #console.print(f"[red]Error calculating Heikin-Ashi colors: {e}[/red]")
         return None, None, None
 
 def get_smbpxy_check(symbol, interval, period='5d'):
