2e82864c41ce23cf3df484033eadb2f5baa4ff5e
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-04 23:44:49 +0530

    Update tblprftpxy.py

--- sys/exe/run/tblprftpxy.py
+++ sys/exe/run/tblprftpxy.py
@@ -2,17 +2,22 @@ import csv
 from rich import print
 from rich.table import Table
 
-def process_csv(csv_file_path):
+def process_csv(csv_file_path, ORDER_df):
     # Set the overall table width
     table_width = 40
 
-    # Create a table to display the selected columns with custom headers
+    # Create a table to display all columns with custom headers
     table = Table(show_header=True, header_style="bold cyan", min_width=table_width)
-    table.add_column("Product", width=3)
-    table.add_column("Source", width=3)
-    table.add_column("Key", width=10)
-    table.add_column("PnL%")
-    table.add_column("PnL")
+    table.add_column("Source")
+    table.add_column("Product")
+    table.add_column("Qty")
+    table.add_column("Key")
+    table.add_column("Smbchk")
+    table.add_column("Yxp")
+    table.add_column("Pxy")
+    table.add_column("DPL%")
+    table.add_column("OPL%")
+    table.add_column("APL%")
 
     # Initialize the total profit variable
     total_profit = 0
@@ -30,7 +35,7 @@ def process_csv(csv_file_path):
             for row in csvreader:
                 # Adjust column names to match your CSV file structure
                 if len(row) == 16:
-                    qty, avg, close, ltp, open_price, high, low, pnl_h, dpnl, CM, PH, key, pxy, yxp, pnl_percentage, pnl = row
+                    source, product, qty, key, smbchk, yxp, pxy, dPL, oPL, aPL, *rest = row
                 else:
                     # Handle cases where the number of columns is different
                     print(f"Skipping row with unexpected number of columns: {row}")
@@ -40,14 +45,15 @@ def process_csv(csv_file_path):
                 key = key.replace("NSE:", "").replace("BSE:", "")
 
                 # Convert numerical values to strings and round them to two decimal places
-                pnl_percentage = str(round(float(pnl_percentage), 2))
-                pnl = str(round(float(pnl), 2))
+                dPL = str(round(float(dPL), 2))
+                oPL = str(round(float(oPL), 2))
+                aPL = str(round(float(aPL), 2))
 
                 # Accumulate the total profit
-                total_profit += float(pnl)
+                total_profit += float(aPL)
 
                 # Add the row to the table
-                table.add_row(CM, PH, key, pnl_percentage, pnl)
+                table.add_row(source, product, qty, key, smbchk, yxp, pxy, dPL, oPL, aPL)
 
     except FileNotFoundError:
         print("File not found!")
@@ -64,13 +70,13 @@ def process_csv(csv_file_path):
 
 # Replace "filePnL.csv" with your actual CSV file path
 csv_file_path = "filePnL.csv"
+# Replace ORDER_df with your actual DataFrame
+ORDER_df = pxy_df[['source','product','qty','key','smbchk','yxp','pxy','dPL%','oPL%','aPL%']]
 # Call the function and get the total_profit value
-total_profit_main = process_csv(csv_file_path)
+total_profit_main = process_csv(csv_file_path, ORDER_df)
 
 # Now you can use total_profit_main in your main code
 # print("Total Profit in Main:", total_profit_main)
-
-
 
 
 
