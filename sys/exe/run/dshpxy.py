bc8cb658e56c61e9493d8d83bbbdc4b0f9a7b136
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-05 05:09:47 +0530

    Update dshpxy.py

--- sys/exe/run/dshpxy.py
+++ sys/exe/run/dshpxy.py
@@ -54,7 +54,7 @@ def get_holdingsinfo(csv_file_path):
 
         table = PrettyTable()
         table.field_names = ['LaksBoard', 'All', 'Green', 'Red']
-        table.add_row(['📈Stocks', total_Stocks_count, green_Stocks_count, red_Stocks_count])
+        table.add_row(['Stocks📈', total_Stocks_count, green_Stocks_count, red_Stocks_count])
         table.add_row(['Invested', convert_to_laks(all_Stocks_capital), convert_to_laks(green_Stocks_capital), convert_to_laks(red_Stocks_capital)])
         table.add_row(['WorthNow', convert_to_laks(all_Stocks_worth), convert_to_laks(green_Stocks_worth), convert_to_laks(red_Stocks_worth)])
 
