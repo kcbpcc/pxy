12c49d1e7319adcfba9082f8eb144dc806ba129c
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-04 15:31:37 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -505,14 +505,14 @@ try:
 
     PRINT_df = pxy_df[['source','product','qty','key','smbchk','yxp','pxy','hsl','PnL%']]
     # Rename columns for display
-    PRINT_df = PRINT_df.rename(columns={'source': 'X', 'product': 'Y', 'qty' : 'Q','smbchk': 'Px'})
+    PRINT_df = PRINT_df.rename(columns={'source': 'X', 'product': 'Y', 'qty' : 'Q','smbchk': 'I'})
     # Conditionally replace values in the 'HP' column
     PRINT_df['X'] = PRINT_df['X'].replace({'holdings': 'H', 'positions': 'P'})
     # Conditionally replace values in the 'CM' column
     PRINT_df['Y'] = PRINT_df['Y'].replace({'CNC': 'C', 'MIS': 'M'})
     PRINT_df['Y'] = PRINT_df['Y'].replace({'CNC': 'C', 'MIS': 'M'})
     PRINT_df['Q'] = PRINT_df['Q'].apply(lambda Q: '+' if Q > 0 else '-')
-    PRINT_df['💫'] = PRINT_df['Px'].apply(lambda Px: '🟢' if Px == 'Bull' else ('🔴' if Px == 'Bear' else ('🌚' if Px == 'Sell' else ('🌞' if Px == 'Buy' else Px))))
+    PRINT_df['I'] = PRINT_df['I'].apply(lambda I: '🟢' if I == 'Bull' else ('🔴' if I == 'Bear' else ('🌚' if I == 'Sell' else ('🌞' if I == 'Buy' else I))))
     # Convert the 'PnL' column to integers
     # Remove 'BSE:' or 'NSE:' from the 'key' column
     PRINT_df['key'] = PRINT_df['key'].str.replace(r'(BSE:|NSE:)', '', regex=True)    
@@ -526,7 +526,7 @@ try:
     PRINT_df_sorted = PRINT_df.copy()
     
     # Apply the lambda function to limit 'chks' to 2 characters
-    PRINT_df_sorted['💫'] = PRINT_df_sorted['Px'].apply(lambda Px: Px[:2] if isinstance(Px, str) else Px)
+    PRINT_df_sorted['I'] = PRINT_df_sorted['I'].apply(lambda I: I[:2] if isinstance(I, str) else I)
     
     # Remove 'BSE:' or 'NSE:' from the 'key' column and limit to 3 characters
     PRINT_df_sorted['key'] = PRINT_df_sorted['key'].str.replace(r'(BSE:|NSE:)', '', regex=True).str[:3]
