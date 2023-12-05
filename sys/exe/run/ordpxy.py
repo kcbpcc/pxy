2a8b50edd68ea66d227b2c70c0b951062f23bd60
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-05 21:40:36 +0530

    Update ordpxy.py

--- sys/exe/run/ordpxy.py
+++ sys/exe/run/ordpxy.py
@@ -13,7 +13,7 @@ def get_open_order_status(symbol):
         broker = get_kite(api="bypass", sec_dir=dir_path)
     except Exception as e:
         remove_token(dir_path)
-        print(traceback.format_exc())
+        #print(traceback.format_exc())
         logging.error(f"{str(e)} unable to get holdings")
         sys.exit(1)
 
