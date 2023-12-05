63f82fd442e21df11fb0c831d9f5f65adbc394ac
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-05 06:13:25 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -553,6 +553,7 @@ try:
     print(f"{BRIGHT_YELLOW}Table–Stocks above @Pr and might reach @Yi{RESET}")
     
     # Print the truncated DataFrame without color
+
     print(PRINT_df_sorted_display.to_string(index=False, justify='left', col_space=-2))
   
 ###########################################################################################################################################################################################################
