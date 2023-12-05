56e9a5f349b23a2e5d14e851058925c02703cd55
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-05 11:01:06 +0530

    Update tblprftpxy.py

--- sys/exe/run/tblprftpxy.py
+++ sys/exe/run/tblprftpxy.py
@@ -6,15 +6,15 @@ def process_csv(csv_file_path):
     # Set the overall table width
     table_width = 40
 
-    # Specify the index of the last column to include
-    last_column_index = 19  # Adjust the index based on the actual position of the "PnL" column in your data
+    # Specify the headers for printing
+    headers_for_printing = ['product', 'source', 'key', 'PL%', 'PnL']
 
-    # Specify the header for printing
-    header_for_printing = "PnL"
-
-    # Create a table to display the selected column with custom header
+    # Create a table to display the selected columns with custom headers
     table = Table(show_header=True, header_style="bold cyan", min_width=table_width)
-    table.add_column(header_for_printing, width=10)  # Adjust the width as needed
+
+    # Add the specified columns to the table with custom headers
+    for header in headers_for_printing:
+        table.add_column(header, width=10)  # Adjust the width as needed
 
     # Initialize the total profit variable
     total_profit = 0
@@ -25,29 +25,35 @@ def process_csv(csv_file_path):
             # Create a CSV reader
             csvreader = csv.reader(csvfile)
 
-            # Iterate over each row in the CSV file and add the last column to the table
+            # Skip the header row
+            header_row = next(csvreader)
+
+            # Find the indices of the last 5 columns based on headers_for_printing
+            last_columns_indices = [header_row.index(col) for col in headers_for_printing]
+
+            # Iterate over each row in the CSV file and add the last 5 columns to the table
             for row in csvreader:
                 # Skip rows with unexpected number of columns
-                if len(row) != last_column_index + 1:
+                if len(row) != len(header_row):
                     print(f"Skipping row with unexpected number of columns: {row}")
                     continue
 
-                # Get the value of the last column ("PnL")
-                pnl_value = row[last_column_index]
+                # Extract values of the last 5 columns
+                last_columns_values = [row[idx] for idx in last_columns_indices]
 
-                # Convert numerical value to string and round it to two decimal places
-                pnl_value = str(round(float(pnl_value), 2))
+                # Convert numerical values to strings and round them to two decimal places
+                last_columns_values = [str(round(float(value), 2)) for value in last_columns_values]
 
                 # Accumulate the total profit
-                total_profit += float(pnl_value)
+                total_profit += float(last_columns_values[-1])  # Assuming PnL is the last column
 
-                # Add the last column from the row to the table
-                table.add_row(pnl_value)
+                # Add the last 5 columns from the row to the table
+                table.add_row(*last_columns_values)
 
     except FileNotFoundError:
         print("File not found!")
 
-    # Print the table with the specified column and header
+    # Print the table with the specified columns and headers
     print(table)
 
     # Print the total profit in INR (₹) format rounded to two decimal places
@@ -64,4 +70,3 @@ total_profit_main = process_csv(csv_file_path)
 
 # Now you can use total_profit_main in your main code
 # print("Total Profit in Main:", total_profit_main)
-
