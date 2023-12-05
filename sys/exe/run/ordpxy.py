c8f3e957b7a03ab7a425dc898744aa5146d02e7e
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-05 13:16:21 +0530

    Update ordpxy.py

--- sys/exe/run/ordpxy.py
+++ sys/exe/run/ordpxy.py
@@ -1,14 +1,14 @@
+# ordpxy.py
+
 import sys
 from toolkit.logger import Logger
 from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
 
-# Configure logging
 logging = Logger(30, dir_path + "main.log")
 
-def list_open_orders():
+def list_open_orders_by_symbol(symbol):
     try:
-        # Assuming kite is defined in the get_kite function
         broker = get_kite(api="bypass", sec_dir=dir_path)
     except Exception as e:
         remove_token(dir_path)
@@ -16,13 +16,10 @@ def list_open_orders():
         sys.exit(1)
 
     try:
-        # Assuming kite is defined somewhere in the get_kite function
-        # Use the 'orders' method to get a list of all orders
         orders = broker.kite.orders()
 
-        # Print information about open orders
         for order in orders:
-            if order['status'] == 'OPEN':
+            if order['status'] == 'OPEN' and order['tradingsymbol'] == symbol:
                 print(f"Symbol: {order['tradingsymbol']}")
                 print(f"Order Status: {order['status']}")
                 print("---")
@@ -31,6 +28,3 @@ def list_open_orders():
         remove_token(dir_path)
         logging.error(f"{str(e)} unable to get orders")
         sys.exit(1)
-
-# Call the function to list open orders
-list_open_orders()
