dba573e5eaa9a413120c64a9c009367f3f5dea4f
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-06 11:55:52 +0530

    Update pluspxy.py

--- sys/exe/run/pluspxy.py
+++ sys/exe/run/pluspxy.py
@@ -6,7 +6,7 @@ import traceback
 class Trendlyne:
 
     base_url = "https://trendlyne.com/"
-    entry_url = base_url + "fundamentals/v1/stock-screener/425413/pluspxy/index/NIFTY50/nifty-50/"
+    entry_url = base_url + "fundamentals/v1/stock-screener/425413/pluspxy/index/NIFTY500/nifty-500/"
 #https://trendlyne.com/fundamentals/v1/stock-screener/425413/pluspxy/all/all////fundamentals/v1/stock-screener/429696/minuspxy/index/NIFTY50/nifty-50/
 
 
