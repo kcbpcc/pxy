16e4d1b3fe0ec46d3e8c4282e583fe896cad7e67
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-06 23:40:23 +0530

    Update smbpxy.py

--- sys/exe/run/smbpxy.py
+++ sys/exe/run/smbpxy.py
@@ -2,57 +2,60 @@ import yfinance as yf
 import pandas as pd
 import warnings
 from rich.console import Console
+from yfinance import NoDataError, InsufficientDataError
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
-def calculate_last_three_heikin_ashi_colors(symbol, interval, period='5d'):
+console = Console()
+
+intervals = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+periods = [2, 3, 4, 5, 6, 7]
+
+def calculate_last_two_heikin_ashi_colors(symbol, period, interval):
     try:
-        data = yf.Ticker(symbol).history(period=period, interval=f'{interval}m')
-        
-        #if data.empty or len(data) < 3:
-            #raise ValueError("Insufficient data points for Heikin-Ashi calculation")
+        # Using specified periods and intervals for past and current candle
+        data = yf.Ticker(symbol).history(period=f'{periods}d', interval=f'{intervals}m')
 
         ha_close = (data['Open'] + data['High'] + data['Low'] + data['Close']) / 4
         ha_open = (data['Open'].shift(1) + data['Close'].shift(1)) / 2
 
         current_color = 'Bear' if ha_close.iloc[-1] < ha_open.iloc[-1] else 'Bull'
         last_closed_color = 'Bear' if ha_close.iloc[-2] < ha_open.iloc[-2] else 'Bull'
-        second_last_closed_color = 'Bear' if ha_close.iloc[-3] < ha_open.iloc[-3] else 'Bull'
 
-        return current_color, last_closed_color, second_last_closed_color
+        return current_color, last_closed_color
 
     except Exception as e:
-        print(f"Exception occurred: {e}")
-        return None, None, None
+        console.print(f"[red]Exception occurred: {e}[/red]")
+        return 'Error', 'Error'
 
-
-def get_smbpxy_check(symbol, interval, period='5d'):
+def get_smbpxy_check(symbol):
     try:
-        current_color, last_closed_color, second_last_closed_color = calculate_last_three_heikin_ashi_colors(symbol, interval, period)
+        # Loop through all intervals and periods
+        for interval in intervals:
+            for period in periods:
+                current_color, last_closed_color = calculate_last_two_heikin_ashi_colors(symbol, period, interval)
 
-        if current_color and last_closed_color and second_last_closed_color:
-            if current_color == 'Bear' and last_closed_color == 'Bear':
-                smbpxy = 'Bear'
-            elif current_color == 'Bull' and last_closed_color == 'Bull':
-                smbpxy = 'Bull'
-            elif current_color == 'Bear' and last_closed_color == 'Bull':
-                smbpxy = 'Sell'
-            elif current_color == 'Bull' and last_closed_color == 'Bear':
-                smbpxy = 'Buy'
-            else:
-                smbpxy = 'None'
-        else:
-            smbpxy = 'None'
+                if current_color and last_closed_color:
+                    if current_color == 'Bear' and last_closed_color == 'Bear':
+                        return 'Bear'
+                    elif current_color == 'Bull' and last_closed_color == 'Bull':
+                        return 'Bull'
+                    elif current_color == 'Bear' and last_closed_color == 'Bull':
+                        return 'Sell'
+                    elif current_color == 'Bull' and last_closed_color == 'Bear':
+                        return 'Buy'
+                    else:
+                        return 'None'
 
-        return smbpxy
+        return 'None'
 
     except NoDataError:
-        #console.print(f"[red]No data found for {symbol}, symbol may be delisted[/red]")
-        return None
+        console.print(f"[red]No data found for {symbol}, symbol may be delisted[/red]")
+        return 'None'
     except InsufficientDataError:
-        #console.print(f"[red]Insufficient data points for Heikin-Ashi calculation for {symbol}[/red]")
-        return None
+        console.print(f"[red]Insufficient data points for Heikin-Ashi calculation for {symbol}[/red]")
+        return 'None'
     except Exception as e:
-        #console.print(f"[red]Error determining smbpxy check: {e}[/red]")
+        console.print(f"[red]Error determining smbpxy check: {e}[/red]")
         return 'None'
