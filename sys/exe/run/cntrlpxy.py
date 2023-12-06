e8532abe4b8797d2b5c4f7e7deb5313b1d033738
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-06 09:55:54 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -680,6 +680,13 @@ try:
         print(right_aligned_format.format(f"Booked: {BRIGHT_GREEN if result > 0 else BRIGHT_RED}{round(result)}{RESET}"))
         print(left_aligned_format.format(f"psellPnL:{BRIGHT_GREEN if total_PnL_percentage_mis_sell >= 0 else BRIGHT_RED}{total_PnL_percentage_mis_sell}{RESET}"), end="")
         print(right_aligned_format.format(f"pbuyPnL:{BRIGHT_GREEN if total_PnL_percentage_positions_buy >= 0 else BRIGHT_RED}{total_PnL_percentage_positions_buy}{RESET}"))
+
+        # Always print "Table" in bright yellow
+        print(f"{BRIGHT_YELLOW}Table–Stocks above @Pr and might reach @Yi{RESET}")
+        
+        # Print the truncated DataFrame without color
+    
+        print(PRINT_df_sorted_display.to_string(index=False, justify='left', col_space=-2))
        
         subprocess.run(['python3', 'mktpxy.py'])
 
