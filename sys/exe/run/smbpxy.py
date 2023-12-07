c8c7d3149afd36ce0a292594fca62afd4afba55e
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-07 13:46:59 +0530

    Update smbpxy.py

--- sys/exe/run/smbpxy.py
+++ sys/exe/run/smbpxy.py
@@ -2,6 +2,8 @@ import yfinance as yf
 import pandas as pd
 import warnings
 from rich.console import Console
+from rich.progress import track
+from requests.exceptions import Timeout
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -9,7 +11,7 @@ warnings.filterwarnings("ignore", category=UserWarning)
 console = Console()
 
 intervals = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-periods = [2, 3, 4, 5, 6, 7]
+periods = [1, 2, 3, 4, 5, 6, 7]
 
 def calculate_last_two_heikin_ashi_colors(symbol, period, interval):
     try:
@@ -24,27 +26,34 @@ def calculate_last_two_heikin_ashi_colors(symbol, period, interval):
 
         return current_color, last_closed_color
 
+    except Timeout:
+        console.print("[red]Timeout error: Request to Yahoo Finance timed out[/red]")
+        raise
     except Exception as e:
         console.print(f"[red]Exception occurred: {e}[/red]")
         return 'Error', 'Error'
 
 def get_smbpxy_check(symbol):
     try:
-        for interval in intervals:
+        for interval in track(intervals, description="Intervals"):
             for period in periods:
-                current_color, last_closed_color = calculate_last_two_heikin_ashi_colors(symbol, period, interval)
+                try:
+                    current_color, last_closed_color = calculate_last_two_heikin_ashi_colors(symbol, period, interval)
 
-                if current_color and last_closed_color:
-                    if current_color == 'Bear' and last_closed_color == 'Bear':
-                        return 'Bear'
-                    elif current_color == 'Bull' and last_closed_color == 'Bull':
-                        return 'Bull'
-                    elif current_color == 'Bear' and last_closed_color == 'Bull':
-                        return 'Sell'
-                    elif current_color == 'Bull' and last_closed_color == 'Bear':
-                        return 'Buy'
-                    else:
-                        return 'None'
+                    if current_color and last_closed_color:
+                        if current_color == 'Bear' and last_closed_color == 'Bear':
+                            return 'Bear'
+                        elif current_color == 'Bull' and last_closed_color == 'Bull':
+                            return 'Bull'
+                        elif current_color == 'Bear' and last_closed_color == 'Bull':
+                            return 'Sell'
+                        elif current_color == 'Bull' and last_closed_color == 'Bear':
+                            return 'Buy'
+                        else:
+                            return 'None'
+
+                except Exception as e:
+                    console.print(f"[red]Error in period {period} and interval {interval}: {e}[/red]")
 
         return 'None'
 
@@ -56,12 +65,18 @@ def get_smbpxy_check(symbol):
 symbol = 'AAPL'
 min_interval = 1
 max_interval = 60
+max_retries = 3
 
 for interval in range(min_interval, max_interval + 1):
-    result = get_smbpxy_check(symbol)
-    if result != 'None':
-        print(f"Smbpxy check result for interval {interval}: {result}")
-        break
-    else:
-        print(f"No data found for interval {interval}. Trying the next interval.")
+    for retry in range(max_retries):
+        try:
+            result = get_smbpxy_check(symbol)
+            if result != 'None':
+                print(f"Smbpxy check result for interval {interval}: {result}")
+                break
+            else:
+                print(f"No data found for interval {interval}. Trying the next interval.")
+        except Timeout:
+            print(f"Timeout error. Retrying...")
+
 
