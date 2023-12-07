ce54793b1e12f3e06d1d4e01a976c62977507eba
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-07 09:17:05 +0530

    Update mktpxy.py

--- sys/exe/run/mktpxy.py
+++ sys/exe/run/mktpxy.py
@@ -17,7 +17,7 @@ symbol = '^NSEI'
 
 # Intervals in minutes
 intervals = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-periods = [2,3,4,5,6,7]
+periods = [1,2,3,4,5,6,7]
 
 # Create a Console instance for rich print formatting
 console = Console()
