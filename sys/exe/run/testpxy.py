d3b8d67ccfce0ea681d3ab06bc875c2b50e88bb1
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-07 21:30:55 +0530

    Update testpxy.py

--- sys/exe/run/testpxy.py
+++ sys/exe/run/testpxy.py
@@ -1,29 +1,27 @@
-# Values
-row_buy = {'smbchk': 'Buy', 'pr': 1, 'xl': 2, 'yi': 3, '_pr': -1, '_xl': -2, '_yi': -3}
-row_bull = {'smbchk': 'Bull', 'pr': 1, 'xl': 2, 'yi': 3, '_pr': -1, '_xl': -2, '_yi': -3}
-row_sell = {'smbchk': 'Sell', 'pr': 1, 'xl': 2, 'yi': 3, '_pr': -1, '_xl': -2, '_yi': -3}
-row_bear = {'smbchk': 'Bear', 'pr': 1, 'xl': 2, 'yi': 3, '_pr': -1, '_xl': -2, '_yi': -3}
+# Given values
+row = {'smbchk': '', 'pr': 1, 'xl': 2, 'yi': 3, '_pr': -1, '_xl': -2, '_yi': -3}
 
-# Calculate and print results in a table
-print("| smbchk | pxy  | yxp  |")
-print("|--------|------|------|")
+# Evaluate Buy
+row['smbchk'] = 'Buy'
+pxy_buy = calculate_pxy(row)
+yxp_buy = calculate_yxp(row)
+print("Buy - pxy:", pxy_buy, "yxp:", yxp_buy)
 
-# Buy
-pxy_buy = calculate_pxy(row_buy)
-yxp_buy = calculate_yxp(row_buy)
-print(f"| Buy    | {pxy_buy}  | {yxp_buy}  |")
+# Evaluate Bull
+row['smbchk'] = 'Bull'
+pxy_bull = calculate_pxy(row)
+yxp_bull = calculate_yxp(row)
+print("Bull - pxy:", pxy_bull, "yxp:", yxp_bull)
 
-# Bull
-pxy_bull = calculate_pxy(row_bull)
-yxp_bull = calculate_yxp(row_bull)
-print(f"| Bull   | {pxy_bull}  | {yxp_bull}  |")
+# Evaluate Sell
+row['smbchk'] = 'Sell'
+pxy_sell = calculate_pxy(row)
+yxp_sell = calculate_yxp(row)
+print("Sell - pxy:", pxy_sell, "yxp:", yxp_sell)
 
-# Sell
-pxy_sell = calculate_pxy(row_sell)
-yxp_sell = calculate_yxp(row_sell)
-print(f"| Sell   | {pxy_sell}  | {yxp_sell}  |")
+# Evaluate Bear
+row['smbchk'] = 'Bear'
+pxy_bear = calculate_pxy(row)
+yxp_bear = calculate_yxp(row)
+print("Bear - pxy:", pxy_bear, "yxp:", yxp_bear)
 
-# Bear
-pxy_bear = calculate_pxy(row_bear)
-yxp_bear = calculate_yxp(row_bear)
-print(f"| Bear   | {pxy_bear}  | {yxp_bear}  |")
