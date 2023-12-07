15ee531a8660794c3ee2e238d9641798c6ca5a5a
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-07 05:33:16 +0530

    Update BUYpxy.py

--- sys/exe/run/BUYpxy.py
+++ sys/exe/run/BUYpxy.py
@@ -6,8 +6,8 @@ import traceback
 class Trendlyne:
 
     base_url = "https://trendlyne.com/"
-    entry_url = base_url + "fundamentals/v1/stock-screener/432332/pluspxy/index/NIFTY500/nifty-500/"
-#https://trendlyne.com/fundamentals/v1/stock-screener/432332/pluspxy/index/NIFTY500/nifty-500/
+    entry_url = base_url + "fundamentals/v1/stock-screener/432332/buypxy/all/all/"
+#https://trendlyne.com/fundamentals/v1/stock-screener/432332/buypxy/all/all/
 
     def __init__(self):
         fake_response = requests.get(self.base_url)
