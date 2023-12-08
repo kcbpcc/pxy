c4d791a791c4e138b147093e70794497bcc78703
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-08 14:34:01 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -707,7 +707,7 @@ try:
         print(right_aligned_format.format(f"Booked: {BRIGHT_GREEN if result > 0 else BRIGHT_RED}{round(result)}{RESET}"))
         print(left_aligned_format.format(f"MIS(-):{BRIGHT_GREEN if total_PnL_percentage_mis_sell >= 0 else BRIGHT_RED}{total_PnL_percentage_mis_sell}{RESET}"), end="")
         print(right_aligned_format.format(f"MIS(+):{BRIGHT_GREEN if total_PnL_mis_buy >= 0 else BRIGHT_RED}{total_PnL_mis_buy}{RESET}"))
-                
+        print("*" * 42)
 
         print(f'{SILVER}{UNDERLINE}🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛{RESET}')
         mktpxy = get_market_check('^NSEI')
