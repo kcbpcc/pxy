a40095c9ce240fe33d81283579925f925c3cf487
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-08 22:01:16 +0530

    Update pluspxy.py

--- sys/exe/run/pluspxy.py
+++ sys/exe/run/pluspxy.py
@@ -6,9 +6,10 @@ import traceback
 class Trendlyne:
 
     base_url = "https://trendlyne.com/"
-    entry_url = base_url + "fundamentals/v1/stock-screener/432332/investpxy/all/all/"
-#https://trendlyne.com/fundamentals/v1/stock-screener/432332/investpxy/all/all/
-
+    entry_url = base_url + "fundamentals/v1/stock-screener/432332/buy-plus-pxy/all/all/"
+#All - https://trendlyne.com/fundamentals/v1/stock-screener/432332/buy-plus-pxy/all/all/
+#All - https://trendlyne.com/fundamentals/v1/stock-screener/432332/buy-plus-pxy/all/all/
+    
     def __init__(self):
         fake_response = requests.get(self.base_url)
         fake_cookies = fake_response.cookies
