56da3e1572256084283449380d254e7740f35012
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-11 13:05:54 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -380,12 +380,12 @@ try:
     
     combined_df[['pr', 'xl', 'yi', '_pr', '_xl', '_yi']] = combined_df.apply(
         lambda row: pd.Series({
-            'pr': round(max(0.5, round(0.0 + (row['strength'] * 1.0), 2) * 1 - epsilon), 2),
-            'xl': round(max(1, round(0.0 + (row['strength'] * 1.0), 2) * 1.5 - epsilon), 2),
-            'yi': round(max(1.5, round(0.0 + (row['strength'] * 1.0), 2) * 2 - epsilon), 2),
-            '_pr': round(min(-0.5, round(0.0 + (row['weakness'] * 1.0), 2) * 1 - epsilon), 2),
-            '_xl': round(min(-1, round(0.0 + (row['weakness'] * 1.0), 2) * 1.5 - epsilon), 2),
-            '_yi': round(min(-1.5, round(0.0 + (row['weakness'] * 1.0), 2) * 2 - epsilon), 2),
+            'pr': round(max(0.4, round(0.0 + (row['strength'] * 1.0), 2) * 1 - epsilon), 2),
+            'xl': round(max(0.8, round(0.0 + (row['strength'] * 1.0), 2) * 1.5 - epsilon), 2),
+            'yi': round(max(1.2, round(0.0 + (row['strength'] * 1.0), 2) * 2 - epsilon), 2),
+            '_pr': round(min(-0.4, round(0.0 + (row['weakness'] * 1.0), 2) * 1 - epsilon), 2),
+            '_xl': round(min(-0.8, round(0.0 + (row['weakness'] * 1.0), 2) * 1.5 - epsilon), 2),
+            '_yi': round(min(-1.2, round(0.0 + (row['weakness'] * 1.0), 2) * 2 - epsilon), 2),
         }), axis=1
     )
     
