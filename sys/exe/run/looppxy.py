182afaa46ef81a13c1ddab41db8eaaa29b146aab
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-11 05:49:52 +0530

    Update looppxy.py

--- sys/exe/run/looppxy.py
+++ sys/exe/run/looppxy.py
@@ -8,12 +8,20 @@ def calculate_loop_duration(current_time):
     interval_2_start = datetime.strptime("09:14", "%H:%M").time()
     interval_2_end = datetime.strptime("09:44", "%H:%M").time()
 
+    interval_3_start = datetime.strptime("09:45", "%H:%M").time()
+    interval_3_end = datetime.strptime("03:43", "%H:%M").time()
+
     # Convert current time to time object
     current_time = current_time.time()
 
     # Check if current time is within the defined intervals
     if interval_1_start <= current_time <= interval_1_end or interval_2_start <= current_time <= interval_2_end:
         return 1
+    elif interval_3_start <= current_time or current_time <= interval_3_end:
+        # Calculate the remaining time until the end of interval_3
+        remaining_time = timedelta(hours=12) - (current_time - interval_3_start)
+        # Convert the remaining time to hours
+        return round(remaining_time.total_seconds() / 3600)
     else:
         return 14
 
