81110dd7f4a5b9ccf200e90b6a13396f87bb3230
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-11 22:36:43 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -115,6 +115,69 @@ def order_place(index, row):
         #print(traceback.format_exc())
         logging.error(f"{str(e)} while placing order")
     return False
+
+###########################################################################################################################################################################################################
+def order_place_avg(index, row):
+    try:
+        exchsym = str(index).split(":")
+        if len(exchsym) >= 2:
+            logging.info(f"Placing order for {exchsym[1]}, {str(row)}")
+            order_id = broker.order_place(
+                tradingsymbol=exchsym[1],
+                exchange=exchsym[0],
+                transaction_type='BUY',
+                quantity=int(row['qty']),
+                order_type='LIMIT',
+                product='CNC',
+                variety='regular',
+                price=round_to_paise(row['ltp'], +0.3)
+            )
+            if order_id:
+                logging.info(f"Order {order_id} placed for {exchsym[1]} successfully")                                
+                # Write the row to the CSV file here
+                with open(csv_file_path, 'a', newline='') as csvfile:
+                    csvwriter = csv.writer(csvfile)
+                    csvwriter.writerow(row.tolist())  # Write the selected row to the CSV file
+                    try:
+                        import telegram
+                        import asyncio
+                    
+                        columns_to_drop = ['smbchk', 'oPL%', 'pstp', '_pstp', 'qty', 'close', 'open', 'high', 'low', 'PL%_H', 'dPL%', 'pxy','yxp']
+                    
+                        # Dropping specified columns from the row
+                        for column in columns_to_drop:
+                            if column in row:
+                                del row[column]
+                    
+                        message_text = '\n'.join(f"{value} {key.ljust(30)}" for key, value in row.items() if key != 'dtype') + f"\nhttps://www.tradingview.com/chart/?symbol={row['key']}"
+
+                        
+                        # Define the bot token and your Telegram username or ID
+                        bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
+                        user_usernames = ('-4022487175')  # Replace with your Telegram username or ID
+                        
+                        # Function to send a message to Telegram
+                        async def send_telegram_message(message_text):
+                            bot = telegram.Bot(token=bot_token)
+                            await bot.send_message(chat_id=user_usernames, text=message_text)
+                    
+                    except Exception as e:
+                        # Handle the exception (e.g., log it) and continue with your code
+                        print(f"Error sending message to Telegram: {e}")
+                    
+                    # Send the 'row' content as a message to Telegram immediately after printing the row
+                    loop = asyncio.get_event_loop()
+                    loop.run_until_complete(send_telegram_message(message_text))
+                
+                return True                
+            else:
+                logging.error("Order placement failed")       
+        else:
+            logging.error("Invalid format for 'index'")    
+    except Exception as e:
+        #print(traceback.format_exc())
+        logging.error(f"{str(e)} while placing order")
+    return False
 ###########################################################################################################################################################################################################
 def mis_order_sell(index, row):
     try:
@@ -640,7 +703,7 @@ try:
                         
                     ):
                         try:                            
-                            is_placed = order_place_buy(key, row) if get_open_order_status(symbol_in_order) == "NO" else False
+                            is_placed = order_place_avg(key, row) if get_open_order_status(symbol_in_order) == "NO" else False
                             if is_placed:
                                 # Print the row before placing the order
                                 print(row)                                
