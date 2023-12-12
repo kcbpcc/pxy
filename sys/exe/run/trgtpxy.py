7e07906e5c4437c3f95904172a8db5adc655c283
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-12 17:18:37 +0530

    Create trgtpxy.py

---
+++ sys/exe/run/trgtpxy.py
@@ -0,0 +1,21 @@
+# trgtpxy.py
+
+def calculate_trgtpxy(timpxy, nse_action):
+    if nse_action == "Bullish":
+        return round(float(timpxy), 2)
+    elif nse_action == "Bull":
+        return round(float(timpxy) * 0.90, 2)
+    elif nse_action == "Bear":
+        return round(float(timpxy) * 0.50, 2)
+    elif nse_action == "Bearish":
+        return round(float(timpxy) * 0.35, 2)
+    else:
+        # Default value when none of the conditions are met
+        return 5
+
+if __name__ == "__main__":
+    # Example usage:
+    timpxy_value = 10.0  # Replace with your actual value
+    nse_action_value = "Bullish"  # Replace with your actual value
+    trgtpxy_result = calculate_trgtpxy(timpxy_value, nse_action_value)
+    print("trgtpxy Result:", trgtpxy_result)
