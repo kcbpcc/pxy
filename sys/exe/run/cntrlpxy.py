31d7c3fcd5519daefbbba148f0ec446b0261a9a8
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-13 10:37:57 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -676,7 +676,7 @@ try:
                     elif (
                         row['qty'] < 0 and
                         row['product'] == 'MIS' and
-                        ((row['PL%']) < -0.7) 
+                        ((row['PL%']) < -0.6) 
                     ):
 
                         try:
