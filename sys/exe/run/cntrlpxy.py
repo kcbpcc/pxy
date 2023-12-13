02c7fe8d7e6cc04f0d310bfb0f0f5930fb2f20d1
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-13 12:22:41 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -580,6 +580,7 @@ try:
     # Assuming PRINT_df_sorted_display is your DataFrame
     filtered_df = PRINT_df_sorted_display[PRINT_df_sorted_display['PL%'] > 1.4]
     mis_filtered_df = PRINT_df_sorted_display[PRINT_df_sorted_display['Y'] == 'M']
+    mis_filtered_df = mis_filtered_df.sort_values(by='PL%', ascending=False)
    # Printing the filtered DataFrame without index, left justification, and adjusted column spacing
     print(f"{BRIGHT_YELLOW}Table–CNC Stocks in positions and holdings{RESET}")
     print(filtered_df.to_string(index=False, justify='left', col_space=-2))
