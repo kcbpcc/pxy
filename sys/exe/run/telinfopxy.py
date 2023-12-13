c334bf57307512ecad0e7e742295a738fee31e9e
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-13 06:28:28 +0530

    Update telinfopxy.py

--- sys/exe/run/telinfopxy.py
+++ sys/exe/run/telinfopxy.py
@@ -1,12 +1,10 @@
 import subprocess
-import schedule
 import time
 import telegram
 
 # Define the bot token and your Telegram username or ID
 bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
 user_usernames = ('-4022487175')  # Replace with your Telegram username or ID
-
 
 # Function to send a message to Telegram
 async def send_telegram_message(message_text):
@@ -16,7 +14,7 @@ async def send_telegram_message(message_text):
 def run_and_send_message():
     try:
         # Run the Python program and capture the output
-        output = subprocess.check_output(['python3', cntrlpxy.py], text=True)
+        output = subprocess.check_output(['python3', 'cntrlpxy.py'], text=True)
 
         # Send the output as a message via Telegram
         telegram_message = f"Program Output:\n\n{output}"
@@ -27,10 +25,6 @@ def run_and_send_message():
         error_message = f"Error running the program: {e}"
         send_telegram_message(error_message)
 
-# Schedule the task to run every hour from 3:30 AM to 9:30 AM UTC
-schedule.every().hour.at(':30').do(run_and_send_message).tag('task')
+# Call the function whenever you want the script to run
+run_and_send_message()
 
-# Run the scheduler
-while True:
-    schedule.run_pending()
-    time.sleep(1)
