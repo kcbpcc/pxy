fe59d94d661a795d4b1ae2ab2793d049e882e86a
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-13 11:58:10 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -580,10 +580,15 @@ try:
     # Assuming PRINT_df_sorted_display is your DataFrame
     filtered_df = PRINT_df_sorted_display[PRINT_df_sorted_display['PL%'] > 1.4]
     mis_filtered_df = PRINT_df_sorted_display[
-        (PRINT_df_sorted_display['PL%'] < 0) & 
-        (PRINT_df_sorted_display['PnL'] < 0) & 
-        (PRINT_df_sorted_display['Y'] == 'MIS')
+        (
+            (PRINT_df_sorted_display['PnL'] > 0) & (PRINT_df_sorted_display['PL%'] < 0)
+        ) | (
+            (PRINT_df_sorted_display['PnL'] < 0) & (PRINT_df_sorted_display['PL%'] > 0)
+        ) | (
+            (PRINT_df_sorted_display['Y'].isin(['MIS', 'M']))
+        )
     ]
+
     # Printing the filtered DataFrame without index, left justification, and adjusted column spacing
     print(f"{BRIGHT_YELLOW}Table–CNC Stocks in positions and holdings{RESET}")
     print(filtered_df.to_string(index=False, justify='left', col_space=-2))
