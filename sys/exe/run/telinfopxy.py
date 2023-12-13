4aa34eaf2dd035433c98eccf65a6456d178e2635
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-13 06:59:01 +0530

    Update telinfopxy.py

--- sys/exe/run/telinfopxy.py
+++ sys/exe/run/telinfopxy.py
@@ -1,7 +1,6 @@
 import subprocess
 import asyncio
 import telegram
-from html import escape  # Import escape function to sanitize HTML
 
 # Define the bot token and your Telegram username or ID
 bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
@@ -10,8 +9,7 @@ user_usernames = '-4022487175'  # Replace with your Telegram username or ID
 # Function to send a message to Telegram
 async def send_telegram_message(message_text):
     bot = telegram.Bot(token=bot_token)
-    sanitized_message = escape(message_text)  # Sanitize HTML
-    await bot.send_message(chat_id=user_usernames, text=sanitized_message, parse_mode='HTML')
+    await bot.send_message(chat_id=user_usernames, text=message_text, parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
 async def run_and_send_message():
     try:
@@ -19,12 +17,12 @@ async def run_and_send_message():
         output = subprocess.check_output(['python3', 'cntrlpxy.py'], text=True)
 
         # Send the output as a message via Telegram
-        telegram_message = f"Program Output:\n\n{output}"
+        telegram_message = f"Program Output:\n\n`{output}`"  # Using backticks for inline code formatting
         await send_telegram_message(telegram_message)
 
     except subprocess.CalledProcessError as e:
         # Handle errors if the subprocess fails
-        error_message = f"Error running the program: {e}"
+        error_message = f"Error running the program: `{e}`"  # Using backticks for inline code formatting
         await send_telegram_message(error_message)
 
 # Run the asynchronous function using asyncio.run
