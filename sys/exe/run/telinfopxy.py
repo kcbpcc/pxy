44c14bbaad79a6cebf533f8d6426f23734457e3e
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-13 06:53:00 +0530

    Update telinfopxy.py

--- sys/exe/run/telinfopxy.py
+++ sys/exe/run/telinfopxy.py
@@ -1,6 +1,7 @@
 import subprocess
 import asyncio
 import telegram
+from html import escape  # Import escape function to sanitize HTML
 
 # Define the bot token and your Telegram username or ID
 bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
@@ -9,9 +10,8 @@ user_usernames = '-4022487175'  # Replace with your Telegram username or ID
 # Function to send a message to Telegram
 async def send_telegram_message(message_text):
     bot = telegram.Bot(token=bot_token)
-    # Use HTML formatting to reduce font size
-    formatted_message = f'<small>{message_text}</small>'
-    await bot.send_message(chat_id=user_usernames, text=formatted_message, parse_mode='HTML')
+    sanitized_message = escape(message_text)  # Sanitize HTML
+    await bot.send_message(chat_id=user_usernames, text=sanitized_message, parse_mode='HTML')
 
 async def run_and_send_message():
     try:
@@ -30,6 +30,7 @@ async def run_and_send_message():
 # Run the asynchronous function using asyncio.run
 if __name__ == "__main__":
     asyncio.run(run_and_send_message())
+
 
 
 
