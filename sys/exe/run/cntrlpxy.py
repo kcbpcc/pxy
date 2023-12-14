0b93cc248a64dc8cc0ccb27c21d8853a823ff86c
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-14 08:20:26 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -623,7 +623,7 @@ try:
                         row['PL%'] > (cnc_filter * 0.5) and
                         row['source'] == 'holdings' and
                         (
-                            (row['PL%'] > (cnc_filter + trgtpxy) and row['smbchk'] != 'Bull or Buy')or
+                            (row['PL%'] > (cnc_filter + trgtpxy) and row['smbchk'] != 'Bull or Buy') or
                             (row['dPL%'] < 0 and row['oPL%'] < 0) or
                             (row['PL%_H'] > cnc_filter and row['PL%'] < cnc_filter )
                         )
