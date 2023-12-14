4754042c67b9accda78fd55e1b7105443e026ad2
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-14 08:07:39 +0530

    Update sellminuspxy.py

--- sys/exe/run/sellminuspxy.py
+++ sys/exe/run/sellminuspxy.py
@@ -6,7 +6,7 @@ import traceback
 class Trendlyne:
 
     base_url = "https://trendlyne.com/"
-    entry_url = base_url + "fundamentals/v1/stock-screener/429696/sell-minus-pxy/index/NIFTY200/nifty-200/"
+    entry_url = base_url + "fundamentals/v1/stock-screener/429696/sell-minus-pxy/index/NIFTY50/nifty-50/"
     
 
     def __init__(self):
