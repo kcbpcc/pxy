5c4eadfdeb38a682b5e812543366452e8169baa1
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-16 12:14:24 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -348,7 +348,9 @@ try:
     subprocess.run(['python3', 'dshpxy.py'])
     mktpxy = get_market_check('^NSEI')
     print(f'{SILVER}{UNDERLINE}🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛{RESET}')
-
+    from selfpxy import get_random_spiritual_message
+    random_message = get_random_spiritual_message()
+    print(random_message)
 ###########################################################################################################################################################################################################
     smb500_list = pd.read_csv('smb500.csv')['tradingsymbol'].tolist()
     combined_df['smbchk'] = combined_df.apply(lambda row: get_smbpxy_check(row['tradingsymbol'] + ".NS") if row['qty'] != 0 and row['tradingsymbol'] in smb500_list and get_smbpxy_check(row['tradingsymbol'] + ".NS") is not None else mktpxy, axis=1)
