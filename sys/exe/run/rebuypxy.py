58e6d81563462f029736e896451042aae19bae66
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-16 11:57:47 +0530

    Update rebuypxy.py

--- sys/exe/run/rebuypxy.py
+++ sys/exe/run/rebuypxy.py
@@ -10,15 +10,20 @@ def update_reinvest_csv(key):
             key_exists = any(row['key'] == key for row in rows)
 
             if not key_exists:
+                # Add the key to the CSV file
                 with open(reinvest_file, 'a', newline='') as csvfile:
                     fieldnames = ['key']
                     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    # Write the header if the file is empty
+                    if not rows:
+                        writer.writeheader()
                     writer.writerow({'key': key})
                 return True
             else:
                 return False
 
     except FileNotFoundError:
+        # If the file doesn't exist, create it and add the key
         with open(reinvest_file, 'w', newline='') as csvfile:
             fieldnames = ['key']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
