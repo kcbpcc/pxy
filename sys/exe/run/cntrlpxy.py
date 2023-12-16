a2845a6ff94560c869da360b237438fd2462aa50
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-16 22:41:37 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -625,7 +625,7 @@ try:
             engine.runAndWait()
         
         # Text to voice
-        market_info = f"Today Market is {status}, running power at {power}, candle switched {switch}, Booked profit is {book}, Day P&L is {number}, running target is {gr}, and remaining funds {ddgg}"
+        market_info = f"Today Market is {nse_action}, running power at {nse_power}, candle switched {switch}, Booked profit is {result}, Day P&L is {total_dPnL}, running target is {time_target}, and remaining funds {available_cash}"
         
         print("\nReading market information:")
         text_to_speech(market_info)       
