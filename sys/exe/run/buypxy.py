363d5832448aaf46f32f81c952b677dddac2c4c1
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-16 10:53:09 +0530

    Update buypxy.py

--- sys/exe/run/buypxy.py
+++ sys/exe/run/buypxy.py
@@ -12,16 +12,16 @@ import os
 from fundpxy import calculate_decision
 from mktpxy import get_market_check
 import asyncio
-mktchk = get_market_check('^NSEI')
 
+mktchk = get_market_check('^NSEI')
 logging = Logger(10)
 holdings = dir_path + "holdings.csv"
 black_file = dir_path + "blacklist.txt"
+
 try:
     sys.stdout = open('output.txt', 'w')
     broker = get_kite(api="bypass", sec_dir=dir_path)
-##    sys.stdout.close()
-##    sys.stdout = sys.__stdout__
+
     if fileutils.is_file_not_2day(holdings):
         logging.debug("getting holdings for the day ...")
         resp = broker.kite.holdings()
@@ -29,7 +29,7 @@ try:
             df = get(resp)
             logging.debug(f"writing to csv ... {holdings}")
             df.to_csv(holdings, index=False)
-        with open(black_file, 'w+') as bf:
+        with open(black_file, 'w+'):
             pass
 except Exception as e:
     print(traceback.format_exc())
@@ -41,14 +41,11 @@ decision = calculate_decision()
 
 if decision == "YES":
     try:
-        lst = []
-        file_size_in_bytes = os.path.getsize(holdings)
-        logging.debug(f"holdings file size: {file_size_in_bytes} bytes")
-        if file_size_in_bytes > 50:
-            logging.debug(f"reading from csv ...{holdings}")
-            df_holdings = pd.read_csv(holdings)
-            if not df_holdings.empty:
-                lst = df_holdings['tradingsymbol'].to_list()
+        # Read the fileHPdf.csv directly
+        df_fileHPdf = pd.read_csv('fileHPdf.csv')
+
+        # Extract tradingsymbols from df_fileHPdf
+        lst = df_fileHPdf['tradingsymbol'].to_list()
 
         # get list from Trendlyne
         lst_tlyne = []
@@ -57,9 +54,10 @@ if decision == "YES":
             print(pd.DataFrame(
                 lst_dct_tlyne).set_index('tradingsymbol').rename_axis('Trendlyne'), "\n")
             lst_tlyne = [dct['tradingsymbol'] for dct in lst_dct_tlyne]
+
     except Exception as e:
         print(traceback.format_exc())
-        logging.error(f"{str(e)} unable to read holdings or Trendlyne calls")
+        logging.error(f"{str(e)} unable to read Trendlyne calls")
         sys.exit(1)
 
     try:
@@ -67,37 +65,39 @@ if decision == "YES":
             logging.info(f"reading trendlyne ...{lst_tlyne}")
             lst_tlyne = [
                 x for x in lst_tlyne if x not in lst]
+
             logging.info(f"filtered from holdings: {lst}")
 
             # get lists from positions and orders
             lst_dct_positions = broker.positions
             lst_dct_orders = broker.orders
-            
+
             if lst_dct_positions and any(lst_dct_positions):
                 symbols_positions = [dct['symbol'] for dct in lst_dct_positions]
             else:
                 symbols_positions = []
-            
+
             if lst_dct_orders and any(lst_dct_orders):
                 symbols_orders = [dct['symbol'] for dct in lst_dct_orders]
             else:
                 symbols_orders = []
-            
+
             # Combine symbols from positions and orders
             all_symbols = symbols_positions + symbols_orders
-            
+
             # Assuming lst_tlyne is defined somewhere before this block
             lst_tlyne = lst_tlyne if lst_tlyne else []  # Initialize lst_tlyne if not defined
-            
+
             # Filter lst_tlyne based on combined symbols
             lst_tlyne = [x for x in lst_tlyne if x not in all_symbols]
-            
+
             logging.info(f"filtered from positions and orders ...{lst_tlyne}")
 
     except Exception as e:
         print(traceback.format_exc())
         logging.error(f"{str(e)} unable to read positions")
         sys.exit(1)
+
 
     def calc_target(ltp, perc):
         resistance = round_to_paise(ltp, perc)
