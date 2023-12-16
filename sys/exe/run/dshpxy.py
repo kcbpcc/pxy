a5010c31257decdc871fafb89d412035c430d944
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-16 14:08:54 +0530

    Update dshpxy.py

--- sys/exe/run/dshpxy.py
+++ sys/exe/run/dshpxy.py
@@ -86,12 +86,13 @@ def get_holdingsinfo(csv_file_path):
         )
 
         table.add_row(
-            "💵P&L💵",
-            f'{Fore.RED}{Style.BRIGHT}{format_value(all_Stocks_profit_loss)}{Style.RESET_ALL}',
-            str(round(all_Stocks_profit_loss)).rjust(0),  # Adjusted width to 10 and left-justified
-            str(round(green_Stocks_profit_loss)).rjust(0),  # Adjusted width to 10 and left-justified
-            str(round(red_Stocks_profit_loss)).rjust(0)  # Adjusted width to 10 and left-justified
-        )
+                    "💵P&L💵",
+                    f'{Fore.RED}{Style.BRIGHT}{format_value(all_Stocks_profit_loss)}{Style.RESET_ALL}',
+                    str(round(all_Stocks_profit_loss)),
+                    str(round(green_Stocks_profit_loss)),
+                    str(round(red_Stocks_profit_loss))
+                )
+
         
         console.print(table)
 
