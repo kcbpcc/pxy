2a9c5e9e5bc637f034738165ecacc80cf7a4cb95
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-16 15:42:39 +0530

    Update dshpxy.py

--- sys/exe/run/dshpxy.py
+++ sys/exe/run/dshpxy.py
@@ -95,7 +95,7 @@ def get_holdingsinfo(csv_file_path):
         
         console.print(table)
         
-        print("  Number of Stocks Sold💸💸💸: {}".format(zero_qty_count))
+        print("  Number of Stocks Sold 💸💸💸: {}".format(zero_qty_count))
         print(" " * 42)
     except Exception as e:
         print(f"An error occurred: {e}")
