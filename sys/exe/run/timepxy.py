12f3e1c5f935fe2c476ef4f353fffcbb4bd22cb8
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-16 16:25:26 +0530

    Update timepxy.py

--- sys/exe/run/timepxy.py
+++ sys/exe/run/timepxy.py
@@ -25,4 +25,5 @@ def calculate_timpxy():
 
 # Example usage:
 result = calculate_timpxy()
-print(f"PXY® is running on timepxy ⏰   {result}   ⏰")
+print(f"PXY® is running on timepxy ⏰ {result} ⏰")
+print(" " * 42)
