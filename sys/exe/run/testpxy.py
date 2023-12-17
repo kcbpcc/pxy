cadaeccdb40f3cb58ef9c79c883923644a228b7a
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-17 19:54:41 +0530

    Update testpxy.py

--- sys/exe/run/testpxy.py
+++ sys/exe/run/testpxy.py
@@ -1,12 +1,18 @@
+import asyncio
 from telegram import Bot
 
-# Define the bot token and user ID
-bot_token = '6603707685:AAFhWgPpliYjDqeBY24UyDipBbuBavcb4Bo'  # Replace with your actual bot token
-user_id = '-4022487175'  # Replace with your Telegram user ID
+async def send_test_message():
+    # Define the bot token and user ID
+    bot_token = '6603707685:AAFhWgPpliYjDqeBY24UyDipBbuBavcb4Bo'  # Replace with your actual bot token
+    user_id = '-4022487175'  # Replace with your Telegram user ID
 
-# Create a bot instance
-bot = Bot(token=bot_token)
+    # Create a bot instance
+    bot = Bot(token=bot_token)
 
-# Send a test message
-test_message = "This is a test message!"
-bot.send_message(chat_id=user_id, text=test_message)
+    # Send a test message
+    test_message = "This is a test message!"
+    await bot.send_message(chat_id=user_id, text=test_message)
+
+# Run the event loop
+asyncio.run(send_test_message())
+
