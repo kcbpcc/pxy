68ed05554f5d26e8dc57e82d0e3ad18ac8d0bb06
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-17 21:25:59 +0530

    Update testpxy.py

--- sys/exe/run/testpxy.py
+++ sys/exe/run/testpxy.py
@@ -4,7 +4,7 @@ from telegram import Bot
 async def send_test_message():
     # Define the bot token and user ID
     bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
-    user_id = '-4022487175'  # Replace with your Telegram user ID
+    user_id = '-4080532935'  # Replace with your Telegram user ID
 
     # Create a bot instance
     bot = Bot(token=bot_token)
