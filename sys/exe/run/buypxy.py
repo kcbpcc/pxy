c404492d5d5c693c9a712e63f434932cd107d6db
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-17 19:03:30 +0530

    Update buypxy.py

--- sys/exe/run/buypxy.py
+++ sys/exe/run/buypxy.py
@@ -139,6 +139,29 @@ if decision == "YES":
                         f"BUY {order_id} placed for {dct['tradingsymbol']} successfully")
                     # No need to calculate remaining available cash in this case
                     return dct['tradingsymbol'], remaining_cash
+                    import telegram
+                    import asyncio
+                    
+                    try:
+                        message_text = f"{ltp} \nhttps://www.tradingview.com/chart/?symbol={tradingsymbol}"
+                    
+                        # Define the bot token and your Telegram username or ID
+                        bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
+                        user_usernames = ('-4022487175')  # Replace with your Telegram username or ID
+                    
+                        # Function to send a message to Telegram
+                        async def send_telegram_message(message_text):
+                            bot = telegram.Bot(token=bot_token)
+                            await bot.send_message(chat_id=user_usernames, text=message_text)
+                    
+                    except Exception as e:
+                        # Handle the exception (e.g., log it) and continue with your code
+                        print(f"Error sending message to Telegram: {e}")
+                    
+                    # Send the 'row' content as a message to Telegram immediately after printing the row
+                    loop = asyncio.get_event_loop()
+                    loop.run_until_complete(send_telegram_message(message_text))
+
             else:
                 logging.warning(
                     f"Skipping {dct['tradingsymbol']}: Remaining Cash: {int(remaining_cash)}"
