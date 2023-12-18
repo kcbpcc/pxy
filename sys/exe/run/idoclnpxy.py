04c7b5a89396d482af1bbb7e329c2b8f66a33263
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-18 14:21:45 +0530

    Update idoclnpxy.py

--- sys/exe/run/idoclnpxy.py
+++ sys/exe/run/idoclnpxy.py
@@ -1,12 +1,24 @@
 import csv
 
-# Specify the path to your CSV file
-file_path = 'filePnL.csv'
+# Specify the path to your first CSV file
+file_path_pnl = 'filePnL.csv'
 
-# Open the file in write mode to truncate its contents
-with open(file_path, 'w', newline='') as csvfile:
-    # Create a CSV writer object
-    csv_writer = csv.writer(csvfile)
+# Open the first file in write mode to truncate its contents
+with open(file_path_pnl, 'w', newline='') as csvfile_pnl:
+    # Create a CSV writer object for the first file
+    csv_writer_pnl = csv.writer(csvfile_pnl)
 
     # Write an empty row to clear the contents
-    csv_writer.writerow([])
+    csv_writer_pnl.writerow([])
+
+# Specify the path to your second CSV file
+file_path_reinvest = 'reinvest.csv'
+
+# Open the second file in write mode to truncate its contents
+with open(file_path_reinvest, 'w', newline='') as csvfile_reinvest:
+    # Create a CSV writer object for the second file
+    csv_writer_reinvest = csv.writer(csvfile_reinvest)
+
+    # Write an empty row to clear the contents
+    csv_writer_reinvest.writerow([])
+
