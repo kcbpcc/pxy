50334b0cf3ab9778fb1e8c5bae41cb686e527d45
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-20 11:30:40 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -508,7 +508,7 @@ try:
     
     if not cnc_filtered_df.empty:
         print(f"{BRIGHT_YELLOW}Portfolio base:{cnc_filter}|level:{cnc_target}|target:{time_target}{RESET}")
-        print(cnc_filtered_df.to_string(index=False, justify='left', col_space=0)) 
+        print(cnc_filtered_df.to_string(index=False, justify='left', col_space=0, col_sep=''))
  
     if not mis_filtered_df.empty:
         print(f"{BRIGHT_YELLOW}Chronicles of My Intraday Destiny {RESET}") 
