ad759b3b7c35ce6760117edc241c04e3013e0835
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-22 00:45:31 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -369,7 +369,6 @@ try:
     )
     from nftpxy import nse_action, nse_power   
     combined_df['fPL%'] = combined_df.apply(lambda row: max(1.4, round(0.4 + (row['smb_power'] + nse_power), 2)), axis=1)
-    cnc_target = round(3 + (nse_power * 3.6), 2)
     combined_df['tPL%'] = combined_df.apply(lambda row: max(2.4, round(trgtpxy * (row['smb_power'] + nse_power), 2)), axis=1)
 
 
