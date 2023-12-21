af1444c5ce36e09ad006f050c650ee852eabf6b0
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-21 19:44:44 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -661,10 +661,11 @@ try:
     print(left_aligned_format.format(f"Status:{BRIGHT_GREEN if nse_action in ('Bullish', 'Bull') else BRIGHT_RED}{nse_action}{RESET}"), end="")
     print(right_aligned_format.format(f"Booked:{BRIGHT_GREEN if result > 0 else BRIGHT_RED}{round(result)}{RESET}"))
     print("-" * 42)
-
+    from dshpxy import zero_qty_count
+    print("Number of Stocks Sold 💸💸💸: {}".format(zero_qty_count).rjust(38))
     print(f'{SILVER}{UNDERLINE}🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛{RESET}')
     #mktpxy = get_market_check('^NSEI')    
-
+    
 
 except Exception as e:
     remove_token(dir_path)
