3dda86ebc39e9a6a56670e5e6c89189824df8cf3
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-21 20:11:09 +0530

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -122,7 +122,7 @@ while True:
             console.print("🌟 [bold]Market on standby![/bold] 🍿💰📊")
 
         return mktpxy        
-    print("-" * 42)  
+    
     # Call the function and store the result in a variable
     mktpxy = get_market_check('^NSEI')
 
