7c391f416ac69a26aaf4b7f18faac6b0a14f280c
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-21 06:52:44 +0530

    Update looppxy.py

--- sys/exe/run/looppxy.py
+++ sys/exe/run/looppxy.py
@@ -2,14 +2,14 @@ from datetime import datetime, timedelta
 
 def calculate_loop_duration(current_time):
     # Define time intervals
-    interval_1_start = datetime.strptime("03:41", "%H:%M").time()
-    interval_1_end = datetime.strptime("04:15", "%H:%M").time()
+    interval_1_start = datetime.strptime("03:30", "%H:%M").time()
+    interval_1_end = datetime.strptime("04:00", "%H:%M").time()
     
-    interval_2_start = datetime.strptime("09:14", "%H:%M").time()
+    interval_2_start = datetime.strptime("09:15", "%H:%M").time()
     interval_2_end = datetime.strptime("10:00", "%H:%M").time()
 
-    interval_3_start = datetime.strptime("10:01", "%H:%M").time()
-    interval_3_end = datetime.strptime("03:40", "%H:%M").time()
+    interval_3_start = datetime.strptime("10:00", "%H:%M").time()
+    interval_3_end = datetime.strptime("03:30", "%H:%M").time()
 
     # Convert current time to datetime object with today's date
     current_datetime = datetime.combine(datetime.today(), current_time)
