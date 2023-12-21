160eca33ab6c42b66368724fc4c6086e0b283417
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-21 07:02:59 +0530

    Update idoclnpxy.py

--- sys/exe/run/idoclnpxy.py
+++ sys/exe/run/idoclnpxy.py
@@ -11,14 +11,3 @@ with open(file_path_pnl, 'w', newline='') as csvfile_pnl:
     # Write an empty row to clear the contents
     csv_writer_pnl.writerow([])
 
-# Specify the path to your second CSV file
-file_path_reinvest = 'reinvest.csv'
-
-# Open the second file in write mode to truncate its contents
-with open(file_path_reinvest, 'w', newline='') as csvfile_reinvest:
-    # Create a CSV writer object for the second file
-    csv_writer_reinvest = csv.writer(csvfile_reinvest)
-
-    # Write an empty row to clear the contents
-    csv_writer_reinvest.writerow([])
-
