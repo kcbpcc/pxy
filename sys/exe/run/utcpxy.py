124f1ffd5431e4780d07a0bb5555c91c82cf06b1
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-22 20:46:14 +0530

    Update utcpxy.py

--- sys/exe/run/utcpxy.py
+++ sys/exe/run/utcpxy.py
@@ -1,6 +1,24 @@
 from datetime import datetime, timezone
 
-def utc():
+def peak_time():
     # Get the current UTC time
     utc_time = datetime.utcnow().replace(tzinfo=timezone.utc)
-    return utc_time
+
+    # Define the peak time ranges
+    peak_time_1_start = datetime.strptime("03:45", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_1_end = datetime.strptime("04:00", "%H:%M").replace(tzinfo=timezone.utc)
+    
+    peak_time_2_start = datetime.strptime("09:45", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_2_end = datetime.strptime("10:00", "%H:%M").replace(tzinfo=timezone.utc)
+
+    # Check if the current time is within the peak time ranges
+    peak = (peak_time_1_start <= utc_time <= peak_time_1_end) or \
+              (peak_time_2_start <= utc_time <= peak_time_2_end)
+
+    return peak
+
+if __name__ == "__main__":
+    if peak_time():
+        print("It's peak time!")
+    else:
+        print("It's not peak time.")
