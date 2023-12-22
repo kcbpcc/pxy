1e3745119538e7c16a03eb4d40bd1c96631789ee
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-22 13:51:34 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -314,7 +314,7 @@ try:
             return 5.0
         elif 0.25 <= nse_power < 0.5:
             return 2.5
-        elif 0.1 <= nse_power < 0.25:
+        elif 0.0 <= nse_power < 0.25:
             return 1.0
         else:
             return 5.0  # Default value if none of the conditions are met
