40d5ff35885abd07452068f0c0cb7dfc11418277
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-23 05:00:46 +0530

    Update trgtpxy.py

--- sys/exe/run/trgtpxy.py
+++ sys/exe/run/trgtpxy.py
@@ -2,13 +2,13 @@
 
 def calculate_trgtpxy(nse_power):
     if 0.90 <= nse_power <= 1.0:
-        return 10.0
+        return 9.0
     elif 0.75 <= nse_power < 0.90:
-        return 5.0
+        return 7.0
     elif 0.5 <= nse_power < 0.75:
         return 5.0
-    elif 0.25 <= nse_power < 0.5:
-        return 2.5
+    elif 0.3 <= nse_power < 0.5:
+        return 3.0
     elif 0.0 <= nse_power < 0.25:
         return 1.0
     else:
