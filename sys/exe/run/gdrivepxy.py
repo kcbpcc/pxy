75769fed295d5930d031ad5bbebe740a4d2d9aa6
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-23 10:35:42 +0530

    Update gdrivepxy.py

--- sys/exe/run/gdrivepxy.py
+++ sys/exe/run/gdrivepxy.py
@@ -1,17 +1,12 @@
-from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import os
 
 def authenticate_drive():
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()  # Creates local webserver and handles authentication.
-
-    drive = GoogleDrive(gauth)
+    drive = GoogleDrive()
     return drive
 
 def upload_file(drive, local_file_path, drive_folder_id):
     file_metadata = {
-        'title': os.path.basename(local_file_path),
+        'title': local_file_path,
         'parents': [{'id': drive_folder_id}]
     }
     file_drive = drive.CreateFile(file_metadata)
@@ -19,8 +14,8 @@ def upload_file(drive, local_file_path, drive_folder_id):
     print(f'File ID: {file_drive.get("id")}')
 
 if __name__ == '__main__':
-    LOCAL_FILE_PATH = 'bordpxy.txt'  # Use a relative path if the file is in the same directory
-    DRIVE_FOLDER_ID = '1iEBvg-uH1bRUTAmR2GjiEc_D7fAaxL5R'  # Use the folder ID from the shared link
+    LOCAL_FILE_PATH = 'bordpxy.txt'
+    DRIVE_FOLDER_ID = '1iEBvg-uH1bRUTAmR2GjiEc_D7fAaxL5R'
 
     drive = authenticate_drive()
     upload_file(drive, LOCAL_FILE_PATH, DRIVE_FOLDER_ID)
