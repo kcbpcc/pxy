47605c917705adf9163720c427a6467f0f056b07
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-23 17:06:27 +0530

    Update teldashpxy.py
    commit

--- sys/exe/run/teldashpxy.py
+++ sys/exe/run/teldashpxy.py
@@ -1,19 +1,19 @@
 import asyncio
 import aiohttp
 
-# Replace 'YOUR_BOT_TOKEN' with your actual Telegram bot token
-BOT_TOKEN = "6396096532:AAG5adz_SeUwV8WLn7miteljk_pRrpt8mO0"
+# Replace with your actual bot token
+BOT_TOKEN = "6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo"
 
-# Replace 'YOUR_CHAT_ID' with your chat ID
-CHAT_ID = -4067167377
+# Replace with your Telegram username or ID
+USER_USERNAME = "-4022487175"
 
-# Replace '/home/userland/pxy/sys/exe/run/bordpxy.txt' with the actual path to your file
+# Replace with the actual path to your file
 FILE_PATH = "/home/userland/pxy/sys/exe/run/bordpxy.txt"
 
-async def send_message(chat_id, message, bot_token):
+async def send_message(username, message, bot_token):
     api_endpoint = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     params = {
-        "chat_id": str(chat_id),  # Ensure chat_id is a string
+        "chat_id": username,
         "text": message,
     }
 
@@ -24,10 +24,11 @@ async def send_message(chat_id, message, bot_token):
 async def main():
     with open(FILE_PATH, "r") as file:
         file_content = file.read()
-        response = await send_message(CHAT_ID, file_content, BOT_TOKEN)
+        response = await send_message(USER_USERNAME, file_content, BOT_TOKEN)
         print(response)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
+
 
