67bd4433def2b4bd9dc9f84cabd9dd818a9814cc
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-23 10:50:57 +0530

    Update gdrivepxy.py

--- sys/exe/run/gdrivepxy.py
+++ sys/exe/run/gdrivepxy.py
@@ -1,10 +1,2 @@
-from gupload import GoogleDriveUpload
+curl -L -F "file=@/path/to/your/bordpxy.txt" "https://drive.google.com/upload/drive/folders/1iEBvg-uH1bRUTAmR2GjiEc_D7fAaxL5R?usp=drive_open"
 
-def upload_file(file_id, local_file_path):
-    GoogleDriveUpload().upload(local_file_path, file_id)
-
-if __name__ == '__main__':
-    DRIVE_FOLDER_ID = '1iEBvg-uH1bRUTAmR2GjiEc_D7fAaxL5R'  # Replace with your folder ID
-    LOCAL_FILE_PATH = 'bordpxy.txt'  # Replace with your local file path
-
-    upload_file(DRIVE_FOLDER_ID, LOCAL_FILE_PATH)
