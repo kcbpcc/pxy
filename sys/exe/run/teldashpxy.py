abfbc862d645c930acc150093aa48c555714d013
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-23 17:04:00 +0530

    Update teldashpxy.py

--- sys/exe/run/teldashpxy.py
+++ sys/exe/run/teldashpxy.py
@@ -1,5 +1,4 @@
 import asyncio
-from aiohttp import FormData
 import aiohttp
 
 # Replace 'YOUR_BOT_TOKEN' with your actual Telegram bot token
@@ -11,20 +10,24 @@ CHAT_ID = -4067167377
 # Replace '/home/userland/pxy/sys/exe/run/bordpxy.txt' with the actual path to your file
 FILE_PATH = "/home/userland/pxy/sys/exe/run/bordpxy.txt"
 
-async def send_file(chat_id, file_path, bot_token):
-    api_endpoint = f"https://api.telegram.org/bot{bot_token}/sendDocument"
+async def send_message(chat_id, message, bot_token):
+    api_endpoint = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    params = {
+        "chat_id": str(chat_id),  # Ensure chat_id is a string
+        "text": message,
+    }
 
     async with aiohttp.ClientSession() as session:
-        form_data = FormData()
-        form_data.add_field("document", open(file_path, "rb"))
-        params = {"chat_id": str(chat_id)}  # Ensure chat_id is a string
-        async with session.post(api_endpoint, params=params, data=form_data) as response:
+        async with session.post(api_endpoint, params=params) as response:
             return await response.text()
 
 async def main():
-    response = await send_file(CHAT_ID, FILE_PATH, BOT_TOKEN)
-    print(response)
+    with open(FILE_PATH, "r") as file:
+        file_content = file.read()
+        response = await send_message(CHAT_ID, file_content, BOT_TOKEN)
+        print(response)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
+
