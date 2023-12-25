6fae61bcd2f5034daab5575323e876d6ed8fbd47
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-25 18:43:36 +0530

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -27,9 +27,6 @@ def printbord(Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, t
     output_lines.append(left_aligned_format.format(f"Intraday:{BRIGHT_GREEN if total_PnL_percentage_mis_sell >= 0 else BRIGHT_RED}{total_PnL_percentage_mis_sell}{RESET}") +
                         right_aligned_format.format(f"Delivery:{BRIGHT_GREEN if total_PnL_cnc_buy >= 0 else BRIGHT_RED}{total_PnL_cnc_buy}{RESET}"))   
 
-               
-    output_lines.append(left_aligned_format.format(f"Capital:{round((all_Stocks_capital_lacks), 2)}{RESET}") +
-                        right_aligned_format.format(f"Value:{round((all_Stocks_worth_lacks), 2)}{RESET}"))
 
     output_lines.append(left_aligned_format.format(f"Capital:{BRIGHT_RED }{all_Stocks_capital_lacks}{RESET}") +
                         right_aligned_format.format(f"Value:{BRIGHT_GREEN }{all_Stocks_worth_lacks}{RESET}"))
