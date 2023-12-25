9355bf85d7a52e2e6c98d9303bd776344a850cfe
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-25 17:56:54 +0530

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -25,6 +25,16 @@ def printbord(Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, t
                         right_aligned_format.format(f"DayPnL%:{BRIGHT_GREEN if total_dPnL_percentage > 0 else BRIGHT_RED}{round(total_dPnL_percentage, 2)}{RESET}"))
     output_lines.append(left_aligned_format.format(f"Intraday:{BRIGHT_GREEN if total_PnL_percentage_mis_sell >= 0 else BRIGHT_RED}{total_PnL_percentage_mis_sell}{RESET}") +
                         right_aligned_format.format(f"Delivery:{BRIGHT_GREEN if total_PnL_cnc_buy >= 0 else BRIGHT_RED}{total_PnL_cnc_buy}{RESET}"))
+    
+                
+    output_lines.append(left_aligned_format.format(f"Capital:{round(all_Stocks_capital, 2)}{RESET}") +
+                        right_aligned_format.format(f"Value:{all_Stocks_worth}{RESET}"))
+    output_lines.append(left_aligned_format.format(f"Losers:{BRIGHT_RED }{red_Stocks_count}{RESET}") +
+                        right_aligned_format.format(f"Gainers:{BRIGHT_GREEN }{green_Stocks_count}{RESET}"))
+
+    
+                
+
     output_lines.append(left_aligned_format.format(f"Switch:{BRIGHT_YELLOW}{switch}{RESET}") +
                         right_aligned_format.format(f"Funds:{BRIGHT_GREEN if available_cash > 12000 else BRIGHT_YELLOW}{available_cash:.0f}{RESET}"))
     output_lines.append(left_aligned_format.format(f"Open%:{BRIGHT_GREEN if Open_Change >= 0 else BRIGHT_RED}{round(Open_Change, 2)}{RESET}") +
