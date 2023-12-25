8d669bfb70200d5b3e98d76cfc10acd9ff656422
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-25 19:24:10 +0530

    Update tist.py

--- sys/exe/run/tist.py
+++ sys/exe/run/tist.py
@@ -15,7 +15,7 @@ def print_current_datetime_in_ist():
 
     # Format the date and time with emojis
     formatted_datetime = (
-        f" 🗓️{ist_now.strftime('%d')} "  # Different calendar emoji
+        f"🗓️{ist_now.strftime('%d')} "  # Different calendar emoji
         f"{ist_now.strftime('%B')} {ist_now.strftime('%Y')} |"  # Month, year
         f" 🕛{ist_now.strftime('%A')} |"   # Day
         f" ⏰{ist_now.strftime('%I:%M%p')}"  # Time
