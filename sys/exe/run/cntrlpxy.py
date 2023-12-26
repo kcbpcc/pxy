180eabe2e3298d14454ef1aef1fa9148472136c1
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-26 21:56:51 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -265,7 +265,8 @@ try:
     trgtpxy = calculate_trgtpxy(nse_power)
     from nftpxy import nse_action, nse_power   
     combined_df['fPL%'] = combined_df.apply(lambda row: max(1.4, round((1 + row['smb_power'] ), 2)), axis=1)
-    combined_df['tPL%'] = combined_df.apply(lambda row: max(row['fPL%'], round(trgtpxy * (row['smb_power'] * row['fPL%']), 2)), axis=1)
+    combined_df['tPL%'] = combined_df.apply(lambda row: max(row['fPL%'], row['fPL%'] + round(10 * (row['smb_power'] * row['fPL%']), 2)), axis=1)
+
 ###########################################################################################################################################################################################################
     subprocess.run(['python3', 'prftpxy.py'])
 ###########################################################################################################################################################################################################
