7dd9dedbac823a58655bcea673cdeb0223699dd1
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-29 23:08:22 +0530

    Update buypluspxy.py

--- sys/exe/run/buypluspxy.py
+++ sys/exe/run/buypluspxy.py
@@ -6,7 +6,7 @@ import traceback
 class Trendlyne:
 
     base_url = "https://trendlyne.com/"
-    entry_url = base_url + "fundamentals/v1/stock-screener/432332/buy-plus-pxy/index/NIFTY200/nifty-200/"
+    entry_url = base_url + "fundamentals/v1/stock-screener/432332/buy-plus-pxy/index/NIFTY100/nifty-100/"
     #( ( ( ( Previous Day Open - Prev Day Close Rs ) / Prev Day Close Rs ) * 100 ) < 1.4 ) AND ( ( ( ( Day High Rs - Previous Day Open ) / Previous Day Open ) * 100 ) < 1.4 ) AND ( Day Volume Primary Exch / Week Volume Avg Primary Exch > 1.4 ) AND ( Previous Day Open > Prev Day Close Rs ) AND ( Current Price Rs > Previous Day Open ) AND ( Day Volume Primary Exch >= 10000 ) AND ( Previous Day Open > Day Low Rs ) AND ( Current Price Rs > Day Open Rs ) AND ( MACD > 0 )
     def __init__(self):
         fake_response = requests.get(self.base_url)
