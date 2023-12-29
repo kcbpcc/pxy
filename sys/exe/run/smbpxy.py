90dd827c796376cb193b297bd5c3684a6310b008
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-29 22:49:53 +0530

    Update smbpxy.py

--- sys/exe/run/smbpxy.py
+++ sys/exe/run/smbpxy.py
@@ -55,7 +55,7 @@ def calculate_last_three_heikin_ashi_colors(symbol, interval):
     
         current_color = 'Bear' if day_open > ltp else 'Bull'
         last_closed_color = 'Bear' if day_open > ltp else 'Bull'
-
+        second_closed_color = 'Bear' if day_open > ltp else 'Bull'
     else:
         # Fetch real-time data for the specified interval
         data = yf.Ticker(symbol).history(period=f'{periods[0]}d', interval=f'{interval}m')
@@ -67,35 +67,36 @@ def calculate_last_three_heikin_ashi_colors(symbol, interval):
         # Calculate the colors of the last three closed candles
         current_color = 'Bear' if ha_close.iloc[-1] < ha_open.iloc[-1] else 'Bull'
         last_closed_color = 'Bear' if ha_close.iloc[-2] < ha_open.iloc[-2] else 'Bull'
+        second_closed_color = 'Bear' if ha_close.iloc[-3] < ha_open.iloc[-3] else 'Bull'
 
-    return current_color, last_closed_color
+    return current_color, last_closed_color, second_closed_color
 
 def get_smbpxy_check(symbol):
     try:
         # Loop through all intervals and periods
         for interval in intervals:
             for period in periods:
-                current_color, last_closed_color = calculate_last_three_heikin_ashi_colors(symbol, interval)
+                current_color, last_closed_color, second_closed_color = calculate_last_three_heikin_ashi_colors(symbol, interval)
 
                 if current_color and last_closed_color:
-                    if current_color == 'Bear' and last_closed_color == 'Bear':
+                    if current_color == 'Bear' and last_closed_color == 'Bear' and second_closed_color == 'Bear':
                         return 'Bear'
-                    elif current_color == 'Bull' and last_closed_color == 'Bull':
+                    elif current_color == 'Bull' and last_closed_color == 'Bull' and second_closed_color == 'Bull':
                         return 'Bull'
-                    elif current_color == 'Bear' and last_closed_color == 'Bull':
+                    elif current_color == 'Bear' and last_closed_color == 'Bear' and second_closed_color == 'Bull':
                         return 'Sell'
-                    elif current_color == 'Bull' and last_closed_color == 'Bear':
+                    elif current_color == 'Bull' and last_closed_color == 'Bull' and second_closed_color == 'Bear':
                         return 'Buy'
                     else:
-                        return 'None'
+                        return 'pxy can not be concluded'
 
-        return 'None'
-
-    except Exception as e:
-        console.print(f"[red]Error determining smbpxy check: {e}[/red]")
-        return 'None'
-
+        return 'pxy can not be concluded'
 
     except Exception as e:
         console.print(f"[red]Error determining smbpxy check: {e}[/red]")
-        return 'None'
+        return 'pxy can not be concluded'
+
+
+    except Exception as e:
+        console.print(f"[red]Error determining smbpxy check: {e}[/red]")
+        return 'pxy can not be concluded'
