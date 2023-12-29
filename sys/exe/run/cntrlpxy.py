bffe540e3f9890684c34be630cde2db6b1e85c01
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-29 09:33:12 +0530

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -262,6 +262,7 @@ try:
     }), axis=1
     )
     from nftpxy import nse_action, nse_power   
+    threshold = 3
     combined_df['fPL%'] = combined_df['smb_power'].apply(lambda x: max(round(np.exp(np.clip(x, -threshold, threshold)), 2), 1.4))
     combined_df['tPL%'] = combined_df['fPL%'].apply(lambda x: max(np.exp(x * nse_power**2), 1.9))
 ###########################################################################################################################################################################################################
