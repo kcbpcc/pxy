ae42e378fe8cff6acd3d49424e4625819d075ac7
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-31 19:42:59 +0530

    Update yxpbuy.py

--- sys/exe/run/yxpbuy.py
+++ sys/exe/run/yxpbuy.py
@@ -11,7 +11,7 @@ def analyze_stock(symbol):
 
         # Calculate Heikin-Ashi candles
         data['HA_Close'] = (data['Open'] + data['High'] + data['Low'] + data['Close']) / 4
-        data['HA_Open'] = (data['Open'].shift(1) + data['Close'].shift(1)) / 2
+        data['HA_Open'] = (data['HA_Open'].shift(1) + data['HA_Close'].shift(1)) / 2
         data['HA_High'] = data[['High', 'HA_Open', 'HA_Close']].max(axis=1)
         data['HA_Low'] = data[['Low', 'HA_Open', 'HA_Close']].min(axis=1)
 
@@ -23,10 +23,10 @@ def analyze_stock(symbol):
 
         if yesterday_ha_close > yesterday_ha_open and today_ha_close < today_ha_open:
             # Check if the last 5-minute candle today is red and the current candle is green
-            last_5min_ha_close = data['HA_Close'].iloc[-2]
-            last_5min_ha_open = data['HA_Open'].iloc[-2]
-            current_ha_close = data['HA_Close'].iloc[-1]
-            current_ha_open = data['HA_Open'].iloc[-1]
+            last_5min_ha_close = data['HA_Close'].iloc[-3]
+            last_5min_ha_open = data['HA_Open'].iloc[-3]
+            current_ha_close = data['HA_Close'].iloc[-2]
+            current_ha_open = data['HA_Open'].iloc[-2]
 
             if last_5min_ha_close > last_5min_ha_open and current_ha_close < current_ha_open:
                 return 'Buy'
