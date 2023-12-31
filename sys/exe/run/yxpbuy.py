bcae5039a7240cfb3aa4354166a962fd30845e58
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-31 22:06:00 +0530

    Update yxpbuy.py

--- sys/exe/run/yxpbuy.py
+++ sys/exe/run/yxpbuy.py
@@ -24,9 +24,9 @@ def analyze_stock(symbol):
         today_close = data['Close'].iloc[-1]
         today_open = data['Open'].iloc[-1]
         
-        if daybeforeyesterday_close < daybeforeyesterday_open and yesterday_close > yesterday_open and today_close > today_open and (nse_action == 'Bull' or nse_action == 'Bullish'):
+        if daybeforeyesterday_close < daybeforeyesterday_open and yesterday_close > yesterday_open and today_close > today_open and (nse_action == 'Bull' or nse_action == 'Bullish') and mktpxy == Buy:
             return 'Buy'
-        elif daybeforeyesterday_close > daybeforeyesterday_open and yesterday_close < yesterday_open and today_close < today_open and (nse_action == 'Bear' or nse_action == 'Bearish'):
+        elif daybeforeyesterday_close > daybeforeyesterday_open and yesterday_close < yesterday_open and today_close < today_open and (nse_action == 'Bear' or nse_action == 'Bearish') and mktpxy == Sell:
             return 'Sell'
         else:
             return 'None'
