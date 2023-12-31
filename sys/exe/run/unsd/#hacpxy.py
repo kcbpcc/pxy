88aef4881efa6dfb04f778585f292de809cebc4c
PXY <148702748+kcbpcc@users.noreply.github.com>
2023-12-31 15:23:52 +0530

    Update #hacpxy.py

--- sys/exe/run/unsd/#hacpxy.py
+++ sys/exe/run/unsd/#hacpxy.py
@@ -1,11 +1,9 @@
 import yfinance as yf
-import os
-import sys
 
-def day_ha_check(symbol):
+def analyze_stock(symbol):
     try:
-        # Download historical data for the last 2 days
-        data = yf.download(symbol, period="2d")
+        # Download historical data for the last 2 days with 1-minute intervals
+        data = yf.download(symbol, period="2d", interval="1m")
 
         # Calculate Heikin-Ashi candles
         data['HA_Close'] = (data['Open'] + data['High'] + data['Low'] + data['Close']) / 4
@@ -13,17 +11,23 @@ def day_ha_check(symbol):
         data['HA_High'] = data[['High', 'HA_Open', 'HA_Close']].max(axis=1)
         data['HA_Low'] = data[['Low', 'HA_Open', 'HA_Close']].min(axis=1)
 
-        # Extract yesterday's and today's Heikin-Ashi values
-        yesterday_ha_open = data['HA_Open'].iloc[-2]
+        # Check if yesterday's candle is red and today's candle is green
         yesterday_ha_close = data['HA_Close'].iloc[-2]
-        today_ha_open = data['HA_Open'].iloc[-1]
+        yesterday_ha_open = data['HA_Open'].iloc[-2]
         today_ha_close = data['HA_Close'].iloc[-1]
+        today_ha_open = data['HA_Open'].iloc[-1]
 
-        # Implement your conditions for buying or selling
-        if today_ha_open > today_ha_close > yesterday_ha_open and today_ha_open > yesterday_ha_open:
-            return 'Buy'
-        elif today_ha_open < today_ha_close < yesterday_ha_open and today_ha_open < yesterday_ha_open:
-            return 'Sell'
+        if yesterday_ha_close > yesterday_ha_open and today_ha_close < today_ha_open:
+            # Check if the last 5-minute candle today is red and the current candle is green
+            last_5min_ha_close = data['HA_Close'].iloc[-2]
+            last_5min_ha_open = data['HA_Open'].iloc[-2]
+            current_ha_close = data['HA_Close'].iloc[-1]
+            current_ha_open = data['HA_Open'].iloc[-1]
+
+            if last_5min_ha_close > last_5min_ha_open and current_ha_close < current_ha_open:
+                return 'Buy'
+            else:
+                return 'Hold'
         else:
             return 'Hold'
 
@@ -33,5 +37,6 @@ def day_ha_check(symbol):
 
 # Example of how to use the function
 symbol = 'AAPL'
-decision = day_ha_check(symbol)
+decision = analyze_stock(symbol)
 print(f"Decision for {symbol}: {decision}")
+
