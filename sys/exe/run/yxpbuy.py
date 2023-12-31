6d7e7c36ff5e49cb470fbb27da023d4d43d45c08
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-01-01 00:29:16 +0530

    Update yxpbuy.py

--- sys/exe/run/yxpbuy.py
+++ sys/exe/run/yxpbuy.py
@@ -121,78 +121,82 @@ if decision == "YES":
 
     symbols_to_sell = []  # Replace this with the list obtained from the
 
-import pandas as pd
-import yfinance as yf
-from nftpxy import nse_action
-from mktpxy import get_market_check
+    import pandas as pd
+    import yfinance as yf
+    from nftpxy import nse_action
+    from mktpxy import get_market_check
 
-def analyze_stock(symbol, nse_action):  # Add nse_action as an argument
-    try:
-        # Append ".NS" to the symbol to specify the NSE exchange
-        symbol_with_exchange = symbol + ".NS"
+    def analyze_stock(symbol, nse_action):  # Add nse_action as an argument
+        try:
+            # Append ".NS" to the symbol to specify the NSE exchange
+            symbol_with_exchange = symbol + ".NS"
 
-        # Download historical stock data for the last 2 days with a daily interval
-        data = yf.download(symbol_with_exchange, period="7d", interval="1d")
-        
-        # Calculate Heikin-Ashi candles for daily data
-        data['Close'] = data['Close']
-        data['Open'] = data['Open']
+            # Download historical stock data for the last 2 days with a daily interval
+            data = yf.download(symbol_with_exchange, period="7d", interval="1d")
+            
+            # Calculate Heikin-Ashi candles for daily data
+            data['Close'] = data['Close']
+            data['Open'] = data['Open']
 
-        # Check if yesterday's candle is red and today's candle is green
-        daybeforeyesterday_close = data['Close'].iloc[-3]
-        daybeforeyesterday_open = data['Open'].iloc[-3]
-        yesterday_close = data['Close'].iloc[-2]
-        yesterday_open = data['Open'].iloc[-2]
-        today_close = data['Close'].iloc[-1]
-        today_open = data['Open'].iloc[-1]
-        mktpxy = get_market_check(symbol_with_exchange)
-        
-        if (
-            daybeforeyesterday_close < daybeforeyesterday_open
-            and yesterday_close > yesterday_open
-            and today_close > today_open
-            and (nse_action == 'Bull' or nse_action == 'Bullish')
-            and mktpxy == 'Buy'
-        ):
-            return 'Buy'
-        elif (
-            daybeforeyesterday_close > daybeforeyesterday_open
-            and yesterday_close < yesterday_open
-            and today_close < today_open
-            and (nse_action == 'Bear' or nse_action == 'Bearish')
-            and mktpxy == 'Sell'
-        ):
-            return 'Sell'
-        else:
-            return 'None'
+            # Check if yesterday's candle is red and today's candle is green
+            daybeforeyesterday_close = data['Close'].iloc[-3]
+            daybeforeyesterday_open = data['Open'].iloc[-3]
+            yesterday_close = data['Close'].iloc[-2]
+            yesterday_open = data['Open'].iloc[-2]
+            today_close = data['Close'].iloc[-1]
+            today_open = data['Open'].iloc[-1]
+            mktpxy = get_market_check(symbol_with_exchange)
+            
+            if (
+                daybeforeyesterday_close < daybeforeyesterday_open
+                and yesterday_close > yesterday_open
+                and today_close > today_open
+                and (nse_action == 'Bull' or nse_action == 'Bullish')
+                and mktpxy == 'Buy'
+            ):
+                return 'Buy'
+            elif (
+                daybeforeyesterday_close > daybeforeyesterday_open
+                and yesterday_close < yesterday_open
+                and today_close < today_open
+                and (nse_action == 'Bear' or nse_action == 'Bearish')
+                and mktpxy == 'Sell'
+            ):
+                return 'Sell'
+            else:
+                return 'None'
 
-    except Exception as e:
-        print(f"Error during data download for {symbol}: {e}")
-        return 'Error'
+        except Exception as e:
+            print(f"Error during data download for {symbol}: {e}")
+            return 'Error'
 
-df_yxp500 = pd.read_csv('yxp500.csv')
-df_HPdf = pd.read_csv('fileHPdf.csv')
+    df_yxp500 = pd.read_csv('yxp500.csv')
+    df_HPdf = pd.read_csv('fileHPdf.csv')
 
-# Use 'tradingsymbol' as the column name
-symbol_list_yxp500 = df_yxp500['tradingsymbol'].tolist()
+    # Use 'tradingsymbol' as the column name
+    symbol_list_yxp500 = df_yxp500['tradingsymbol'].tolist()
 
-# Exclude symbols from fileHPdf.csv
-symbol_list_to_analyze = [symbol for symbol in symbol_list_yxp500 if symbol not in df_HPdf['tradingsymbol'].tolist()]
+    # Exclude symbols from fileHPdf.csv
+    symbol_list_to_analyze = [symbol for symbol in symbol_list_yxp500 if symbol not in df_HPdf['tradingsymbol'].tolist()]
 
-# Initialize an empty list to store symbols to sell
-symbols_to_sell = []
+    # Initialize an empty list to store symbols to sell
+    symbols_to_sell = []
 
-# Analyze each symbol
-for symbol in symbol_list_to_analyze:
-    decision = analyze_stock(symbol, nse_action)  # Pass nse_action as an argument
-    print(f"Decision for {symbol}: {decision}")
+    # Analyze each symbol
+    for symbol in symbol_list_to_analyze:
+        decision = analyze_stock(symbol, nse_action)  # Pass nse_action as an argument
+        print(f"Decision for {symbol}: {decision}")
 
-    # Check if the decision is 'Sell' and append the symbol to the list
-    if decision == 'Sell':
-        symbols_to_sell.append(symbol)
+        # Check if the decision is 'Sell' and append the symbol to the list
+        if decision == 'Sell':
+            symbols_to_sell.append(symbol)
 
-# Print the list of symbols to sell
-print("Symbols to Sell:", symbols_to_sell)
+    # Print the list of symbols to sell
+    print("Symbols to Sell:", symbols_to_sell)
+
+elif decision == "NO":
+    # Perform actions for "NO"
+    print("\033[91mNo Funds Avalable \033[0m")
 
 elif decision == "NO":
     # Perform actions for "NO"
