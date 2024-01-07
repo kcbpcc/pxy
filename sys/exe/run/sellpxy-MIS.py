bd8607e617a24a823f48b31503feb69c96dc1863
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-01-07 17:31:16 +0530

    Update sellpxy-MIS.py

--- sys/exe/run/sellpxy-MIS.py
+++ sys/exe/run/sellpxy-MIS.py
@@ -1,15 +1,85 @@
 #PXY®
+import sys
+import os
 import time
+import datetime
 import subprocess
 import warnings
+import csv
+import random
+import asyncio
+import logging
+import numpy as np
+import pandas as pd
+import yfinance as yf
 from rich import print
 from rich.console import Console
 from rich.style import Style
-import sys
-import yfinance as yf
-import os
-import pandas as pd
+import telegram
+from colorama import Fore, Style
+from cnstpxy import dir_path, sellbuff, secs, perc_col_name
+from toolkit.logger import Logger
+from toolkit.currency import round_to_paise
+from login_get_kite import get_kite, remove_token
+from mktpxy import get_market_check
+from nftpxy import nse_action, nse_power, Day_Change, Open_Change
+from bukdpxy import sum_last_numerical_value_in_each_row
+from swchpxy import analyze_stock
+from selfpxy import get_random_spiritual_message
 
+############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
+def order_place(index, row):
+    try:
+        exchsym = str(index).split(":")
+        if len(exchsym) >= 2:
+            logging.info(f"Placing order for {exchsym[1]}, {str(row)}")
+            order_id = broker.order_place(
+                tradingsymbol=exchsym[1],
+                exchange=exchsym[0],
+                transaction_type='SELL',
+                quantity=int(row['qty']),
+                order_type='LIMIT',
+                product='CNC',
+                variety='regular',
+                price=round_to_paise(row['ltp'], -0.3)
+            )
+            if order_id:
+                logging.info(f"Order {order_id} placed for {exchsym[1]} successfully")                                
+                # Write the row to the CSV file here
+                with open(csv_file_path, 'a', newline='') as csvfile:
+                    csvwriter = csv.writer(csvfile)
+                    csvwriter.writerow(row.tolist())  # Write the selected row to the CSV file
+                    try:
+                        import telegram
+                        import asyncio
+                        columns_to_drop = ['smb_power', 'oPL%', 'pstp', '_pstp', 'qty', 'close', 'open', 'high', 'low', 'PL%_H', 'dPL%', 'pxy','yxp']
+                        # Dropping specified columns from the row
+                        for column in columns_to_drop:
+                            if column in row:
+                                del row[column]
+                        message_text = f"{str(row):>10} \nhttps://www.tradingview.com/chart/?symbol={key}\nBooked profit until now: {result}"
+                        # Define the bot token and your Telegram username or ID
+                        bot_token = '6409002088:AAH9mu0lfjvHl_IgRAgX7YrjJQa2Ew9qaLo'  # Replace with your actual bot token
+                        user_usernames = ('-4022487175')  # Replace with your Telegram username or ID
+                        # Function to send a message to Telegram
+                        async def send_telegram_message(message_text):
+                            bot = telegram.Bot(token=bot_token)
+                            await bot.send_message(chat_id=user_usernames, text=message_text)
+                    except Exception as e:
+                        # Handle the exception (e.g., log it) and continue with your code
+                        print(f"Error sending message to Telegram: {e}")
+                    # Send the 'row' content as a message to Telegram immediately after printing the row
+                    loop = asyncio.get_event_loop()
+                    loop.run_until_complete(send_telegram_message(message_text))
+                return True                
+            else:
+                logging.error("Order placement failed")       
+        else:
+            logging.error("Invalid format for 'index'")    
+    except Exception as e:
+        #print(traceback.format_exc())
+        logging.error(f"{str(e)} while placing order")
+    return False
 ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
 
 # Set the python3IOENCODING environment variable to 'utf-8'
@@ -98,6 +168,7 @@ def get_smbpxy_check(symbol):
         console.print(f"[red]Error determining smbpxy check: {e}[/red]")
         return 'NONE'
 
+############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
 
 csv_file_path = 'zlistpxy.csv'
 symbol_df = pd.read_csv(csv_file_path, header=0)  # Assuming the header is in the first row
