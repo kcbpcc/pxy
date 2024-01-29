bf1d4d78716a94dee893c68a0b4dab6ab1113db1
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-01-29 10:55:13 +0530

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -29,6 +29,9 @@ while True:
     onemincandlesequance, mktpxy = get_market_check()
     from nftpxy import nse_action, nse_power, Day_Change, Open_Change
     importlib.reload(sys.modules['nftpxy'])  # Correct the usage
+    from optpxy import get_optpxy
+    importlib.reload(sys.modules['optpxy'])  # Correct the usage 
+    optpxy = get_optpxy()
     from cyclepxy import cycle
     importlib.reload(sys.modules['cyclepxy'])  # Correct the usage
     print(f"Cycle 🎡 : {cycle} seconds".rjust(40))
@@ -36,21 +39,21 @@ while True:
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
     # Determine the market check based on the candle colors and use rich.print to format output
     if mktpxy == 'Bear':
-        print("It's a 🔴-Bear-🔴 time, selling now...")
+        print("It's a 🔴-Bear-🔴 time, selling now.....")
         subprocess.run(['python3', 'cntrlpxy.py'])
         console.print(f"|📅{Day_Change}|⌛️{Open_Change}|⚡{nse_power}|[bold]Bearish sentiment![/bold]", style=bear_style)
     elif mktpxy == 'Bull':
-        print("It's a 🟢-Bull-🟢 time, Buying now...")
+        print("It's a 🟢-Bull-🟢 time, Buying now......")
         #subprocess.run(['python3', 'buypxy.py']) if nse_action in ["Bull", "Bullish"] and mktpxy in ["Bull", "Buy"] else None
-        #subprocess.run(['python3', 'buypxy.py']) if nse_power < 0.14 and mktpxy in ["Bull", "Buy"] else None
+        #subprocess.run(['python3', 'buypxy.py']) if nse_power < 0.28 and mktpxy in ["Bull", "Buy"] else None
         subprocess.run(['python3', 'cntrlpxy.py'])
         console.print(f"|📅{Day_Change}|⌛️{Open_Change}|⚡{nse_power}|[bold]Bullish sentiment![/bold]", style=bull_style)
     elif mktpxy == 'Sell':
-        print("It's a ⤵️-Sell-⤵️ time, selling now...")
+        print("It's a ⤵️-Sell-⤵️ time, selling now.....")
         subprocess.run(['python3', 'cntrlpxy.py'])
         console.print(f"|📅{Day_Change}|⌛️{Open_Change}|⚡{nse_power}|[bold]Time to sell![/bold]", style=sell_style)
     elif mktpxy == 'Buy':
-        print("It's a ⤴️-Buy-⤴️ time, Buying now...")
+        print("It's a ⤴️-Buy-⤴️ time, Buying now.......")
         #subprocess.run(['python3', 'buypxy.py']) if nse_action in ["Bull", "Bullish"] and mktpxy in ["Bull", "Buy"] else None
         subprocess.run(['python3', 'buypxy.py']) if nse_power < 0.14 and mktpxy in ["Bull", "Buy"] else None
         subprocess.run(['python3', 'cntrlpxy.py'])
@@ -59,22 +62,21 @@ while True:
         subprocess.run(['python3', 'cntrlpxy.py'])
         console.print(f"{nse_action} Day||⚡{nse_power} ⚡||[bold]Market on standby![/bold]")
     # Call the function and store the result in a variable
+    #subprocess.run(['python3', 'tistpxy.py'])  # Run 'tistpxy.py' using subprocess
     subprocess.run(['python3', 'cndlpxy.py'])  # Run 'cndlpxy.py' using subprocess
-    print("-" * 42)  # Print a line of 42 dashes
     console.print(onemincandlesequance)  # Print the content of 'onemincandlesequance' using the 'console.print' method
     # console.print("|", style=green_style if mktpxy in ["Buy", "Bull"] else red_style)  # Commented out line
     print("-" * 42)  # Print a line of 42 dashes
-    if mktpxy == "Bull":
-        console.print("🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛", style=green_style)
-    elif mktpxy == "Buy":
-        console.print("🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛", style=green_style)
-    elif mktpxy == "Sell":
-        console.print("🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛", style=red_style)
-    elif mktpxy == "Bear":
-        console.print("🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛", style=red_style)
+    if optpxy == "Bull":
+        console.print("[bold]🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛[/bold]", style=green_style)
+    elif optpxy == "Buy":
+        console.print("[bold]🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛[/bold]", style=green_style)
+    elif optpxy == "Sell":
+        console.print("[bold]🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛[/bold]", style=red_style)
+    elif optpxy == "Bear":
+        console.print("[bold]🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛[/bold]", style=red_style)
     else:
         console.print("🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛", style=standby_style)
-    subprocess.run(['python3', 'tistpxy.py'])  # Run 'tistpxy.py' using subprocess
     print("-" * 42)  # Print another line of 42 dashes
     def progress_bar(duration):
         for i in range(duration):
