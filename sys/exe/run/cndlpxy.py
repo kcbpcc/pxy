64ca7ed8a6695a570db86f6354cc5dba260222b2
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-01-30 22:25:29 +0800

    Update cndlpxy.py

--- sys/exe/run/cndlpxy.py
+++ sys/exe/run/cndlpxy.py
@@ -60,17 +60,18 @@ def dayprinter(o, h, l, c, prev_close):
     arrow = " ━━🟢🟢━━ " if c > prev_close else " ━━🔴🔴━━ "
     print(f"Yesterday:{int(prev_close)} {arrow} Today-Now:{int(c)}")
     
-    # Print the colored bar graph with red and green emojis
-    print(Fore.LIGHTWHITE_EX + '==' * int((n / 100) * total_length), end='')
+    print(Fore.LIGHTWHITE_EX + '━' * int((n / 100) * total_length), end='')
+
     if c > o:
         print(Fore.GREEN + '▌' * int((x / 100) * total_length) + Style.RESET_ALL, end='')
-    if o > c:
+    elif o > c:
         print(Fore.RED + '▌' * int((x / 100) * total_length) + Style.RESET_ALL, end='')
-    print(Fore.LIGHTWHITE_EX + '==' * int((m / 100) * total_length))
-
+    
+    print(Fore.LIGHTWHITE_EX + '━' * int((m / 100) * total_length))
+    
     # Determine the color based on the comparison of today's close with yesterday's close
     color = Fore.GREEN if c > prev_close else Fore.RED
-
+    
 # Example usage in the main program
 previous_day_close = get_previous_day_close()
 today_close = get_today_close()
