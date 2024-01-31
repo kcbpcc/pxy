d18c5bcbfa897a0c9c40fadb441528305fa9ae54
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-01 07:28:40 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -628,7 +628,7 @@ try:
              result_nrml, total_PnL_cnc_buy, total_PnL_nrml_buy, available_cash, auto_value,
              nse_action, nse_power,red_Stocks_count,green_Stocks_count,all_Stocks_capital_lacks,all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage, mktpxy)
 ###########################################################################################################################################################################################################
-    from cndlpxy import option_to_trade()
+    from cndlpxy import option_to_trade
     option_to_trade() = option_value
     print(f"PE and CE Buy optins both@: {option_value}")
     # Check if DataFrame is empty
