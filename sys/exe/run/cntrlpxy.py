28eb21069ca8174d6dbc801e397e1a3cab280072
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-01 09:29:13 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -565,7 +565,7 @@ try:
                         row['avg'] != 0 and
                         row['product'] in ['NRML', 'MIS'] and
                         auto_value == 'AUTO'and
-                        row['PL%'] > row['otPL%']
+                        nrml_percentage_return > row['otPL%']
                     ):
                         try:                            
                             is_placed = nrml_order_place(key, row) if get_open_order_status(symbol_in_order) == "NO" else False
