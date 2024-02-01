f985d5c0ab86d02c1d478356bc199dc74869a524
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-01 23:59:20 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -53,7 +53,7 @@ def printbord(Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, t
                         right_aligned_format.format(f"Day PnL:{BRIGHT_GREEN if total_dPnL > 0 else BRIGHT_RED}{int(round(total_dPnL, 2))}{RESET}"))   
     output_lines.append(left_aligned_format.format(f"Opt Drive:{BRIGHT_GREEN if auto_value_status == 'Yes' else BRIGHT_RED}{auto_value}{RESET}") +
                         right_aligned_format.format(f"My Day:{BRIGHT_GREEN if total_dPnL_percentage >= 0 else BRIGHT_RED}{round(total_dPnL_percentage, 2)}%{RESET}"))
-    output_lines.append(left_aligned_format.format(f"Options:{BRIGHT_GREEN if total_PnL_nrml_buy >= 0 else BRIGHT_RED}{int(total_PnL_nrml_buy)}{RESET}") +
+    output_lines.append(left_aligned_format.format(f"Trend:{onemincandlesequance}{RESET}") +
                         right_aligned_format.format(f"Stocks:{BRIGHT_GREEN if total_PnL_cnc_buy >= 0 else BRIGHT_RED}{int(total_PnL_cnc_buy)}{RESET}")) 
     output_lines.append(left_aligned_format.format(f"Extras:{BRIGHT_GREEN if result_nrml >= 0 else BRIGHT_RED}{str(int(result_nrml)).zfill(5)}{RESET}") +
                         right_aligned_format.format(f"Booked:{BRIGHT_GREEN if result > 0 else BRIGHT_RED}{str(round(result)).zfill(5)}{RESET}"))
