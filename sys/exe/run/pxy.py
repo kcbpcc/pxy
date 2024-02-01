c6cfbd26a227b1b14fd7da51d75d91df89b0f767
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-02 01:15:20 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -61,6 +61,7 @@ while True:
     # Call the function and store the result in a variable
     #subprocess.run(['python3', 'tistpxy.py'])  # Run 'tistpxy.py' using subprocess
     subprocess.run(['python3', 'cndlpxy.py'])  # Run 'cndlpxy.py' using subprocess
+    subprocess.run(['python3', 'tistpxy.py'])
     console.print(onemincandlesequance)  # Print the content of 'onemincandlesequance' using the 'console.print' method
     print("━" * 42)  # Print another line of 42 dashes
 
@@ -75,7 +76,6 @@ while True:
         console.print("[bold]🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛[/bold]", style=red_style)
     else:
         console.print("🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛", style=standby_style)
-    subprocess.run(['python3', 'tistpxy.py'])
     def progress_bar(duration):
         for i in range(duration):
             time.sleep(1)
