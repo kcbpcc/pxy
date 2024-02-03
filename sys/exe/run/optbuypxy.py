ac99f12f06203d81140d05f84c862e77deb4049d
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-03 15:51:50 +0800

    Update optbuypxy.py

--- sys/exe/run/optbuypxy.py
+++ sys/exe/run/optbuypxy.py
@@ -69,7 +69,9 @@ ltp_PE = get_ltp("NFO", symbol_PE)
 ltp_CE = get_ltp("NFO", symbol_CE)
 
 # Get user confirmation
-print("Do you want to execute", symbol_CE, symbol_PE, ltp_CE, ltp_PE)
+print(symbol_CE, symbol_PE)
+print(ltp_CE, ltp_PE)
+print("Do you want to execute")
 start_time = time.time()
 user_confirmation = ''
 while time.time() - start_time < 120:
