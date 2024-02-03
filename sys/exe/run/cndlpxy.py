34d3ee18f706e54242993d1816e0a4c33e9680b3
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-04 06:47:06 +0800

    Update cndlpxy.py

--- sys/exe/run/cndlpxy.py
+++ sys/exe/run/cndlpxy.py
@@ -57,7 +57,10 @@ def dayprinter(o, h, l, c, prev_close):
         m = 100 - n - x
 
     # Print both the previous day's close and today's close in a single sentence with color
-
+    arrow = "🟩🟩" if c > prev_close else "🟥🟥"
+    ha_close = (o + h + l + c) / 4
+    haarrow = "👆👆" if ha_close > prev_close else "👇👇"
+    print(f"Yesterday:{int(prev_close)}{haarrow}{arrow}Today-Now:{int(c)}")  
 
     print(Fore.LIGHTWHITE_EX + '━' * int((n / 100) * total_length), end='')
 
@@ -70,10 +73,7 @@ def dayprinter(o, h, l, c, prev_close):
     
     # Determine the color based on the comparison of today's close with yesterday's close
     color = Fore.GREEN if c > prev_close else Fore.RED
-    arrow = "🟩" if c > prev_close else "🟥"
-    ha_close = (o + h + l + c) / 4
-    haarrow = "👆" if ha_close > prev_close else "👇"
-    print(f"Yesterday:{int(prev_close)}{arrow}{haarrow}Today-Now:{int(c)}")   
+ 
 def option_to_trade():
     today_data = get_nifty50_data().iloc[-1][OHLC_COLUMNS]
     today_open = today_data['Open']
