cbb138fe64c94f618b1ed183903b23eaeff0a5a2
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-05 17:37:25 +0800

    Update cndlpxy.py

--- sys/exe/run/cndlpxy.py
+++ sys/exe/run/cndlpxy.py
@@ -57,7 +57,7 @@ def dayprinter(o, h, l, c, prev_close):
         m = 100 - n - x
 
     # Print both the previous day's close and today's close in a single sentence with color
-    arrow = (f"{Fore.GREEN}🟩") if c > prev_close else ("-", f"{Fore.RED}🟥")
+    arrow = (f"{Fore.GREEN}🟩") if c > prev_close else (f"{Fore.RED}🟥")
     print(f"{int(prev_close)}{arrow}{int(c)}", end='')   
     print(Fore.LIGHTWHITE_EX + '=' * int((n / 100) * total_length), end='')
 
