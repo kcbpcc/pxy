0727dd6e7e004f14a2118525f2b9d8c5b5a9214b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-05 12:33:52 +0800

    Update cndlpxy.py

--- sys/exe/run/cndlpxy.py
+++ sys/exe/run/cndlpxy.py
@@ -3,11 +3,14 @@ import yfinance as yf
 import pandas as pd
 import colorama
 from colorama import Fore, Style  # Add Style to the imports
+
 colorama.init(autoreset=True)
 
 OHLC_COLUMNS = ['Open', 'High', 'Low', 'Close']
+
 def get_nifty50_data(days=2):
     ticker_symbol = "^NSEI"  # NIFTY50 index symbol on Yahoo Finance
+
     try:
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=FutureWarning)
@@ -19,7 +22,9 @@ def get_nifty50_data(days=2):
 
         # Extract OHLC data
         ohlc_data = nifty_data[OHLC_COLUMNS]
+
         return ohlc_data
+
     except Exception as e:
         print(f"Error fetching data: {e}")
         return pd.DataFrame()  # Return an empty DataFrame in case of an error
@@ -39,7 +44,7 @@ def get_today_close():
         return None  # Handle the case when data is not available
 
 def dayprinter(o, h, l, c, prev_close):
-    total_length = 24
+    total_length = 20
     
     # Calculate the lengths of different segments as percentages
     if c > o:
@@ -50,11 +55,22 @@ def dayprinter(o, h, l, c, prev_close):
         n = round(((c - (l-1)) / ((h+1) - (l-1))) * 100)
         x = round(((o - c) / ((h+1) - (l-1))) * 100)
         m = 100 - n - x
+
+    # Print both the previous day's close and today's close in a single sentence with color
+    arrow = " ━━🟢🟢━━ " if c > prev_close else " ━━🔴🔴━━ "
+    #print(f"Yesterday:{int(prev_close)} {arrow} Today-Now:{int(c)}")   
+    print(Fore.LIGHTWHITE_EX + '━' * int((n / 100) * total_length), end='')
+
+    if c > o:
+        print(Fore.GREEN + '▌' * int((x / 100) * total_length) + Style.RESET_ALL, end='')
+    elif o > c:
+        print(Fore.RED + '▌' * int((x / 100) * total_length) + Style.RESET_ALL, end='')
     
-    ha_close = (o + h + l + c) / 4
-    haarrow = f"{Fore.GREEN}👆" if ha_close > prev_close else f"{Fore.RED}👇"
-    arrow = (f"{Fore.GREEN}ﮩ٨ـﮩﮩ٨ـ") if c > prev_close else ("-", f"{Fore.RED}ﮩ٨ـﮩﮩ٨ـ")
-    print((f"{Fore.GREEN}{'▌' * int((x / 100) * total_length)}" if c > o else f"{Fore.RED}{'▌' * int((x / 100) * total_length)}" if o > c else "") + f"{Style.RESET_ALL}{Fore.LIGHTWHITE_EX}{'━' * int((m / 100) * total_length)}" + f"{int(prev_close)}{arrow}{int(c)}{haarrow}" + f"{Fore.LIGHTWHITE_EX}{'━' * int((n / 100) * total_length)}")
+    print(Fore.LIGHTWHITE_EX + '━' * int((m / 100) * total_length))
+    
+    # Determine the color based on the comparison of today's close with yesterday's close
+    color = Fore.GREEN if c > prev_close else Fore.RED
+    
 def option_to_trade():
     today_data = get_nifty50_data().iloc[-1][OHLC_COLUMNS]
     today_open = today_data['Open']
