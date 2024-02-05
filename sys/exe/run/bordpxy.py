9f480cb624ce69dbea7c9c34f94da46e87537617
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-05 18:26:49 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -45,7 +45,7 @@ def printbord(Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, t
     )    
     output_lines.append(left_aligned_format.format(f"Funds:{BRIGHT_GREEN if available_cash > 12000 else BRIGHT_YELLOW}{available_cash:.0f}{RESET}") +
                         right_aligned_format.format(f"Winners:{BRIGHT_YELLOW}{zero_qty_count}{RESET}"))
-    output_lines.append(left_aligned_format.format(f"A/C Delta:{BRIGHT_GREEN if ydaypnl_to_print > 0 else BRIGHT_RED}{round(ydaypnl_to_print, 0) * 1000}{RESET}") +
+    output_lines.append(left_aligned_format.format(f"A/C Delta:{BRIGHT_GREEN if ydaypnl_to_print > 0 else BRIGHT_RED}{round(ydaypnl_to_print, 5) * 1000}{RESET}") +
                         right_aligned_format.format(f"Day PnL:{BRIGHT_GREEN if total_dPnL > 0 else BRIGHT_RED}{int(round(total_dPnL, 2))}{RESET}"))   
     output_lines.append(left_aligned_format.format(f"Opt Drive:{BRIGHT_GREEN if auto_value_status == 'Yes' else BRIGHT_RED}{auto_value}{RESET}") +
                         right_aligned_format.format(f"My Day:{BRIGHT_GREEN if total_dPnL_percentage >= 0 else BRIGHT_RED}{round(total_dPnL_percentage, 2)}%{RESET}"))
