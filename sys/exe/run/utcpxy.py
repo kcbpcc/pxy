73b9d5b751770aa0a38b685bf0e51ba7267845a3
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-06 18:59:45 +0800

    Update utcpxy.py

--- sys/exe/run/utcpxy.py
+++ sys/exe/run/utcpxy.py
@@ -11,21 +11,20 @@ def peak_time():
     peak_time_2_start = datetime.strptime("09:52", "%H:%M").replace(tzinfo=timezone.utc)
     peak_time_2_end = datetime.strptime("10:00", "%H:%M").replace(tzinfo=timezone.utc)
 
-    # Calculate the time gap between peak_time_1_end and peak_time_2_start
-    time_gap = peak_time_2_start - peak_time_1_end
-
-    # Check if the current time falls within the time gap
-    in_time_gap = peak_time_1_end <= utc_time <= peak_time_1_end + time_gap
+    peak_time_3_start = datetime.strptime("04:01", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_3_end = datetime.strptime("09:51", "%H:%M").replace(tzinfo=timezone.utc)
 
     # Check if the current time is within the peak time ranges
     in_peak_time_1 = peak_time_1_start <= utc_time <= peak_time_1_end
     in_peak_time_2 = peak_time_2_start <= utc_time <= peak_time_2_end
+    in_peak_time_3 = peak_time_3_start <= utc_time <= peak_time_3_end
 
     if in_peak_time_1:
         return "peakstart"
     elif in_peak_time_2:
         return "peakend"
-    elif in_time_gap:
+    elif in_peak_time_3:
         return "nonpeak"
     else:
-        return "nonmarket"  # or any other label you prefer for times outside peak and non-peak
+        return "nonmkt"
+
