99f4934dc991626bab180415477a2051cd8340e1
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-07 19:04:21 +0800

    Update utcpxy.py

--- sys/exe/run/utcpxy.py
+++ sys/exe/run/utcpxy.py
@@ -9,7 +9,7 @@ def peak_time():
     peak_time_1_end = datetime.strptime("04:00", "%H:%M").replace(tzinfo=timezone.utc)
     
     peak_time_2_start = datetime.strptime("14:45", "%H:%M").replace(tzinfo=timezone.utc)
-    peak_time_2_end = datetime.strptime("24:00", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_2_end = datetime.strptime("23:59", "%H:%M").replace(tzinfo=timezone.utc)
 
     peak_time_3_start = peak_time_1_end
     peak_time_3_end = peak_time_2_start
