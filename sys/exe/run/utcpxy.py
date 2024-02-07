d41c6e75c401ca7314feb805074bb8b4ffa04866
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-07 15:54:19 +0800

    Update utcpxy.py

--- sys/exe/run/utcpxy.py
+++ sys/exe/run/utcpxy.py
@@ -1,28 +1,28 @@
 from datetime import datetime, timezone
 
-def peak_time():
-    # Get the current UTC time
-    current_utc_time = datetime.utcnow().time()
-
+def peak_time(utc_time):
     # Define the peak time ranges
-    peak_start_1 = datetime.strptime("03:45", "%H:%M").time()
-    peak_end_1 = datetime.strptime("04:00", "%H:%M").time()
+    peak_start_1 = datetime.strptime("03:45", "%H:%M").replace(tzinfo=timezone.utc).time()
+    peak_end_1 = datetime.strptime("04:00", "%H:%M").replace(tzinfo=timezone.utc).time()
     
-    non_peak_start = datetime.strptime("04:00", "%H:%M").time()
-    non_peak_end = datetime.strptime("09:45", "%H:%M").time()
+    non_peak_start = datetime.strptime("04:00", "%H:%M").replace(tzinfo=timezone.utc).time()
+    non_peak_end = datetime.strptime("09:45", "%H:%M").replace(tzinfo=timezone.utc).time()
 
-    peak_start_2 = datetime.strptime("09:45", "%H:%M").time()
-    peak_end_2 = datetime.strptime("10:00", "%H:%M").time()
+    peak_start_2 = datetime.strptime("09:45", "%H:%M").replace(tzinfo=timezone.utc).time()
+    peak_end_2 = datetime.strptime("10:00", "%H:%M").replace(tzinfo=timezone.utc).time()
 
     # Check if the current time is within the peak time ranges
-    if peak_start_1 <= current_utc_time < peak_end_1:
+    if peak_start_1 <= utc_time < peak_end_1:
         return "peakstart"
-    elif non_peak_start <= current_utc_time < non_peak_end:
+    elif non_peak_start <= utc_time < non_peak_end:
         return "nonpeak"
-    elif peak_start_2 <= current_utc_time < peak_end_2:
+    elif peak_start_2 <= utc_time < peak_end_2:
         return "peakend"
     else:
         return "nonmkt"
 
-print(peak_time())
+# Get the current UTC time
+current_utc_time = datetime.utcnow().replace(tzinfo=timezone.utc).time()
+
+print(peak_time(current_utc_time))
 
