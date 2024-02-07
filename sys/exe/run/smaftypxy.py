914d65834e9e5758b0bbccaef38c057d3c989dfb
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-07 14:45:48 +0800

    Update smaftypxy.py

--- sys/exe/run/smaftypxy.py
+++ sys/exe/run/smaftypxy.py
@@ -1,9 +1,6 @@
 import yfinance as yf
 import warnings
 
-# Suppress FutureWarning
-warnings.simplefilter(action='ignore', category=FutureWarning)
-
 # Step 1: Retrieve historical price data for Nifty50
 nifty_ticker = yf.Ticker("^NSEI")
 data = nifty_ticker.history(period='1d', interval='1d')
@@ -14,8 +11,12 @@ sma_50_nifty = data['Close'].rolling(window=50).mean()
 # Step 3: Get the present Nifty close
 present_nifty_close = data['Close'].iloc[-1]
 
-# Step 4: Compare present Nifty close with 50-day SMA of Nifty50
-if present_nifty_close > sma_50_nifty.iloc[-1]:
-    print("50NIFTY")
-else:
-    print("NIFTY50")
+# Step 4: Suppress FutureWarning temporarily for this section
+with warnings.catch_warnings():
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+
+    # Step 5: Compare present Nifty close with 50-day SMA of Nifty50
+    if present_nifty_close > sma_50_nifty.iloc[-1]:
+        print("NIFTYBULL")
+    else:
+        print("NIFTYBEAR")
