87d46e273bc990c9e08747082b17fdee9c3fa84a
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-07 19:57:25 +0800

    Update utcpxy.py

--- sys/exe/run/utcpxy.py
+++ sys/exe/run/utcpxy.py
@@ -1,35 +1,43 @@
 from datetime import datetime, timezone
 
 def peak_time():
-    # Get the current UTC time
-    utc_time = datetime.now(timezone.utc)
+    # Get the current UTC time without date component
+    current_utc_time = datetime.utcnow().time()
+
+    # Print current time for debugging
+    print("Current UTC Time (without date):", current_utc_time)
 
     # Define the peak time ranges
-    peak_time_0_start = datetime.strptime("00:00", "%H:%M").replace(tzinfo=timezone.utc)
-    peak_time_0_end = datetime.strptime("03:44", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_0_start = datetime.strptime("00:00", "%H:%M").time()
+    peak_time_0_end = datetime.strptime("03:44", "%H:%M").time()
+    print("Peak Time 0 Range:", peak_time_0_start, "-", peak_time_0_end)
 
-    peak_time_1_start = datetime.strptime("03:45", "%H:%M").replace(tzinfo=timezone.utc)
-    peak_time_1_end = datetime.strptime("03:59", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_1_start = datetime.strptime("03:45", "%H:%M").time()
+    peak_time_1_end = datetime.strptime("03:59", "%H:%M").time()
+    print("Peak Time 1 Range:", peak_time_1_start, "-", peak_time_1_end)
 
-    peak_time_2_start = datetime.strptime("04:00", "%H:%M").replace(tzinfo=timezone.utc)
-    peak_time_2_end = datetime.strptime("09:50", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_2_start = datetime.strptime("04:00", "%H:%M").time()
+    peak_time_2_end = datetime.strptime("09:50", "%H:%M").time()
+    print("Peak Time 2 Range:", peak_time_2_start, "-", peak_time_2_end)
 
-    peak_time_3_start = datetime.strptime("09:51", "%H:%M").replace(tzinfo=timezone.utc)
-    peak_time_3_end = datetime.strptime("17:01", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_3_start = datetime.strptime("09:51", "%H:%M").time()
+    peak_time_3_end = datetime.strptime("17:01", "%H:%M").time()
+    print("Peak Time 3 Range:", peak_time_3_start, "-", peak_time_3_end)
 
-    peak_time_4_start = datetime.strptime("17:02", "%H:%M").replace(tzinfo=timezone.utc)
-    peak_time_4_end = datetime.strptime("23:59", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_4_start = datetime.strptime("17:02", "%H:%M").time()
+    peak_time_4_end = datetime.strptime("23:59", "%H:%M").time()
+    print("Peak Time 4 Range:", peak_time_4_start, "-", peak_time_4_end)
 
     # Check current time against defined ranges
-    if peak_time_0_start <= utc_time <= peak_time_0_end:
+    if peak_time_0_start <= current_utc_time <= peak_time_0_end:
         return "NONMKT"
-    elif peak_time_1_start <= utc_time <= peak_time_1_end:
+    elif peak_time_1_start <= current_utc_time <= peak_time_1_end:
         return "PEAKSTART"
-    elif peak_time_2_start <= utc_time <= peak_time_2_end:
+    elif peak_time_2_start <= current_utc_time <= peak_time_2_end:
         return "NONPEAK"
-    elif peak_time_3_start <= utc_time <= peak_time_3_end:
+    elif peak_time_3_start <= current_utc_time <= peak_time_3_end:
         return "PEAKEND"
-    elif peak_time_4_start <= utc_time <= peak_time_4_end:
+    elif peak_time_4_start <= current_utc_time <= peak_time_4_end:
         return "NONMKT"
 
     # If none of the above conditions are met, return a default value
