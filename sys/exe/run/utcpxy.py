bf171b2e02d3d4c4a2816388fa0a550056871fc1
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-07 16:14:57 +0800

    Update utcpxy.py

--- sys/exe/run/utcpxy.py
+++ sys/exe/run/utcpxy.py
@@ -11,8 +11,8 @@ def peak_time():
     peak_time_2_start = datetime.strptime("09:45", "%H:%M").replace(tzinfo=timezone.utc)
     peak_time_2_end = datetime.strptime("10:00", "%H:%M").replace(tzinfo=timezone.utc)
 
-    peak_time_3_start = datetime.strptime("04:01", "%H:%M").replace(tzinfo=timezone.utc)
-    peak_time_3_end = datetime.strptime("09:44", "%H:%M").replace(tzinfo=timezone.utc)
+    peak_time_3_start = peak_time_1_end
+    peak_time_3_end = peak_time_2_start
 
     if peak_time_1_start <= utc_time <= peak_time_1_end:
         print("Within peak time 1")
@@ -26,3 +26,7 @@ def peak_time():
     else:
         print("Outside of peak times")
         return "nonmkt"
+
+# Print the value returned by the peak_time() function
+print(peak_time())
+
