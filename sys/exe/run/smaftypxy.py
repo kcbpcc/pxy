8d8c70543573c3f23727b9172ee6dfec17b0274f
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-07 14:47:20 +0800

    Update smaftypxy.py

--- sys/exe/run/smaftypxy.py
+++ sys/exe/run/smaftypxy.py
@@ -2,9 +2,8 @@ import yfinance as yf
 import warnings
 
 # Step 1: Retrieve historical price data for Nifty50
-nifty_ticker = yf.Ticker("^NSEI")
-data = nifty_ticker.history(period='1d', interval='1d')
 
+data = yf.Ticker('^NSEI').history(period="5d", interval="5m")
 # Step 2: Calculate the 50-day SMA of Nifty50
 sma_50_nifty = data['Close'].rolling(window=50).mean()
 
