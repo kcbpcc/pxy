2474e40f5445f402c668bb463d277db2b76e940c
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-07 21:57:35 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -57,14 +57,15 @@ while True:
     elif mktpxy == 'Sell':
         print("It's a ⤵️-Sell-⤵️ time, selling now.....")
         subprocess.run(['python3', 'cntrlpxy.py'])
+        subprocess.run(['python3', 'buypepxy.py']) if > 0.9 else None
     elif mktpxy == 'Buy':
         print("It's a ⤴️-Buy-⤴️ time, Buying now.......")
         subprocess.run(['python3', 'buypxy.py']) if SMAfty == 'BULLNIFTY' and peak == 'NONPEAK' else None
         subprocess.run(['python3', 'cntrlpxy.py'])
+        subprocess.run(['python3', 'buycepxy.py']) if < 0.1 else None
     elif mktpxy == 'None':
         subprocess.run(['python3', 'cntrlpxy.py'])
     # Call the function and store the result in a variable
-    subprocess.run(['python3', 'optbuypxy.py']) if nse_power < 0.1 or nse_power > 0.9 else None
     subprocess.run(['python3', 'buypxy.py']) if peak == 'PEAKEND' else None
     subprocess.run(['python3', 'cndlpxy.py'])  # Run 'cndlpxy.py' using subprocess 
     try:
