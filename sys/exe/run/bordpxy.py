708e03fc1eace51cf56a47bc7994f4bd19158e57
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-07 09:54:53 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -2,6 +2,8 @@
 from acvalue import process_acvalue, get_current_acvalue
 from optpxy import get_optpxy
 optpxy = get_optpxy()
+from macdpxy import calculate_macd_signal
+macd = calculate_macd_signal("^NSEI")
 def printbord(Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, total_dPnL_percentage,
              result_nrml, total_PnL_cnc_buy, total_PnL_nrml_buy, available_cash, auto_value,
              nse_action, nse_power,red_Stocks_count,green_Stocks_count,all_Stocks_capital_lacks,all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage, mktpxy):                
