10959af6d1d4869156359460e248da0ca7ebe8aa
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-08 23:12:26 +0800

    Update smaftypxy.py

--- sys/exe/run/smaftypxy.py
+++ sys/exe/run/smaftypxy.py
@@ -17,9 +17,9 @@ def check_nifty_status():
 
         # Compare present Nifty close with 50-day SMA of Nifty50
         if present_nifty_close > sma_50_nifty.iloc[-1]:
-            return "⬆SMA50⬆"
+            return "SMA50🟩"
         else:
-            return "⬇SMA50⬇"
+            return "SMA50🟥"
 
 # Example usage
 #print(check_nifty_status())
