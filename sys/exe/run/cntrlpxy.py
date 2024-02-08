3366309b039c6083dd1344c17d54f07ca0cd2fda
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-08 22:06:30 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -584,7 +584,7 @@ try:
         # print(nrml_filtered_df)
     else:
         filtered_df = nrml_filtered_df[nrml_filtered_df['qty'] != 0].copy()
-        filtered_df.loc[:, ['Invested', 'PL%']] = filtered_df[['Invested', 'PL%']].round(0)
+        filtered_df.loc[:, ['Invested', 'PL%']] = filtered_df[['Invested', 'PL%']].round(0).astype(int)
         formatted_lines = filtered_df[['Invested', 'key', 'qty', 'PL%', 'PnL']].to_string(index=False, header=False).split('\n')
         formatted_lines_sorted = sorted(formatted_lines, key=lambda x: x.split()[1][:-2])
         # Set max_width to 42
