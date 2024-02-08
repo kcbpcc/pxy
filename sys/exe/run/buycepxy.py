32bca0a71dae02f7372338a17ed58455c6473c1b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-08 08:01:17 +0800

    Update buycepxy.py

--- sys/exe/run/buycepxy.py
+++ sys/exe/run/buycepxy.py
@@ -28,7 +28,7 @@ try:
             broker = get_kite(api="bypass", sec_dir=dir_path)
         except Exception as e:
             remove_token(dir_path)
-            traceback.format_exc())
+            traceback.format_exc()
             logging.error(f"{str(e)} unable to get holdings")
             sys.exit(1)
 
