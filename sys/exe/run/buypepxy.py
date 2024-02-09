cef0c1b2a06a6321bde85d2ad6a927c9cbc02bc7
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-09 13:39:36 +0800

    Update buypepxy.py

--- sys/exe/run/buypepxy.py
+++ sys/exe/run/buypepxy.py
@@ -39,7 +39,7 @@ async def send_telegram_message(message_text):
     try:
         # Define the bot token and your Telegram username or ID
         bot_token = '6924826872:AAHTiMaXmjyYbGsCFhdZlRRXkyfZTpsKPug'  # Replace with your actual bot token
-        user_usernames = ('-4135910842')  # Replace with your Telegram username or ID
+        user_usernames = '-4135910842'  # Replace with your Telegram username or ID
 
         # Create a Telegram bot
         bot = telegram.Bot(token=bot_token)
