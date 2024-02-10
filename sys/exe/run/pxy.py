5f3916beee919172df15fa74a52169759215af73
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-10 10:59:04 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -85,8 +85,7 @@ while True:
         console.print("[bold]🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛[/bold]", style=red_style)
     else:
         console.print("🏛🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛🏛", style=standby_style)
-    padding = ' ' * max(0, 43 - len(random_message))
-    console.print(padding + "[bold]" + random_message + "[/bold]", style=standby_style)
+    console.print("[bold]" + random_message + "[/bold]", style=standby_style)
     from rich.console import Console
     from rich.style import Style
     import time
