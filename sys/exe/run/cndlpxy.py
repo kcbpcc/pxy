ff1d98878290bfd70f5afe88f549eb7754516de4
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-10 08:16:52 +0800

    Update cndlpxy.py

--- sys/exe/run/cndlpxy.py
+++ sys/exe/run/cndlpxy.py
@@ -78,6 +78,7 @@ def dayprinter(o, h, l, c, prev_close):
         # Print both the previous day's close and today's close in a single sentence with color
         SMAftywave = (f"{Fore.GREEN}ﮩﮩ٨") if SMAfty == 'up' else (f"{Fore.RED}ﮩﮩ٨")
         print(f"🔆{day_change_sign}{Day_Change}⌛️{open_change_sign}{Open_Change}⚡{nse_power}|", end='')   
+        print (f"{SMAftywave}{onemincandlesequance}🚦{macd}")
         print(Fore.LIGHTWHITE_EX + '━' * int((n / 100) * total_length), end='')
     
         if c > o:
@@ -85,9 +86,7 @@ def dayprinter(o, h, l, c, prev_close):
         elif o > c:
             print(Fore.RED + '━' * int((x / 100) * total_length) + Style.RESET_ALL, end='')
         
-        print(Fore.LIGHTWHITE_EX + '━' * int((m / 100) * total_length), end='')
-    
-        print (f"{SMAftywave}{onemincandlesequance}🚦{macd}")
+        print(Fore.LIGHTWHITE_EX + '━' * int((m / 100) * total_length))
     except Exception as e:
         pass
     
