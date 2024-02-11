a3eeffe53e1ff49284138b9c5f40bd024764765d
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-11 15:19:13 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -599,7 +599,7 @@ try:
             )
         )
         filtered_df['PL%'] = filtered_df['PL%'].astype(int)
-        formatted_lines = filtered_df[['option_power', 'Invested', 'key', 'qty','otPL%', 'PL%', 'PnL']].to_string(index=False, header=False).split('\n')
+        formatted_lines = filtered_df[['Invested', 'key', 'qty','otPL%', 'PL%', 'PnL']].to_string(index=False, header=False).split('\n')
         formatted_lines_sorted = sorted(formatted_lines, key=lambda x: x.split()[1][:-2])
         # Set max_width to 42
         max_width = 42
