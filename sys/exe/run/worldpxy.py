fabccdd726721be370120e9235a343e2d18cc713
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-11 19:31:26 +0800

    Update worldpxy.py

--- sys/exe/run/worldpxy.py
+++ sys/exe/run/worldpxy.py
@@ -20,8 +20,8 @@ def calculate_sentiment(today_close, yesterday_close):
 # Dictionary of major stock exchanges with weights based on their significance
 exchanges = {
     "^DJI": {"name": "US-D&J", "weight": 0.35},
-    "^IXIC": {"name": "US-Nasdaq", "weight": 0.30},
-    "^GSPC": {"name": "US-S&p", "weight": 0.35},
+    "^IXIC": {"name": ",Nasdaq", "weight": 0.30},
+    "^GSPC": {"name": ",US-S&P", "weight": 0.35},
     "^FTSE": {"name": "UK", "weight": 0.20},
     "^GDAXI": {"name": "Germany", "weight": 0.15},
     "^FCHI": {"name": "France", "weight": 0.15},
@@ -50,7 +50,7 @@ for name, price_today in closing_prices_today.items():
     price_yesterday = closing_prices_yesterday[name]
     sentiment = calculate_sentiment(price_today, price_yesterday)
     sentiment_style = "green" if sentiment == "Bullish" else "red" if sentiment == "Bearish" else "default"
-    index_info += f"[{sentiment_style}]{name}[/{sentiment_style}]  "
+    index_info += f"[{sentiment_style}]{name}[/{sentiment_style}]"
 
 # Print all index names in one row with sentiment color
 console.print(index_info)
