67aba27d5959ffe26e028ac198068c58049d08e8
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-19 21:48:37 +0800

    Update buyoptpxy.py

--- sys/exe/run/buyoptpxy.py
+++ sys/exe/run/buyoptpxy.py
@@ -157,9 +157,9 @@ async def main():
     option_type = None  # Default value
     
     # Determine option type based on mktpxy
-    if mktpxy == 'Buy': # and nse_action == 'Bullish' and SMAfty == 'down'
+    if mktpxy == 'Buy'and nse_action == 'Bullish' and SMAfty == 'down':
         option_type = 'CE'  # Call Option
-    elif mktpxy == 'Sell': # and nse_action == 'Bearish' and SMAfty == 'up':
+    elif mkttpxy == 'Sell' and nse_action == 'Bearish' and SMAfty == 'up':
         option_type = 'PE'  # Put Option
     else:
         # Handle the case where mktpxy doesn't match any condition
