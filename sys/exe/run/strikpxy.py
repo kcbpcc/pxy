4218ae1b573ef7abb14858a8b2cbfe340ba8dcd3
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-20 21:02:11 +0800

    Update strikpxy.py

--- sys/exe/run/strikpxy.py
+++ sys/exe/run/strikpxy.py
@@ -1,15 +1,5 @@
 import yfinance as yf
 
-# Define the symbols
-symbols = ['^NSEI', '^NSEBANK', 'NIFTY_FIN_SERVICE.NS', 'NIFTY_MIDCAP_100.NS']
-
-# Function to get current price with no decimals
-def get_current_prices(symbol):
+def get_current_price(symbol):
     data = yf.Ticker(symbol).history(period="1d")
-    current_price = int(data['Close'].iloc[-1])
-    return current_price, current_price * 0.1, current_price * 0.2, current_price * 0.3
-
-# Get and print current prices
-for symbol in symbols:
-    options, boptions, foptions, moptions = get_current_prices(symbol)
-    print(f"Symbol: {symbol}, Options: {options}, BOptions: {boptions}, FOptions: {foptions}, MOptions: {moptions}")
+    return int(data['Close'].iloc[-1])
