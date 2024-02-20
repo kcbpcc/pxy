977e8b13a5d1bf331808487c49a24c215e4f9cda
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-20 23:06:33 +0800

    Update strikpxy.py

--- sys/exe/run/strikpxy.py
+++ sys/exe/run/strikpxy.py
@@ -8,9 +8,12 @@ def get_current_price(symbol):
     data = yf.Ticker(symbol).history(period="1d")
     return int(data['Close'].iloc[-1])
 
+def round_to_nearest_100(price):
+    return int(round(price / 100) * 100)
+
 def get_prices():
-    noptions = get_current_price('^NSEI')
-    boptions = get_current_price('^NSEBANK')
-    foptions = get_current_price('NIFTY_FIN_SERVICE.NS')
-    moptions = get_current_price('NIFTY_MIDCAP_100.NS')
+    noptions = round_to_nearest_100(get_current_price('^NSEI'))
+    boptions = round_to_nearest_100(get_current_price('^NSEBANK'))
+    foptions = round_to_nearest_100(get_current_price('NIFTY_FIN_SERVICE.NS'))
+    moptions = round_to_nearest_100(get_current_price('NIFTY_MIDCAP_100.NS'))
     return noptions, boptions, foptions, moptions
