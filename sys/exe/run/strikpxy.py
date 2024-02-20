6bc6e1bd606668cbaaaaca72adfb7d61a9078ccc
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-20 21:03:29 +0800

    Update strikpxy.py

--- sys/exe/run/strikpxy.py
+++ sys/exe/run/strikpxy.py
@@ -1,5 +1,19 @@
 import yfinance as yf
+import warnings
+
+# Suppress warnings
+warnings.filterwarnings("ignore")
 
 def get_current_price(symbol):
     data = yf.Ticker(symbol).history(period="1d")
     return int(data['Close'].iloc[-1])
+
+noptions = get_current_price('^NSEI')
+boptions = get_current_price('^NSEBANK')
+foptions = get_current_price('NIFTY_FIN_SERVICE.NS')
+moptions = get_current_price('NIFTY_MIDCAP_100.NS')
+
+print("Current price of Nifty:", current_price_nifty)
+print("Current price of BankNifty:", current_price_banknifty)
+print("Current price of Nifty Financial Services:", current_price_fin_service)
+print("Current price of Nifty Midcap 100:", current_price_midcap)
