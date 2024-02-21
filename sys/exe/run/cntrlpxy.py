858e5b2424fee54fadb6c184933e86d6504c04db
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-21 11:12:44 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -603,7 +603,7 @@ try:
             filtered_df.loc[:, 'option_power'] = filtered_df['smb_power'].apply(lambda smb_power: '⚪' if smb_power > 0.8 else ('🟢' if 0.5 < smb_power <= 0.8 else ('🟠' if 0.3 < smb_power <= 0.5 else ('🔴' if smb_power <= 0.3 else smb_power))))
             filtered_df.loc[:, 'PL%'] = filtered_df['PL%'].astype(int)
             formatted_lines = filtered_df[['Invested', 'key', 'qty','otPL%', 'PL%', 'PnL']].to_string(index=False, header=False).split('\n')
-            formatted_lines_sorted = sorted(formatted_lines, key=lambda x: int(x.split()[1][:-2]))
+            formatted_lines_sorted = sorted(formatted_lines, key=lambda x: (x[0], int(x.split()[1][:-2])))
             # Set max_width to 42
             max_width = 42
             # Iterate over each line and format it with color based on PnL value
