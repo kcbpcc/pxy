5bc507db2eaa5f9903a65623f61d7a8ff4818b76
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-21 10:09:13 +0800

    Update buynoptpxy.py

--- sys/exe/run/buynoptpxy.py
+++ sys/exe/run/buynoptpxy.py
@@ -16,7 +16,7 @@ from utcpxy import peak_time
 from macdpxy import calculate_macd_signal
 from smaftypxy import check_nifty_status
 from mktrndpxy import get_market_status_for_symbol
-nmktpxy = get_market_status_for_symbol('NIFTY')
+nmktpxy = get_market_status_for_symbol("^NSEI")
 
 onemincandlesequance, mktpxy = get_market_check()
 optpxy = get_optpxy()
