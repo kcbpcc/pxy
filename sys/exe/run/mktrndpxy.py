376dea203e4e6d1e9cbf0139f54aafd551b54155
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-22 14:00:48 +0800

    Update mktrndpxy.py

--- sys/exe/run/mktrndpxy.py
+++ sys/exe/run/mktrndpxy.py
@@ -38,7 +38,7 @@ def is_market_sell(current_color, last_closed_color):
 def is_market_buy(current_color, last_closed_color):
     return current_color == 'Bull' and last_closed_color == 'Bear'
 
-def calculate_last_twenty_heikin_ashi_colors(symbol, period="5d", interval="5m"):
+def calculate_last_twenty_heikin_ashi_colors(symbol, period="5d", interval="3m"):
     data = fetch_data(symbol, period, interval)
     return calculate_heikin_ashi_colors(data)
 
@@ -54,7 +54,7 @@ def get_market_status(current_color, last_closed_color):
     else:
         return 'None'
 
-def get_market_status_for_symbol(symbol, period="5d", interval="5m"):
+def get_market_status_for_symbol(symbol, period="5d", interval="3m"):
     current_color, last_closed_color = calculate_last_twenty_heikin_ashi_colors(symbol, period, interval)
     return get_market_status(current_color, last_closed_color)
 
