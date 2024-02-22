355441985aab9f15ec8a26d976d7eccb0ce9bf96
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-22 13:16:18 +0800

    Update smaoptpxy.py

--- sys/exe/run/smaoptpxy.py
+++ sys/exe/run/smaoptpxy.py
@@ -7,18 +7,18 @@ def sma_above_or_below(symbol):
     
     # Calculate 50-minute SMA
     data['SMA_50'] = data['Close'].rolling(window=50).mean()
-    
+    data['SMA_200'] = data['Close'].rolling(window=200).mean()
     # Get the last row (latest data)
     last_row = data.iloc[-1]
     
     # Get the latest closing price and 50-minute SMA
-    latest_price = last_row['Close']
     sma_50 = last_row['SMA_50']
+    sma_200 = last_row['SMA_200']
     
     # Check if the latest price is above or below the 50-minute SMA
-    if latest_price > sma_50:
+    if sma_50 > sma_200:
         smaopt= "above"
-    elif latest_price < sma_50:
+    elif sma_50 < sma_200:
         smaopt= "below"
     else:
         smaopt= "equal to"
