56898013bd6c2400e881f6aa10eace8ad621052f
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-23 00:04:04 +0800

    Update strikpxy.py

--- sys/exe/run/strikpxy.py
+++ sys/exe/run/strikpxy.py
@@ -9,11 +9,14 @@ def get_current_price(symbol):
     return int(data['Close'].iloc[-1])
 
 def round_to_nearest_100(price):
-    return int(round(price / 100) * 100)
+    return round(price / 100) * 100
+
+def round_to_nearest_100_or_50(price):
+    return round(price / 50) * 50 if price % 100 < 50 else round(price / 100) * 100
 
 def get_prices():
-    noptions = round_to_nearest_100(get_current_price('^NSEI'))
-    boptions = round_to_nearest_100(get_current_price('^NSEBANK'))
-    foptions = round_to_nearest_100(get_current_price('NIFTY_FIN_SERVICE.NS'))
-    moptions = round_to_nearest_100(get_current_price('NIFTY_MID_SELECT.NS'))
+    noptions = round_to_nearest_100_or_50(get_current_price('^NSEI'))
+    boptions = round_to_nearest_100(get_current_price('^NSEBANK'))  # Keep NSEBANK as nearest hundred only
+    foptions = round_to_nearest_100_or_50(get_current_price('NIFTY_FIN_SERVICE.NS'))
+    moptions = round_to_nearest_100_or_50(get_current_price('NIFTY_MID_SELECT.NS'))
     return noptions, boptions, foptions, moptions
