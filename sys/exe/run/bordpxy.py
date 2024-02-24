0728a0529df1ffea03840e14a314bf64f26e92f5
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-24 10:49:51 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -50,9 +50,9 @@ def printbord(Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, t
     output_lines.append(left_aligned_format.format(f"Count:{BRIGHT_YELLOW}{str(nrmlall_Stocks_count).zfill(3)}{RESET}") +
                         right_aligned_format.format(f"Count:{BRIGHT_YELLOW}{str(all_Stocks_count).zfill(3)}{RESET}"))
     output_lines.append(left_aligned_format.format(f"Capital:{((nrmlall_Stocks_capital)/100000):.3f}{RESET}") +
-                        right_aligned_format.format(f"Capital:{all_Stocks_capital_lacks}{RESET}"))
+                        right_aligned_format.format(f"Capital:{all_Stocks_capital_lacks:.3f}{RESET}"))
     output_lines.append(left_aligned_format.format(f"Value:{((nrmlall_Stocks_worth)/100000):.3f}{RESET}") +
-                        right_aligned_format.format(f"Value:{all_Stocks_worth_lacks}{RESET}"))
+                        right_aligned_format.format(f"Value:{all_Stocks_worth_lacks:.3f}{RESET}"))
     output_lines.append(left_aligned_format.format(f"Unrealized:{BRIGHT_GREEN if total_PnL_nrml_buy >= 0 else BRIGHT_RED}{int(total_PnL_nrml_buy)}{RESET}") +
                         right_aligned_format.format(f"Unrealized:{BRIGHT_GREEN if total_PnL_cnc_buy >= 0 else BRIGHT_RED}{int(total_PnL_cnc_buy)}{RESET}")) 
     output_lines.append(left_aligned_format.format(f"Realized:{BRIGHT_GREEN if result_nrml >= 0 else BRIGHT_RED}{str(int(result_nrml)).zfill(5)}{RESET}") +
