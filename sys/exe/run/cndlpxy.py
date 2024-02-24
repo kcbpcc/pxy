06c0d3493728c34f37196c98b31ca47c440214e9
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-24 17:30:30 +0800

    Update cndlpxy.py

--- sys/exe/run/cndlpxy.py
+++ sys/exe/run/cndlpxy.py
@@ -92,7 +92,7 @@ def dayprinter(o, h, l, c, prev_close):
         
         print(Fore.LIGHTWHITE_EX + '━' * m_length, end='')
     
-        print (f" ⚡{nse_power:.2f}🚦{macd}")
+        print (f" ⚡{nse_power:.2f} 🚦{macd}")
     except Exception as e:
         pass
     
