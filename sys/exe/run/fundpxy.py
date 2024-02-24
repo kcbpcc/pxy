7f2761ad329f5d06a665781708cf90c9a40ddc85
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-24 15:01:01 +0800

    Update fundpxy.py

--- sys/exe/run/fundpxy.py
+++ sys/exe/run/fundpxy.py
@@ -38,7 +38,7 @@ def calculate_decision():
         available_cash = response["equity"]["available"]["live_balance"]
 
         # Define 'YES' or 'NO' based on the available cash
-        decision = "YES" if available_cash > 40000 else "NO"
+        decision = "YES" if available_cash > 50000 else "NO"
 
         # Print the decision
         status_emoji = "✅" if decision == "YES" else "❌" if decision == "NO" else "❓"
