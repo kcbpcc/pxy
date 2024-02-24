47f103af03394cfe2da9c4edb39b244139e2ce3f
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-24 16:13:40 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -29,7 +29,7 @@ def printbord(Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, t
     right_aligned_format = "{:>" + str(column_width) + "}"
                
     output_lines.append(left_aligned_format.format(f"A/C Cap:{BRIGHT_YELLOW}{round(capital, 2)}{RESET}") +
-                        right_aligned_format.format(f"A/C Now:{BRIGHT_YELLOW}{round(acvalue_to_print + 3.58, 2)}{RESET}"))
+                        right_aligned_format.format(f"{BRIGHT_YELLOW}{round(acvalue_to_print + 3.58, 2)}{RESET}:A/C Now"))
     output_lines.append(left_aligned_format.format(f"A/C Profit:{BRIGHT_GREEN if profit > 0 else BRIGHT_RED}{round((profit+3.58), 2)}{RESET}") +
                         right_aligned_format.format(f"A/C Loss:{BRIGHT_GREEN if total_PnL >= 0 else BRIGHT_RED}{round((total_PnL/100000), 2)}{RESET}"))
     #output_lines.append(left_aligned_format.format(f"Flush:{BRIGHT_GREEN if green_Stocks_profit_loss > 0 else BRIGHT_RED}{round(green_Stocks_profit_loss)}{RESET}") +
