99391b74e17511e4d386bed61c15d585181837bb
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-25 21:16:42 +0800

    Update cndlpxy.py

--- sys/exe/run/cndlpxy.py
+++ sys/exe/run/cndlpxy.py
@@ -93,8 +93,8 @@ def dayprinter(o, h, l, c, prev_close):
         # Determine the color based on the comparison of today's close with yesterday's close
         color = Fore.GREEN if c > prev_close else Fore.RED
         
-        SMAftywave = f"{Fore.GREEN}ﮩ٨ـﮩﮩ٨ـ{Style.RESET_ALL}" if SMAfty == 'up' else f"{Fore.RED}ﮩ٨ـﮩﮩ٨ـ{Style.RESET_ALL}"
-        print(f"🔆{day_change_sign}{Day_Change:.2f}⌛️{open_change_sign}{Open_Change:.2f}⚡{SMAftywave}{nse_power:.2f}{onemincandlesequance}🚦{macd}PE{pedepth}|CE{pedepth}")
+        SMAftywave = f"{Fore.GREEN}ﮩ٨ـﮩـ{Style.RESET_ALL}" if SMAfty == 'up' else f"{Fore.RED}ﮩ٨ـﮩﮩ٨ـ{Style.RESET_ALL}"
+        print(f"🔆{day_change_sign}{Day_Change:.2f}⌛️{open_change_sign}{Open_Change:.2f}⚡{nse_power:.2f}{SMAftywave}{onemincandlesequance}🚦{macd}PE{pedepth}|CE{pedepth}")
     except Exception as e:
         pass
 
