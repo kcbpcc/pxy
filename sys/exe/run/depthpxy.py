04db9a8f02b6dac342d225b6392367ff7cd76bd8
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-25 13:05:19 +0800

    Update depthpxy.py

--- sys/exe/run/depthpxy.py
+++ sys/exe/run/depthpxy.py
@@ -33,12 +33,11 @@ def calculate_consecutive_candles():
                 consecutive_count = 1
                 current_color = color
 
-        # Return the latest value
         if current_color is not None:
             if current_color == 'green':
-                return +{consecutive_count}
+                return consecutive_count
             else:
-                return -{consecutive_count}
+                return -consecutive_count
 
     except Exception as e:
         return f"An error occurred: {e}"
