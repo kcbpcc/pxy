4db076b34251026f6a22cf42648fc372f37ed5ba
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-02-26 16:12:43 +0800

    Update cyclepxy.py

--- sys/exe/run/cyclepxy.py
+++ sys/exe/run/cyclepxy.py
@@ -29,7 +29,7 @@ def calculate_cycle(current_time):
         # Convert the remaining time to seconds
         return round(remaining_time.total_seconds())
     else:
-        return 10
+        return 8
 
 # Get the current UTC time
 current_utc_time = datetime.utcnow().time()
