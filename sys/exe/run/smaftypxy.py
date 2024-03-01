51757213b83e9317b7483516ec6ef44a9555c2b4
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-03-01 08:55:47 +0800

    Update smaftypxy.py

--- sys/exe/run/smaftypxy.py
+++ sys/exe/run/smaftypxy.py
@@ -6,7 +6,7 @@ def check_nifty_status():
     data = yf.Ticker('^NSEI').history(period="5d", interval="1m")
 
     # Calculate the 50-day SMA of Nifty50
-    sma_50_nifty = data['Close'].rolling(window=50).mean()
+    sma_100_nifty = data['Close'].rolling(window=100).mean()
 
     # Get the present Nifty close
     present_nifty_close = data['Close'].iloc[-1]
@@ -15,8 +15,8 @@ def check_nifty_status():
     with warnings.catch_warnings():
         warnings.simplefilter(action='ignore', category=FutureWarning)
 
-        # Compare present Nifty close with 50-day SMA of Nifty50
-        if present_nifty_close > sma_50_nifty.iloc[-1]:
+        # Compare present Nifty close with 100-day SMA of Nifty50
+        if present_nifty_close > sma_100_nifty.iloc[-1]:
             return "up"
         else:
             return "down"
