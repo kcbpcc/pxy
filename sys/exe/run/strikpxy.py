ec5278ba935a780fabddf2d087cece22ac9a7533
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-03-01 14:43:11 +0800

    Update strikpxy.py

--- sys/exe/run/strikpxy.py
+++ sys/exe/run/strikpxy.py
@@ -6,10 +6,10 @@ warnings.filterwarnings("ignore")
 
 def get_current_price(symbol):
     data = yf.Ticker(symbol).history(period="2d", interval="1m")
-
-    price = data.iloc[-1]  # Fixed missing assignment of last_row
-
-    return price  # Added return statement
+    data['SMA_50'] = data['Close'].rolling(window=50).mean()
+    last_row = data.iloc[-1]  # Fixed missing assignment of last_row
+    SMA_50 = int(last_row['SMA_50'])  # Fixed syntax for accessing 'SMA_50' column
+    return SMA_50  # Added return statement
 
 def round_to_nearest_100(price):
     return round(price / 100) * 100
@@ -19,8 +19,7 @@ def round_to_nearest_100_or_50(price):
 
 def get_prices():
     noptions = round_to_nearest_100_or_50(get_current_price('^NSEI'))
-    boptions = round_to_nearest_100(get_current_price('^NSEBANK'))
+    boptions = round_to_nearest_100(get_current_price('^NSEBANK'))  # Keep NSEBANK as nearest hundred only
     foptions = round_to_nearest_100_or_50(get_current_price('NIFTY_FIN_SERVICE.NS'))
-    moptions = round_to_nearest_100_or_50(get_current_price('NIFTY_MID_SELECT.NS'))
+    moptions = round_to_nearest_100_or_50(get_current_price('NIFTY_MID_SELECT.NS'))  # Fixed function call
     return noptions, boptions, foptions, moptions
-
