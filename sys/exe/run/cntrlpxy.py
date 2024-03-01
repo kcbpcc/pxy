a29c1706acf6abd07f532f847147a738f92f53ca
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-03-01 10:44:17 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -598,9 +598,8 @@ try:
                         auto_value == 'AUTO' and
                         'NFO:' in row['key'] and
                                             
-                        (('CE' in row['key'] and row['PL%'] > 1.4 and mmktpxy in ["Sell", "Bear"]) or
-                         ('PE' in row['key'] and row['PL%'] > 1.4 and mmktpxy in ["Buy", "Bull"]) or
-                         (row['PL%'] > row['otPL%']))
+                        (('CE' in row['key'] and row['PL%'] > 0 and mmktpxy in ["Sell", "Bear"]) or
+                         ('PE' in row['key'] and row['PL%'] > 0 and mmktpxy in ["Buy", "Bull"]))
                     ):
                         try:                            
                             is_placed = nrml_order_place(key, row) if get_open_order_status(symbol_in_order) == "NO" else False
