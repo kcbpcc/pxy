8ce5637d19b65603fc79316a5b07a7ac26a87ed0
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-03-02 13:08:30 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -564,9 +564,8 @@ try:
     printbord(Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, total_dPnL_percentage,
              result_nrml, total_PnL_cnc_buy, total_PnL_nrml_buy, available_cash,
              nse_action, nse_power,all_Stocks_count, red_Stocks_count,green_Stocks_count,all_Stocks_capital_lacks,all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage, mktpxy,nrmlall_Stocks_count ,nrmlall_Stocks_capital ,nrmlall_Stocks_worth ,nrmlall_Stocks_profit_loss)
-    subprocess.run(['python3', 'bcndlpxy.py']) 
     subprocess.run(['python3', 'worldpxy.py'])
-
+    subprocess.run(['python3', 'bcndlpxy.py']) 
 ###########################################################################################################################################################################################################
 except Exception as e:
     remove_token(dir_path)
