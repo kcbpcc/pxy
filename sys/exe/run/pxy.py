d56029db8994409a8c2b48e6820d6d555b5d1d21
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-03-03 21:44:15 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -34,7 +34,6 @@ while True:
     importlib.reload(sys.modules['utcpxy'])  # Correct the usage
     peak = peak_time()
     #print(f"...lets fire again 🚀 Cycle 🎡 : {cycle} seconds".rjust(40))
-    #subprocess.run(['python3', 'tistpxy.py'])
     from macdpxy import calculate_macd_signal
     importlib.reload(sys.modules['macdpxy'])  # Correct the usage
     macd = calculate_macd_signal("^NSEI")
