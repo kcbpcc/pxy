81d89deeb7546457c5dfbc9541d60d2df1b233df
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-03-09 14:44:17 +0800

    Update buynoptpxy.py

--- sys/exe/run/buynoptpxy.py
+++ sys/exe/run/buynoptpxy.py
@@ -7,17 +7,11 @@ import asyncio
 from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
 from mktpxy import get_market_check
-from nftpxy import nse_action, nse_power, Day_Change, Open_Change
 from strikpxy import get_prices
 noptions, _, _, _ = get_prices()
-from cyclepxy import cycle
-from utcpxy import peak_time
 from macdpxy import calculate_macd_signal
 from mktrndpxy import get_market_status_for_symbol
-onemincandlesequance, mktpxy = get_market_check("^NSEI")
-peak = peak_time()
-macd = calculate_macd_signal("^NSEI")
-SMAfty = check_nifty_status()
+nmktpxy = get_market_status_for_symbol("^NSEI")
 from smaoptpxy import sma_above_or_below
 smanifty = sma_above_or_below("^NSEI")
 
@@ -135,7 +129,7 @@ async def main():
     option_type = None  # Default value
     
     # Determine option type based on nmktpxy
-    if nmktpxy == 'Sell'and smanifty != 'above':
+    if mktpxy == 'Sell'and smanifty != 'above':
         option_type = 'PE'  # Put Option
     else:
         # Handle the case where nmktpxy doesn't match any condition
