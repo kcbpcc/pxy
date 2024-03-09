0aa1cc11c7cf0e5f720bed962cf89c4ee558600a
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-03-09 11:53:08 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -487,12 +487,12 @@ try:
             # Handle any other exceptions that may occur during the loop
             print(f"An unexpected error occurred: {e}")        
 ###########################################################################################################################################################################################################
+    subprocess.run(['python3', 'tistpxy.py'])
     if not stocks_filtered_df.empty:
         #print("━" * 42)
         #print(f"{BRIGHT_YELLOW}HP|CM|STOCK     |fPL%|tPL%|PL% |PL |Q|TR{RESET}")
         #print("━" * 42)
         print(stocks_filtered_df.to_string(index=False, justify='left', col_space=-0, header=False))
-    subprocess.run(['python3', 'tistpxy.py'])
 ###########################################################################################################################################################################################################
     from smapxy import check_index_status
     from macdpxy import calculate_macd_signal
