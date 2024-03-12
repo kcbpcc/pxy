5b75d098f3cab20e3eb624de8f1c7c7246251511
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-03-12 12:23:51 +0800

    Update importpxy.py

--- sys/exe/run/dxy/importpxy.py
+++ sys/exe/run/dxy/importpxy.py
@@ -12,21 +12,10 @@ import telegram
 import time
 import traceback
 from colorama import Fore, Style
-from login_get_kite import get_kite, remove_token
+
 from time import sleep
 from toolkit.currency import round_to_paise
 from toolkit.logger import Logger
-from toolkit.logger import Logger
-from toolkit.currency import round_to_paise
-from login_get_kite import get_kite, remove_token
-import sys
-from time import sleep
-import traceback
-import os
-import subprocess
-from cnstpxy import dir_path
-from colorama import Fore, Style
-import csv
-import telegram
-import asyncio
-##################################################################################
+
+
+from logonpxy import get_kite, remove_token
