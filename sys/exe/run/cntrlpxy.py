a5f6313e5bd4b54acc5515145e49ff486693778c
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-03-29 12:48:48 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -343,6 +343,96 @@ try:
     total_dPnL_percentage = (total_dPnL / combined_df_positive_qty['Invested'].sum()) * 100 if combined_df_positive_qty['Invested'].sum() != 0 else 0
     total_dPnL = round(combined_df_positive_qty['dPnL'].sum())
 ###########################################################################################################################################################################################################
+    import pandas as pd
+    from prettytable import PrettyTable
+    from colorama import Fore, Style
+    
+    def convert_to_laks(value):
+        return f'{value/100000:.4f}'
+    
+    def format_value(value):
+        if value == 'Profit & Loss':
+            return 'Profit & Loss'
+        return f'{value:.0f}' if isinstance(value, (int, float)) else value
+    
+    def colorize(value):
+        if isinstance(value, (int, float)):
+            if value < 0:
+                return f'{Fore.RED}{Style.BRIGHT}{format_value(value)}{Style.RESET_ALL}'
+            elif value > 0:
+                return f'{Fore.GREEN}{Style.BRIGHT}{format_value(value)}{Style.RESET_ALL}'
+            else:
+                return f'{Style.BRIGHT}{format_value(value)}{Style.RESET_ALL}'
+    
+    def get_holdingsinfo(holdings_df):
+        try:
+            selected_holdings_df = holdings_df[(holdings_df['qty'] != 0)]    
+            
+            selected_columns = ['tradingsymbol','product', 'qty', 'close_price', 'average_price', 'ltp']
+            selected_holdings_df = selected_holdings_df[selected_columns].copy()
+        
+            selected_holdings_df['cap'] = (selected_holdings_df['qty'] * selected_holdings_df['average_price']).astype(int)
+            selected_holdings_df['unrealized'] = ((selected_holdings_df['ltp'] - selected_holdings_df['average_price']) * selected_holdings_df['qty']).round(2)
+            selected_holdings_df['perc'] = ((selected_holdings_df['unrealized'] / selected_holdings_df['cap']) * 100).where(selected_holdings_df['cap'] > 0)
+    
+            green_Stocks_df = selected_holdings_df[selected_holdings_df['perc'] > 1.4]
+            green_Stocks_count = len(green_Stocks_df)
+            green_Stocks_capital = green_Stocks_df['cap'].sum()
+            green_Stocks_worth = green_Stocks_df['ltp'].dot(green_Stocks_df['qty']).round(4)
+            green_Stocks_profit_loss = (green_Stocks_worth - green_Stocks_capital).round(4)
+            green_Stocks_capital_rercentage  = round(((green_Stocks_worth - green_Stocks_capital) / green_Stocks_capital) * 100, 2)
+    
+            red_Stocks_df = selected_holdings_df[selected_holdings_df['perc'] < 0]
+            red_Stocks_count = len(red_Stocks_df)
+            red_Stocks_capital = red_Stocks_df['cap'].sum()
+            red_Stocks_worth = red_Stocks_df['ltp'].dot(red_Stocks_df['qty']).round(4)
+            red_Stocks_profit_loss = (red_Stocks_worth - red_Stocks_capital).round(4)
+    
+            all_Stocks_df = selected_holdings_df
+            all_Stocks_count = len(selected_holdings_df)
+            all_Stocks_capital = selected_holdings_df['cap'].sum()
+            all_Stocks_worth = selected_holdings_df['ltp'].dot(selected_holdings_df['qty']).round(4)
+            all_Stocks_profit_loss = (all_Stocks_worth - all_Stocks_capital).round(4)
+    
+            nrmlall_Stocks_df = selected_holdings_df[selected_holdings_df['product'].isin(['NRML', 'MIS'])]
+            nrmlall_Stocks_count = len(nrmlall_Stocks_df)
+            nrmlall_Stocks_capital = nrmlall_Stocks_df['cap'].sum()
+            nrmlall_Stocks_worth = (nrmlall_Stocks_df['ltp'] * nrmlall_Stocks_df['qty']).round(4).sum()
+            nrmlall_Stocks_profit_loss = (nrmlall_Stocks_worth - nrmlall_Stocks_capital).round(4)
+    
+            day_change = all_Stocks_worth - selected_holdings_df['close_price'].dot(selected_holdings_df['qty']).round(4)
+            day_change_percentage = ((day_change / selected_holdings_df['close_price'].dot(selected_holdings_df['qty']).round(4)) * 100) if selected_holdings_df['close_price'].dot(selected_holdings_df['qty']).round(4) != 0 else 0
+            table = PrettyTable()
+            table.field_names = ['📉 Board', 'Total', 'Green', 'Red']
+            table.add_row(['Stocks📈', all_Stocks_count, green_Stocks_count, red_Stocks_count])
+            table.add_row(['Invested', convert_to_laks(all_Stocks_capital), convert_to_laks(green_Stocks_capital), convert_to_laks(red_Stocks_capital)])
+            table.add_row(['WorthNow', convert_to_laks(all_Stocks_worth), convert_to_laks(green_Stocks_worth), convert_to_laks(red_Stocks_worth)])
+            if all_Stocks_profit_loss < 0:
+                table.add_row(['💰₹💰P&L', f'{Style.BRIGHT}{Fore.RED}{format_value(all_Stocks_profit_loss)}{Style.RESET_ALL}', colorize(green_Stocks_profit_loss), colorize(red_Stocks_profit_loss)])
+            else:
+                table.add_row(['💰₹💰P&L', f'{format_value(all_Stocks_profit_loss)}', colorize(green_Stocks_profit_loss), colorize(red_Stocks_profit_loss)])
+            table.align = 'r'
+            #print(table)
+            zero_qty_count = holdings_df[holdings_df['qty'] == 0].shape[0]
+            #print("Number of Stocks Sold 💸💸💸: {}".format(zero_qty_count).rjust(38))
+            all_Stocks_capital_lacks = all_Stocks_capital/100000
+            all_Stocks_worth_lacks = all_Stocks_worth/100000
+            return all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage,nrmlall_Stocks_count ,nrmlall_Stocks_capital ,nrmlall_Stocks_worth ,nrmlall_Stocks_profit_loss
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
+    
+    
+    # Assuming combined_df is your DataFrame
+    get_holdingsinfo(combined_df)
+    
+    
+    
+    
+    
+    
+    
+###########################################################################################################################################################################################################
     subprocess.run(['python3', 'worldpxy.py'])
     subprocess.run(['python3', 'tistpxy.py'])
     from dshpxy import get_holdingsinfo
