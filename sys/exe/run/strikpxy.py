f56261ffe416781e01d70d798afc5bebb352b502
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-01 00:13:57 +0800

    Update strikpxy.py

--- sys/exe/run/strikpxy.py
+++ sys/exe/run/strikpxy.py
@@ -4,10 +4,10 @@ import warnings
 # Suppress warnings
 warnings.filterwarnings("ignore")
 
-def get_current_price(symbol):
+def get_50sma_price(symbol):
     data = yf.Ticker(symbol).history(period="1d", interval="1m")  # Fetch only one day of data
-    current_price = data['Close'].iloc[-1]  # Get the last available price
-    return current_price
+    50sma_price = data['Close'].rolling(window=50).mean()
+    return 50sma_price
 
 def round_to_nearest_100(price):
     return round(price / 100) * 100
@@ -16,8 +16,8 @@ def round_to_nearest_100_or_50(price):
     return round(price / 50) * 50 if price % 100 < 50 else round(price / 100) * 100
 
 def get_prices():
-    noptions = round_to_nearest_100_or_50(get_current_price('^NSEI'))
-    boptions = round_to_nearest_100(get_current_price('^NSEBANK'))  # Keep NSEBANK as nearest hundred only
-    foptions = round_to_nearest_100_or_50(get_current_price('NIFTY_FIN_SERVICE.NS'))
-    moptions = round_to_nearest_100_or_50(get_current_price('NIFTY_MID_SELECT.NS'))  # Fixed function call
+    noptions = round_to_nearest_100_or_50(get_50sma_price('^NSEI'))
+    boptions = round_to_nearest_100(get_50sma_price('^NSEBANK'))  # Keep NSEBANK as nearest hundred only
+    foptions = round_to_nearest_100_or_50(get_50sma_price('NIFTY_FIN_SERVICE.NS'))
+    moptions = round_to_nearest_100_or_50(get_50sma_price('NIFTY_MID_SELECT.NS'))  # Fixed function call
     return noptions, boptions, foptions, moptions
