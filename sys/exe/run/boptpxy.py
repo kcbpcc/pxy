8f80054f0ad112a629345036643feeacd2f029e8
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-02 14:53:01 +0800

    Update boptpxy.py

--- sys/exe/run/boptpxy.py
+++ sys/exe/run/boptpxy.py
@@ -54,8 +54,7 @@ def check_existing_positions(broker, symbol):
     positions_response = broker.kite.positions()
     positions_net = positions_response['net']
     for position in positions_net:
-        # Check if 'tradingsymbol' matches and 'quantity' is at least 50
-        if position['tradingsymbol'] == symbol and position['quantity'] >= 50:
+        if position['tradingsymbol'] == symbol and position['quantity'] < -100:
             return True
     return False
 
