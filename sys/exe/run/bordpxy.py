569b85cc21ca52bcef75a8ca756e89c31711f3ce
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-03 07:42:32 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -23,8 +23,9 @@ def printbord(total_postions_m2m, total_m2m, optpxy, Day_Change, result, total_P
                         right_aligned_format.format(f"A/C Value:{BRIGHT_YELLOW}{round((acvalue_to_print + hide), 2)}{RESET}"))
     output_lines.append(left_aligned_format.format(f"A/C Profit:{BRIGHT_GREEN if profit > 0 else BRIGHT_RED}{round((profit), 2)}{RESET}") +
                         right_aligned_format.format(f"A/C Loss:{BRIGHT_GREEN if total_PnL >= 0 else BRIGHT_RED}{round((total_PnL/100000), 2)}{RESET}"))
-    output_lines.append(left_aligned_format.format(f"Flush: {BRIGHT_GREEN if green_Stocks_profit_loss > 0 else BRIGHT_RED}{round(green_Stocks_profit_loss)}{RESET}".ljust(5)) +
-                        right_aligned_format.format(f"Flush%: {BRIGHT_GREEN if green_Stocks_capital_rercentage > 1.4 else BRIGHT_RED}{green_Stocks_capital_rercentage}{RESET}".ljust(5)))
+    output_lines.append(left_aligned_format.format(f"Flush: {BRIGHT_GREEN if green_Stocks_profit_loss > 0 else BRIGHT_RED}{round(green_Stocks_profit_loss)}{RESET}".zfill(5)) +
+                        right_aligned_format.format(f"Flush%: {BRIGHT_GREEN if green_Stocks_capital_rercentage > 1.4 else BRIGHT_RED}{green_Stocks_capital_rercentage}{RESET}".zfill(5)))
+
                           
     output_lines.append(
         left_aligned_format.format(
