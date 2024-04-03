824e1117b7e1636a31b2797cfea64b208c914cb0
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-03 09:35:04 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -18,10 +18,11 @@ def printbord(total_postions_m2m, total_m2m, optpxy, Day_Change, result, total_P
     column_width = 30
     
     print(
-        f"Flush:{BRIGHT_GREEN if green_Stocks_profit_loss > 0 else BRIGHT_RED}{round(green_Stocks_profit_loss)}{RESET}"
-        +(GREEN + "   ﮩ٨ﮩ٨ـvﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـ   " if nsma == "up" else RED + "   ﮩ٨ﮩ٨ـvﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـ   " if nsma == "down" else YELLOW + "   ﮩ٨ﮩ٨ـvﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـ   " + RESET)
-        +f"Flush%:{BRIGHT_GREEN if green_Stocks_capital_rercentage > 1.4 else BRIGHT_RED}{green_Stocks_capital_rercentage}"
+        f"Flush:{(BRIGHT_GREEN if green_Stocks_profit_loss > 0 else BRIGHT_RED){round(green_Stocks_profit_loss):>5}{RESET}".zfill(5)
+        +((GREEN + "   ﮩ٨ﮩ٨ـvﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـ   " if nsma == "up" else RED + "   ﮩ٨ﮩ٨ـvﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـ   " if nsma == "down" else YELLOW + "   ﮩ٨ﮩ٨ـvﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـ   " + RESET))
+        +f"Flush%:{(BRIGHT_GREEN if green_Stocks_capital_rercentage > 1.4 else BRIGHT_RED){green_Stocks_capital_rercentage:.4}{RESET}".zfill(4)
     )
+
     
     left_aligned_format = "{:<" + str(column_width) + "}"
     right_aligned_format = "{:>" + str(column_width) + "}"
