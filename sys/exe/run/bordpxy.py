a28f7982f7f02cd1c471af85d7effef2fe42acbd
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-03 08:16:21 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -23,7 +23,7 @@ def printbord(total_postions_m2m, total_m2m, optpxy, Day_Change, result, total_P
                         right_aligned_format.format(f"A/C Value:{BRIGHT_YELLOW}{round((acvalue_to_print + hide), 2)}{RESET}"))
     output_lines.append(left_aligned_format.format(f"A/C Profit:{BRIGHT_GREEN if profit > 0 else BRIGHT_RED}{round((profit), 2)}{RESET}") +
                         right_aligned_format.format(f"A/C Loss:{BRIGHT_GREEN if total_PnL >= 0 else BRIGHT_RED}{round((total_PnL/100000), 2)}{RESET}"))
-    output_lines.append(
+    print(
         left_aligned_format.format(
             f"Flush:{BRIGHT_GREEN if green_Stocks_profit_loss > 0 else BRIGHT_RED}{round(green_Stocks_profit_loss)}{RESET}".zfill(5)
         )
