cf0dab81ebeb75bdf115e784a095a481656c41d3
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-03 14:27:46 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -551,8 +551,8 @@ try:
         # Convert 'PL%' column to integer
         filtered_df.loc[:, 'PL%'] = filtered_df['PL%'].astype(int)
     
-        filtered_df.loc[filtered_df['key'].str.endswith('CE'), 'key'] = '🟥 ' + filtered_df.loc[filtered_df['key'].str.endswith('CE'), 'key']
-        filtered_df.loc[filtered_df['key'].str.endswith('PE'), 'key'] = '🟩 ' + filtered_df.loc[filtered_df['key'].str.endswith('PE'), 'key']
+        filtered_df.loc[filtered_df['key'].str.endswith('CE'), 'key'] = filtered_df.loc[filtered_df['key'].str.endswith('CE'), 'key'] + '🟥 '
+        filtered_df.loc[filtered_df['key'].str.endswith('PE'), 'key'] = filtered_df.loc[filtered_df['key'].str.endswith('PE'), 'key'] +  '🟩 '
     
         filtered_df = filtered_df.sort_values(by='PL%')
     
@@ -562,7 +562,7 @@ try:
             elif row['product'] == 'NRML':
                 filtered_df.at[index, 'product'] = '⏰'
     
-        formatted_lines = filtered_df[['key', 'Invested', 'qty', 'PnL','m2m']].to_string(index=False, header=False).split('\n')
+        formatted_lines = filtered_df[['key', 'Invested', 'qty','m2m', 'PnL']].to_string(index=False, header=False).split('\n')
         max_width = 42
         for line in formatted_lines:
             values = line.split()
