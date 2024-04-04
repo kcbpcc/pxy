14b81bb3836d97dc351c8eeac9fce8b1c473d2df
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-04 23:10:08 +0800

    Update cntrlpxy.py

--- sys/exe/run/cntrlpxy.py
+++ sys/exe/run/cntrlpxy.py
@@ -583,7 +583,7 @@ try:
                     color_code = RESET
             else:
                 color_code = RESET
-            print(color_code + (line[:-3] + line[-3:].rjust(3)).rjust(42) + RESET)
+            print(color_code + (line[:-3] + line[-3:].rjust(3)).rjust(41) + RESET)
     else:
         print(YELLOW + "..............no options yet in the swing." + RESET)
 
