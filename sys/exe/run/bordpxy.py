2475b56895525ac3dda284d9e8322b658cfcd9a3
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-05 14:27:34 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -49,6 +49,6 @@ def printbord(total_nrml_m2m, total_cnc_m2m, optpxy, Day_Change, result, total_P
 
     full_output = '\n'.join(output_lines)
     print(full_output)
-    print((GREEN if nsma == "up" else RED if nsma == "down" else YELLOW) + "   ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩ" + RESET)
+    print((GREEN if nsma == "up" else RED if nsma == "down" else YELLOW) + "ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩ" + RESET)
     with open("bordpxy.csv", "w") as file:
         file.write(full_output)
