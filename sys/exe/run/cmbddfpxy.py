aba1a66f475e745ebab802e6148c2294c421a0e1
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-05 22:12:23 +0800

    Update cmbddfpxy.py

--- sys/exe/run/cmbddfpxy.py
+++ sys/exe/run/cmbddfpxy.py
@@ -1,16 +1,14 @@
-import sys
-import traceback
-import pandas as pd
 from toolkit.logger import Logger
 from login_get_kite import get_kite, remove_token
+import sys
+import traceback
+import os
 from cnstpxy import dir_path
-from toolkit.logger import Logger
-from toolkit.currency import round_to_paise
-from login_get_kite import get_kite, remove_token
-import sys
-from time import sleep
-import traceback
+import csv
+import asyncio
+
 file_path = 'filePnL.csv'
+
 logging = Logger(30, dir_path + "main.log")
 
 try:
@@ -21,6 +19,8 @@ except Exception as e:
     print(traceback.format_exc())
     logging.error(f"{str(e)} unable to get holdings")
     sys.exit(1)
+
+file_path = 'filePnL.csv'
 
 def get_holdingsinfo(resp_list, broker):
     try:
@@ -41,16 +41,43 @@ def get_positionsinfo(resp_list, broker):
         return None
 
 try:
+    import sys
+    import traceback
+    import pandas as pd
+    import datetime
+    import time
+    from login_get_kite import get_kite, remove_token
+    from cnstpxy import dir_path
+    from toolkit.logger import Logger
+    from toolkit.currency import round_to_paise
+    import csv
+    from cnstpxy import sellbuff, secs, perc_col_name
+    from time import sleep
+    import subprocess
+    import random
+    import os
+    import numpy as np
+    import asyncio
+
+
+    file_path = 'filePnL.csv'
     logging.debug("Are we having any holdings to check")
     holdings_response = broker.kite.holdings()
     positions_response = broker.kite.positions()['net']
     holdings_df = get_holdingsinfo(holdings_response, broker)
     positions_df = get_positionsinfo(positions_response, broker)
-
+    try:
+        response = broker.kite.margins()
+        available_cash = response["equity"]["available"]["live_balance"]
+        # Rest of your code that depends on the 'available_cash' variable
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Handle the error as needed
+        # Set available_cash to 0 or any other default value
+        available_cash = 0
     holdings_df['key'] = holdings_df['exchange'] + ":" + holdings_df['tradingsymbol'] if not holdings_df.empty else None
     positions_df['key'] = positions_df['exchange'] + ":" + positions_df['tradingsymbol'] if not positions_df.empty else None
     combined_df = pd.concat([holdings_df, positions_df], ignore_index=True)
-
     lst = combined_df['key'].tolist()
     resp = broker.kite.ohlc(lst)
     dct = {
@@ -78,9 +105,7 @@ try:
     combined_df['dPnL'] = combined_df['value'] - combined_df['Yvalue']
     combined_df['dPL%'] = (combined_df['dPnL'] / combined_df['Yvalue']) * 100
     print(combined_df)
-
 except Exception as e:
     print(f"An error occurred: {e}")
     traceback.print_exc()
     sys.exit(1)
-
