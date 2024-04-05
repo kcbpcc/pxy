50c2a0753a8e3e68a6512d81a2cadde4d4fe2adc
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-05 23:37:13 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -29,7 +29,7 @@ finally:
 
 from cmbddfpxy import process_data
 combined_df = process_data()
-opt_df = combined_df[combined_df['key_column_name'].str.contains('NFO', case=False)].copy()
+opt_df = combined_df[combined_df['key'].str.contains('NFO', case=False)].copy()
 opt_df['CP'] = opt_df['key'].apply(lambda x: '🟥' if x.endswith('PE') else ('🟩' if x.endswith('CE') else None))
 opt_df.loc[:, 'key'] = opt_df['key'].str.replace('NFO:', '')  # Remove 'NFO:' from the 'key' column
 opt_df = opt_df[['key', 'Invested', 'qty', 'PnL', 'CP']]
