cf7f2f6fa0f9f1d1020d665ddc30173cf724e1ee
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-05 10:00:52 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -49,7 +49,7 @@ while True:
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
     subprocess.run(['python3', 'buycncpxy.py']) if (nse_power < 0.1 and peak == 'NONPEAK') or peak == 'PEAKEND' else None
     subprocess.run(['python3', 'buyoptpxy.py'])
-    subprocess.run(['python3', 'cntrlpxy.py']) 
+    subprocess.run(['python3', 'cntrlcncpxy.py']) 
     subprocess.run(['python3', 'cndlpxy.py'])
     subprocess.run(['python3', 'worldpxy.py'])
     console.print("[bold]   PXY® PreciseXceleratedYield Pvt Ltd™   [/bold]",
