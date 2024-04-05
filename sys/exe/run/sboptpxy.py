c118691898402d377d9e9caf9aeb19361cfb0858
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-05 08:32:56 +0800

    Update sboptpxy.py

--- sys/exe/run/sboptpxy.py
+++ sys/exe/run/sboptpxy.py
@@ -30,19 +30,24 @@ async def send_telegram_message(message_text):
 from datetime import datetime, timedelta
 def get_this_thursday():
     current_date = datetime.now()
+    if current_date.weekday() == 3:  # If today is Thursday
+        current_date += timedelta(days=7)  # Move to next Thursday
     days_until_this_thursday = (3 - current_date.weekday() + 7) % 7
-    if days_until_this_thursday == 0:
-        return current_date.strftime("%y"), current_date.strftime("%m"), current_date.strftime("%d").zfill(2)
     this_thursday = current_date + timedelta(days=days_until_this_thursday)
     last_day_of_month = (this_thursday.replace(day=28) + timedelta(days=4)).replace(day=1) - timedelta(days=1)
     if this_thursday.month != (this_thursday + timedelta(days=7)).month:
         if this_thursday.day > last_day_of_month.day - 7:
-            return this_thursday.strftime("%y"), this_thursday.strftime("%m"), this_thursday.strftime("%d").zfill(2)
+            return (
+                this_thursday.strftime("%y"),
+                this_thursday.strftime("%m"),
+                this_thursday.strftime("%d").zfill(2),
+            )
     expiry_year = this_thursday.strftime("%y")
     month_number = int(this_thursday.strftime("%m"))
     expiry_month = str(month_number) if month_number <= 9 else this_thursday.strftime("%m")
     expiry_day = this_thursday.strftime("%d").zfill(2)
     return expiry_year, expiry_month, expiry_day
+
 
 def construct_symbol(expiry_year, expiry_month, expiry_day, option_type):
     # Convert expiry_month to a single digit string if it's less than or equal to 9
