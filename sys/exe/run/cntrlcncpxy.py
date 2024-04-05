23a65581d6350ebed193cf513d3df48840a9abc2
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-06 07:31:41 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -258,7 +258,7 @@ try:
                          row['product'] == 'CNC' and
                          row['PL%'] > 1.4 ) and
                         (
-                            (row['PL%'] > row['tPL%']) or ((row['PL%'] > 1.4) and (total_dPnL < 0))
+                            (row['PL%'] > row['tPL%']) or (row['oPL%'] < 0) or (row['dPL%'] < 0)
                         )
                     ):
                         try:
