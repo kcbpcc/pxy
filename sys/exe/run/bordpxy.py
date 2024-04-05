6206c28df3525cad3af779ccce3fdf0240c2f3d0
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-05 18:59:02 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -50,11 +50,13 @@ def printbord(total_nrml_m2m, total_cnc_m2m, optpxy, Day_Change, result, total_P
     full_output = '\n'.join(output_lines)
     print(full_output)
     color_code = ""
+    color_code = ""
     if mktpxy in ["Buy", "Bull"]:
         color_code = BRIGHT_GREEN + UNDERLINE
     elif mktpxy in ["Sell", "Bear"]:
         color_code = BRIGHT_RED + UNDERLINE
-    print(color_code + "[bold]   PXY® PreciseXceleratedYield Pvt Ltd™   [/bold]" + RESET)
+    
+    print(color_code + "PXY® PreciseXceleratedYield Pvt Ltd™" + RESET)
     with open("bordpxy.csv", "w") as file:
         file.write(full_output)
     
