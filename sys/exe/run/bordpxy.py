ff7c7a629db182f612a93343770246c1d71abb1e
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-05 13:44:59 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -50,7 +50,7 @@ def printbord(total_postions_m2m, total_m2m, optpxy, Day_Change, result, total_P
     print(
         f"Flush:{(BRIGHT_GREEN if green_Stocks_profit_loss > 0 else BRIGHT_RED)}{round(green_Stocks_profit_loss):>5}{RESET}".zfill(5)
         + ((GREEN if nsma == "up" else RED if nsma == "down" else YELLOW) + "   ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩ   " + RESET)
-        + f"Flush%:{(BRIGHT_GREEN if green_Stocks_capital_rercentage > 1.4 else BRIGHT_RED)}{green_Stocks_capital_rercentage:.2f}{RESET}".zfill(4)
+        + f"Flush%:{(BRIGHT_GREEN if green_Stocks_capital_rercentage > 1.4 else BRIGHT_RED)}{green_Stocks_capital_rercentage:.2f}{RESET}".zfill(5)
     )
     with open("bordpxy.csv", "w") as file:
         file.write(full_output)
