159f740a1118b2df242918e479a61e77b87d4b02
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-06 02:01:58 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -304,7 +304,7 @@ try:
             print(f"An unexpected error occurred: {e}")        
 ###########################################################################################################################################################################################################
     printbord(total_nrml_m2m, total_cnc_m2m, optpxy, Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, total_dPnL_percentage,
-             result_nrml, total_PnL_stocks_buy, total_PnL_options_buy, available_cash,
+             result_nrml, total_PnL_stocks_buy, available_cash,
              nse_power,all_Stocks_count, red_Stocks_count,green_Stocks_count,all_Stocks_capital_lacks,all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage, mktpxy,nrmlall_Stocks_count ,nrmlall_Stocks_capital ,nrmlall_Stocks_worth ,nrmlall_Stocks_profit_loss, nsma)
 ###########################################################################################################################################################################################################
     if not stocks_filtered_df.empty:
