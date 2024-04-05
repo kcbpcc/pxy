a2c7a2c011893dc0ea3d6e40e4d8e85bf5a4d3ec
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-05 13:53:25 +0800

    Update dshpxy.py

--- sys/exe/run/dshpxy.py
+++ sys/exe/run/dshpxy.py
@@ -29,7 +29,13 @@ def get_holdingsinfo(combined_df):
         nfom2m_df = combined_df[combined_df['key'].str.contains("NFO:")].copy()
         nfom2m_df['m2m'] = pd.to_numeric(nfom2m_df['m2m'], errors='coerce')
         nfom2m_df['m2m'].fillna(0, inplace=True)
-        total_m2m = nfom2m_df['m2m'].sum()
+        total_nrml_m2m = nfom2m_df['m2m'].sum()
+
+        nfom2m_df = combined_df[combined_df['key'].str.contains("NFO:") & (combined_df['source'] == 'positions')].copy()
+        nfom2m_df['m2m'] = pd.to_numeric(nfom2m_df['m2m'], errors='coerce')
+        nfom2m_df['m2m'].fillna(0, inplace=True)
+        total_cnc_m2m = nfom2m_df['m2m'].sum()
+
     
         selected_holdings_df['cap'] = (selected_holdings_df['qty'] * selected_holdings_df['average_price']).astype(int)
         selected_holdings_df['unrealized'] = ((selected_holdings_df['ltp'] - selected_holdings_df['average_price']) * selected_holdings_df['qty']).round(2)
@@ -75,7 +81,7 @@ def get_holdingsinfo(combined_df):
         zero_qty_count = combined_df[combined_df['qty'] == 0].shape[0]
         all_Stocks_capital_lacks = all_Stocks_capital/100000
         all_Stocks_worth_lacks = all_Stocks_worth/100000
-        return total_m2m, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage, nrmlall_Stocks_count, nrmlall_Stocks_capital, nrmlall_Stocks_worth, nrmlall_Stocks_profit_loss
+        return total_nrml_m2m, total_cnc_m2m, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage, nrmlall_Stocks_count, nrmlall_Stocks_capital, nrmlall_Stocks_worth, nrmlall_Stocks_profit_loss
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
