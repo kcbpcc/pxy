143bb1afb986ac371eada193fcdc774f88b5174e
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-06 02:27:28 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -9,43 +9,33 @@ from cyclepxy import cycle
 from clorpxy import RED, GREEN
 
 console = Console()
-
 subprocess.run(['python3', 'cpritepxy.py'])
 
 while True:
     from selfpxy import get_random_spiritual_message
     importlib.reload(sys.modules['selfpxy'])  # Correct the usage
     random_message = get_random_spiritual_message()
-    
     from mktpxy import get_market_check
     importlib.reload(sys.modules['mktpxy'])  # Correct the usage
     onemincandlesequance, mktpxy = get_market_check('^NSEI')
-    
     from nftpxy import nse_action, nse_power, Day_Change, Open_Change, OPTIONS
     importlib.reload(sys.modules['nftpxy'])  # Correct the usage
-    
     from cyclepxy import cycle
     importlib.reload(sys.modules['cyclepxy'])  # Correct the usage
-    
     from utcpxy import peak_time
     importlib.reload(sys.modules['utcpxy'])  # Correct the usage
     peak = peak_time()
-    
     from macdpxy import calculate_macd_signal
     importlib.reload(sys.modules['macdpxy'])  # Correct the usage
     macd = calculate_macd_signal("^NSEI")
-    
     from smapxy import check_index_status
     importlib.reload(sys.modules['smapxy'])  # Correct the usage
     nsma = check_index_status('^NSEI')
-    
     from mktrndpxy import get_market_status_for_symbol
     importlib.reload(sys.modules['mktrndpxy'])
     nmktpxy = get_market_status_for_symbol('^NSEI')
-    
     subprocess.run(['python3', 'acvaluepxy.py']) if peak == 'PEAKSTART' else None
     subprocess.run(['python3', 'tistpxy.py'])
-    
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
     subprocess.run(['python3', 'buycncpxy.py']) if (nse_power < 0.1 and peak == 'NONPEAK') or peak == 'PEAKEND' else None
     subprocess.run(['python3', 'buyoptpxy.py']) if (nse_power > 0.9 and peak == 'NONPEAK') or (Day_Change < 0 or Open_Change < 0) else None
@@ -53,7 +43,6 @@ while True:
     subprocess.run(['python3', 'cntrloptpxy.py']) 
     subprocess.run(['python3', 'cndlpxy.py'])
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
-    
     console.print(random_message)
     
     def progress_bar(duration, mktpxy):
