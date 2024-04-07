e2155e1e080bb20a4cb47df3c6392f5d2fb2d618
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-07 08:57:40 +0800

    Create sleeppxy.py

---
+++ sys/exe/run/sleeppxy.py
@@ -0,0 +1,23 @@
+import importlib
+import subprocess
+import time
+from rich.console import Console
+import sys
+from cyclepxy import cycle
+
+console = Console()
+
+def progress_bar(duration, mktpxy):
+    for _ in range(int(duration)):
+        time.sleep(1)
+        if mktpxy == 'Buy':
+            console.print('[green]🏛 PXY®[/]👆', end='')  # Up arrow + handshake
+        elif mktpxy == 'Sell':
+            console.print('[red]🏛 PXY®[/]👇', end='')  # Down arrow + handshake
+        elif mktpxy == 'Bull':
+            console.print('[green]🏛 PXY®[/]👉', end='')  # Right arrow + handshake
+        elif mktpxy == 'Bear':
+            console.print('[red]🏛 PXY®[/]👈', end='')  # Left arrow + handshake
+        else:
+            console.print('[yellow]🏛 PXY®[/]🤝', end='')  # Neutral with handshake
+    console.print()
