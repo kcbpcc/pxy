0fff247b18236d219ae3719a7fe2e6a575dc83a7
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-07 09:04:57 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -38,22 +38,9 @@ while True:
     subprocess.run(['python3', 'cntrlcncpxy.py']) 
     subprocess.run(['python3', 'cntrloptpxy.py']) 
     subprocess.run(['python3', 'cndlpxy.py'])
+    from sleeppxy import progress_bar
     import time
     from rich.console import Console
-    def progress_bar(duration, mktpxy):
-        console = Console()
-        for _ in range(int(duration)):
-            time.sleep(1)
-            if mktpxy == 'Buy':
-                console.print('[green]🏛 PXY®[/]👆', end='')  # Up arrow + handshake
-            elif mktpxy == 'Sell':
-                console.print('[red]🏛 PXY®[/]👇', end='')  # Down arrow + handshake
-            elif mktpxy == 'Bull':
-                console.print('[green]🏛 PXY®[/]👉', end='')  # Right arrow + handshake
-            elif mktpxy == 'Bear':
-                console.print('[red]🏛 PXY®[/]👈', end='')  # Left arrow + handshake
-            else:
-                console.print('[yellow]🏛 PXY®[/]🤝', end='')  # Neutral with handshake
-        console.print()
-    progress_bar(cycle, mktpxy)
+    progress_bar(5, mktpxy)
+
 
