3d4d0fc04f4efd0975e6a603a98071e8cc466536
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-07 19:28:06 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -1,3 +1,4 @@
+###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
 import pandas as pd
 from toolkit.logger import Logger
 from toolkit.currency import round_to_paise
@@ -16,10 +17,10 @@ from bukdpxy import sum_last_numerical_value_in_each_row
 from cmbddfpxy import process_data
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 combined_df = process_data()
-###########################################################################################################################################################################################################
+###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
 file_path = 'filePnL.csv'
 result = sum_last_numerical_value_in_each_row(file_path)  
-###########################################################################################################################################################################################################
+###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
 logging = Logger(30, dir_path + "main.log")
 try:
     sys.stdout = open('output.txt', 'w')
@@ -30,7 +31,7 @@ except Exception as e:
     logging.error(f"{str(e)} unable to get holdings")
     sys.exit(1)
 file_path = 'filePnL.csv'
-###########################################################################################################################################################################################################
+####################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™#######################################################################################################################
 def get_order_status(symbol, broker):
     try:
         orders = broker.kite.orders()
@@ -41,7 +42,7 @@ def get_order_status(symbol, broker):
         logging.error(f"Error fetching orders: {str(e)}")
         return "ERROR"  # Unable to fetch orders due to error
     return "NO"  # No orders found for the symbol
-###########################################################################################################################################################################################################
+#####################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™######################################################################################################################
 def stocks_sell_order_place(index, row):
     try:
         exchsym = str(index).split(":")
@@ -57,7 +58,7 @@ def stocks_sell_order_place(index, row):
                 variety='regular',
                 price=round_to_paise(row['ltp'], -0.2)
             )
-###########################################################################################################################################################################################################            
+######################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™#####################################################################################################################            
             if order_id:
                 logging.info(f"Order {order_id} placed for {exchsym[1]} successfully")                                
                 # Write the row to the CSV file here
@@ -94,7 +95,7 @@ def stocks_sell_order_place(index, row):
         #print(traceback.format_exc())
         logging.error(f"{str(e)} while placing order")
     return False
-###########################################################################################################################################################################################################
+############################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™###############################################################################################################
 def stocks_avg_order_place(index, row):
     try:
         exchsym = str(index).split(":")
@@ -140,7 +141,7 @@ def stocks_avg_order_place(index, row):
         # print(traceback.format_exc())
         logging.error(f"{str(e)} while placing order")
     return False
-###########################################################################################################################################################################################################
+###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
 try:
     import sys
     import traceback
@@ -158,7 +159,6 @@ try:
     import os
     import numpy as np
     from mktpxy import get_market_check
-    onemincandlesequance, mktpxy = get_market_check('^NSEI')
     import importlib
     from nftpxy import nse_action, nse_power, Day_Change, Open_Change, OPTIONS  
     import math
@@ -168,27 +168,28 @@ try:
     import asyncio
     from selfpxy import get_random_spiritual_message
     from macdpxy import calculate_macd_signal
+    from smapxy import check_index_status
+    from tabulate import tabulate
+    from optpxy import get_opt_check
+    from dshpxy import get_holdingsinfo
+    from bordpxy import printbord
+###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
+    onemincandlesequance, mktpxy = get_market_check('^NSEI')
     macd = calculate_macd_signal("^NSEI")
     random_message = get_random_spiritual_message()
     switch = analyze_stock()
     file_path = 'filePnL.csv'
     result = sum_last_numerical_value_in_each_row(file_path)  
-    from smapxy import check_index_status
     nsma = check_index_status('^NSEI')
-    from tabulate import tabulate
-    from optpxy import get_opt_check
     optpxy = get_opt_check('^NSEI')
-    from dshpxy import get_holdingsinfo
-    total_nrml_m2m, total_cnc_m2m, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage,nrmlall_Stocks_count ,nrmlall_Stocks_capital ,nrmlall_Stocks_worth ,nrmlall_Stocks_profit_loss = get_holdingsinfo(combined_df)    
-    from bordpxy import printbord
-#########################################################################################################################################################################################################
+####################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™#####################################################################################################################
     try:
         response = broker.kite.margins()
         available_cash = response["equity"]["available"]["live_balance"]
     except Exception as e:
         print(f"An error occurred: {e}")
         available_cash = 0
-###########################################################################################################################################################################################################
+###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
     epsilon = 1e-10
     def calculate_smb_power(row):
         start = row['low'] if row['source'] == 'holdings' else (row['avg'] if row['source'] == 'positions' else ValueError("Invalid value in 'source' column"))
@@ -199,12 +200,12 @@ try:
             return 0.5
     combined_df['smb_power'] = combined_df.apply(calculate_smb_power, axis=1)
     threshold = 3
-###########################################################################################################################################################################################################
+###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
     combined_df['fPL%'] = combined_df['smb_power'].apply(lambda x: round(np.exp(np.clip(((x + nse_power) / 2), -threshold, threshold)), 2))
     combined_df['tPL%'] = np.round(np.maximum(combined_df['fPL%'], np.maximum(1.4, np.round(np.exp(np.clip(((combined_df['fPL%'] + nse_power) / 2), -threshold, threshold)), 2)) * 1), 2)
-    combined_df['min'] = np.where(Open_Change > 0, 5, 3) # Fixed the closing quotation mark here
+    combined_df['min'] = np.where(Day_Change > 0, 5, 3) # Fixed the closing quotation mark here
     combined_df['tPL%'] = np.where(nsma == 'up', np.maximum(2 * combined_df['tPL%'], combined_df['min']), np.where(nsma == 'down', np.maximum(combined_df['tPL%'] * 1, combined_df['min']), combined_df['min']))
-###########################################################################################################################################################################################################
+###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
     numeric_columns = ['fPL%','tPL%','smb_power','oPL%','qty', 'average_price', 'Invested','Yvalue', 'ltp','close', 'open', 'high', 'low','value', 'PnL', 'PL%', 'dPnL', 'dPL%']
     combined_df[numeric_columns] = combined_df[numeric_columns].round(2)
     filtered_df = combined_df[(combined_df['product'] == 'CNC') & (combined_df['qty'] != 0)]
@@ -215,7 +216,7 @@ try:
     total_PnL_stocks_buy = round(stocks_buy_df['PnL'].sum()) if not stocks_buy_df.empty else 0
     total_dPnL = round(combined_df_positive_qty['dPnL'].sum())
     total_dPnL_percentage = (total_dPnL / combined_df_positive_qty['Invested'].sum()) * 100 if combined_df_positive_qty['Invested'].sum() != 0 else 0
-###########################################################################################################################################################################################################
+####################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™#######################################################################################################################
     lstchk_file = "fileHPdf.csv"
     combined_df.to_csv(lstchk_file, index=False)
     pxy_df = filtered_df.copy()[['tPL%','fPL%','oPL%','dPL%','PnL', 'PL%','smb_power','Invested','source','product', 'qty','avg','ltp', 'open', 'high', 'close', 'low','key']]
@@ -226,7 +227,7 @@ try:
     PRINT_df['HP'] = PRINT_df['HP'].replace({'holdings': '📌', 'positions': '🎯'})
     PRINT_df['TR'] = PRINT_df['TR'].apply(lambda TR: '⚪' if TR > 0.8 else ('🟢' if 0.5 < TR <= 0.8 else ('🟠' if 0.3 < TR <= 0.5 else ('🔴' if TR <= 0.3 else TR))))
     PRINT_df['key'] = PRINT_df['key'].str.replace(r'BSE:|NSE:', '', regex=True)
-###########################################################################################################################################################################################################    
+########################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™###################################################################################################################    
     PRINT_df_sorted = PRINT_df.copy()
     PRINT_df_sorted['TR'] = PRINT_df_sorted['TR'].apply(lambda TR: TR[:2] if isinstance(TR, str) else TR)
     PRINT_df_sorted['key'] = PRINT_df_sorted['key'].str.replace(r'(BSE:|NSE:|NFO:)', '', regex=True).str[:8].str.ljust(8, ' ')
@@ -234,7 +235,7 @@ try:
     pd.set_option('display.max_colwidth', 1)
     PRINT_df_sorted_display = PRINT_df_sorted.copy()
     stocks_filtered_df = PRINT_df_sorted_display[PRINT_df_sorted_display['PL%'] > 1.4].sort_values(by='PL%')
-###########################################################################################################################################################################################################   
+########################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™###################################################################################################################   
     csv_file_path = "filePnL.csv"
     selected_rows = []
     if Open_Change < 0 :
@@ -249,17 +250,17 @@ try:
                     row['high'] > 0 and
                     row['low'] > 0 and
                     row['close'] > 0 and
-                    row['ltp'] != 0                   
+                    row['ltp'] != 0 and
+                    nse_power < 0.9 
                 ):                            
-###########################################################################################################################################################################################################                    
+############################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™###############################################################################################################                    
                     if (
                         (row['qty'] > 0 and
                          row['avg'] != 0 and
-                         nse_power < 0.9 and
                          row['product'] == 'CNC' and
                          row['PL%'] > 1.4 ) and
                         (
-                            (row['PL%'] > row['tPL%']) or (total_dPnL < 5000)
+                            (row['PL%'] > row['tPL%']) or (Day_Change < 0)  
                         )
                     ):
                         try:
@@ -273,14 +274,13 @@ try:
                         except Exception as e:
                             # Handle any other exceptions that may occur during order placement
                             print(f"An unexpected error occurred while placing an order for key {key}: {e}")
-###########################################################################################################################################################################################################     
+##############################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™#############################################################################################################     
                     elif (
                         (row['qty'] > 0 and
                          row['avg'] != 0 and
                          row['Invested'] < 20000 and
-                         available_cash > 10000 and
-                         nse_power < 0.3 and
-                         mktpxy in ['Buy', 'Bull'] and
+                         available_cash > 30000 and
+                         mktpxy in ['Buy'] and
                          row['PL%'] < -20)
                     ):
                         try:                            
@@ -297,8 +297,8 @@ try:
         except Exception as e:
             # Handle any other exceptions that may occur during the loop
             print(f"An unexpected error occurred: {e}")        
- 
-###########################################################################################################################################################################################################
+#############################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™##############################################################################################################
+    total_nrml_m2m, total_cnc_m2m, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage,nrmlall_Stocks_count ,nrmlall_Stocks_capital ,nrmlall_Stocks_worth ,nrmlall_Stocks_profit_loss = get_holdingsinfo(combined_df)    
     printbord(total_nrml_m2m, total_cnc_m2m, optpxy, Day_Change, result, total_PnL_percentage, total_dPnL, total_PnL, total_dPnL_percentage,
              total_PnL_stocks_buy, available_cash,
              nse_power,all_Stocks_count, red_Stocks_count,green_Stocks_count,all_Stocks_capital_lacks,all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage, mktpxy,nrmlall_Stocks_count ,nrmlall_Stocks_capital ,nrmlall_Stocks_worth ,nrmlall_Stocks_profit_loss, nsma)
@@ -306,8 +306,9 @@ try:
     if not stocks_filtered_df.empty:
         print('\n'.join([line.rjust(40) for line in stocks_filtered_df.to_string(index=False, header=False).split('\n')]))
         print((GREEN if nsma == "up" else RED if nsma == "down" else YELLOW) + "ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩ" + RESET)
-###########################################################################################################################################################################################################
+############################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™###############################################################################################################
 except Exception as e:
     remove_token(dir_path)
     print(traceback.format_exc())
     logging.error(f"{str(e)} in the main loop")
+###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
