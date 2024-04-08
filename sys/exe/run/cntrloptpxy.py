4f7013384bc461f6917b9c72678f4d1bd53b0f84
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-08 12:25:12 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -74,6 +74,7 @@ combined_df = process_data()
 opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
 opt_df['key'] = opt_df['key'].str.replace('NFO:', '') 
 opt_df['PL%'] = (opt_df['PnL'] / opt_df['Invested']) * 100
+opt_df['PL%'] = opt_df['PL%'].fillna(0)
 opt_df['PL%'] = opt_df['PL%'].astype(int)  
 opt_df = opt_df[['key', 'Invested', 'qty', 'PL%', 'PnL','product']]
 total_invested = opt_df['Invested'].sum()
