78c94c9f3a52cb2b25641f7c19cb2ce339f57399
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-08 19:23:41 +0800

    Update cmbddfpxy.py

--- sys/exe/run/cmbddfpxy.py
+++ sys/exe/run/cmbddfpxy.py
@@ -75,8 +75,8 @@ def process_data():
         combined_df['qty'] = combined_df.apply(lambda row: int(row['quantity'] + row['t1_quantity']) if row['source'] == 'holdings' else int(row['quantity']), axis=1)
         combined_df['oPL%'] = combined_df.apply(lambda row: round((((row['ltp'] - row['open']) / row['open']) * 100), 2) if row['open'] != 0 else 0, axis=1)
         combined_df['dPL%'] = combined_df.apply(lambda row: round((((row['ltp'] - row['close']) / row['close']) * 100), 2) if row['close'] != 0 else 0, axis=1)
-        
 
+        combined_df['pnl'] = combined_df['pnl'].astype(int)
         combined_df['avg'] = combined_df['average_price']
         combined_df['Invested'] = (combined_df['qty'] * combined_df['avg']).round(0).astype(int)
         combined_df['value'] = combined_df['qty'] * combined_df['ltp']
