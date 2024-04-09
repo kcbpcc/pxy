f1d5e3679625d450079b6ed93f02ad3aa2e6ed17
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-09 13:14:07 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -217,8 +217,7 @@ try:
     combined_df['fPL%'] = combined_df['smb_power'].apply(lambda x: round(np.exp(np.clip(((x + x ) / 2), -threshold, threshold)), 2))
     combined_df['tPL%'] = np.round(np.maximum(combined_df['fPL%'], np.maximum(1.4, np.round(np.exp(np.clip(((combined_df['fPL%'] + combined_df['fPL%']) / 2), -threshold, threshold)), 2)) * 1), 2)
     combined_df['min'] = 1.5
-    combined_df['credit'] = np.maximum(0.5 * ((nse_power > 0) + (Day_Change > 0) + (Open_Change > 0)), 0.3)
-    combined_df['tPL%'] = combined_df['credit'] + (np.where(nsma == 'up', np.maximum(1 * combined_df['tPL%'], combined_df['min']), np.where(nsma == 'down', np.maximum(combined_df['tPL%'] * 0.5, combined_df['min']), combined_df['min'])))
+     combined_df['tPL%'] = (np.where(nsma == 'up', np.maximum(1 * combined_df['tPL%'], combined_df['min']), np.where(nsma == 'down', np.maximum(combined_df['tPL%'] * 0.5, combined_df['min']), combined_df['min'])))
 ###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
     numeric_columns = ['fPL%','tPL%','smb_power','oPL%','qty', 'average_price', 'Invested','Yvalue', 'ltp','close', 'open', 'high', 'low','value', 'PnL', 'PL%', 'dPnL', 'dPL%']
     combined_df[numeric_columns] = combined_df[numeric_columns].round(2)
