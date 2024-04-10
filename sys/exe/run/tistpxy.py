b584e8e9e577f1b530d21f9bd468daf182c29a31
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-10 21:30:13 +0800

    Update tistpxy.py

--- sys/exe/run/tistpxy.py
+++ sys/exe/run/tistpxy.py
@@ -1,33 +1,25 @@
-
-
 from datetime import datetime
 import pytz
-
+from nftpxy import Day_Change
+from clorpxy import BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, RESET
 def print_current_datetime_in_ist():
-    # Get the current time in UTC
     utc_now = datetime.utcnow()
-
-    # Set the UTC timezone
     utc_timezone = pytz.timezone('UTC')
     utc_now = utc_timezone.localize(utc_now)
-
-    # Convert to IST (Indian Standard Time)
     ist_timezone = pytz.timezone('Asia/Kolkata')
     ist_now = utc_now.astimezone(ist_timezone)
-
     formatted_datetime = (
-        f"\033[93m\033[4m🗓️ {ist_now.strftime('%d')} "  # Bright yellow color and underline
+        f"🗓️ {ist_now.strftime('%d')} "  # Bright yellow color and underline
         f"{ist_now.strftime('%B'):9} {ist_now.strftime('%Y')}|"  # Month, year
-        f"🕛 {ist_now.strftime('%A'):9}"  # Day
-        f"⏰{ist_now.strftime('%I:%M%p')}\033[0m"  # Time (reset color)
+        f"🕛{ist_now.strftime('%A'):9}|"  # Day
+        f"⏰{ist_now.strftime('%I:%M%p')}"  # Time (reset color)
     )
-    
-    # Print the formatted date and time
-    print(formatted_datetime)
-    
-    # Time with white color
-    formatted_time = f"\033[97m⏰ {ist_now.strftime('%I:%M%p')}\033[0m"  # Time (reset color)
-    return formatted_time
+    if Day_Change > 0:
+        print(f"{BRIGHT_GREEN}{formatted_datetime}{RESET}")
+    elif Day_Change < 0:
+        print(f"{BRIGHT_RED}{formatted_datetime}{RESET}")
+    else:
+        print(f"{BRIGHT_YELLOW}{formatted_datetime}{RESET}")
+print_current_datetime_in_ist()
 
-if __name__ == "__main__":
-    print_current_datetime_in_ist()
+
