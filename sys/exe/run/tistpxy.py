70d03663d88856ed9adc2fc07dcfb27a58c74278
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-10 21:10:39 +0800

    Update tistpxy.py

--- sys/exe/run/tistpxy.py
+++ sys/exe/run/tistpxy.py
@@ -1,32 +1,35 @@
 from datetime import datetime
 import pytz
+
+# Constants from nftpxy and clorpxy modules
 from nftpxy import nse_action, nse_power, Day_Change, Open_Change, OPTIONS  
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 
 def print_current_datetime_in_ist():
+    # Get the current time in UTC
     utc_now = datetime.utcnow()
+
+    # Set the UTC timezone
     utc_timezone = pytz.timezone('UTC')
     utc_now = utc_timezone.localize(utc_now)
+
+    # Convert to IST (Indian Standard Time)
     ist_timezone = pytz.timezone('Asia/Kolkata')
     ist_now = utc_now.astimezone(ist_timezone)
-    formatted_datetime = (
-        f"{BRIGHT_YELLOW}\033[4m🗓️ {ist_now.strftime('%d')} "  
-        f"{ist_now.strftime('%B'):9} {ist_now.strftime('%Y')}|"  
-        f"🕛 {ist_now.strftime('%A'):9}"  
-        f"{formatted_time()}{RESET}"  
-    )
-    print(formatted_datetime)
-    
-def formatted_time():
-    ist_now = datetime.now(pytz.timezone('Asia/Kolkata'))
-    formatted_time = f"\033[97m⏰ {ist_now.strftime('%I:%M%p')}"
-    return formatted_time
 
-if __name__ == "__main__":
-    print_current_datetime_in_ist()
+    formatted_datetime = (
+        f"🗓️ {ist_now.strftime('%d')} "  # Bright yellow color and underline
+        f"{ist_now.strftime('%B'):9} {ist_now.strftime('%Y')}|"  # Month, year
+        f"🕛 {ist_now.strftime('%A'):9}|"  # Day
+        f"⏰{ist_now.strftime('%I:%M%p')}"  # Time (reset color)
+    )
     if Day_Change > 0:
         print(formatted_datetime, f"{BRIGHT_GREEN}Day Change: {Day_Change}{RESET}")
     elif Day_Change < 0:
         print(formatted_datetime, f"{BRIGHT_RED}Day Change: {Day_Change}{RESET}")
     else:
         print(formatted_datetime, f"{BRIGHT_YELLOW}Day Change: {Day_Change}{RESET}")
+
+# Test the function
+print_current_datetime_in_ist()
+
