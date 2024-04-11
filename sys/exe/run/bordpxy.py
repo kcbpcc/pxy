c80c004f2632a78fb137147459479a3568829ead
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-11 16:26:40 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -58,7 +58,7 @@ def printbord(all_Stocks_yworth, total_cnc_m2m, mktpxy, available_cash, nse_acti
     output_lines.append(left_aligned_format.format(f"Funds:{BRIGHT_GREEN if available_cash > 12000 else BRIGHT_YELLOW}{available_cash:.0f}{RESET}") +
                         right_aligned_format.format(f"Day-P&L:{BRIGHT_GREEN if (all_Stocks_worth_lacks - all_Stocks_yworth) >= 0 else BRIGHT_RED}{round((all_Stocks_worth_lacks - all_Stocks_yworth), 2)}{RESET}"))
     
-    output_lines.append(left_aligned_format.format(f"Extras:{BRIGHT_GREEN if result >= 0 else BRIGHT_RED}{str(int(result)).zfill(5)}{RESET}") +
+    output_lines.append(left_aligned_format.format(f"Extras:{BRIGHT_GREEN if booked >= 0 else BRIGHT_RED}{str(int(booked)).zfill(5)}{RESET}") +
                       right_aligned_format.format(f"BOOKED:{GREEN if booked > 0 else GREEN}{str(round(booked)).zfill(5)}{RESET}"))
     
     full_output = '\n'.join(output_lines)
