a963c12495de941108e754a690e7a7ca8603969b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-11 15:46:05 +0800

    Update dshpxy.py

--- sys/exe/run/dshpxy.py
+++ sys/exe/run/dshpxy.py
@@ -54,6 +54,12 @@ def get_holdingsinfo(combined_df):
         nrml_nfom2m_df['m2m'].fillna(0, inplace=True)
         total_nrml_m2m = nrml_nfom2m_df['m2m'].sum()
 
+        nrmlall_Stocks_df = selected_positions_df[selected_positions_df['product'].isin(['NRML'])]
+        nrmlall_Stocks_count = len(nrmlall_Stocks_df)
+        nrmlall_Stocks_capital = nrmlall_Stocks_df['cap'].sum()
+        nrmlall_Stocks_worth = (nrmlall_Stocks_df['ltp'] * nrmlall_Stocks_df['qty']).round(4).sum()
+        nrmlall_Stocks_profit_loss = (nrmlall_Stocks_worth - nrmlall_Stocks_capital).round(4)
+
         return total_nrml_m2m, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_percentage
 
     except Exception as e:
@@ -66,7 +72,7 @@ def get_holdingsinfo(combined_df):
 # Assuming you have your DataFrame combined_df ready
 result = get_holdingsinfo(combined_df)
 if result is not None:
-    total_nrml_m2m, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_percentage = result
+    nrmlall_Stocks_worth, total_nrml_m2m, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_percentage = result
     print(f"Total NRML M2M: {total_nrml_m2m}")
     print(f"All Stocks Count: {all_Stocks_count}")
     print(f"Red Stocks Count: {red_Stocks_count}")
