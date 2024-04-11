ef6d7a3c98274a666b254cbbcfe9279a547f4731
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-12 00:35:49 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -56,7 +56,7 @@ def printbord(total_opts_invested_lacks, green_Stocks_capital_lacks, red_Stocks_
     output_lines.append(left_aligned_format.format(f"Delta:{BRIGHT_GREEN if ydaypnl_to_print >= 0 else BRIGHT_RED}{int(ydaypnl_to_print * 100000)}{RESET}") +
                         right_aligned_format.format(f"Postions:{BRIGHT_GREEN if total_cnc_m2m > 0 else BRIGHT_RED}{str(int(total_cnc_m2m)).zfill(5)}{RESET}"))
     
-    output_lines.append(left_aligned_format.format(f"Funds:{BRIGHT_GREEN if available_cash > 12000 else BRIGHT_YELLOW}{available_cash:.0f}{RESET}") +
+    output_lines.append(left_aligned_format.format(f"OpsCap:{BRIGHT_GREEN if total_opts_invested_lacks >= 0 else BRIGHT_RED}{total_opts_invested_lacks:.0f}{RESET}") +
                         right_aligned_format.format(f"Day-P&L:{BRIGHT_GREEN if (all_Stocks_worth_lacks - all_Stocks_yworth_lacks)*100000 >= 0 else BRIGHT_RED}{int(round((all_Stocks_worth_lacks - all_Stocks_yworth_lacks) * 100000, 0))}{RESET}"))
     
     output_lines.append(left_aligned_format.format(f"OpsCap:{BRIGHT_GREEN if total_opts_invested_lacks >= 0 else BRIGHT_RED}{str(int(total_opts_invested_lacks)).zfill(5)}{RESET}") +
