33c1a6648a26ee8d67dcd1e53062cef22ad48580
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-11 10:58:07 +0800

    Update dshpxy.py

--- sys/exe/run/dshpxy.py
+++ sys/exe/run/dshpxy.py
@@ -1,36 +1,27 @@
 import pandas as pd
 from prettytable import PrettyTable
+
 def convert_to_laks(value):
     return f'{value/100000:.4f}'
+
 def format_value(value):
     if value == 'Profit & Loss':
         return 'Profit & Loss'
     return f'{value:.0f}' if isinstance(value, (int, float)) else value
+
 def get_holdingsinfo(combined_df):
     try:
         if "m2m" not in combined_df.columns:
             combined_df['m2m'] = 0
-            
+
+        ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################            
         selected_holdings_df = combined_df[(combined_df['qty'] != 0) & (combined_df['product'] == 'CNC')]
-        
-        
-
-        
-        cnc_nfom2m_df = combined_df[(combined_df['key'].str.contains("NSE:|BSE:") & (combined_df['source'] == 'positions') & (combined_df['qty'] > 0))].copy()
-        cnc_nfom2m_df['m2m'] = pd.to_numeric(cnc_nfom2m_df['m2m'], errors='coerce')
-        cnc_nfom2m_df['m2m'].fillna(0, inplace=True)
-        total_cnc_m2m = cnc_nfom2m_df['m2m'].sum()
-
-        bkd_cnc_nfom2m_df = combined_df[(combined_df['key'].str.contains("NSE:|BSE:")) & (combined_df['source'] == 'holdings') & (combined_df['qty'] == 0)].copy()
-        bkd_cnc_nfom2m_df['m2m'] = pd.to_numeric(bkd_cnc_nfom2m_df['m2m'], errors='coerce')
-        bkd_cnc_nfom2m_df['m2m'].fillna(0, inplace=True)
-        bkd_total_cnc_m2m = bkd_cnc_nfom2m_df['m2m'].sum()
         
         selected_holdings_df['cap'] = (selected_holdings_df['qty'] * selected_holdings_df['average_price']).astype(int)
         selected_holdings_df['unrealized'] = ((selected_holdings_df['ltp'] - selected_holdings_df['average_price']) * selected_holdings_df['qty']).round(2)
-        selected_holdings_df['perc'] = ((selected_holdings_df['unrealized'] / selected_holdings_df['cap']) * 100).where(selected_holdings_df['cap'] > 0)
+        selected_holdings_df['perc'] = ((selected_holdings_df['unrealized'] / selected_holdings_df['cap']) * 100)
 
-        green_Stocks_df = selected_holdings_df[(selected_holdings_df['perc'] > 0) & (selected_holdings_df['product'] == 'CNC')]
+        green_Stocks_df = selected_holdings_df[(selected_holdings_df['perc'] > 0)]
         green_Stocks_count = len(green_Stocks_df)
         green_Stocks_capital = green_Stocks_df['cap'].sum()
         green_Stocks_worth = green_Stocks_df['ltp'].dot(green_Stocks_df['qty']).round(4)
@@ -49,37 +40,32 @@ def get_holdingsinfo(combined_df):
         all_Stocks_worth = selected_holdings_df['ltp'].dot(selected_holdings_df['qty']).round(4)
         all_Stocks_profit_loss = (all_Stocks_worth - all_Stocks_capital).round(4)
 
-        nrmlall_Stocks_df = selected_holdings_df[selected_holdings_df['product'].isin(['NRML'])]
-        nrmlall_Stocks_count = len(nrmlall_Stocks_df)
-        nrmlall_Stocks_capital = nrmlall_Stocks_df['cap'].sum()
-        nrmlall_Stocks_worth = (nrmlall_Stocks_df['ltp'] * nrmlall_Stocks_df['qty']).round(4).sum()
-        nrmlall_Stocks_profit_loss = (nrmlall_Stocks_worth - nrmlall_Stocks_capital).round(4)
-
-        
-        selected_positions_df = combined_df[(combined_df['qty'] != 0) & (combined_df['product'] != 'CNC')]
-
-        
-        selected_columns = ['tradingsymbol','key', 'm2m', 'product', 'qty', 'close_price', 'average_price', 'ltp']
-        selected_holdings_df = selected_holdings_df[selected_columns].copy()
-        nrml_nfom2m_df = combined_df[combined_df['key'].str.contains("NFO:")].copy()
-        nrml_nfom2m_df['m2m'] = pd.to_numeric(nrml_nfom2m_df['m2m'], errors='coerce')
-        nrml_nfom2m_df['m2m'].fillna(0, inplace=True)
-        total_nrml_m2m = nrml_nfom2m_df['m2m'].sum()
-        
         day_change = all_Stocks_worth - selected_holdings_df['close_price'].dot(selected_holdings_df['qty']).round(4)
         day_change_percentage = ((day_change / selected_holdings_df['close_price'].dot(selected_holdings_df['qty']).round(4)) * 100) if selected_holdings_df['close_price'].dot(selected_holdings_df['qty']).round(4) != 0 else 0
         
         zero_qty_count = combined_df[combined_df['qty'] == 0].shape[0]
         all_Stocks_capital_lacks = all_Stocks_capital/100000
         all_Stocks_worth_lacks = all_Stocks_worth/100000
+
+        ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################           
         
-        return bkd_total_cnc_m2m, total_nrml_m2m, total_cnc_m2m, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage, nrmlall_Stocks_count, nrmlall_Stocks_capital, nrmlall_Stocks_worth, nrmlall_Stocks_profit_loss
+        selected_positions_df = combined_df[(combined_df['qty'] != 0) & (combined_df['product'] != 'CNC')]
+        
+        nrml_nfom2m_df = selected_positions_df[selected_positions_df['key']].copy()
+        nrml_nfom2m_df['m2m'] = pd.to_numeric(nrml_nfom2m_df['m2m'], errors='coerce')
+        nrml_nfom2m_df['m2m'].fillna(0, inplace=True)
+        total_nrml_m2m = nrml_nfom2m_df['m2m'].sum()
+       
+        ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################           
+        
+        return total_nrml_m2m, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_rercentage
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
 
 # Call the function with the combined_df
 #get_holdingsinfo(combined_df)
+
 
 
 
