f62bf75fd52610fb559a40cf820ee09e11dea56a
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-11 19:44:26 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -4,6 +4,8 @@ booked = sum_last_numerical_value_in_each_row(file_path)
 from acvaluepxy import process_acvalue, get_current_acvalue
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 def printbord(green_Stocks_capital_lacks, red_Stocks_capital_lacks, nsma, all_Stocks_yworth_lacks, total_cnc_m2m, mktpxy, available_cash, nse_action, nse_power, Day_Change, Open_Change, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_percentage):
+    
+    
     color_code = ""
     if Open_Change > 0:
         color_code = BRIGHT_GREEN + UNDERLINE
@@ -58,7 +60,7 @@ def printbord(green_Stocks_capital_lacks, red_Stocks_capital_lacks, nsma, all_St
     output_lines.append(left_aligned_format.format(f"Funds:{BRIGHT_GREEN if available_cash > 12000 else BRIGHT_YELLOW}{available_cash:.0f}{RESET}") +
                         right_aligned_format.format(f"Day-P&L:{BRIGHT_GREEN if (all_Stocks_worth_lacks - all_Stocks_yworth_lacks)*100000 >= 0 else BRIGHT_RED}{int(round((all_Stocks_worth_lacks - all_Stocks_yworth_lacks) * 100000, 0))}{RESET}"))
     
-    output_lines.append(left_aligned_format.format(f"G{green_Stocks_capital_lacks:.2f}R{red_Stocks_capital_lacks:.2f}").zfill(12) + 
+    output_lines.append(left_aligned_format.format(f"G{0}R{0}").zfill(5) + 
                        right_aligned_format.format(f"BOOKED:{GREEN if booked > 0 else GREEN}{str(int(booked)).zfill(5)}{RESET}"))
 
   
