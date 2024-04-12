75adb9893a57b436ad03fa177ab8285aa455d64d
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-13 07:38:12 +0800

    Update predictpxy.py

--- sys/exe/run/predictpxy.py
+++ sys/exe/run/predictpxy.py
@@ -1,22 +1,15 @@
-# Importing required modules
 from nftpxy import get_nse_action
 from smapxy import check_index_status
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
-
-# Getting data
 ha_nse_action, nse_power, Day_Change, Open_Change = get_nse_action()
 nsma = check_index_status('^NSEI')
-
-# Defining variables
 Bearish = "Bearish"
 Bullish = "Bullish"
-
-# Predicting market sentiment
 if ha_nse_action == Bearish and nsma == "down":
     predictmkt = BRIGHT_RED + "hopeless" + RESET
 elif ha_nse_action == Bullish and nsma == "up":
     predictmkt = BRIGHT_GREEN + "hopeful" + RESET
 else:
     predictmkt = BOLD + "useless" + RESET
-
 print("Predicted market sentiment:", predictmkt)
+return predictmkt
