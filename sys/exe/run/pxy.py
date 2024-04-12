74988747f25edcda91d935cb90ab81140887ccd0
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-12 13:37:42 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -34,7 +34,7 @@ while True:
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
     subprocess.run(['python3', 'worldpxy.py']) 
     subprocess.run(['python3', 'buyoptpxy.py']) 
-    subprocess.run(['python3', 'buycncpxy.py']) if not (Open_Change < 0 and Day_Change < 0) and mktpxy == 'Buy' else None
+    subprocess.run(['python3', 'buycncpxy.py']) if not (Open_Change < 0 and Day_Change < 0 and nsma == 'down') and mktpxy == 'Buy' else None
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
     subprocess.run(['python3', 'cntrlcncpxy.py']) 
     subprocess.run(['python3', 'cntrloptpxy.py']) 
