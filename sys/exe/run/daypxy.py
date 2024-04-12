316c3b6a266779b621c1108962243667e177b181
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-13 06:40:09 +0800

    Update daypxy.py

--- sys/exe/run/daypxy.py
+++ sys/exe/run/daypxy.py
@@ -10,7 +10,8 @@ from utcpxy import peak_time
 from macdpxy import calculate_macd_signal
 from smapxy import check_index_status
 from depthpxy import calculate_consecutive_candles
-from nftpxy import ha_nse_action, nse_power, Day_Change, Open_Change 
+from nftpxy import get_nse_action
+nse_action, nse_power, Day_Change, Open_Change  = get_nse_action()
 
 console = Console()
 warnings.simplefilter(action='ignore', category=FutureWarning)
