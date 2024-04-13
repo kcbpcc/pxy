3a9fe731b4ec62c8a15f5e7d2ab339fdda520546
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-13 08:06:40 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -45,6 +45,6 @@ while True:
     subprocess.run(['python3', 'cndlpxy.py'])
     subprocess.run(['python3', 'selfpxy.py'])
     subprocess.run(['python3', 'daypxy.py']) 
-    print(f"PXY® Predicted market sentiment: {mktpredict.rjust(42)} {'👇' if mktpredict == 'hopeless' else '☝️' if mktpredict == 'hopeful' else '🤏'}")
+    print(f"PXY® Predicted market sentiment: {mktpredict.rjust(40)} {'👇' if mktpredict == 'hopeless' else '☝️' if mktpredict == 'hopeful' else '🤏'}")
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
     progress_bar(cycle, mktpxy)
