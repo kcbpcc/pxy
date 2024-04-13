86195c1243403cb597b40f2ba12bc4e4620332dd
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-13 09:42:42 +0800

    Update buyoptpxy.py

--- sys/exe/run/buyoptpxy.py
+++ sys/exe/run/buyoptpxy.py
@@ -125,8 +125,8 @@ async def main():
         sys.stdout = sys.__stdout__
     
     count_CE, count_PE = count_positions_by_type(broker)
-    print(f"CE positions: {count_CE}")
-    print(f"PE positions: {count_PE}")
+    print(f"CE positions: {count_CE}🔥🔥,           PE positions: {count_PE}💧💧")
+
     
     expiry_year, expiry_month, expiry_day = get_this_thursday()
 
