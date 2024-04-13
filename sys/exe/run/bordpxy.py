3cda1dc26a60f9d01f6331e3ef424d85113865ed
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-13 19:12:36 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -4,15 +4,8 @@ booked = sum_last_numerical_value_in_each_row(file_path)
 from acvaluepxy import process_acvalue, get_current_acvalue
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 def printbord(all_Stocks_worth_dpnl, nsma, all_Stocks_yworth_lacks, total_cnc_m2m, mktpxy, available_cash, ha_nse_action, nse_power, Day_Change, Open_Change, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_percentage):
-    color_code = ""
-    if Open_Change > 0:
-        color_code = BRIGHT_GREEN + UNDERLINE
-    elif Open_Change < 0:
-        color_code = BRIGHT_RED + UNDERLINE
-    else:  # Open_Change == 0
-        color_code = BRIGHT_YELLOW + UNDERLINE
-    text = "PXY® PreciseXceleratedYield Pvt Ltd™"
-    print(color_code + text.center(42) + RESET)
+
+    subprocess.run(['python3', 'daypxy.py']) 
     total_opts_invested_lacks = 0
     output_lines = []
     acvalue = ((all_Stocks_worth_lacks) + (total_opts_invested_lacks) + (available_cash / 100000))  
