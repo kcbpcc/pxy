199975b3787f4dc769366c0487efda12175280a4
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-13 18:06:53 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -51,4 +51,4 @@ while True:
     subprocess.run(['python3', 'daypxy.py']) 
     print(f"    PXY® Predicted market sentiment : {mktpredict}")
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
-    progress_bar(cycle, mktpxy)
+    progress_bar(5, mktpxy)
