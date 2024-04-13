f2ecab8dd6cac6e6fc344b70e9477d2dcb779922
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-13 11:05:12 +0800

    Update buyoptpxy.py

--- sys/exe/run/buyoptpxy.py
+++ sys/exe/run/buyoptpxy.py
@@ -133,7 +133,7 @@ async def main():
     
     expiry_year, expiry_month, expiry_day = get_this_thursday()
 
-    option_type = 'CE' if (mktpxy == 'Buy' and CE_weight < 1) else ('PE' if (mktpxy == 'Sell' and PE_weight < 1) else (print(f"Market-{mktpxy} and Weight:{weight} - let's wait 👀🔍👀🔍👀") or sys.exit(1)))
+    option_type = 'CE' if (mktpxy == 'Buy' and CE_weight > 1) else ('PE' if (mktpxy == 'Sell' and PE_weight > 1) else (print(f"Market-{mktpxy} and Weight:{weight} - let's wait 👀🔍👀🔍👀") or sys.exit(1)))
 
     symbol = construct_symbol(expiry_year, expiry_month, expiry_day, option_type)
 
