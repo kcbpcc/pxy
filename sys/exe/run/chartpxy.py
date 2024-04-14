01d1fe2431885ba69ef7384f6bfa2ad05d535393
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-15 00:25:19 +0800

    Update chartpxy.py

--- sys/exe/run/chartpxy.py
+++ sys/exe/run/chartpxy.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from asciichartpy import plot
-from colorpy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
+from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv('acvalue.csv')
