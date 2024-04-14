8e7ef652b10d0f44bba344feea53d5497c3c11c3
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-14 11:06:53 +0800

    Update predictpxy.py

--- sys/exe/run/predictpxy.py
+++ sys/exe/run/predictpxy.py
@@ -1,6 +1,5 @@
 from nftpxy import get_nse_action
 from smapxy import check_index_status
-from clorpxy import BRIGHT_RED, BRIGHT_GREEN, BOLD, RESET
 
 def predict_market_sentiment():
     ha_nse_action, nse_power, Day_Change, Open_Change = get_nse_action()
@@ -9,13 +8,14 @@ def predict_market_sentiment():
     Bullish = "Bullish"
 
     if ha_nse_action == Bearish and nsma == "down":
-        mktpredict = BRIGHT_RED + "FALL" + RESET
+        mktpredict = "FALL"
     elif ha_nse_action == Bullish and nsma == "up":
-        mktpredict = BRIGHT_GREEN + "RISE" + RESET
+        mktpredict = "RISE"
     else:
-        mktpredict = BOLD + "SIDE" + RESET
+        mktpredict = "SIDE"
 
     return mktpredict
 
 # Call the function to predict market sentiment and print the result
 predicted_sentiment = predict_market_sentiment()
+#print(predicted_sentiment)
