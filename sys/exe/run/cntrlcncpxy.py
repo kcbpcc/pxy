d3e08a0b862fab920f7dd0d3d7d5f1e3802bec45
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-16 07:39:17 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -17,7 +17,7 @@ from bukdpxy import sum_last_numerical_value_in_each_row
 from cmbddfpxy import process_data
 combined_df = process_data()
 from cdslchkpxy import cdslcheck
-cdsl_df = cdslcheck(combined_df)
+check_cdsl_df = cdslcheck(combined_df)
 print(check_cdsl_df[['cdsldate', 'cdslqty', 'qty', 'tradingsymbol']])
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 ###################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™########################################################################################################################
