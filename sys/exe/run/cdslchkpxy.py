1f539d3fbe8c77eda3b362b101546fe1378d0d19
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-16 07:36:56 +0800

    Update cdslchkpxy.py

--- sys/exe/run/cdslchkpxy.py
+++ sys/exe/run/cdslchkpxy.py
@@ -4,6 +4,9 @@ from datetime import datetime
 def cdslcheck(combined_df):
     cdsl_df = combined_df.copy()
 
+    # Convert 'authorised_date' column to datetime format
+    cdsl_df['authorised_date'] = pd.to_datetime(cdsl_df['authorised_date'])
+
     # Combine date and quantity in the format 'YYYY-MM-DD HH:MM:SS': qty
     cdsl_df['cdsl_info'] = cdsl_df.apply(lambda row: f"{row['authorised_date']}: {row['authorised_quantity']}", axis=1)
 
@@ -11,8 +14,9 @@ def cdslcheck(combined_df):
     today = datetime.now().date()
     today_datetime = datetime.combine(today, datetime.min.time())
 
-    # Filter the DataFrame based on the conditions: cdsldate is not today's date
-    check_cdsl_df = cdsl_df[(cdsl_df['authorised_date'].dt.date != today_datetime.date())]
+    # Filter the DataFrame based on the conditions: authorised_date is not today's date
+    check_cdsl_df = cdsl_df[(cdsl_df['authorised_date'].dt.date != today_datetime)]
 
     # Return the filtered DataFrame
     return check_cdsl_df
+
