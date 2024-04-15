e4a2469a0fddd6e29b6e3ed25f46fa1827c188d9
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-15 16:29:46 +0800

    Update cdslchkpxy.py

--- sys/exe/run/cdslchkpxy.py
+++ sys/exe/run/cdslchkpxy.py
@@ -2,17 +2,17 @@ import pandas as pd
 from datetime import datetime
 
 def cdslcheck(combined_df):
-   # Create a copy of the DataFrame to avoid modifying the original DataFrame
+    # Create a copy of the DataFrame to avoid modifying the original DataFrame
     cdsl_df = combined_df.copy()
 
     # Extract date and quantity from the 'authorisation' column
-    cdsl_df['cdsldate'] = cdsl_df['authorisation'].str.extract(r"(\d{4}-\d{2}-\d{2})")
+    cdsl_df['cdsldate'] = cdsl_df['authorisation'].str.extract(r"\{'pre': \{'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})': (\d+)\}}")
     
-    # Extract quantity, handling NaN values gracefully
-    cdsl_df['cdslqty'] = cdsl_df['authorisation'].str.extract(r": {'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}': (\d+)}}").astype(float).fillna(0).astype(int)
-
     # Convert the extracted date to datetime object
     cdsl_df['cdsldate'] = pd.to_datetime(cdsl_df['cdsldate'])
+
+    # Extracted quantity directly from the pattern, no need for conversion
+    cdsl_df['cdslqty'] = cdsl_df['authorisation'].str.extract(r"\{'pre': \{'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}': (\d+)\}}").astype(int)
 
     # Get today's date and convert it to datetime object
     today = datetime.now().date()
