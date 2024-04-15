a97db1851d33739a8546b00f11e1da4be390434f
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-15 16:26:47 +0800

    Update cdslchkpxy.py

--- sys/exe/run/cdslchkpxy.py
+++ sys/exe/run/cdslchkpxy.py
@@ -2,14 +2,14 @@ import pandas as pd
 from datetime import datetime
 
 def cdslcheck(combined_df):
-    # Create a copy of the DataFrame to avoid modifying the original DataFrame
+   # Create a copy of the DataFrame to avoid modifying the original DataFrame
     cdsl_df = combined_df.copy()
 
     # Extract date and quantity from the 'authorisation' column
     cdsl_df['cdsldate'] = cdsl_df['authorisation'].str.extract(r"(\d{4}-\d{2}-\d{2})")
     
     # Extract quantity, handling NaN values gracefully
-    cdsl_df['cdslqty'] = cdsl_df['authorisation'].str.extract(r": (\d+).*}}$").astype(float).fillna(0).astype(int)
+    cdsl_df['cdslqty'] = cdsl_df['authorisation'].str.extract(r": {'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}': (\d+)}}").astype(float).fillna(0).astype(int)
 
     # Convert the extracted date to datetime object
     cdsl_df['cdsldate'] = pd.to_datetime(cdsl_df['cdsldate'])
