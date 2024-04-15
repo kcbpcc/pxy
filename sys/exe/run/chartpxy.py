ca6dae927f406f93744c4dedc84afa475163d0a2
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-15 08:29:59 +0800

    Update chartpxy.py

--- sys/exe/run/chartpxy.py
+++ sys/exe/run/chartpxy.py
@@ -28,4 +28,4 @@ latest_record = df['acvalue'].iloc[-1]
 previous_record = df['acvalue'].iloc[-2]
 delta = int((latest_record - previous_record) * 100000)
 
-print("Delta:" + "📊📊📊.A/C Chart.📊📊📊" + str(delta))
+print("📊📊📊.A/C Chart.📊📊📊" + "Delta:" + str(delta))
