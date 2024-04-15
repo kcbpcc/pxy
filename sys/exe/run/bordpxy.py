95cf2ef40660271564aa95b4e90c0f84732d2560
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-15 22:54:01 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -19,7 +19,7 @@ def printbord(optworth, all_Stocks_worth_dpnl, nsma, all_Stocks_yworth_lacks, to
     left_aligned_format = "{:<" + str(column_width) + "}"
     right_aligned_format = "{:>" + str(column_width) + "}"
 
-    output_lines.append(left_aligned_format.format(f"Profit:{BRIGHT_YELLOW}{round((acvalue_to_print - 18.51), 2)}{RESET}") +
+    output_lines.append(left_aligned_format.format(f"Profit:{BRIGHT_YELLOW}{round((acvalue_to_print - 15.10), 2)}{RESET}") +
                         right_aligned_format.format(f"Loss:{BRIGHT_GREEN if (all_Stocks_capital_lacks - all_Stocks_worth_lacks)*-1 >= 0 else BRIGHT_RED}{round(((all_Stocks_capital_lacks - all_Stocks_worth_lacks)*-1), 2)}{RESET}"))
     output_lines.append(left_aligned_format.format(f"Flush:{BRIGHT_GREEN if green_Stocks_profit_loss > 0 else BRIGHT_RED}{round(green_Stocks_profit_loss)}{RESET}") +
                         right_aligned_format.format(f"Flush%:{BRIGHT_GREEN if green_Stocks_capital_percentage > 1.4 else BRIGHT_RED}{green_Stocks_capital_percentage}{RESET}"))                                  
