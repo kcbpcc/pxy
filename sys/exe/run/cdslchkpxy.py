0b871c0db91173c74f8316b32c985593889e8fb0
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-16 07:35:54 +0800

    Update cdslchkpxy.py

--- sys/exe/run/cdslchkpxy.py
+++ sys/exe/run/cdslchkpxy.py
@@ -4,24 +4,15 @@ from datetime import datetime
 def cdslcheck(combined_df):
     cdsl_df = combined_df.copy()
 
-    # Extract date and quantity from the 'authorisation' column
-    date_qty_extract = cdsl_df['authorisation'].str.extract(r"'pre': {'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})': (\d+)}")
-
     # Combine date and quantity in the format 'YYYY-MM-DD HH:MM:SS': qty
-    cdsl_df['cdsl_info'] = date_qty_extract.apply(lambda x: f"{x[0]}: {x[1]}", axis=1)
-
-    # Convert the extracted quantity to integers and create 'cdslqty' column
-    cdsl_df['cdslqty'] = date_qty_extract[1].astype(float).fillna(0).astype(int)
-
-    # Convert the extracted date to datetime format
-    cdsl_df['cdsldate'] = pd.to_datetime(date_qty_extract[0], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+    cdsl_df['cdsl_info'] = cdsl_df.apply(lambda row: f"{row['authorised_date']}: {row['authorised_quantity']}", axis=1)
 
     # Get today's date and convert it to datetime object
     today = datetime.now().date()
     today_datetime = datetime.combine(today, datetime.min.time())
 
-    # Filter the DataFrame based on the conditions: cdsldate is not today's date and cdslqty is not equal to qty
-    check_cdsl_df = cdsl_df[(cdsl_df['cdsldate'].dt.date != today_datetime.date())]
+    # Filter the DataFrame based on the conditions: cdsldate is not today's date
+    check_cdsl_df = cdsl_df[(cdsl_df['authorised_date'].dt.date != today_datetime.date())]
 
     # Return the filtered DataFrame
     return check_cdsl_df
