a2e7088e0a3e4024b812b8533333cb34d43ad5ad
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-16 08:01:19 +0800

    Update cdslchkpxy.py

--- sys/exe/run/cdslchkpxy.py
+++ sys/exe/run/cdslchkpxy.py
@@ -22,6 +22,9 @@ def cdslcheck(combined_df):
     today_datetime = datetime.combine(today, datetime.min.time())
 
     # Filter the DataFrame based on the conditions: authorised_date is not today's date
-    check_cdsl_df = cdsl_df[(cdsl_df['cdsldate'].dt.date != today_datetime.date()) & (cdsl_df['qty'] != cdsl_df['cdslqty'])]
+    check_cdsl_df = cdsl_df[(cdsl_df['cdsldate'].dt.date == today_datetime.date()) & 
+                            (cdsl_df['qty'] != cdsl_df['cdslqty']) &
+                            (cdsl_df['product'] == 'CNC') & 
+                            (cdsl_df['source'] == 'holdings')]
 
     return check_cdsl_df
