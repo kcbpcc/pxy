1d02a8844bc2a39a368ecbe6050433e84c9bb545
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-16 16:13:14 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -57,17 +57,17 @@ def place_order(tradingsymbol, quantity, transaction_type, order_type, product):
         return None
 ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
 def exit_options(key, pl_percentage, quantity, pnl):
-    if (key.endswith('CE') and pl_percentage >= mvtrgt_ce and quantity > 0) or (key.endswith('PE') and pl_percentage >= mvtrgt_pe and quantity > 0):
-        try:
-            resp = broker.kite.ltp(key)
-            if resp and isinstance(resp, dict):
-                ltp = resp[key]['last_price']
-            place_order(key, quantity, 'SELL', 'MARKET', 'NRML')  
-            message = f"Exit order placed for {key} @ {ltp} successfully.\nPL: {pnl}, PL%: {pl_percentage}%"
-            print(message)
-            send_telegram_message(message)
-        except Exception as e:
-            print(f"Error placing exit order for {key}: {e}")
+    try:
+        resp = broker.kite.ltp(key)
+        if resp and isinstance(resp, dict):
+            ltp = resp[key]['last_price']
+            if (key.endswith('CE') and pl_percentage >= mvtrgt_ce and quantity > 0) or (key.endswith('PE') and pl_percentage >= mvtrgt_pe and quantity > 0):
+                place_order(key, quantity, 'SELL', 'MARKET', 'NRML')  
+                message = f"Exit order placed for {key} @ {ltp} successfully.\nPL: {pnl}, PL%: {pl_percentage}%"
+                print(message)
+                send_telegram_message(message)
+    except Exception as e:
+        print(f"Error placing exit order for {key}: {e}")
 
 try:
     sys.stdout = open('output.txt', 'w')
