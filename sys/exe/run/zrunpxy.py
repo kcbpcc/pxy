61cd9d7736a23c6424e64cbdf4c0db989bbb626b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-18 06:14:05 +0800

    Update zrunpxy.py

--- sys/exe/run/zrunpxy.py
+++ sys/exe/run/zrunpxy.py
@@ -161,7 +161,7 @@ async def main():
     
     expiry_year, expiry_month, expiry_day = get_this_thursday()
 
-    option_type = 'PE' if (mktpxy == 'Buy' and CE_weight < 1 and count_CE < 4) else ('CE' if (mktpxy == 'Sell' and PE_weight < 1 and count_PE < 4) else (print(f"Market-{mktpxy} or Unbalanced-let's wait 🔍👀".rjust(39)) or sys.exit(1)))
+    option_type = 'PE' if (mktpxy == 'Buy') else ('CE' if (mktpxy == 'Sell') else (print(f"Market-{mktpxy} or not a time-let's wait 🔍👀".rjust(39)) or sys.exit(1)))
     symbol = construct_symbol(expiry_year, expiry_month, expiry_day, option_type)
 
     position_exists = check_existing_positions(broker, symbol)
