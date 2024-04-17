8a057b143b1032bd69db112784da1a29dab1eb2d
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-17 13:51:45 +0800

    Update chartpxy.py

--- sys/exe/run/chartpxy.py
+++ sys/exe/run/chartpxy.py
@@ -1,9 +1,24 @@
 import pandas as pd
 from asciichartpy import plot
 from clorpxy import SILVER, BRIGHT_RED, BRIGHT_GREEN
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv('acvalue.csv')
+# Define the scope and credentials for Google Sheets API
+SCOPES = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('accvalue.json', SCOPES)
+
+# Authorize the client using the credentials
+client = gspread.authorize(creds)
+
+# Open the Google Sheets document by its title
+sheet = client.open('accvalue').sheet1
+
+# Get all records from the Google Sheet
+data = sheet.get_all_records()
+
+# Convert the data to a DataFrame
+df = pd.DataFrame(data)
 
 # Convert 'date' column to datetime format
 df['date'] = pd.to_datetime(df['date'])
@@ -23,10 +38,10 @@ chart = plot(df['acvalue'].tolist(), {'height': 10, 'format': "{:,.2f}", 'color'
 
 # Print ASCII chart
 print(chart)
+
 # Calculate delta
 latest_record = df['acvalue'].iloc[-1]
 previous_record = df['acvalue'].iloc[-2]
 delta = int((latest_record - previous_record) * 100000)
 delta_color = BRIGHT_GREEN if delta >= 0 else BRIGHT_RED
 print(" 📊📊📊.A/C Chart.📊📊📊     Delta: {}{}".format(delta_color, delta))
-
