51bd7b75cbc9d6e46125114331b4d57eadbc7234
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-18 05:52:19 +0800

    Update zrunpxy.py

--- sys/exe/run/zrunpxy.py
+++ sys/exe/run/zrunpxy.py
@@ -1,13 +1,3 @@
-import os
-
-def play_beep():
-    if os.name == 'nt':  # for Windows
-        os.system("echo -e '\a'")
-    else:  # for Linux and macOS
-        os.system("echo '\a'")
-
-if __name__ == "__main__":
-    play_beep()
 #buyoptpxy.py
 adjest = 0
 import traceback
