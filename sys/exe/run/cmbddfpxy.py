bacbf0c02c09e1eb7c103b070393ba8deb9bdf06
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-19 12:45:34 +0800

    Update cmbddfpxy.py

--- sys/exe/run/cmbddfpxy.py
+++ sys/exe/run/cmbddfpxy.py
@@ -86,7 +86,10 @@ def process_data():
 
         combined_df['outqty'] = positions_df['key'].map(holdings_df.set_index('key')['used_quantity'])
         combined_df['in'] = positions_df['key'].map(holdings_df.set_index('key')['average_price'])
-        combined_df['out'] = positions_df['key'].map(positions_df.set_index('key')['buy_price'])  # Mapping sell price with key
+        if positions_df.empty:
+            combined_df['in'] = combined_df['out']
+        else:
+            combined_df['out'] = positions_df['key'].map(positions_df.set_index('key')['buy_price'])
         combined_df.loc[combined_df['outqty'] == 0, ['outqty', 'in', 'out']] = 0
         combined_df['booked'] = ( (combined_df['outqty'] * combined_df['out']) -  (combined_df['outqty'] * combined_df['in']))
      
