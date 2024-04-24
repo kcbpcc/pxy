5333d188b7c16a780b0d5b5d573bb70e60acf75b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-24 23:58:42 +0800

    Update niftychartpxy.py

--- sys/exe/run/niftychartpxy.py
+++ sys/exe/run/niftychartpxy.py
@@ -30,7 +30,7 @@ for i in range(1, len(df)):
         trend_direction.append(SILVER)
 
 # Create ASCII chart with colored trend
-chart = plot(df.tolist(), {'height': 20, 'format': "{:.0f}", 'color': trend_direction})
+chart = plot(df.tolist(), {'height': 10, 'format': "{:.0f}", 'color': trend_direction})
 
 # Print ASCII chart
 print(chart)
