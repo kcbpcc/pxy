0fbcb183c31eb3e056160ee66429e87f850c26a5
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-24 13:37:27 +0800

    Update buycncpxy.py

--- sys/exe/run/buycncpxy.py
+++ sys/exe/run/buycncpxy.py
@@ -135,5 +135,5 @@ if decision == "YES":
         print(f"Remaining Cash💰: {int(round(remaining_cash/1000))}K")
 elif decision == "NO":
     # Perform actions for "NO"
-    print("\033[91mNo sufficient funds available \033[0m")
+    print(f"\033[91mNo sufficient funds available Cash💰: {int(round(available_cash/1000))}K\033[0m")
     print("-" * 42)
