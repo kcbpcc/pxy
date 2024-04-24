f140deb790f0892e6433214d681110899f447741
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-24 21:33:16 +0800

    Update buyoptpxy.py

--- sys/exe/run/buyoptpxy.py
+++ sys/exe/run/buyoptpxy.py
@@ -30,28 +30,8 @@ async def send_telegram_message(message_text):
         print(f"Error sending message to Telegram: {e}")
 
 # Define function to get this week's Thursday date
-
-from datetime import datetime, timedelta
-
-def get_this_thursday(adjest=7):
-    current_date = datetime.now()
-    days_until_this_thursday = (3 - current_date.weekday() + 7) % 7
-    if days_until_this_thursday == 0:
-        # If today is Thursday, move to the next Thursday
-        days_until_this_thursday = 7
-    this_thursday = current_date + timedelta(days=days_until_this_thursday)
-    last_day_of_month = (this_thursday.replace(day=28) + timedelta(days=4)).replace(day=1) - timedelta(days=1)
-    if this_thursday.month != (this_thursday + timedelta(days=7)).month:
-        if this_thursday.day > last_day_of_month.day - 7:
-            expiry_year = this_thursday.strftime("%y")
-            expiry_month = this_thursday.strftime("%b").upper()  # Convert to all caps
-            expiry_day = ''  # Empty day
-            return expiry_year, expiry_month, expiry_day
-    expiry_year = this_thursday.strftime("%y")
-    expiry_month = this_thursday.strftime("%b").upper()  # Convert to all caps
-    expiry_day_adjust = timedelta(days=adjest)  # Adjustment of days
-    expiry_day = (this_thursday - expiry_day_adjust).strftime("%d").zfill(2)
-    return expiry_year, expiry_month, expiry_day
+from thudaypxy import get_this_thursday
+expiry_year, expiry_month, expiry_day = get_this_thursday()
     
 def construct_symbol(expiry_year, expiry_month, expiry_day, option_type):
     # Convert expiry_month to a single digit string if it's less than or equal to 9
