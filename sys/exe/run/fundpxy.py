7df4dd1e8d4a05314701f247ca8a51e6d0c7f3d1
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-24 13:13:09 +0800

    Update fundpxy.py

--- sys/exe/run/fundpxy.py
+++ sys/exe/run/fundpxy.py
@@ -37,8 +37,7 @@ def calculate_decision():
         # Use the 'margins' method to get margin data without specifying a segment
         try:
             response = broker.kite.margins()
-            print(response.keys())
-            available_cash = response["delivery"]
+            available_cash = response['equity']['delivery']
         except Exception as e:
             print(f"An error occurred: {e}")
             available_cash = 0
