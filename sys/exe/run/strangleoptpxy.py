c363b0538b7156964613cb2e1379874448282f35
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-24 21:35:45 +0800

    Update strangleoptpxy.py

--- sys/exe/run/strangleoptpxy.py
+++ sys/exe/run/strangleoptpxy.py
@@ -13,6 +13,8 @@ from smapxy import check_index_status
 nsma = check_index_status('^NSEI')
 from mktpxy import get_market_check
 onemincandlesequance, mktpxy = get_market_check('^NSEI')
+from thudaypxy import get_this_thursday
+expiry_year, expiry_month, expiry_day = get_this_thursday()
 
 async def send_telegram_message(message_text):
     try:
@@ -27,24 +29,6 @@ async def send_telegram_message(message_text):
         # Handle the exception (e.g., log it) and continue with your code
         print(f"Error sending message to Telegram: {e}")
 
-# Define function to get this week's Thursday date
-from datetime import datetime, timedelta
-def get_this_thursday():
-    current_date = datetime.now()
-    days_until_this_thursday = (3 - current_date.weekday() + 7) % 7
-    if days_until_this_thursday == 0:
-        return current_date.strftime("%y"), current_date.strftime("%m"), current_date.strftime("%d").zfill(2)
-    this_thursday = current_date + timedelta(days=days_until_this_thursday)
-    last_day_of_month = (this_thursday.replace(day=28) + timedelta(days=4)).replace(day=1) - timedelta(days=1)
-    if this_thursday.month != (this_thursday + timedelta(days=7)).month:
-        if this_thursday.day > last_day_of_month.day - 7:
-            return this_thursday.strftime("%y"), this_thursday.strftime("%m"), this_thursday.strftime("%d").zfill(2)
-    expiry_year = this_thursday.strftime("%y")
-    month_number = int(this_thursday.strftime("%m"))
-    expiry_month = str(month_number) if month_number <= 9 else this_thursday.strftime("%m")
-    expiry_day_adjest = timedelta(days=adjest)  # Example adjustment of 7 days
-    expiry_day = (this_thursday - expiry_day_adjest).strftime("%d").zfill(2)
-    return expiry_year, expiry_month, expiry_day
 
 def construct_symbol(expiry_year, expiry_month, expiry_day, option_type, strike):
     if len(expiry_month) == 2 and expiry_month.startswith("0"):
