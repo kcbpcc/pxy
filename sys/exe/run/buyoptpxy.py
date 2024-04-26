19ece9fcddf85392a7bfe2cadffa7ae065772c95
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-27 04:53:26 +0800

    Update buyoptpxy.py

--- sys/exe/run/buyoptpxy.py
+++ sys/exe/run/buyoptpxy.py
@@ -1,5 +1,3 @@
-#buyoptpxy.py
-adjest = 0
 import traceback
 import sys
 import logging
@@ -8,65 +6,43 @@ import asyncio
 from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
 from strikpxy import get_prices
-_, CE_Strike, PE_Strike, _ = get_prices()
 from macdpxy import calculate_macd_signal
 from smapxy import check_index_status
-nsma = check_index_status('^NSEI')
 from mktpxy import get_market_check
-onemincandlesequance, mktpxy = get_market_check('^NSEI')
 from datetime import datetime, timedelta
-from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
-print("━" * 42)
+from clorpxy import BRIGHT_YELLOW, RESET
+
+_, CE_Strike, PE_Strike, _ = get_prices()
+nsma = check_index_status('^NSEI')
+onemincandlesequance, mktpxy = get_market_check('^NSEI')
+
 async def send_telegram_message(message_text):
     try:
-        # Define the bot token and your Telegram username or ID
-        bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'  # Replace with your actual bot token
-        user_usernames = '-4128494197'  # Replace with your Telegram username or ID
-        # Create a Telegram bot
+        bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'
+        user_usernames = '-4128494197'
         bot = telegram.Bot(token=bot_token)
-        # Send the message to Telegram
         await bot.send_message(chat_id=user_usernames, text=message_text)
     except Exception as e:
-        # Handle the exception (e.g., log it) and continue with your code
         print(f"Error sending message to Telegram: {e}")
 
-# Define function to get this week's Thursday date
+def last_thursday_of_month(year, month):
+    last_day_of_month = datetime(year, month, 1) + timedelta(days=32)
+    last_day_of_month = last_day_of_month.replace(day=1) - timedelta(days=1)
+    last_thursday = last_day_of_month - timedelta(days=(last_day_of_month.weekday() + 1) % 7)
+    return last_thursday
 
-from datetime import datetime, timedelta
-
-from datetime import datetime, timedelta
-
-def get_this_thursday(adjest=7):
+def get_expiry_date():
     current_date = datetime.now()
-    days_until_this_thursday = (3 - current_date.weekday() + 7) % 7
-    if days_until_this_thursday == 0:
-        expiry_year = current_date.strftime("%y")
-        expiry_month = current_date.strftime("%b").upper()  # Convert to all caps
-        expiry_day = current_date.strftime("%d").zfill(2)
-        return expiry_year, expiry_month, expiry_day
-    this_thursday = current_date + timedelta(days=days_until_this_thursday)
-    last_day_of_month = (this_thursday.replace(day=28) + timedelta(days=4)).replace(day=1) - timedelta(days=1)
-    if this_thursday.month != (this_thursday + timedelta(days=7)).month:
-        if this_thursday.day > last_day_of_month.day - 7:
-            expiry_year = this_thursday.strftime("%y")
-            expiry_month = this_thursday.strftime("%b").upper()  # Convert to all caps
-            expiry_day = ''  # Empty day
-            return expiry_year, expiry_month, expiry_day
-    expiry_year = this_thursday.strftime("%y")
-    expiry_month = this_thursday.strftime("%b").upper()  # Convert to all caps
-    expiry_day_adjust = timedelta(days=adjest)  # Adjustment of days
-    expiry_day = (this_thursday - expiry_day_adjust).strftime("%d").zfill(2)
-    return expiry_year, expiry_month, expiry_day
+    next_month = current_date.month + 1 if current_date.month < 12 else 1
+    next_year = current_date.year if current_date.month < 12 else current_date.year + 1
+    expiry_date = last_thursday_of_month(next_year, next_month)
+    return expiry_date.year, expiry_date.strftime("%b").upper()
 
-def construct_symbol(expiry_year, expiry_month, expiry_day, option_type):
-    # Convert expiry_month to a single digit string if it's less than or equal to 9
+def construct_symbol(expiry_year, expiry_month, option_type):
     if len(expiry_month) == 2 and expiry_month.startswith("0"):
         expiry_month = expiry_month[1]
     noptions = PE_Strike if option_type == "PE" else (CE_Strike if option_type == "CE" else None)
-    if expiry_day is None:
-        return f"NIFTY{expiry_year}{expiry_month}{noptions}{option_type}"
-    else:
-        return f"NIFTY{expiry_year}{expiry_month}{expiry_day}{noptions}{option_type}"
+    return f"NIFTY{expiry_year}{expiry_month}{noptions}{option_type}"
 
 def count_positions_by_type(broker):
     positions_response = broker.kite.positions()
@@ -80,7 +56,6 @@ def count_positions_by_type(broker):
             count_PE += 1
     return count_CE, count_PE
 
-
 def check_existing_positions(broker, symbol):
     positions_response = broker.kite.positions()
     positions_net = positions_response['net']
@@ -88,7 +63,6 @@ def check_existing_positions(broker, symbol):
         if position['tradingsymbol'] == symbol and abs(position['quantity']) >= 50:
             return True
     return False
-
 
 async def place_order(broker, symbol, transaction_type, product_type, quantity, order_type, price=None):
     try:
@@ -118,41 +92,28 @@ async def place_order(broker, symbol, transaction_type, product_type, quantity, 
 
 async def main():
     try:
-        # Redirect sys.stdout to 'output.txt'
-        with open('output.txt', 'w') as file:
-            sys.stdout = file
-
-            try:
-                broker = get_kite(api="bypass", sec_dir=dir_path)
-            except Exception as e:
-                remove_token(dir_path)
-                print(traceback.format_exc())
-                logging.error(f"{str(e)} unable to get holdings")
-                sys.exit(1)
-
-    finally:
-        # Reset sys.stdout to its default value
-        sys.stdout = sys.__stdout__
+        broker = get_kite(api="bypass", sec_dir=dir_path)
+    except Exception as e:
+        remove_token(dir_path)
+        print(traceback.format_exc())
+        logging.error(f"{str(e)} unable to get holdings")
+        sys.exit(1)
     
     count_CE, count_PE = count_positions_by_type(broker)
     PE_weight = count_PE - count_CE
     CE_weight = count_CE - count_PE
     weight = abs(count_PE - count_CE)
 
-    print(f"{BRIGHT_YELLOW}🔥CE positions:{count_CE} 📈━{weight}━📉 PE positions:{count_PE}💧{RESET}".rjust(42))
-
-    expiry_year, expiry_month, expiry_day = get_this_thursday()
+    expiry_year, expiry_month = get_expiry_date()
 
     option_type = 'CE' if (mktpxy == 'Bull' and CE_weight < 1 and count_CE < 4) else ('PE' if (mktpxy == 'Sell' and PE_weight < 1 and count_PE < 4) else (print(f"Market-{mktpxy} or Unbalanced-let's wait 🔍👀".rjust(39))) or sys.exit(1))
-    symbol = construct_symbol(expiry_year, expiry_month, expiry_day, option_type)
+    symbol = construct_symbol(expiry_year, expiry_month, option_type)
 
     position_exists = check_existing_positions(broker, symbol)
     
     if not position_exists:
-        # await the coroutine
         buy_order_placed, buy_order_id = await place_order(broker, symbol, 'BUY', 'NRML', 50, 'MARKET')
         if buy_order_placed:
-            # Send Telegram message
             await send_telegram_message(f"🛫🛫🛫 👉👉👉 ENTRY order placed for {symbol} placed successfully.")
             print(f"{symbol} BUY order placed successfully.")
     else:
@@ -161,5 +122,4 @@ async def main():
 async def run_main():
     await main()
 
-# Run the asynchronous function using asyncio.run()
 asyncio.run(run_main())
