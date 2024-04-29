8cc86eacc67ef4987432ad872c5667c4657168e9
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-29 19:08:13 +0530

    Update buyoptpxy.py

--- sys/exe/run/buyoptpxy.py
+++ sys/exe/run/buyoptpxy.py
@@ -54,32 +54,6 @@ def check_existing_positions(broker, symbol):
             return True
     return False
 
-async def place_order(broker, symbol, transaction_type, product_type, quantity, order_type, price=None):
-    try:
-        if price is None:
-            order_id = broker.order_place(
-                tradingsymbol=symbol,
-                quantity=quantity,
-                exchange="NFO",
-                transaction_type=transaction_type,
-                order_type=order_type,
-                product=product_type
-            )
-        else:
-            order_id = broker.order_place(
-                tradingsymbol=symbol,
-                quantity=quantity,
-                exchange="NFO",
-                transaction_type=transaction_type,
-                order_type=order_type,
-                product=product_type,
-                price=price
-            )
-        return True, order_id
-    except Exception as e:
-        print(f"Error placing order for {symbol}: {e}")
-        return False, None
-
 async def main():
     try:
         # Redirect sys.stdout to 'output.txt'
