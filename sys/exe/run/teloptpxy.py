11c2e2e7e7d737ccf82560973625ed9eba6936e4
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-30 09:23:17 +0530

    Update teloptpxy.py

--- sys/exe/run/teloptpxy.py
+++ sys/exe/run/teloptpxy.py
@@ -1,13 +1,18 @@
 import telegram
-async def send_telegram_message(message_text):
+bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'
+user_usernames = ('-4128494197',)
+def send_telegram_message(message):
     try:
-        # Define the bot token and your Telegram username or ID
-        bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'  # Replace with your actual bot token
-        user_usernames = ('-4128494197',)  # Replace with your Telegram username or ID
-        # Create a Telegram bot
-        bot = telegram.Bot(token=bot_token)
-        # Send the message to Telegram
-        await bot.send_message(chat_id=user_usernames, text=message_text)
+        for username in user_usernames:
+            url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+            payload = {
+                'chat_id': username,
+                'text': message
+            }
+            response = requests.post(url, data=payload)
+            if response.status_code != 200:
+                print(f"Failed to send Telegram message. Status code: {response.status_code}")
+            else:
+                print("Telegram message sent successfully.")
     except Exception as e:
-        # Handle the exception (e.g., log it) and continue with your code
-        print(f"Error sending message to Telegram: {e}")
+        print(f"Error sending Telegram message: {e}")
