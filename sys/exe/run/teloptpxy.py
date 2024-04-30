ecb33a9204bfb2bef55efaed577e5413d9e8cb9b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-04-30 10:50:23 +0530

    Update teloptpxy.py

--- sys/exe/run/teloptpxy.py
+++ sys/exe/run/teloptpxy.py
@@ -1,7 +1,7 @@
 import telegram
 bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'
 user_usernames = ('-4128494197',)
-def send_telegram_message(message):
+def async send_telegram_message(message):
     try:
         for username in user_usernames:
             url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
