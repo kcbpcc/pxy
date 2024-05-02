242a81eb75e31efc718cec12db7385388759c3db
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-02 15:21:35 +0530

    Update utcpxy.py

--- sys/exe/run/utcpxy.py
+++ sys/exe/run/utcpxy.py
@@ -18,10 +18,10 @@ def peak_time():
     #print("Peak Time 1 Range:", peak_time_1_start, "-", peak_time_1_end)
 
     peak_time_2_start = datetime.strptime("03:53", "%H:%M").time()
-    peak_time_2_end = datetime.strptime("09:54", "%H:%M").time()
+    peak_time_2_end = datetime.strptime("09:51", "%H:%M").time()
     #print("Peak Time 2 Range:", peak_time_2_start, "-", peak_time_2_end)
 
-    peak_time_3_start = datetime.strptime("09:55", "%H:%M").time()
+    peak_time_3_start = datetime.strptime("09:51", "%H:%M").time()
     peak_time_3_end = datetime.strptime("10:01", "%H:%M").time()
     #print("Peak Time 3 Range:", peak_time_3_start, "-", peak_time_3_end)
 
