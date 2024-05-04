88bb612f7150f326c982bfcc488eaba701584123
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-05 00:33:37 +0530

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -133,6 +133,9 @@ summary_statement += f"{color_code}{summary_sentence}{RESET}"
 # Calculate P&L percentage within each group
 print_df['PnL_percentage'] = print_df['PnL'] / print_df['Invested']
 
+# Reset index before grouping
+print_df.reset_index(drop=True, inplace=True)
+
 grouped_df = print_df.groupby('strike')
 
 # Sort grouped_df by P&L percentage
