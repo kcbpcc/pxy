1191c0c3bb5a0bc92b22b337dc18f9d9ed35c8e6
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-05 08:23:07 +0530

    Update rsnprntpxy.py

--- sys/exe/run/rsnprntpxy.py
+++ sys/exe/run/rsnprntpxy.py
@@ -16,7 +16,7 @@ async def process_orders(broker, available_cash, CE_position_exists, PE_position
             reason = f"{'Y' if CE_position_exists else 'N'}|not Buy |" if not CE_position_exists else ""
             reason += "Have 3" if count_CE >= 3 else ""
             if reason:
-                print(f"{CE_symbol}: {reason: >{40 - len(CE_symbol)}}")
+                print(f"{CE_symbol}: {reason: >{39 - len(CE_symbol)}}")
                 print("━" * 42)
 
         if not PE_position_exists and mktpxy == 'Sell' and count_PE < 3:
@@ -35,7 +35,7 @@ async def process_orders(broker, available_cash, CE_position_exists, PE_position
             reason = f"{'Y' if PE_position_exists else 'N'}|not Sell|" if not PE_position_exists else ""
             reason += "Have 3" if count_PE >= 3 else ""
             if reason:
-                print(f"{PE_symbol}: {reason: >{40 - len(PE_symbol)}}")
+                print(f"{PE_symbol}: {reason: >{39 - len(PE_symbol)}}")
                 print("━" * 42)
     else:
         print(f"\033[91mNo sufficient funds available Cash💰: {int(round(available_cash/1000))}K\033[0m")
