53498c20fbcad9720d7f8ef745b5388ce040d21c
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-07 10:13:21 +0530

    Update niftychartpxy.py

--- sys/exe/run/niftychartpxy.py
+++ sys/exe/run/niftychartpxy.py
@@ -11,7 +11,7 @@ ticker_symbol = "^NSEI"
 nifty_data = yf.Ticker(ticker_symbol)
 
 # Fetch historical data for the last 5 periods with 1-hour interval
-nifty_hist_1h = nifty_data.history(period="5d", interval="1h")[-5:]
+nifty_hist_1h = nifty_data.history(period="5d", interval="1h")[-8:]
 
 # Fetch historical data for the last 25 periods with 1-minute, 5-minute, 15-minute, and 30-minute intervals
 nifty_hist_1m = nifty_data.history(period="5d", interval="1m")[-5:]
@@ -20,7 +20,7 @@ nifty_hist_15m = nifty_data.history(period="5d", interval="15m")[-5:]
 nifty_hist_30m = nifty_data.history(period="5d", interval="30m")[-5:]
 
 # Fetch historical data for the last 5 periods with 1-day interval
-nifty_hist_1d = nifty_data.history(period="5d", interval="1d")[-5:]
+nifty_hist_1d = nifty_data.history(period="5d", interval="1d")[-3:]
 
 # Combine all data to get the last 30 data points
 nifty_hist = pd.concat([nifty_hist_1d, nifty_hist_1h, nifty_hist_1m, nifty_hist_5m, nifty_hist_15m, nifty_hist_30m])
