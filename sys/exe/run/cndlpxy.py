0d14b988f28ff48c36be9ae1fbfe5723b97b433e
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-08 06:29:39 +0530

    Update cndlpxy.py

--- sys/exe/run/cndlpxy.py
+++ sys/exe/run/cndlpxy.py
@@ -1,29 +1,13 @@
 import warnings
-import importlib
-import subprocess
-import time
-import warnings
-from rich.console import Console
-console = Console()
-warnings.simplefilter(action='ignore', category=FutureWarning)
 import yfinance as yf
 import pandas as pd
-import colorama
-from colorama import Fore, Style  # Add Style to the imports
+from rich.console import Console
+from colorama import Fore, Style
 from mktpxy import get_market_check
-from nftpxy import get_nse_action
-nse_action, nse_power, Day_Change, Open_Change  = get_nse_action()
 from utcpxy import peak_time
 from macdpxy import calculate_macd_signal
 from smapxy import check_index_status
-import subprocess
-import sys
-onemincandlesequance, mktpxy = get_market_check("^NSEI")
-peak = peak_time()
-macd = calculate_macd_signal("^NSEI")
-nsma = check_index_status("^NSEI")
 from depthpxy import calculate_consecutive_candles
-cedepth, pedepth = calculate_consecutive_candles("^NSEI")
 colorama.init(autoreset=True)
 OHLC_COLUMNS = ['Open', 'High', 'Low', 'Close']
 def get_nifty50_data(period="7d"):
