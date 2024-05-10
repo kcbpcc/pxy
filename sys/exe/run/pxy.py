2c682739665a0fe5dd342136ed8f9c0676147e82
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-10 10:04:27 +0530

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -60,7 +60,7 @@ while True:
     subprocess.run(['python3', 'niftychartpxy.py'])
     subprocess.run(['python3', 'daypxy.py'])  
     subprocess.run(['python3', 'cndlpxy.py'])
-    subprocess.run(['python3', 'entrycncpxy.py']) if (mktpxy == "Buy" and peak == 'NONPEAK' and nse_power < 0.35) or peak == 'PEAKEND' else None
+    subprocess.run(['python3', 'buycncpxy.py']) if (mktpxy == "Buy" and peak == 'NONPEAK' and nse_power < 0.35) or peak == 'PEAKEND' else None
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
     subprocess.run(['python3', 'cntrlcncpxy.py'])
     print("━" * 42)
