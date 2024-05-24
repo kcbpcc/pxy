bd9c8a19d025718055d3ecac0abf7dc783ea6a92
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-24 12:53:46 +0800

    Update zrunpxy.py

--- sys/exe/run/zrunpxy.py
+++ sys/exe/run/zrunpxy.py
@@ -8,8 +8,8 @@ import logging
 logging = Logger(30, dir_path + "main.log")
 
 try:
-    # Replace 'your_api_key' with your actual API key
-    broker = get_kite(api_key="avku59f296gcvrv0", access_token="CTHBT6DLB6AEYZBQRN2KHABXB4GFPLCW", sec_dir=dir_path)
+    # Read credentials from YAML file
+    broker = get_kite(sec_dir=dir_path)
     
     # Retrieve holdings
     holdings = broker.holdings()
