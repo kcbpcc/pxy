892357a2c5f8fb158fcb218bb8f39d386ea3b074
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-24 12:52:50 +0800

    Update zrunpxy.py

--- sys/exe/run/zrunpxy.py
+++ sys/exe/run/zrunpxy.py
@@ -1,4 +1,3 @@
-import sys
 import traceback
 from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
@@ -9,24 +8,21 @@ import logging
 logging = Logger(30, dir_path + "main.log")
 
 try:
-    sys.stdout = open('output.txt', 'w')
     # Replace 'your_api_key' with your actual API key
     broker = get_kite(api_key="avku59f296gcvrv0", access_token="CTHBT6DLB6AEYZBQRN2KHABXB4GFPLCW", sec_dir=dir_path)
     
     # Retrieve holdings
     holdings = broker.holdings()
     
-    # Print holdings
-    print("Holdings:")
-    for holding in holdings:
-        print(f"Instrument: {holding['tradingsymbol']}, Quantity: {holding['quantity']}, Average Price: {holding['average_price']}")
+    if holdings:
+        # Print holdings
+        print("Holdings:")
+        for holding in holdings:
+            print(f"Instrument: {holding['tradingsymbol']}, Quantity: {holding['quantity']}, Average Price: {holding['average_price']}")
+    else:
+        print("No holdings found.")
 except Exception as e:
     remove_token(dir_path)
     print(traceback.format_exc())
     logging.error(f"{str(e)} unable to get holdings")
     sys.exit(1)
-finally:
-    # Ensure to close the file and restore stdout
-    if sys.stdout != sys.__stdout__:
-        sys.stdout.close()
-        sys.stdout = sys.__stdout__
