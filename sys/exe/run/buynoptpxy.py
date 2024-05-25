f27550db26c6a263d3940fdc10ac3d567995c0f2
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-26 02:33:18 +0800

    Update buynoptpxy.py

--- sys/exe/run/buynoptpxy.py
+++ sys/exe/run/buynoptpxy.py
@@ -64,7 +64,7 @@ async def main():
             sys.stdout = file
 
             try:
-                broker = get_kite(api="bypass", sec_dir=dir_path)
+                broker = get_kite()
             except Exception as e:
                 remove_token(dir_path)
                 print(traceback.format_exc())
