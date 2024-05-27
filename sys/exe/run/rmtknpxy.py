35b658bd1c67ec0b117b3d663c499871daa9bc97
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-05-28 00:41:01 +0800

    Update rmtknpxy.py

--- sys/exe/run/rmtknpxy.py
+++ sys/exe/run/rmtknpxy.py
@@ -10,9 +10,9 @@ def remove_contents(directory):
         if os.path.exists(full_path):
             # Remove all contents of the directory
             shutil.rmtree(full_path)
-            print("Let's clean up👉🏻🗑️ and prepare to switch🔛")
+            print("Let's clean up📇and prepare to switch♻️")
         else:
-            print("Let's clean up👉🏻🗑️ and prepare to switch🔛")
+            print("Let's clean up📇and prepare to switch♻️")
     except Exception as e:
         print(f"Error occurred: {e}")
 
