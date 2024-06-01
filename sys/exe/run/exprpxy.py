37dcc6a258b61633c0c56b9c2aadf6f9056f7be6
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-01 17:26:48 +0800

    Update exprpxy.py

--- sys/exe/run/exprpxy.py
+++ sys/exe/run/exprpxy.py
@@ -4,17 +4,16 @@ def month_expiry_date():
     # Get the current date
     current_date = datetime.now()
 
-    # Add 9 days to the current date
-    expiry_date_offset = current_date + timedelta(days=9)
+    # Add 7 days to the current date
+    expiry_date_offset = current_date + timedelta(days=7)
 
     # Format the month as abbreviated name (e.g., APR, MAY, JUN)
     expiry_month = expiry_date_offset.strftime("%b").upper()
 
     # Format the year as 2-digit number
     expiry_year = expiry_date_offset.strftime("%y")
-
-    # Format the day
-    expiry_day = expiry_date_offset.strftime("%d")
+    
+    # Set expiry_day to None
+    expiry_day = None
 
     return expiry_year, expiry_month, expiry_day
-
