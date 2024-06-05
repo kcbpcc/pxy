5e4646d4fdc693dbfa725a7ebf41ed75891085c3
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-05 09:07:04 +0800

    Update rsnprntpxy.py

--- sys/exe/run/rsnprntpxy.py
+++ sys/exe/run/rsnprntpxy.py
@@ -1,7 +1,7 @@
 
 async def process_orders(broker, available_cash, CE_position_exists, PE_position_exists, CE_symbol, PE_symbol, count_CE, count_PE, mktpxy):
     from ordoptpxy import place_order
-    from teloptpxy import send_telegram_message
+    from telinoptpxy import send_telegram_message
     if available_cash > 10000:
         if not CE_position_exists and mktpxy == 'Buy' and count_CE < 3:
             if CE_symbol.startswith('BANKNIFTY'):
