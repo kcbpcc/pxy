78ab794a63036798f87df782fa136edefbb93914
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-05 08:29:27 +0800

    Create iteloptpxy.py

---
+++ sys/exe/run/iteloptpxy.py
@@ -0,0 +1,16 @@
+import telegram
+async def send_telegram_message(message_text):
+    try:
+        # Define the bot token and your Telegram username or ID
+        bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'  # Replace with your actual bot token
+        user_usernames = '-4128494197'  # Replace with your Telegram username or ID
+
+        # Create a Telegram bot
+        bot = telegram.Bot(token=bot_token)
+
+        # Send the message to Telegram
+        await bot.send_message(chat_id=user_usernames, text=message_text)
+
+    except Exception as e:
+        # Handle the exception (e.g., log it) and continue with your code
+        print(f"Error sending message to Telegram: {e}")
