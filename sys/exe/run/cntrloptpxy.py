06b8570a8bed4c1f97a52de3e7590460a3bb0941
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-20 17:07:54 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -131,7 +131,7 @@ summary_sentence = f"{color_code_summary}SUMMARY: CAP:{total_invested_all} P&L:{
 summary_statement = summary_sentence
 subprocess.run(['python3', 'bcndlpxy.py'])
 subprocess.run(['python3', 'bniftychartpxy.py'])
-bsma = check_index_status('^NSEI')
+bsma = check_index_status('^NSEBANK')
 print((GREEN if bsma == "up" else RED if bsma == "down" else YELLOW) + "ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩ" + RESET)
 grouped_df = print_df.groupby('strike')
 for group, data in grouped_df:
