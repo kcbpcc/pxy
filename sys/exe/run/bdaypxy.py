6e599c0cb6a0e80adfeadc5646b36fd7bff5f1fa
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-25 06:36:25 +0800

    Update bdaypxy.py

--- sys/exe/run/bdaypxy.py
+++ sys/exe/run/bdaypxy.py
@@ -10,7 +10,7 @@ from utcpxy import peak_time
 from macdpxy import calculate_macd_signal
 from smapxy import check_index_status
 from depthpxy import calculate_consecutive_candles
-from nftpxy import get_nse_action
+from bftpxy import get_nse_action
 ha_nse_action, nse_power, Day_Change, Open_Change  = get_nse_action()
 
 console = Console()
