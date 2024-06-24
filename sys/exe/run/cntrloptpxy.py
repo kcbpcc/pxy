fa3566bd012e8c3ff7f5f2937255613108be26cf
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-25 07:31:06 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -143,8 +143,7 @@ total_pl_percentage_all = (total_pl_all / total_invested_all) * 100 if total_inv
 color_code_summary = BRIGHT_GREEN if total_pl_percentage_all > 0 else BRIGHT_RED
 summary_sentence = f"{color_code_summary}SUMMARY: CAP:{total_invested_all} P&L:{total_pl_all:5.0f} P&L%:{total_pl_percentage_all:3.0f}%{RESET}"
 summary_statement = summary_sentence
-bsma = check_index_status('^NSEBANK')
-print((GREEN if bsma == "up" else RED if bsma == "down" else YELLOW) + "ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩ" + RESET)
+
 grouped_df = print_df.groupby('strike')
 for group, data in grouped_df:
     total_invested_group = data['Invested'].sum()
