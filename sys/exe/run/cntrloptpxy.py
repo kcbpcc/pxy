714651098c7ec6c4fb09044c521762cd67b02ace
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-25 08:51:59 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -138,7 +138,7 @@ summary_statement = ""
 total_invested_all = print_df['Invested'].sum()
 total_pl_all = print_df['PnL'].sum()
 total_pl_percentage_all = (total_pl_all / total_invested_all) * 100 if total_invested_all != 0 else 0
-color_code_summary = BRIGHT_YELLOW
+color_code_summary = BRIGHT_GREEN if total_pl_percentage_all > 0 else BRIGHT_RED
 summary_sentence = f"{color_code_summary}SUMMARY: CAP:{total_invested_all} P&L:{total_pl_all:5.0f} P&L%:{total_pl_percentage_all:3.0f}%{RESET}"
 summary_statement = summary_sentence
 
@@ -156,4 +156,3 @@ for group, data in grouped_df:
             print(f"{group} {color_code}{summary_sentence}{RESET}")  # No need for .rjust here
 print("━" * 42)
 print(summary_statement +"📊" )
-
