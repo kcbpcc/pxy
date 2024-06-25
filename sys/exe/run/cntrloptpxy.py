3ccc42e703e91d30a2db6850a4a9fd2600a97b15
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-25 08:27:59 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -13,17 +13,6 @@ from timetgtpxy import timetgt
 from nftpxy import ha_nse_action, nse_power, Day_Change, Open_Change
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 from smapxy import check_index_status
-bsma = check_index_status('^NSEBANK')
-
-import sys
-import traceback
-import pandas as pd
-import requests
-import numpy as np
-from login_get_kite import get_kite, remove_token
-from cmbddfpxy import process_data  # Importing process_data function from cmbddfpxy module
-from smapxy import check_index_status
-from clorpxy import BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, RESET
 
 bot_token = '6867988078:AAGNBJqs4Rf8MR4xPGoL1-PqDOYouPan7b0'
 user_usernames = ('-4136531362',)
@@ -37,12 +26,14 @@ def send_telegram_message(message):
                 'text': message
             }
             response = requests.post(url, data=payload)
-            response.raise_for_status()  # Raise exception for non-200 status codes
-            print("Telegram message sent successfully.")
-    except requests.exceptions.RequestException as e:
-        print(f"Failed to send Telegram message: {e}")
+            if response.status_code != 200:
+                print(f"Failed to send Telegram message. Status code: {response.status_code}")
+            else:
+                print("Telegram message sent successfully.")
+    except Exception as e:
+        print(f"Error sending Telegram message: {e}")
 
-def place_order(tradingsymbol, quantity, transaction_type, order_type, product, broker):
+def place_order(tradingsymbol, quantity, transaction_type, order_type, product):
     try:
         order_id = broker.order_place(
             tradingsymbol=tradingsymbol,
@@ -57,34 +48,27 @@ def place_order(tradingsymbol, quantity, transaction_type, order_type, product, 
     except Exception as e:
         print(f"Error placing order: {e}")
         return None
-def determine_target(bsma, key):
-    if (bsma == "up" and "CE" in key) or (bsma == "down" and "PE" in key):
-        return 10
-    else:
-        return 5
-def exit_options(exe_opt_df_grouped, bsma, broker):
-    try:
-        for group_name, group_data in exe_opt_df_grouped:
-            # Calculate target for each row in the group
-            group_data['target'] = group_data.apply(
-                lambda row: determine_target(bsma, row['key']),
-                axis=1
-            )
-            
-            for index, row in group_data.iterrows():
-                if row['PL%'] > row['target']:
-                    order_id = place_order(row['key'], row['qty'], 'SELL', 'MARKET', 'NRML', broker)
-                    
-                    if order_id:
-                        message = f"🛬🛬🛬 👈👈👈 EXIT order placed for option {row['key']} successfully.\nPL: {row['PnL']}, PL%: {row['PL%']}%"
-                        print(message)
-                        send_telegram_message(message)
-                    else:
-                        print(f"Failed to place order for option {row['key']}.")
 
+def exit_options(exe_opt_df):
+    try:
+        for strike_price, data in exe_opt_df:
+            total_invested_group = data['Invested'].sum()
+            total_pl_group = data['PnL'].sum()
+            total_pl_percentage_group = (total_pl_group / total_invested_group) * 100 if total_invested_group != 0 else 0
+            
+            #print(f"Strike Price: {strike_price}")
+            #print(data)
+            
+            if total_pl_percentage_group > 10:
+                for index, row in data.iterrows():
+                    place_order(row['key'], row['qty'], 'SELL', 'MARKET', 'NRML')
+                    
+                message = f"🛬🛬🛬 👈👈👈 EXIT order placed for all options with strike price {strike_price} successfully.\nPL: {total_pl_group}, PL%: {total_pl_percentage_group}%"
+                print(message)
+                send_telegram_message(message)
+                
     except Exception as e:
         print(f"Error placing exit order: {e}")
-
 
 try:
     sys.stdout = open('output.txt', 'w')
@@ -99,75 +83,71 @@ finally:
         sys.stdout.close()
         sys.stdout = sys.__stdout__
 
-# Process data and handle exceptions
-try:
-    combined_df = process_data()
-except Exception as e:
-    print(f"Error processing data: {e}")
-    traceback.print_exc()
-    sys.exit(1)
+import pandas as pd
+from cmbddfpxy import process_data
 
-# Calculate and manipulate dataframes
-try:
-    exe_opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
-    exe_opt_df['key'] = exe_opt_df['key'].str.replace('NFO:', '') 
-    exe_opt_df['PL%'] = (exe_opt_df['PnL'] / exe_opt_df['Invested']) * 100
-    exe_opt_df['PL%'] = exe_opt_df['PL%'].fillna(0)
-    exe_opt_df['target'] = exe_opt_df.apply(
-        lambda row: 10 if ((bsma == 'up' and 'CE' in row['key']) or (bsma == 'down' and 'PE' in row['key'])) else 5,
-        axis=1
-    )
-    exe_opt_df['strike'] = exe_opt_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
+combined_df = process_data()
+exe_opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
+exe_opt_df['key'] = exe_opt_df['key'].str.replace('NFO:', '') 
+exe_opt_df['PL%'] = (exe_opt_df['PnL'] / exe_opt_df['Invested']) * 100
+exe_opt_df['PL%'] = exe_opt_df['PL%'].fillna(0)
 
-    group_by_column = 'tradingsymbol'
-    exe_opt_df_grouped = exe_opt_df.groupby(group_by_column)
+# Define the 'strike' column
+exe_opt_df['strike'] = exe_opt_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
 
-    # Call exit_options with exe_opt_df_grouped and broker
-    exit_options(exe_opt_df_grouped, broker)
+# Grouping by 'strike / 'tradingsymbol' column dynamic
+from utcpxy import peak_time
+peak = peak_time()
+group_by_column = 'tradingsymbol' if peak == 'NONPEAK' else 'strike'
+exe_opt_df = exe_opt_df.groupby(group_by_column)
 
-    opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
-    opt_df['key'] = opt_df['key'].str.replace('NFO:', '') 
-    opt_df['PL%'] = (opt_df['PnL'] / opt_df['Invested']) * 100
-    opt_df['PL%'] = opt_df['PL%'].fillna(0)
-    opt_df['PL%'] = opt_df['PL%'].astype(int) 
-    opt_df['m2m'] = opt_df['m2m'].astype(int)
-    opt_df = opt_df[['key', 'Invested', 'qty', 'PL%', 'PnL','pnl','product','m2m']]
+# Call exit_options with exe_opt_df
+exit_options(exe_opt_df)
 
-    total_invested = opt_df['Invested'].sum()
-    total_pl = opt_df['PnL'].sum()
-    total_opt_m2m = opt_df['m2m'].sum()
-    total_pl_percentage = (total_pl / total_invested) * 100 if total_invested != 0 else 0
+opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
+opt_df['key'] = opt_df['key'].str.replace('NFO:', '') 
+opt_df['PL%'] = (opt_df['PnL'] / opt_df['Invested']) * 100
+opt_df['PL%'] = opt_df['PL%'].fillna(0)
+opt_df['PL%'] = opt_df['PL%'].astype(int) 
+opt_df['m2m'] = opt_df['m2m'].astype(int)
+opt_df = opt_df[['key', 'Invested', 'qty', 'PL%', 'PnL','pnl','product','m2m']]
 
-    # Grouping by 'strike' column
-    print_df = opt_df.copy()
-    print_df['CP'] = opt_df['key'].apply(lambda x: '🟥' if x.endswith('PE') else ('🟩' if x.endswith('CE') else None))
-    print_df['key'] = print_df['key'].str.replace('BANKNIFTY24', 'B').str.replace('NIFTY24', 'N')
-    print_df['strike'] = print_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
-    print_df['MN'] = np.where(print_df['product'] == 'MIS', '⌛', '🔢')
-    print_df = print_df[['MN','strike','Invested', 'qty', 'PL%', 'PnL','CP']]
-    summary_statement = ""
-    total_invested_all = print_df['Invested'].sum()
-    total_pl_all = print_df['PnL'].sum()
-    total_pl_percentage_all = (total_pl_all / total_invested_all) * 100 if total_invested_all != 0 else 0
-    color_code_summary = BRIGHT_YELLOW
-    summary_sentence = f"{color_code_summary}SUMMARY: CAP:{total_invested_all} P&L:{total_pl_all:5.0f} P&L%:{total_pl_percentage_all:3.0f}%{RESET}"
-    summary_statement = summary_sentence
+total_invested = opt_df['Invested'].sum()
+total_pl = opt_df['PnL'].sum()
+total_opt_m2m = opt_df['m2m'].sum()
+total_pl_percentage = (total_pl / total_invested) * 100 if total_invested != 0 else 0
 
-    grouped_df = print_df.groupby('strike')
-    for group, data in grouped_df:
-        total_invested_group = data['Invested'].sum()
-        total_pl_group = data['PnL'].sum()
-        total_pl_percentage_group = (total_pl_group / total_invested_group) * 100 if total_invested_group != 0 else 0
-        if total_invested_group != 0:  # Check if capital is not zero
-            summary_sentence = f"CAP:{total_invested_group} P&L:{total_pl_group:6.0f} P&L%:{total_pl_percentage_group:3.0f}%"
-            color_code = BRIGHT_GREEN if total_pl_percentage_group > 0 else BRIGHT_RED
-            print(data[data['qty'] > 0][['MN', 'strike', 'Invested', 'qty', 'PL%', 'PnL', 'CP']].to_string(header=False, index=False, col_space=[2, 11, 5, 3, 3, 6, 4]))
-            if len(data) >= 2:  # Check if group has two or more entries
-                print(f"{group} {color_code}{summary_sentence}{RESET}")  # No need for .rjust here
-    print("━" * 42)
-    print(summary_statement +"📊" )
+# Grouping by 'strike' column
+print_df = opt_df.copy()
+print_df['CP'] = opt_df['key'].apply(lambda x: '🟥' if x.endswith('PE') else ('🟩' if x.endswith('CE') else None))
+print_df['key'] = print_df['key'].str.replace('BANKNIFTY24', 'B').str.replace('NIFTY24', 'N')
+print_df['strike'] = print_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
+print_df['MN'] = np.where(print_df['product'] == 'MIS', '⌛', '🔢')
+print_df = print_df[['MN','strike','Invested', 'qty', 'PL%', 'PnL','CP']]
 
-except Exception as e:
-    print(f"Error: {e}")
-    traceback.print_exc()
-
+summary_statement = ""
+total_invested_all = print_df['Invested'].sum()
+total_pl_all = print_df['PnL'].sum()
+total_pl_percentage_all = (total_pl_all / total_invested_all) * 100 if total_invested_all != 0 else 0
+color_code_summary = BRIGHT_GREEN if total_pl_percentage_all > 0 else BRIGHT_RED
+summary_sentence = f"{color_code_summary}SUMMARY: CAP:{total_invested_all} P&L:{total_pl_all:5.0f} P&L%:{total_pl_percentage_all:3.0f}%{RESET}"
+summary_statement = summary_sentence
+subprocess.run(['python3', 'bcndlpxy.py'])
+subprocess.run(['python3', 'bniftychartpxy.py'])
+bsma = check_index_status('^NSEBANK')
+subprocess.run(['python3', 'bdaypxy.py']) 
+print((GREEN if bsma == "up" else RED if bsma == "down" else YELLOW) + "ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩ" + RESET)
+grouped_df = print_df.groupby('strike')
+for group, data in grouped_df:
+    total_invested_group = data['Invested'].sum()
+    total_pl_group = data['PnL'].sum()
+    total_pl_percentage_group = (total_pl_group / total_invested_group) * 100 if total_invested_group != 0 else 0
+    if total_invested_group != 0:  # Check if capital is not zero
+        summary_sentence = f"CAP:{total_invested_group} P&L:{total_pl_group:6.0f} P&L%:{total_pl_percentage_group:3.0f}%"
+        color_code = BRIGHT_GREEN if total_pl_percentage_group > 0 else BRIGHT_RED
+        print(data[data['qty'] > 0][['MN', 'strike', 'Invested', 'qty', 'PL%', 'PnL', 'CP']].to_string(header=False, index=False, col_space=[2, 11, 5, 3, 3, 6, 4]))
+        if len(data) >= 2:  # Check if group has two or more entries
+            print(f"{group} {color_code}{summary_sentence}{RESET}")  # No need for .rjust here
+print("━" * 42)
+print(summary_statement +"📊" )
+subprocess.run(['python3', 'cndlpxy.py'])
