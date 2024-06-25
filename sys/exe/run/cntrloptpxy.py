4dbcba80be54121771a788ebf8ab012ebb8e1c15
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-25 08:53:17 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -138,7 +138,7 @@ summary_statement = ""
 total_invested_all = print_df['Invested'].sum()
 total_pl_all = print_df['PnL'].sum()
 total_pl_percentage_all = (total_pl_all / total_invested_all) * 100 if total_invested_all != 0 else 0
-color_code_summary = BRIGHT_GREEN if total_pl_percentage_all > 0 else BRIGHT_RED
+color_code_summary = BRIGHT_YELLOW
 summary_sentence = f"{color_code_summary}SUMMARY: CAP:{total_invested_all} P&L:{total_pl_all:5.0f} P&L%:{total_pl_percentage_all:3.0f}%{RESET}"
 summary_statement = summary_sentence
 
