e92d48863078d05d12737d55f260f728cddf9947
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-27 10:06:44 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -13,6 +13,19 @@ from sleeppxy import progress_bar
 import time
 from rich.console import Console
 from clorpxy import SILVER, UNDERLINE, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
+try:
+    original_stdout = sys.stdout
+    with open('output.txt', 'w') as file:
+        sys.stdout = file
+        try:
+            broker = get_kite()
+        except Exception as e:
+            remove_token(dir_path)
+            print(traceback.format_exc())
+            logging.error(f"{str(e)} unable to get holdings")
+            sys.exit(1)
+finally:
+    sys.stdout = original_stdout
 #subprocess.run(['python3', 'rmtknpxy.py'])
 subprocess.run(['python3', 'cpritepxy.py'])
 while True:
