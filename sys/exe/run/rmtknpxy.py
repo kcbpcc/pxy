514587c16cb5a878baa70762cbb6a30ce619e3aa
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-29 15:02:04 +0800

    Update rmtknpxy.py

--- sys/exe/run/rmtknpxy.py
+++ sys/exe/run/rmtknpxy.py
@@ -1,3 +1,4 @@
+import shutil
 import os
 
 def remove_contents(directory):
@@ -7,19 +8,11 @@ def remove_contents(directory):
         
         # Check if the directory exists
         if os.path.exists(full_path):
-            # Iterate over all the files and directories in the specified directory
-            for filename in os.listdir(full_path):
-                file_path = os.path.join(full_path, filename)
-                try:
-                    if os.path.isfile(file_path) or os.path.islink(file_path):
-                        os.unlink(file_path)  # Remove file or symbolic link
-                    elif os.path.isdir(file_path):
-                        shutil.rmtree(file_path)  # Remove directory and all its contents
-                except Exception as e:
-                    print(f"Failed to delete {file_path}. Reason: {e}")
+            # Remove all contents of the directory
+            shutil.rmtree(full_path)
             print("Let's clean up 📇 and prepare to switch ♻️")
         else:
-            print(f"The directory {full_path} does not exist.")
+            print("Let's clean up 📇 and prepare to switch ♻️")
     except Exception as e:
         print(f"Error occurred: {e}")
 
