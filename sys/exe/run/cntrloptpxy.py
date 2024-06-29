e6ceb6cb65409d3838801a5e5b026ba8ef9c8b82
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-06-29 09:26:44 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -186,7 +186,8 @@ for group, data in grouped_df:
         print(data[data['qty'] > 0][['MN', 'strike', 'Invested', 'qty', 'PL%', 'PnL', 'CP']].to_string(header=False, index=False, col_space=[2, 11, 5, 3, 3, 6, 4]))
         
         if len(data) >= 2:
-            print(f"{(group + ' ' + color_code + summary_sentence + RESET):>43}")
+            formatted_output = f"{group} {color_code}{summary_sentence}{RESET}".rjust(43)
+            print(formatted_output)
 
 print("━" * 42)
 
