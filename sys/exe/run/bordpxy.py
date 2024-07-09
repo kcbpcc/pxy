dd31dabcf632af118b8c1d4629fae4721ad06c97
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-10 05:58:47 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -34,8 +34,8 @@ def printbord(total_cnc_m2m_postions, extras, optworth, all_Stocks_worth_dpnl, n
                         right_aligned_format.format(f"Delta:{BRIGHT_GREEN if ydaypnl_to_print > 0 else BRIGHT_RED}{str(int(ydaypnl_to_print * 100000)).zfill(6)}{RESET}"))
     output_lines.append(left_aligned_format.format(f"Real-P&L:{BRIGHT_GREEN if ((acvalue_to_print - capital)  + hide) > 0 else BRIGHT_RED}{round(((acvalue_to_print - capital)  + hide) , 2)}{RESET}") +
                         right_aligned_format.format(f"Run-P&L:{BRIGHT_GREEN if (all_Stocks_capital_lacks - all_Stocks_worth_lacks)*-1 >= 0 else BRIGHT_RED}{round(((all_Stocks_capital_lacks - all_Stocks_worth_lacks)*-1), 2)}{RESET}"))
-    output_lines.append(left_aligned_format.format(f"Flush#:{BRIGHT_GREEN if green_Stocks_capital_percentage > 0 else BRIGHT_RED}{round(green_Stocks_count)}{RESET}") +
-                        right_aligned_format.format(f"Flush%:{BRIGHT_GREEN if green_Stocks_capital_percentage > 0 else BRIGHT_RED}{green_Stocks_capital_percentage}{RESET}"))                                          
+    output_lines.append(left_aligned_format.format(f"B-mktpxy:{BRIGHT_GREEN if green_Stocks_capital_percentage > 0 else BRIGHT_RED}{bmktpxy}{RESET}") +
+                        right_aligned_format.format(f"N-mktpxy:{BRIGHT_GREEN if green_Stocks_capital_percentage > 0 else BRIGHT_RED}{mktpxy}{RESET}"))                                          
     output_lines.append(
         left_aligned_format.format(
             f"{'Capital'.zfill(7)}:{BRIGHT_YELLOW}{str(round(capital, 2)).zfill(5)}"
