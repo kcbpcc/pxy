06d494a7a21479ec41d20a1e696c8992aa030908
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-12 05:39:22 +0800

    Update cncprftpxy.py

--- sys/exe/run/cncprftpxy.py
+++ sys/exe/run/cncprftpxy.py
@@ -103,7 +103,7 @@ def process_data_total_profit():
         total_profit_combined = total_profit + total_profit_fo
 
         # Format the entire message with right alignment and magenta color
-        output_message = f"As of {current_date}, total profit is {total_profit_combined}"
+        output_message = f"{current_date}'s total profit is {total_profit_combined}"
         formatted_output = f"\033[95m{output_message:>42}\033[0m"
         
         # Print the formatted message
