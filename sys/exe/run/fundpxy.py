fb0161cf875a077b0c5cd989a05097c7ab4f41f4
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-11 16:28:52 +0800

    Update fundpxy.py

--- sys/exe/run/fundpxy.py
+++ sys/exe/run/fundpxy.py
@@ -28,35 +28,23 @@ def calculate_decision():
                 logging.error(f"{str(e)} unable to get holdings")
                 sys.exit(1)
     
-            try:
-                response = broker.kite.margins()
-                print(response)  # Print the response to the output file
-                available_cash = response["equity"]["available"]["live_balance"]
-                delivery_utilised = response["equity"]["utilised"]["delivery"]
-                cash_for_delivery = available_cash - delivery_utilised
-                print(f"Cash available for delivery: {cash_for_delivery}")
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                available_cash = 0
-                cash_for_delivery = 0
-
     finally:
         # Reset sys.stdout to its original value
         sys.stdout = original_stdout
 
     try:
-        # Print the information again to the console
-        response = broker.kite.margins()
-        available_cash = response["equity"]["available"]["live_balance"]
-        delivery_utilised = response["equity"]["utilised"]["delivery"]
-        cash_for_delivery = available_cash - delivery_utilised
-        print(f"Response: {response}")
-        print(f"Cash available for delivery: {cash_for_delivery}")
-
+        # Assuming kite is defined somewhere in the get_kite function
+        # Use the 'margins' method to get margin data without specifying a segment
+        try:
+            response = broker.kite.margins()
+            available_cash = response["equity"]["available"]["live_balance"]
+            #print(f"I have 💰💰💰💰{available_cash/1000:.0f}K💰💰💰 to buy stocks")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            available_cash = 0
         limit = 50000 if peak == 'NONPEAK' else 10000 if peak == 'PEAKEND' else 0
         decision = "YES" if available_cash > limit else "NO"
         optdecision = "YES" if available_cash > 10000 else "NO"
-        
         # Only return the decision, not available_cash
         return decision, optdecision, available_cash, limit
 
@@ -64,4 +52,5 @@ def calculate_decision():
         remove_token(dir_path)
         logging.error(f"{str(e)} unable to get available cash")
         return "NO", "NO", 0, 0
+
 
