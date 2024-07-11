be5de4add315a48b51e78b3d6a57fdfb0baf2c71
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-11 12:28:17 +0800

    Update cmbddfpxy.py

--- sys/exe/run/cmbddfpxy.py
+++ sys/exe/run/cmbddfpxy.py
@@ -4,6 +4,10 @@ import pandas as pd
 from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
 from toolkit.logger import Logger
+import csv
+import os
+import sys
+import traceback
 import logging
 
 logging = Logger(30, dir_path + "main.log")
@@ -26,19 +30,6 @@ def get_positionsinfo(resp_list, broker):
         print(f"An error occurred in positions: {e}")
         return None
 
-def get_ordersinfo(broker):
-    try:
-        orders = broker.kite.orders()
-        if orders:
-            df = pd.DataFrame(orders)
-        else:
-            df = pd.DataFrame(columns=['order_id', 'tradingsymbol', 'quantity', 'price', 'status'])  # Add other necessary columns
-        df.columns = ['o_' + col if col != 'tradingsymbol' else col for col in df.columns]  # Prefix columns with 'o_'
-        return df
-    except Exception as e:
-        print(f"An error occurred in orders: {e}")
-        return None
-
 try:
     sys.stdout = open('output.txt', 'w')
     broker = get_kite()
@@ -48,6 +39,7 @@ except Exception as e:
     logging.error(f"{str(e)} unable to get holdings")
     sys.exit(1)
 finally:
+    # Ensure to close the file and restore stdout
     if sys.stdout != sys.__stdout__:
         sys.stdout.close()
         sys.stdout = sys.__stdout__
@@ -58,12 +50,9 @@ def process_data():
         positions_response = broker.kite.positions()['net']
         holdings_df = get_holdingsinfo(holdings_response, broker)
         positions_df = get_positionsinfo(positions_response, broker)
-        orders_df = get_ordersinfo(broker)
 
-        if holdings_df is not None:
-            holdings_df['key'] = holdings_df['exchange'] + ":" + holdings_df['tradingsymbol']
-        if positions_df is not None:
-            positions_df['key'] = positions_df['exchange'] + ":" + positions_df['tradingsymbol']
+        holdings_df['key'] = holdings_df['exchange'] + ":" + holdings_df['tradingsymbol'] if not holdings_df.empty else None
+        positions_df['key'] = positions_df['exchange'] + ":" + positions_df['tradingsymbol'] if not positions_df.empty else None
         combined_df = pd.concat([holdings_df, positions_df], ignore_index=True)
         lst = combined_df['key'].tolist()
         resp = broker.kite.ohlc(lst)
@@ -85,7 +74,7 @@ def process_data():
         combined_df['qty'] = combined_df.apply(lambda row: int(row['quantity'] + row['t1_quantity']) if row['source'] == 'holdings' else int(row['quantity']), axis=1)
         combined_df['oPL%'] = combined_df.apply(lambda row: round((((row['ltp'] - row['open']) / row['open']) * 100), 2) if row['open'] != 0 else 0, axis=1)
         combined_df['dPL%'] = combined_df.apply(lambda row: round((((row['ltp'] - row['close']) / row['close']) * 100), 2) if row['close'] != 0 else 0, axis=1)
-
+        
         combined_df['pnl'] = combined_df['pnl'].astype(int)
         combined_df['avg'] = combined_df['average_price']
         combined_df['Invested'] = (combined_df['qty'] * combined_df['avg']).round(0).astype(int)
@@ -95,23 +84,26 @@ def process_data():
         combined_df['Yvalue'] = combined_df['qty'] * combined_df['close']
         combined_df['dPnL'] = combined_df['value'] - combined_df['Yvalue']
 
+        #combined_df['outqty'] = positions_df['key'].map(holdings_df.set_index('key')['used_quantity'])
         combined_df['in'] = positions_df['key'].map(holdings_df.set_index('key')['average_price'])
         if positions_df.empty:
             combined_df['in'] = combined_df.get('out', None)
         else:
             combined_df['out'] = positions_df['key'].map(positions_df.set_index('key')['buy_price'])
-
+    
+        # Handle conversion of 'm2m' column to int if it exists
         if "m2m" in combined_df.columns:
             try:
                 combined_df['m2m'] = combined_df['m2m'].astype(int)
             except ValueError:
+                # Handle the case where some values cannot be converted to int
                 pass
         else:
+            # Create the 'm2m' column and set all row values to 0
             combined_df['m2m'] = 0
 
-        combined_df = pd.merge(combined_df, orders_df, on='tradingsymbol', how='left', suffixes=('', '_orders'))
-
         return combined_df
+        
 
     except Exception as e:
         print(f"An error occurred: {e}")
