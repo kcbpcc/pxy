ed157e86a55923e0c696d4b51d65f53287070b5f
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-12 22:06:14 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -63,7 +63,7 @@ def printbord(booked, total_cnc_m2m_postions, extras, optworth, all_Stocks_worth
     )
     output_lines.append(left_aligned_format.format(f"Op&l:{BRIGHT_GREEN if total_opt_m2m > 0 else BRIGHT_RED}{int(round(total_opt_m2m, 0))}{RESET}") +
                         right_aligned_format.format(f"Hp&l:{BRIGHT_GREEN if all_Stocks_worth_dpnl > 0 else BRIGHT_RED}{int(round(all_Stocks_worth_dpnl, 0))}{RESET}"))
-    output_lines.append(left_aligned_format.format(f"Closed:{BRIGHT_GREEN if extras >= 0 else BRIGHT_RED}{str(int(extras)).zfill(5)}{RESET}") +
+    output_lines.append(left_aligned_format.format(f"Closed:{BRIGHT_GREEN if extras >= 0 else BRIGHT_RED}{str(int(optworth)).zfill(5)}{RESET}") +
                         right_aligned_format.format(f"Booked:{GREEN if booked > 0 else RED}{str(round(booked)).zfill(5)}{RESET}"))
     output_lines.append(left_aligned_format.format(f"Closed:{BRIGHT_GREEN if extras >= 0 else BRIGHT_RED}{str(int(extras)).zfill(5)}{RESET}") +
                         right_aligned_format.format(f"PROFIT:{GREEN if booked > 0 else RED}{str(round(booked+extras)).zfill(5)}{RESET}"))
