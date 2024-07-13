63b5f20f265497e5647d050f3c9798a9b891fffa
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-13 14:05:55 +0800

    Update buyboptpxy.py

--- sys/exe/run/buyboptpxy.py
+++ sys/exe/run/buyboptpxy.py
@@ -84,7 +84,7 @@ async def main():
         CE_weight = count_CE - count_PE
         weight = abs(count_PE - count_CE)
 
-        print(f"{BRIGHT_YELLOW}🔥{count_CE:02}:CE positions📈{mktpxy:<4}📉PE positions:{count_PE:02}💧{RESET}")
+        print(f"{BRIGHT_YELLOW}📈{count_CE:02}:CE positions🔥{mktpxy:<4}💧PE positions:{count_PE:02}📉{RESET}")
         
         expiry_year, expiry_month, expiry_day = month_expiry_date()
 
