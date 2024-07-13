96e0167fb3cd49babfcff07d8716f65f1c8a7e16
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-13 21:36:58 +0800

    Update bpredictpxy.py

--- sys/exe/run/bpredictpxy.py
+++ sys/exe/run/bpredictpxy.py
@@ -1,7 +1,7 @@
 from bftpxy import get_bnk_action
 from smapxy import check_index_status
 
-def predict_bmarket_sentiment():
+def predict_bnk_sentiment():
     ha_nse_action, nse_power, Day_Change, Open_Change = get_nse_action()
     nsma = check_index_status('^NSEBANK')
     Bearish = "Bearish"
