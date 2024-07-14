0f713c392dd88af98fb7fb3073cd6cad5ae0c5a4
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-15 07:05:00 +0800

    Update ztstpxy.py

--- sys/exe/run/ztstpxy.py
+++ sys/exe/run/ztstpxy.py
@@ -1,118 +1,110 @@
-import sys
-import traceback
 import pandas as pd
+import numpy as np
 from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
-from toolkit.logger import Logger
+from cmbddfpxy import process_data
+from smapxy import check_index_status
+from utcpxy import peak_time
+from depthpxy import calculate_consecutive_candles
+from clorpxy import BRIGHT_YELLOW, BRIGHT_GREEN, BRIGHT_RED, RESET
 
-# Define logging
-logging = Logger(30, dir_path + "main.log")
+# Check index status
+bsma = check_index_status('^NSEBANK')
+nsma = check_index_status('^NSEI')
 
-def get_holdings_info(resp_list, broker):
-    try:
-        df = pd.DataFrame(resp_list)
-        df['source'] = 'holdings'
-        return df
-    except Exception as e:
-        logging.error(f"Error occurred in get_holdings_info: {e}")
-        return None
+# Get peak time
+peak = peak_time()
 
-def get_positions_info(resp_list, broker):
-    try:
-        df = pd.DataFrame(resp_list)
-        df['source'] = 'positions'
-        return df
-    except Exception as e:
-        logging.error(f"Error occurred in get_positions_info: {e}")
-        return None
+def compute_tgtoptsma(row):
+    global bsma
+    global nsma
+    
+    key = row['key']
+    
+    if (bsma == "up" and key.startswith("BANK") and "CE" in key) or (bsma == "down" and key.startswith("BANK") and "PE" in key):
+        return 4
+    elif (nsma == "up" and key.startswith("NIFTY") and "CE" in key) or (nsma == "down" and key.startswith("NIFTY") and "PE" in key):
+        return 4
+    else:
+        return 4
 
-def process_data():
-    # Redirect stdout to output.txt
-    sys.stdout = open('output.txt', 'w')
+def compute_depth(row):
+    if "CE" in row['key'] and row['key'].startswith("BANK"):
+        return max(row['tgtoptsma'], (3 + bcedepth))
+    elif "PE" in row['key'] and row['key'].startswith("BANK"):
+        return max(row['tgtoptsma'], (3 + bpedepth))
+    elif "CE" in row['key'] and row['key'].startswith("NIFTY"):
+        return max(row['tgtoptsma'], (3 + ncedepth))
+    elif "PE" in row['key'] and row['key'].startswith("NIFTY"):
+        return max(row['tgtoptsma'], (3 + npedepth))
+    else:
+        return 5
 
-    # Attempt to get broker instance
-    broker = None
-    try:
-        broker = get_kite()
-    except Exception as e:
-        remove_token(dir_path)
-        logging.error(f"Error in getting kite instance: {e}")
-        print(traceback.format_exc())
-        sys.exit(1)
-    finally:
-        # Ensure to close the file and restore stdout
-        if sys.stdout != sys.__stdout__:
-            sys.stdout.close()
-            sys.stdout = sys.__stdout__
+try:
+    broker = get_kite()
+except Exception as e:
+    remove_token(dir_path)
+    logging.error(f"{str(e)} unable to get holdings")
+    sys.exit(1)
 
-    try:
-        # If broker is obtained successfully, proceed with data processing
-        holdings_response = broker.kite.holdings()
-        positions_response = broker.kite.positions()['net']
+# Process data and prepare opt_df
+combined_df = process_data()
+bcedepth, bpedepth = calculate_consecutive_candles("^NSEBANK")
+ncedepth, npedepth = calculate_consecutive_candles("^NSEI")
+
+# Filter and process the DataFrame
+opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
+opt_df['key'] = opt_df['key'].str.replace('NFO:', '')
+opt_df['tgtoptsma'] = opt_df.apply(compute_tgtoptsma, axis=1)
+opt_df['tgtoptsmadepth'] = opt_df.apply(compute_depth, axis=1)
+opt_df['PL%'] = (opt_df['PnL'] / opt_df['Invested']) * 100
+opt_df['PL%'] = opt_df['PL%'].fillna(0)
+opt_df['PL%'] = opt_df['PL%'].astype(int)
+opt_df['m2m'] = opt_df['m2m'].astype(int)
+opt_df = opt_df[['key', 'Invested', 'qty', 'PL%', 'PnL', 'pnl', 'product', 'm2m','tgtoptsmadepth']]
+
+total_invested = opt_df['Invested'].sum()
+total_pl = opt_df['PnL'].sum()
+total_opt_pnl = opt_df['m2m'].sum()
+total_pl_percentage = (total_pl / total_invested) * 100 if total_invested != 0 else 0
+
+# Create and process the print_df DataFrame
+print_df = opt_df.copy()
+print_df['CP'] = opt_df['key'].apply(lambda x: '🟠' if x.endswith('PE') else ('🟢' if x.endswith('CE') else None))
+print_df['group'] = print_df['key'].str.extract(r'^(B|N)', expand=False)
+print_df['key'] = print_df['key'].str.replace('BANKNIFTY24', 'B').str.replace('NIFTY24', 'N')
+print_df['strike'] = print_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
+print_df['MN'] = np.where(print_df['product'] == 'MIS', '⌛', '🔢')
+print_df = print_df[['MN', 'strike', 'Invested', 'qty', 'PL%', 'm2m','PnL', 'CP', 'group','tgtoptsmadepth']]
+#print(opt_df)
+
+# Summary calculations
+summary_statement = ""
+total_invested_all = print_df['Invested'].sum()
+total_pl_all = print_df['PnL'].sum()
+total_pl_percentage_all = (total_pl_all / total_invested_all) * 100 if total_invested_all != 0 else 0
+color_code_summary = BRIGHT_YELLOW
+summary_sentence = f"{color_code_summary}SUMMARY-CAP:{total_invested_all:6.0f} P&L:{total_pl_all:7.0f} P&L%:{total_pl_percentage_all:3.0f}%{RESET}"
+summary_statement = summary_sentence
+print(summary_statement + "📊")
+
+# Filter and group data
+filtered_df = print_df[print_df['qty'] > 0]
+grouped_df = filtered_df.groupby('group')
+
+for group, data in grouped_df:
+    total_invested_group = data['Invested'].sum()
+    total_pl_group = data['PnL'].sum()
+    total_pl_percentage_group = (total_pl_group / total_invested_group) * 100 if total_invested_group != 0 else 0
+    
+    if total_invested_group != 0:
+        summary_sentence = f"CAP:{total_invested_group} P&L:{total_pl_group:6.0f} P&L%:{total_pl_percentage_group:3.0f}%"
+        color_code = BRIGHT_GREEN if total_pl_percentage_group > 0 else BRIGHT_RED
+        print(data[data['qty'] > 0][['MN', 'strike', 'Invested', 'qty','tgtoptsmadepth', 'PL%', 'PnL', 'CP']].to_string(header=False, index=False, col_space=[2, 10, 5, 3, 2, 3, 6,2]))
         
-        holdings_df = get_holdings_info(holdings_response, broker)
-        positions_df = get_positions_info(positions_response, broker)
+        if len(data) >= 2:
+            formatted_output = f"{group}{color_code}{summary_sentence}{RESET}".rjust(51)
+            print(formatted_output)
 
-        # Check if 'tradingsymbol' is present in both dataframes
-        if 'tradingsymbol' not in holdings_df.columns or 'tradingsymbol' not in positions_df.columns:
-            raise KeyError("'tradingsymbol' column not found in holdings_df or positions_df")
-
-        # Merge holdings_df and positions_df on 'tradingsymbol'
-        merged_df = pd.merge(holdings_df, positions_df, on='tradingsymbol', how='outer')
-
-        # Filter merged_df to include only rows where product_x == 'CNC' and used_quantity > 0
-        merged_df_filtered = merged_df[(merged_df['product_x'] == 'CNC') & (merged_df['used_quantity'] > 0)].copy()
-
-        # Calculate PL% and PnL
-        merged_df_filtered['SN'] = range(1, len(merged_df_filtered) + 1)  # Serial number starting from 1
-        merged_df_filtered['STOCK'] = merged_df_filtered['tradingsymbol']
-        merged_df_filtered['QTY'] = merged_df_filtered['used_quantity'].astype(int)
-        merged_df_filtered['PL%'] = ((merged_df_filtered['average_price_y'] - merged_df_filtered['average_price_x']) / merged_df_filtered['average_price_y']) * 100
-        merged_df_filtered['PL%'] = merged_df_filtered['PL%'].round(2)
-        merged_df_filtered['PnL'] = merged_df_filtered.apply(lambda row: row['used_quantity'] * (row['average_price_y'] - row['average_price_x']), axis=1).astype(int)
-        
-        # Select specific columns from filtered merged_df and reorder
-        merged_df_filtered = merged_df_filtered[['SN', 'STOCK', 'QTY', 'PL%', 'PnL']]
-
-        # Set column widths for printing
-        col_widths = [3, 12, 8, 7, 8]
-
-        # Convert DataFrame to formatted string with aligned headers and values
-        formatted_str = merged_df_filtered.to_string(index=False, col_space=col_widths)
-
-        # ANSI escape sequence for bright yellow color
-        bright_yellow = '\033[93m'
-        reset_color = '\033[0m'
-
-        # Print headers with bright yellow color
-        headers_str = formatted_str.split('\n', 1)[0]  # Extract headers
-        print(f"{bright_yellow}{headers_str}{reset_color}")
-
-        # Print the rest of the formatted string (values)
-        print(formatted_str[len(headers_str):])
-
-        # Print "Stocks Booked Profit" right-aligned with 42 spaces
-        total_profit = merged_df_filtered['PnL'].sum()
-        print(f"\033[92m{total_profit:>42}\033[0m")
-
-
-
-        return total_profit
-
-    except Exception as e:
-        logging.error(f"Error occurred in process_data: {e}")
-        print(f"An error occurred: {e}")
-        traceback.print_exc()
-        return None
-
-def main():
-    try:
-        process_data()
-    except Exception as e:
-        logging.error(f"Error occurred in main: {e}")
-        print(f"An error occurred in main: {e}")
-        traceback.print_exc()
-
-if __name__ == "__main__":
-    main()
+print("━" * 42)
 
