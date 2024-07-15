e0fd7b4998b46f9ee5e8890dc7caadfeb89c40be
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-15 13:11:05 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -13,6 +13,9 @@ from clorpxy import SILVER, UNDERLINE, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_
 subprocess.run(['python3', 'cpritepxy.py'])
 import os
 import importlib
+
+############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
+
 while True:
     import os
     import sys
@@ -111,10 +114,18 @@ while True:
         print(f"Error handling index status: {e}")
         nsma, bsma = None, None
 
-    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
+    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
+
     print((BRIGHT_GREEN + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'RISE' else (BRIGHT_RED + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'FALL' else (BRIGHT_YELLOW + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'SIDE' else ""))) + RESET)
     subprocess.run(['python3', 'tistpxy.py'])
     subprocess.run(['python3', 'cntrloptpxy.py']) if 'mktpxy' in ['Buy', 'Sell'] or 'bmktpxy' in ['Buy', 'Sell'] else None
+    subprocess.run(['python3', 'buynoptpxy.py'])
+    subprocess.run(['python3', 'buyboptpxy.py'])
+    subprocess.run(['python3', 'buyvolcncpxy.py']) if (peak == 'PEAKEND' and (mktpredict == 'RISE' or mktpredict == 'SIDE')) else None
+    subprocess.run(['python3', 'cntrlcncpxy.py'])
+    
+    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
+
     (lambda: print((BRIGHT_GREEN if nsma == "up" else BRIGHT_RED if nsma == "down" else BRIGHT_YELLOW) + "ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨NIFTY٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ" + RESET))() if 'nsma' in locals() else None
     subprocess.run(['python3', 'buynftbnkpxy.py']) if (peak == 'PEAKEND' and (mktpredict == 'RISE' or mktpredict == 'SIDE')) else None
     subprocess.run(['python3', 'cndlpxy.py'])
@@ -126,17 +137,20 @@ while True:
     subprocess.run(['python3', 'bcndlpxy.py'])
     (lambda: print((BRIGHT_GREEN if bsma == "up" else BRIGHT_RED if bsma == "down" else BRIGHT_YELLOW) + "ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨BANKNIFTY٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨" + RESET))() if 'bsma' in locals() else None    
     subprocess.run(['python3', 'cntrloptpxy.py'])
-    subprocess.run(['python3', 'buynoptpxy.py'])
-    subprocess.run(['python3', 'buyboptpxy.py'])
-    subprocess.run(['python3', 'buyvolcncpxy.py']) if (peak == 'PEAKEND' and (mktpredict == 'RISE' or mktpredict == 'SIDE')) else None
-    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
-    subprocess.run(['python3', 'cntrlcncpxy.py'])
+    
+    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
+
     print((BRIGHT_GREEN + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'RISE' else (BRIGHT_RED + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'FALL' else (BRIGHT_YELLOW + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'SIDE' else ""))) + RESET)
     subprocess.run(['python3', 'selfpxy.py'])
     #subprocess.run(['python3', 'gooboradpxy.py'])
-    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
+    
+    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
+
     from sleeppxy import progress_bar
     from cyclepxy import cycle
-    progress_bar(cycle, mktpxy) #if peak in ["PEAKSART", "PEAKEND", "NONPEAK"] else None)
+    progress_bar(cycle, mktpxy)
+    
+    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
+
 
 
