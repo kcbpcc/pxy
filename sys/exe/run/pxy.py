ddc8e057d59f404e763b7cbacd72757010fd8336
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-16 02:04:50 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -114,7 +114,6 @@ while True:
 
     print((BRIGHT_GREEN + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'RISE' else (BRIGHT_RED + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'FALL' else (BRIGHT_YELLOW + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'SIDE' else ""))) + RESET)
     subprocess.run(['python3', 'tistpxy.py'])
-    subprocess.run(['python3', 'cntrlcncpxy.py'])
     subprocess.run(['python3', 'cntrloptpxy.py']) if mktpxy in ['Bear', 'Bull'] or bmktpxy in ['Bear', 'Bull'] else None
     subprocess.run(['python3', 'buynoptpxy.py']) if mktpxy in ['Buy', 'Sell']  else None
     subprocess.run(['python3', 'buyboptpxy.py']) if bmktpxy in ['Buy', 'Sell'] else None
