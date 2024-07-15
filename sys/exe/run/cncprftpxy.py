34b2a7811f390c0043f2391f16fd2743ca8699d4
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-15 19:21:20 +0800

    Update cncprftpxy.py

--- sys/exe/run/cncprftpxy.py
+++ sys/exe/run/cncprftpxy.py
@@ -6,7 +6,6 @@ from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
 from toolkit.logger import Logger
 
-# Define logging
 logging = Logger(30, dir_path + "main.log")
 
 def get_holdings_info(resp_list, broker):
