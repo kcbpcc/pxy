91ed1d3432c9fd7052c0c322c92503cf078cfee2
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-15 20:30:09 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -337,6 +337,7 @@ try:
     booked = process_data_total_profit()
     print("━" * 42)
     printbord(booked, total_cnc_m2m_postions, extras, optworth, all_Stocks_worth_dpnl, nsma, all_Stocks_yworth_lacks, total_opt_m2m, mktpxy, available_cash, ha_nse_action, nse_power, Day_Change, Open_Change, all_Stocks_count, red_Stocks_count, green_Stocks_count, all_Stocks_capital_lacks, all_Stocks_worth_lacks, zero_qty_count, green_Stocks_profit_loss, green_Stocks_capital_percentage)
+    print("━" * 42)
 #############################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™##############################################################################################################
 except Exception as e:
     remove_token(dir_path)
