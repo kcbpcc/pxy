0676592a1d816c7d772d108f39225c068c559a2a
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-16 03:20:29 +0800

    Update plpxy.py

--- sys/exe/run/plpxy.py
+++ sys/exe/run/plpxy.py
@@ -1,7 +1,7 @@
 import csv
 from tabulate import tabulate
 
-def display_csv_contents(filename, table_format, custom_header):
+def display_csv_contents(filename, custom_header):
     with open(filename, 'r', newline='') as file:
         reader = csv.reader(file)
         rows = list(reader)
@@ -12,13 +12,13 @@ def display_csv_contents(filename, table_format, custom_header):
         headers[headers.index('pnl_y')] = 'PnL'
     
     data = rows[1:]
-    table = tabulate(data, headers=headers, tablefmt=table_format)
+    table = tabulate(data, headers=headers, tablefmt='plain')
     print(f"{custom_header}:")
     print(table)
     print("\n")  # Adding two-line row space
 
 # Display contents of filePnL_nrml.csv
-display_csv_contents('filePnL_nrml.csv', table_format='plain', custom_header='Options Profits')
+display_csv_contents('filePnL_nrml.csv', custom_header='Options Profits')
 
 # Display contents of filePnL.csv
-display_csv_contents('filePnL.csv', table_format='plain', custom_header='Stocks Profits')
+display_csv_contents('filePnL.csv', custom_header='Stocks Profits')
