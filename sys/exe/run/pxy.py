a9a4ffde3d9bd26b55e20f3998f6c82df7ffc90b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-15 19:36:13 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -114,6 +114,7 @@ while True:
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
     print((BRIGHT_GREEN + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'RISE' else (BRIGHT_RED + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'FALL' else (BRIGHT_YELLOW + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'SIDE' else ""))) + RESET)
     subprocess.run(['python3', 'tistpxy.py'])
+    subprocess.run(['python3', 'cntrlcncpxy.py'])
     subprocess.run(['python3', 'cntrloptpxy.py']) if mktpxy in ['Bear', 'Bull'] or bmktpxy in ['Bear', 'Bull'] else None
     subprocess.run(['python3', 'buynoptpxy.py']) if mktpxy in ['Buy', 'Sell']  else None
     subprocess.run(['python3', 'buyboptpxy.py']) if bmktpxy in ['Buy', 'Sell'] else None
@@ -132,7 +133,7 @@ while True:
     subprocess.run(['python3', 'buyboptpxy.py'])
     subprocess.run(['python3', 'buyvolcncpxy.py']) if (peak == 'PEAKEND' and (mktpredict == 'RISE' or mktpredict == 'SIDE')) else None
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################
-    subprocess.run(['python3', 'cntrlcncpxy.py'])
+    
     print((BRIGHT_GREEN + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'RISE' else (BRIGHT_RED + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'FALL' else (BRIGHT_YELLOW + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'SIDE' else ""))) + RESET)
     subprocess.run(['python3', 'selfpxy.py'])
     #subprocess.run(['python3', 'gooboradpxy.py'])
