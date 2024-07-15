2b71819696adb705f5f227fbc3f9b6f8c49babeb
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-16 04:13:31 +0800

    Update plpxy.py

--- sys/exe/run/plpxy.py
+++ sys/exe/run/plpxy.py
@@ -37,10 +37,10 @@ def display_csv_contents(filename, custom_header):
     return subtotal
 
 # Display contents of filePnL_nrml.csv
-subtotal1 = display_csv_contents('filePnL_nrml.csv', custom_header='123')
+subtotal1 = display_csv_contents('filePnL_nrml.csv', custom_header='📊📊📊📊📊📊 Option Profits 📊📊📊📊📊📊')
 
 # Display contents of filePnL.csv
-subtotal2 = display_csv_contents('filePnL.csv', custom_header='233')
+subtotal2 = display_csv_contents('filePnL.csv', custom_header='📈📈📈📈📈📈 Stocks Profits 📈📈📈📈📈📈')
 
 # Calculate and print total of all tables
 total = subtotal1 + subtotal2
