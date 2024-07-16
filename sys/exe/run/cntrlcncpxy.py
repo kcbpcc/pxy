2664e9aa53aafef00243fd7f8caa710ba5abcaec
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-16 15:28:15 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -330,6 +330,7 @@ try:
             print(f"An unexpected error occurred: {e}")
 
 ###########################################################################################################################################################################################################  
+    print("━" * 42)
     if not stocks_filtered_df.empty:
         print('\n'.join([line.rjust(40) for line in stocks_filtered_df.to_string(index=False, header=False).split('\n')]))    
     print("━" * 42)
