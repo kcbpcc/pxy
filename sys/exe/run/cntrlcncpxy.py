2ae52f2d6550b46760c06efcde0cfb7ca6144e15
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-16 11:51:22 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -245,9 +245,24 @@ try:
     PRINT_df_sorted_display = PRINT_df_sorted.copy()
     stocks_filtered_df = PRINT_df_sorted_display[PRINT_df_sorted_display['PL%'] > 1.4].sort_values(by='PL%')
 ########################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™###################################################################################################################   
+    import pandas as pd
+    import os
+    
     csv_file_path = "filePnL.csv"
-    df = pd.read_csv(csv_file_path, header=None)
-    df.columns = ["STOCK", "QTY", "PL%", "PnL"]
+    
+    # Check if the file is empty
+    if os.stat(csv_file_path).st_size == 0:
+        print("The file is empty.")
+        df = pd.DataFrame(columns=["STOCK", "QTY", "PL%", "PnL"])  # Create an empty DataFrame with the specified columns
+    else:
+        try:
+            df = pd.read_csv(csv_file_path, header=None)
+            df.columns = ["STOCK", "QTY", "PL%", "PnL"]
+        except pd.errors.EmptyDataError:
+            print("The file is empty or only contains headers.")
+            df = pd.DataFrame(columns=["STOCK", "QTY", "PL%", "PnL"])  # Create an empty DataFrame with the specified columns
+########################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™###################################################################################################################   
+
     total_dPnL = ((all_Stocks_worth_lacks - all_Stocks_yworth_lacks) * 100000)
     selected_rows = []
     
