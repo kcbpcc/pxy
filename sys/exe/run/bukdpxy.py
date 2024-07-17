f43e4c4d4f84de7d94939bae49d126c6e7312d5b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-17 14:50:09 +0800

    Update bukdpxy.py

--- sys/exe/run/bukdpxy.py
+++ sys/exe/run/bukdpxy.py
@@ -18,7 +18,7 @@ def sum_last_numerical_value_in_each_row(csv_file):
     return total_sum
 
 # Replace 'filePnL.csv' with the path to your actual CSV file
-file_path = 'filePnL.csv'
+file_path = 'pxycncprofit.csv'
 result = sum_last_numerical_value_in_each_row(file_path)
 
 #print(f"Booked: {result}")
