54ede8cd60c9b579d6b734e7f6b2b54053c2ceca
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-17 13:57:21 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -245,7 +245,7 @@ try:
     PRINT_df_sorted_display = PRINT_df_sorted.copy()
     stocks_filtered_df = PRINT_df_sorted_display[PRINT_df_sorted_display['PL%'] > 1.4].sort_values(by='PL%')
 ########################################################################################"PXY® PreciseXceleratedYield Pvt Ltd™###################################################################################################################   
-    csv_file_path = "filePnL.csv"
+    csv_file_path = "pxycncprofit.csv"
     df = pd.read_csv(csv_file_path, header=None)
     df.columns = ["STOCK", "QTY", "PL%", "PnL"]
     total_dPnL = ((all_Stocks_worth_lacks - all_Stocks_yworth_lacks) * 100000)
