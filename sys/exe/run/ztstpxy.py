0f743c09aea807cf5a1fac797887b07f2b2ebb10
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-18 06:05:56 +0800

    Update ztstpxy.py

--- sys/exe/run/ztstpxy.py
+++ sys/exe/run/ztstpxy.py
@@ -1,13 +1,15 @@
+import asyncio
 from telegram import Bot
 
 # Replace with your actual bot token and user ID
 bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'
 chat_id = '-4282665161'
 
-def send_test_message():
+async def send_test_message():
     bot = Bot(token=bot_token)
     message = "This is a test message from your bot."
-    bot.send_message(chat_id=chat_id, text=message)
+    await bot.send_message(chat_id=chat_id, text=message)
 
 if __name__ == "__main__":
-    send_test_message()
+    asyncio.run(send_test_message())
+
