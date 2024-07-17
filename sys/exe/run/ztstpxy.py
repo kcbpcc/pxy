3e4fa96efc1f77ecfabd0497d8a594f90b49a135
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-18 06:07:34 +0800

    Update ztstpxy.py

--- sys/exe/run/ztstpxy.py
+++ sys/exe/run/ztstpxy.py
@@ -2,8 +2,8 @@ import asyncio
 from telegram import Bot
 
 # Replace with your actual bot token and user ID
-bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'
-chat_id = '-4282665161'
+bot_token = '6867988078:AAGNBJqs4Rf8MR4xPGoL1-PqDOYouPan7b0'
+chat_id = '-4136531362'
 
 async def send_test_message():
     bot = Bot(token=bot_token)
@@ -12,4 +12,3 @@ async def send_test_message():
 
 if __name__ == "__main__":
     asyncio.run(send_test_message())
-
