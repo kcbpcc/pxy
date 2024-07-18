48c17ff3760e5409d44108083cf4aed348fa6140
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-19 01:08:26 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -76,6 +76,30 @@ def printbord(booked, total_cnc_m2m_postions, extras, optworth, all_Stocks_worth
 
     # Print the formatted output
     print(full_output)
+
+    summary = (
+        f"Date and Time: {current_datetime}\n"
+        f"BANKNIFTY: {bmktpredict} {arrow_map.get(bmktpxy, '')}\n"
+        f"NIFTYNDEX: {mktpredict} {arrow_map.get(mktpxy, '')}\n"
+        f"Margin: {available_cash}\n"
+        f"Delta: {ydaypnl_to_print * 100000}\n"
+        f"Real-P&L: {round((acvalue_to_print - capital + hide), 2)}\n"
+        f"Run-P&L: {round(((all_Stocks_capital_lacks - all_Stocks_worth_lacks) * -1), 2)}\n"
+        f"Capital: {capital}\n"
+        f"Value: {acvalue_to_print}\n"
+        f"Flush#: {green_Stocks_count}\n"
+        f"Flush%: {green_Stocks_capital_percentage}%\n"
+        f"CE/PE%: {optworth:.2f}%\n"
+        f"Flush: {green_Stocks_profit_loss}\n"
+        f"Positions: {total_cnc_m2m_postions}\n"
+        f"Holdings: {all_Stocks_worth_dpnl}\n"
+        f"Options: {total_opt_m2m}\n"
+        f"BOOKED: {booked}\n"
+        f"CLOSED: {extras}\n"
+        f"PROFIT: {booked + extras}\n"
+    )
+    check_and_send_summary(summary)
+
     
 
 
