20405ad09ab2a7fc30242b2fd15a81685ee7850f
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-19 03:04:32 +0800

    Update telsumrypxy.py

--- sys/exe/run/telsumrypxy.py
+++ sys/exe/run/telsumrypxy.py
@@ -77,7 +77,7 @@ def check_and_send_summary(message, source):
                         break
         
     if already_sent:
-        print("Update already sent for this hour 🔔")
+        print("🔔🔔Update already sent for this hour 🔔")
     else:
         send_summary_to_telegram(message, source)
 
