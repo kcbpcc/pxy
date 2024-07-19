708d0f0435f5ff3774e508e8c2b934ab713e7e20
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-19 22:52:52 +0800

    Update plpxy.py

--- sys/exe/run/plpxy.py
+++ sys/exe/run/plpxy.py
@@ -56,6 +56,8 @@ pxycncprofit_file = 'pxycncprofit.csv'
 pxyoptprofit_file = 'pxyoptprofit.csv'
 
 # Reading and processing pxycncprofit.csv
+print("🏛 🏛 🏛 PXY®🏛🏛 🏛)
+      
 print("💵C&C Profits💵")
 subtotal_cnc = read_csv_and_sum(pxycncprofit_file)
 
