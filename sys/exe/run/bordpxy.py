3babc984664fb8c91c147c5c42f4322b1197f085
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-19 20:00:34 +0800

    Update bordpxy.py

--- sys/exe/run/bordpxy.py
+++ sys/exe/run/bordpxy.py
@@ -80,25 +80,13 @@ def printbord(booked, total_cnc_m2m_postions, extras, optworth, all_Stocks_worth
     print(full_output)
 
     summary = (
-        f"SUMMARY DASH BOARD \n"
-        f"BANKNIFTY: {bmktpredict} {arrow_map.get(bmktpxy, '')}\n"
-        f"NIFTYNDEX: {mktpredict} {arrow_map.get(mktpxy, '')}\n"
-        f"Margin: {int(available_cash)}\n"
-        f"Delta: {int(ydaypnl_to_print * 100000)}\n"
-        f"Real-P&L: {round((acvalue_to_print - capital + hide), 2)}\n"
-        f"Run-P&L: {round(((all_Stocks_capital_lacks - all_Stocks_worth_lacks) * -1), 2)}\n"
-        f"Capital: {capital}\n"
-        f"Value: {acvalue_to_print:.2f}\n"
-        f"Flush#: {green_Stocks_count}\n"
-        f"Flush%: {green_Stocks_capital_percentage}%\n"
-        f"CE/PE%: {optworth:.2f}%\n"
-        f"Flush: {green_Stocks_profit_loss}\n"
-        f"Positions: {int(total_cnc_m2m_postions)}\n"
-        f"Holdings: {int(all_Stocks_worth_dpnl)}\n"
+        f"🏛 🏛 🏛 PXY®🏛🏛 🏛 \n"
+        f"📌 Positions: {int(total_cnc_m2m_postions)}\n"
+        f"💼 Holdings: {int(all_Stocks_worth_dpnl)}\n"
         f"💸 Options: {int(total_opt_m2m)}\n"
         f"✍🏻 BOOKED: {booked}\n"
         f"💵 CLOSED: {extras}\n"
-        f"💰 PROFIT: {booked + extras}\n"
+        f"💰💰💰 PROFIT: {booked + extras}\n"
     )
     check_and_send_summary(summary,'bordpxy')
 
