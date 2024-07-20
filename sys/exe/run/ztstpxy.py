4230dfeeb9550aa7c2c235d16c1ef9eed23090b3
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-20 13:05:58 +0800

    Update ztstpxy.py

--- sys/exe/run/ztstpxy.py
+++ sys/exe/run/ztstpxy.py
@@ -1,19 +1,17 @@
 import asyncio
 from telegram import Bot
 
-# Replace with your actual bot token and user ID
+# Replace with your actual bot token and chat ID
 bot_token = '6867988078:AAGNBJqs4Rf8MR4xPGoL1-PqDOYouPan7b0'
 chat_id = '-4136531362'
 
 async def send_test_message():
     bot = Bot(token=bot_token)
+    symbol = 'TCS'  # Define the trading symbol or replace with your desired value
     message = (
-
-    f"🔍 Check it out on TradingView: [TradingView](https://www.tradingview.com/chart/?symbol={TCS})\n"
-
+        f"🔍 Check it out on TradingView: [TradingView](https://www.tradingview.com/chart/?symbol={symbol})\n"
     )
-
-    await bot.send_message(chat_id=chat_id, text=message)
+    await bot.send_message(chat_id=chat_id, text=message, parse_mode='Markdown')
 
 if __name__ == "__main__":
     asyncio.run(send_test_message())
