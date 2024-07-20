5ac4a337360065841cbf7d804d728325184d1aac
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-20 13:03:30 +0800

    Update ztstpxy.py

--- sys/exe/run/ztstpxy.py
+++ sys/exe/run/ztstpxy.py
@@ -7,7 +7,12 @@ chat_id = '-4136531362'
 
 async def send_test_message():
     bot = Bot(token=bot_token)
-    message = "This is a test message from your bot."
+    message = (
+
+    f"🔍 Check it out on TradingView: [TradingView](https://www.tradingview.com/chart/?symbol={exchsym[1]})\n"
+
+    )
+
     await bot.send_message(chat_id=chat_id, text=message)
 
 if __name__ == "__main__":
