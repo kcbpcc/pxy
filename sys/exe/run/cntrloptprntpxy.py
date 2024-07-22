f9c3adfe2be9a2ef39bcbcc8a67df9f05610c6ae
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-23 06:36:12 +0800

    Update cntrloptprntpxy.py

--- sys/exe/run/cntrloptprntpxy.py
+++ sys/exe/run/cntrloptprntpxy.py
@@ -78,6 +78,8 @@ combined_df = pd.read_csv('pxycombined.csv')
 bcedepth, bpedepth = calculate_consecutive_candles("^NSEBANK")
 ncedepth, npedepth = calculate_consecutive_candles("^NSEI")
 
+nifty_nfo_df = combined_df.loc[(combined_df['key'].str.contains('NFO:NIFTY'))]
+
 if not nifty_nfo_df.empty:
     # extras = nifty_nfo_df.loc[nifty_nfo_df['sell_quantity'] > 0, 'unrealised'].sum()
     nextras = int(nifty_nfo_df.loc[nifty_nfo_df['sell_quantity'] > 0, 'unrealised'].sum()) + ((-1) * int(nifty_nfo_df.loc[nifty_nfo_df['sell_quantity'] > 0, 'PnL'].sum()))
@@ -135,7 +137,6 @@ summary_balance = f"{color_code_summary}SUMMARY-CAP:{total_invested_all:6.0f} P&
 summary_sentence = f"{color_code_summary}SUMMARY-CAP:{total_invested_all:6.0f} P&L:{total_pl_all:7.0f} P&L%:{total_pl_percentage_all:3.0f}%{RESET}"
 summary_statement = summary_sentence
 
-nifty_nfo_df = combined_df.loc[(combined_df['key'].str.contains('NFO:NIFTY'))]
 
 
 
