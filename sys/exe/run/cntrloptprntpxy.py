8110a180a83ff789ddfada553e51397e52175ba6
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-24 04:03:47 +0800

    Update cntrloptprntpxy.py

--- sys/exe/run/cntrloptprntpxy.py
+++ sys/exe/run/cntrloptprntpxy.py
@@ -58,7 +58,7 @@ print_df = print_df[['MN', 'strike', 'Invested', 'qty', 'PL%', 'm2m', 'PnL', 'CP
 # Summary calculations
 summary_statement = ""
 total_invested_all = print_df['Invested'].sum()
-total_pl_all = print_df['PnL'].sum()
+total_pl_all = (print_df['PnL'].sum() + (nextras + bextras))
 total_pl_percentage_all = (total_pl_all / total_invested_all) * 100 if total_invested_all != 0 else 0
 color_code_summary = BRIGHT_YELLOW
 summary_balance = f"{color_code_summary}SUMMARY-CAP:{total_invested_all:6.0f} P&L:{total_pl_all:7.0f} P&L%:{total_pl_percentage_all:3.0f}%{RESET}"
