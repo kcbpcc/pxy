1205691b568cff95564ece2624aeb812a85b5dd9
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-25 02:11:53 +0800

    Update webinputpxy.py

--- sys/exe/run/webinputpxy.py
+++ sys/exe/run/webinputpxy.py
@@ -5,12 +5,14 @@ combined_df = pd.read_csv('pxycombined.csv')
 
 # Filter for exchange 'BSE' or 'NSE' and sort by 'PL%' in descending order for cncpxy
 cncpxy_df = combined_df[(combined_df['exchange'].isin(['BSE', 'NSE']))]
-cncpxy_df = cncpxy_df[['tradingsymbol','oPL%','dPL%', 'PnL', 'PL%']].sort_values(by='PL%', ascending=False)
+cncpxy_df = cncpxy_df[['tradingsymbol', 'oPL%', 'dPL%', 'PnL', 'PL%']].sort_values(by='PL%', ascending=False)
 cncpxy_df.to_csv('cncpxy.csv', index=False)
 
 # Filter for exchange 'NFO' and sort by 'PL%' in descending order for optpxy
 optpxy_df = combined_df[combined_df['exchange'] == 'NFO']
-optpxy_df = optpxy_df[['tradingsymbol', 'unrealised', 'PnL', 'PL%']].sort_values(by='PL%', ascending=False)
+optpxy_df = optpxy_df[['tradingsymbol', 'unrealised', 'PnL', 'PL%']]
+optpxy_df['unrealised'] = optpxy_df['unrealised'].astype(int)  # Convert unrealised to integer
+optpxy_df = optpxy_df.sort_values(by='PL%', ascending=False)
 optpxy_df.to_csv('optpxy.csv', index=False)
 
 print("Files have been created successfully.")
