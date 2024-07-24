0738e379dfee1f5cb8dae3f66bd59105542e3840
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-24 13:45:22 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -168,7 +168,7 @@ exe_opt_df = exe_opt_df[['tradingsymbol', 'm2m', 'PnL', 'PL%']]
 exe_opt_df = exe_opt_df[exe_opt_df['PL%'] > 0]
 
 if exe_opt_df.empty:
-    print("Still fishing, nothing surfaced yet.")
+    print("Still fishing, nothing surfaced yet﹏𓊝﹏𓂁﹏.")
 else:
     print(exe_opt_df.to_string(index=False, header=False))
 
