7906984c351e1fbf09b6757cd24e7be1ce336421
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-25 02:04:48 +0800

    Update webinputpxy.py

--- sys/exe/run/webinputpxy.py
+++ sys/exe/run/webinputpxy.py
@@ -10,7 +10,7 @@ cncpxy_df.to_csv('cncpxy.csv', index=False)
 
 # Filter for exchange 'NFO' and sort by 'PL%' in descending order for optpxy
 optpxy_df = combined_df[combined_df['exchange'] == 'NFO']
-optpxy_df = optpxy_df[['tradingsymbol', 'pnl', 'PnL', 'PL%']].sort_values(by='PL%', ascending=False)
+optpxy_df = optpxy_df[['tradingsymbol', 'unrealised', 'PnL', 'PL%']].sort_values(by='PL%', ascending=False)
 optpxy_df.to_csv('optpxy.csv', index=False)
 
 print("Files have been created successfully.")
