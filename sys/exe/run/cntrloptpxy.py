76f3f6449fc2236cbb2e0a7040bd4d1a444a8158
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-24 13:40:59 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -166,7 +166,7 @@ exe_opt_df['PL%'] = ((exe_opt_df['PnL'] / exe_opt_df['Invested']) * 100).fillna(
 exe_opt_df['m2m'] = exe_opt_df['m2m'].astype(int)
 exe_opt_df = exe_opt_df[['tradingsymbol', 'm2m', 'PnL', 'PL%']]
 exe_opt_df = exe_opt_df[exe_opt_df['PL%'] > 0]
-print(exe_opt_df.to_string(index=False))
+print(exe_opt_df.to_string(index=False, header=False))
 
 
 # Sample data with tgtoptsma hardcoded to 4
