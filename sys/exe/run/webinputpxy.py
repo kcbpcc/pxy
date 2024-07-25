a6f4fc091cb1602552f597741f67e48e7d4569a6
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-25 11:19:05 +0800

    Update webinputpxy.py

--- sys/exe/run/webinputpxy.py
+++ sys/exe/run/webinputpxy.py
@@ -11,10 +11,10 @@ optpxy_df = optpxy_df.sort_values(by='PL%', ascending=False)
 
 # Save the filtered and sorted DataFrame to CSV files
 nifty_df = optpxy_df[optpxy_df['tradingsymbol'].str.startswith('NIFTY')]
-nifty_df.to_csv('NIFTYOptions.csv', index=False)
+nifty_df.to_csv('NIFTYOPTS.csv', index=False)
 
 banknifty_df = optpxy_df[optpxy_df['tradingsymbol'].str.startswith('BANKNIFTY')]
-banknifty_df.to_csv('BANKNIFTYOptions.csv', index=False)
+banknifty_df.to_csv('BANKNIFTYOPTS.csv', index=False)
 
 print("Files have been created successfully.")
 
