423e264f9fdb8ed884c651c43c440ecea888c544
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-25 20:46:15 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -108,8 +108,10 @@ while True:
     subprocess.run(['python3', 'cntrloptpxy.py'])
     subprocess.run(['python3', 'buyboptpxy.py'])
     subprocess.run(['python3', 'buynoptpxy.py'])
-    subprocess.run(['python3', 'cntrloptprntpxy.py', '-s']) if run_type == 's' else None
-    subprocess.run(['python3', 'cntrloptprntpxy.py', '-l']) if run_type == 'l' else None
+    if run_type == 's':
+        subprocess.run(['python3', 'cntrloptprntpxy.py', 's'])
+    elif run_type == 'l':
+        subprocess.run(['python3', 'cntrloptprntpxy.py', 'l'])
     subprocess.run(['python3', 'worldpxy.py']) if run_type == 'l' else None
     subprocess.run(['python3', 'buycncpxy.py']) if peak == 'PEAKEND' and (mktpredict in ['RISE'] or Day_Change > 0 or Open_Change > 0) else None
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
