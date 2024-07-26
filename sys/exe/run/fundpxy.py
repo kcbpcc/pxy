61b415cc30ff5defdaf908c535505b6c58c55e23
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-26 10:15:12 +0800

    Update fundpxy.py

--- sys/exe/run/fundpxy.py
+++ sys/exe/run/fundpxy.py
@@ -43,6 +43,14 @@ def calculate_decision():
             utilized_margin = response["equity"]["utilised"]["debits"]
             available_cash = available_margin - utilized_margin
 
+            print(f"Opening Balance: {opening_balance}")
+            print(f"Live Balance: {live_balance}")
+            print(f"Delivery: {delivery}")
+            print(f"Option Premium: {option_premium}")
+            print(f"Available Margin: {available_margin}")
+            print(f"Utilized Margin: {utilized_margin}")
+            print(f"Available Cash: {available_cash/1000:.0f}K")
+
             #print(f"I have 💰💰💰💰{available_cash/1000:.0f}K💰💰💰 to buy stocks")
         except Exception as e:
             print(f"An error occurred: {e}")
