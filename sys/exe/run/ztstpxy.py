fab4ecbe8acc6553da48d756bf3c68934002b91b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-26 14:23:43 +0800

    Update ztstpxy.py

--- sys/exe/run/ztstpxy.py
+++ sys/exe/run/ztstpxy.py
@@ -102,7 +102,7 @@ def main():
     try:
         # Print initial info and cash available
         print("🍃🍃🍃 Lets Buy NIFTY VOLUME Stocks 🍃🍃🍃")
-        decision, optdecision, available_cash,1, limit = calculate_decision()
+        decision, optdecision, available_cash,fdf, limit = calculate_decision()
         print(f"     Cash:💰{available_cash:.2f}💵 | 🚦{decision}🚦 to Buy")
         
         # Prompt user for strike price
