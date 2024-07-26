c6aac35d065c4b7a40faf31dfbbe8a0f9f3c359d
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-26 18:55:20 +0800

    Update cntrloptprntpxy.py

--- sys/exe/run/cntrloptprntpxy.py
+++ sys/exe/run/cntrloptprntpxy.py
@@ -200,7 +200,7 @@ output_lines.append(
 )
 output_lines.append(
     left_aligned_format.format(f"{BRIGHT_YELLOW}B-{ratio_B}/N-{ratio_N}{RESET}") +
-    right_aligned_format.format(f"OPTS-DPnL:{BRIGHT_GREEN if (nifty_profit + bank_profit) > 0 else BRIGHT_RED}{nifty_profit + bank_profit}{RESET}")
+    right_aligned_format.format(f"Total-PnL:{BRIGHT_GREEN if (nifty_profit + bank_profit +booked) > 0 else BRIGHT_RED}{nifty_profit + bank_profit + booked}{RESET}")
 )
 
 full_output = '\n'.join(output_lines)
