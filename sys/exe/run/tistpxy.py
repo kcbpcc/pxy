3b8030ad9b28afeb8100e0ef4521902a74752394
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-27 08:31:35 +0800

    Update tistpxy.py

--- sys/exe/run/tistpxy.py
+++ sys/exe/run/tistpxy.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import pytz
-from trndpxy import  get_action
-nHA_ACTION, nHA_POWER, nDay_Change, nOpen_Change = get_action(^NSEI')
+from nftpxy import Day_Change
 from clorpxy import BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, RESET
 def print_current_datetime_in_ist():
     utc_now = datetime.utcnow()
@@ -15,12 +14,10 @@ def print_current_datetime_in_ist():
         f"🕛{ist_now.strftime('%A'):9}|"  # Day
         f"⏰{ist_now.strftime('%I:%M%p')}"  # Time (reset color)
     )
-    if nDay_Change > 0:
+    if Day_Change > 0:
         print(f"{BRIGHT_GREEN}{formatted_datetime}{RESET}")
-    elif nDay_Change < 0:
+    elif Day_Change < 0:
         print(f"{BRIGHT_RED}{formatted_datetime}{RESET}")
     else:
         print(f"{BRIGHT_YELLOW}{formatted_datetime}{RESET}")
 print_current_datetime_in_ist()
-
-
