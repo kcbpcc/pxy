aaa224b7752d47c902badacf8fd69d4e12e54817
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-27 11:19:08 +0800

    Update buynoptpxy.py

--- sys/exe/run/buynoptpxy.py
+++ sys/exe/run/buynoptpxy.py
@@ -1,109 +1,51 @@
 import traceback
 import sys
 import logging
+import telegram
 import asyncio
-from datetime import datetime, timedelta
 from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
 from strikpxy import get_prices
+from datetime import datetime, timedelta
+from thupxy import get_this_thursday
 from ordoptpxy import place_order
 from macdpxy import calculate_macd_signal
 from smapxy import check_index_status
 from mktpxy import get_market_check
+from rsnprntpxy import process_orders
 from exprpxy import month_expiry_date
 from hndmktpxy import hand
 from nftpxy import ha_nse_action, nse_power, Day_Change, Open_Change
-from predictpxy import predict_market_sentiment
-from bpredictpxy import predict_bnk_sentiment
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 
-# Initialize various modules and settings
 _, CE_Strike, PE_Strike, _ = get_prices()
 nsma = check_index_status('^NSEI')
 onemincandlesequance, mktpxy = get_market_check('^NSEI')
 showhand = hand(mktpxy)
-mktpredict = predict_market_sentiment()
-bmktpredict = predict_bnk_sentiment()
 
-bnkmaxcount = 15
-nftmaxcount = 24
-
-async def process_orders(broker, available_cash, CE_position_exists, PE_position_exists, CE_symbol, PE_symbol, count_CE, count_PE, mktpxy):
-    from ordoptpxy import place_order
-    from telinoptpxy import send_telegram_message
-
-    try:
-        await handle_CE_orders(broker, CE_position_exists, CE_symbol, count_CE, mktpxy, place_order, send_telegram_message, bnkmaxcount, nftmaxcount, bmktpredict)
-        await handle_PE_orders(broker, PE_position_exists, PE_symbol, count_PE, mktpxy, place_order, send_telegram_message, bnkmaxcount, nftmaxcount, bmktpredict)
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        logging.error(f"Error in process_orders: {str(e)}")
-
-async def handle_CE_orders(broker, CE_position_exists, CE_symbol, count_CE, mktpxy, place_order, send_telegram_message, bnkmaxcount, nftmaxcount, bmktpredict):
-    if not CE_position_exists and mktpxy == 'Buy':
-        quantity = determine_quantity(CE_symbol, count_CE, 'BANKNIFTY', 'NIFTY', bnkmaxcount, nftmaxcount, bmktpredict)
-        if quantity > 0:
-            await execute_order(broker, CE_symbol, quantity, place_order, send_telegram_message)
-            print_order_reason(CE_symbol, CE_position_exists, count_CE, 'Hold')
-        else:
-            print(f"Not placing order for {CE_symbol} as it is maxed out.")
+def construct_symbol(expiry_year, expiry_month, expiry_day, option_type):
+    if len(expiry_month) == 2 and expiry_month.startswith("0"):
+        expiry_month = expiry_month[1]
+    noptions = PE_Strike if option_type == "PE" else (CE_Strike if option_type == "CE" else None)
+    if noptions is None:
+        raise ValueError(f"Invalid option type: {option_type}")
+    if expiry_day is None:
+        return f"NIFTY{expiry_year}{expiry_month}{noptions}{option_type}"
     else:
-        print_order_reason(CE_symbol, CE_position_exists, count_CE, 'Hold')
-
-async def handle_PE_orders(broker, PE_position_exists, PE_symbol, count_PE, mktpxy, place_order, send_telegram_message, bnkmaxcount, nftmaxcount, bmktpredict):
-    if not PE_position_exists and mktpxy == 'Sell':
-        quantity = determine_quantity(PE_symbol, count_PE, 'BANKNIFTY', 'NIFTY', bnkmaxcount, nftmaxcount, bmktpredict)
-        if quantity > 0:
-            await execute_order(broker, PE_symbol, quantity, place_order, send_telegram_message)
-        else:
-            print(f"Not placing order for {PE_symbol} as it is maxed out.")
-    else:
-        print_order_reason(PE_symbol, PE_position_exists, count_PE, 'Hold')
-
-def determine_quantity(symbol, count, banknifty_prefix, nifty_prefix, bnkmaxcount, nftmaxcount, bmktpredict):
-    if symbol.startswith(banknifty_prefix) and count < bnkmaxcount:
-        if symbol.endswith("PE") and bmktpredict == "FALL":
-            return 15
-        elif symbol.endswith("CE") and bmktpredict == "RISE":
-            return 15
-        else:
-            return 15
-    elif symbol.startswith(nifty_prefix) and count < nftmaxcount:
-        return 25
-    else:
-        return 0
-
-async def execute_order(broker, symbol, quantity, place_order, send_telegram_message):
-    buy_order_placed, buy_order_id = await place_order(broker, symbol, 'BUY', 'NRML', quantity, 'MARKET')
-    if buy_order_placed:
-        await send_telegram_message(f"🛫🛫🛫 🌱🌱🌱 ENTRY order placed for {symbol} successfully.")
-        print(f"{symbol} BUY order placed successfully.")
-    else:
-        print(f"Failed to place BUY order for {symbol}")
-
-def print_order_reason(symbol, position_exists, count, action):
-    reason = f"|{action}|{'🥚' if position_exists else '🧺'}|"
-    reason += " MaxedOut" if count >= (bnkmaxcount if symbol.startswith('BANKNIFTY') else nftmaxcount) else ""
-    if reason:
-        print(f"{symbol}: {reason: >{39 - len(symbol)}}")
+        return f"NIFTY{expiry_year}{expiry_month}{expiry_day}{noptions}{option_type}"
 
 def count_positions_by_type(broker):
-    """
-    Counts the number of CE and PE positions from the broker's portfolio.
-    
-    :param broker: The broker instance used to fetch positions.
-    :return: Tuple containing the count of CE positions and PE positions.
-    """
-    try:
-        positions_response = broker.kite.positions()
-        positions_net = positions_response['net']
-        count_CE = sum(1 for pos in positions_net if pos['tradingsymbol'].endswith('CE') and abs(pos['quantity']) >= 25)
-        count_PE = sum(1 for pos in positions_net if pos['tradingsymbol'].endswith('PE') and abs(pos['quantity']) >= 25)
-        return count_CE, count_PE
-    except Exception as e:
-        print(f"Error counting positions: {e}")
-        logging.error(f"Error counting positions: {e}")
-        return 0, 0
+    positions_response = broker.kite.positions()
+    positions_net = positions_response['net']
+    count_CE = 0
+    count_PE = 0
+    for position in positions_net:
+        if position['tradingsymbol'].startswith('NIFTY') and abs(position['quantity']) >= 25:
+            if position['tradingsymbol'].endswith('CE'):
+                count_CE += 1
+            elif position['tradingsymbol'].endswith('PE'):
+                count_PE += 1
+    return count_CE, count_PE
 
 def check_existing_positions(broker, symbol):
     positions_response = broker.kite.positions()
@@ -113,14 +55,6 @@ def check_existing_positions(broker, symbol):
             return True
     return False
 
-def construct_symbol(expiry_year, expiry_month, expiry_day, strike_price, option_type):
-    if len(expiry_month) == 2 and expiry_month.startswith("0"):
-        expiry_month = expiry_month[1]
-    if expiry_day is None:
-        return f"NIFTY{expiry_year}{expiry_month}{strike_price}{option_type}"
-    else:
-        return f"NIFTY{expiry_year}{expiry_month}{expiry_day}{strike_price}{option_type}"
-
 async def main():
     try:
         # Redirect sys.stdout to 'output.txt'
@@ -129,12 +63,10 @@ async def main():
 
             try:
                 broker = get_kite()
-                print("Broker instance obtained successfully.")
             except Exception as e:
                 remove_token(dir_path)
                 print(traceback.format_exc())
                 logging.error(f"{str(e)} unable to get holdings")
-                print(f"Error obtaining broker instance: {e}")
                 sys.exit(1)
 
     finally:
@@ -146,43 +78,36 @@ async def main():
         decision, optdecision, available_cash, live_balance, limit = calculate_decision()
 
         count_CE, count_PE = count_positions_by_type(broker)
-        print(f"Positions count - CE: {count_CE}, PE: {count_PE}")
+        PE_weight = count_PE - count_CE
+        CE_weight = count_CE - count_PE
+        weight = abs(count_PE - count_CE)
 
+        print(f"{BRIGHT_YELLOW}{count_PE:02}📉:PE positions💧N-{showhand}🔥CE positions:📈{count_CE:02}{RESET}")
+        
         expiry_year, expiry_month, expiry_day = month_expiry_date()
-        print(f"Expiry date - Year: {expiry_year}, Month: {expiry_month}, Day: {expiry_day}")
 
-        async def process_multiple_orders(symbols, option_type):
-            print(f"Processing multiple orders for option type: {option_type}")
-            for symbol in symbols:
-                position_exists = check_existing_positions(broker, symbol)
-                await process_orders(broker, available_cash, position_exists, position_exists, symbol, symbol, count_CE, count_PE, mktpxy) 
-                print(f"Processed order for symbol: {symbol}")
+        CE_symbol = construct_symbol(expiry_year, expiry_month, expiry_day, 'CE')
+        PE_symbol = construct_symbol(expiry_year, expiry_month, expiry_day, 'PE')
 
-        if mktpredict == "RISE":
-            CE_symbols = [construct_symbol(expiry_year, expiry_month, None, str(CE_Strike + i), 'CE') for i in [0, 100, 200]]
-            print(f"RISE strategy - CE symbols: {CE_symbols}")
-            await process_multiple_orders(CE_symbols, "CE")
+        CE_position_exists = check_existing_positions(broker, CE_symbol)
+        PE_position_exists = check_existing_positions(broker, PE_symbol)
 
-            PE_symbol = construct_symbol(expiry_year, expiry_month, None, str(PE_Strike), 'PE')
-            print(f"RISE strategy - Processing PE symbol: {PE_symbol}")
-            PE_position_exists = check_existing_positions(broker, PE_symbol)
-            await process_orders(broker, available_cash, PE_position_exists, PE_position_exists, PE_symbol, PE_symbol, count_CE, count_PE, mktpxy) 
-
-        elif mktpredict == "FALL":
-            PE_symbols = [construct_symbol(expiry_year, expiry_month, None, str(PE_Strike + i), 'PE') for i in [0, 100, 200]]
-            print(f"FALL strategy - PE symbols: {PE_symbols}")
-            await process_multiple_orders(PE_symbols, "PE")
-
-            CE_symbol = construct_symbol(expiry_year, expiry_month, None, str(CE_Strike), 'CE')
-            print(f"FALL strategy - Processing CE symbol: {CE_symbol}")
-            CE_position_exists = check_existing_positions(broker, CE_symbol)
-            await process_orders(broker, available_cash, CE_position_exists, CE_position_exists, CE_symbol, CE_symbol, count_CE, count_PE, mktpxy) 
+        if mktpxy in ("Buy", "Sell"):
+            await process_orders(broker, available_cash, CE_position_exists, PE_position_exists, CE_symbol, PE_symbol, count_CE, count_PE, mktpxy)
 
     except Exception as e:
-        print(f"An error occurred during main execution: {e}")
-        logging.error(f"An error occurred during main execution: {e}")
+        print(f"Error: {e}")
+        logging.error(f"Error in main(): {e}")
 
-# Run the main function
+async def run_main():
+    await main()
+
+# Run the asynchronous function using asyncio.run()
+def sync_main():
+    asyncio.run(run_main())
+
+sync_main()
+
 if __name__ == '__main__':
     asyncio.run(main())
 
