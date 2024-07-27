10fef5fc44ba8e6a9e2ad6e719922ec3a77e2497
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-28 01:57:26 +0800

    Update ztstpxy.py

--- sys/exe/run/ztstpxy.py
+++ sys/exe/run/ztstpxy.py
@@ -1,2 +1,103 @@
+import sys
+import traceback
+import pandas as pd
+import requests
+import logging
+from login_get_kite import get_kite, remove_token
+from cnstpxy import dir_path
+from cmbddfpxy import process_data
+from smapxy import check_index_status
+from utcpxy import peak_time
+from depthpxy import calculate_consecutive_candles
+from mktpxy import get_market_check
+from predictpxy import predict_market_sentiment
+from bpredictpxy import predict_bnk_sentiment
+mktpredict = predict_market_sentiment()
+bmktpredict = predict_bnk_sentiment()
+bonemincandlesequance, bmktpxy = get_market_check('^NSEBANK')
+nonemincandlesequance, nmktpxy = get_market_check('^NSEI')
+from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 
+bsma = check_index_status('^NSEBANK')
+nsma = check_index_status('^NSEI')
+arrow_map = {"Buy": "↗", "Sell": "↘", "Bull": "↑", "Bear": "↓"}
+peak = peak_time()
+
+bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'
+user_usernames = ('-4282665161',)
+
+def send_telegram_message(message):
+    try:
+        for username in user_usernames:
+            url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+            payload = {'chat_id': username, 'text': message}
+            response = requests.post(url, data=payload)
+            if response.status_code != 200:
+                print(f"Failed to send Telegram message. Status code: {response.status_code}")
+            else:
+                print("Telegram message sent successfully.")
+    except Exception as e:
+        print(f"Error sending Telegram message: {e}")
+
+def place_order(tradingsymbol, quantity, transaction_type, order_type, product, broker):
+    try:
+        order_id = broker.order_place(
+            tradingsymbol=tradingsymbol,
+            quantity=quantity,
+            exchange='NFO',
+            transaction_type=transaction_type,
+            order_type=order_type,
+            product=product
+        )
+        print(f"Order placed successfully. Order ID: {order_id}")
+        return order_id
+    except Exception as e:
+        print(f"Error placing order: {e}")
+        return None
+
+def exit_options(blnc_opt_df, broker):
+    total_opt_pnl = calculate_totals(blnc_opt_df)
+    try:
+        for index, row in blnc_opt_df.iterrows():
+            total_pl_percentage = row['PL%']
+            tgtoptsmadepth = row['tgtoptsmadepth']
+            
+            if total_pl_percentage > tgtoptsmadepth and row['PnL'] > 400:
+                place_order(row['key'], row['qty'], 'SELL', 'MARKET', 'NRML', broker)
+                message = (
+                    f"🛬🛬🛬 🎯🎯🎯 EXIT order placed {row['key']} successfully.\n"
+                    f"🎯 Target PL%: {round(tgtoptsmadepth, 4)}%\n"
+                    f"🏆 Reached PL%: {round(total_pl_percentage, 2)}%\n"
+                    f"📉 Sell Price: {row['ltp']}\n"
+                    f"📈 Buy Price: {row['avg']}\n"
+                    f"💰 Booked Profit: {row['PnL']}\n"
+                    f"Total Booked:💰 {total_opt_pnl} 📣"
+                )
+                print(message)
+                send_telegram_message(message)
+    except Exception as e:
+        print(f"Error placing exit order: {e}")
+
+try:
+    sys.stdout = open('output.txt', 'w')
+    broker = get_kite()
+except Exception as e:
+    remove_token(dir_path)
+    print(traceback.format_exc())
+    logging.error(f"{str(e)} unable to get holdings")
+    sys.exit(1)
+finally:
+    if sys.stdout != sys.__stdout__:
+        sys.stdout.close()
+        sys.stdout = sys.__stdout__
+
+combined_df = process_data()
+blnc_opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
+blnc_opt_df['key'] = blnc_opt_df['key'].str.replace('NFO:', '') 
+blnc_opt_df['PL%'] = (blnc_opt_df['PnL'] / blnc_opt_df['Invested']) * 100
+blnc_opt_df['PL%'] = blnc_opt_df['PL%'].fillna(0)
+
+blnc_opt_df['strike'] = blnc_opt_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
+
+print(blnc_opt_df.to_string(index=False, col_space=10, header=True))
 
