633bffeeae2746e08401be2302e4c63f4f7475c4
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-28 20:31:32 +0800

    Update buyboptpxy.py

--- sys/exe/run/buyboptpxy.py
+++ sys/exe/run/buyboptpxy.py
@@ -87,8 +87,8 @@ async def main():
         PE_weight = count_PE - count_CE
         CE_weight = count_CE - count_PE
         weight = abs(count_PE - count_CE)
-
-        print(f"{BRIGHT_YELLOW}{count_PE:02}📉:PE positions💧B-{showhand}🔥CE positions:📈{count_CE:02}{RESET}")
+        strike_price = get_prices()[1]  # Assuming this returns the current strike price
+        print(f"{BRIGHT_YELLOW}{count_PE:02} 📉:PE 🔥━━━━━ {strike_price} {showhand} ━━━━━💧 CE:📈 {count_CE:02}{RESET}")
 
         expiry_year, expiry_month, expiry_day = month_expiry_date()
 
