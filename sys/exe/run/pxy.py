f47087900b0db78690122571e1c3ce0c49de70d9
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-29 15:13:34 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -114,6 +114,7 @@ while True:
     elif run_type == 'l':
         subprocess.run(['python3', 'extoptpxy.py', 'l'])
         subprocess.run(['python3', 'cntrloptprntpxy.py', 'l'])
+    subprocess.run(['python3', 'cntrlcncpxy.py']) if run_type == 'l' else subprocess.run(['python3', 'cntrlcncpxy.py', '-short'])
     subprocess.run(['python3', 'worldpxy.py']) if run_type == 'l' else None
     subprocess.run(['python3', 'buycncpxy.py']) if peak == 'PEAKEND' and (mktpredict in ['RISE'] or Day_Change > 0 or Open_Change > 0) else None
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
@@ -125,7 +126,6 @@ while True:
     subprocess.run(['python3', 'bdaypxy.py'])  if run_type == 'l' else None  
     subprocess.run(['python3', 'bcndlpxy.py']) if run_type == 'l' else None  
     (lambda: print((BRIGHT_GREEN if bsma == "up" else BRIGHT_RED if bsma == "down" else BRIGHT_YELLOW) + "ﮩ٨ﮩ٨ـﮩ٨ﮩ٨ـﮩ٨ـﮩﮩ٨BANKNIFTY٨ﮩ٨ـﮩ٨ـﮩﮩ٨ﮩ٨ـﮩ٨ﮩ٨" + RESET))() if run_type == 'l' and 'bsma' in locals() else None
-    subprocess.run(['python3', 'cntrlcncpxy.py']) if run_type == 'l' else subprocess.run(['python3', 'cntrlcncpxy.py', '-short'])
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
     print((BRIGHT_GREEN + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'RISE' else (BRIGHT_RED + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'FALL' else (BRIGHT_YELLOW + UNDERLINE + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if mktpredict == 'SIDE' else ""))) + RESET)
     subprocess.run(['python3', 'selfpxy.py'])
