c897cadc4e433b7936973266bb131b014d1eddef
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-29 19:01:24 +0800

    Update bpredictpxy.py

--- sys/exe/run/bpredictpxy.py
+++ sys/exe/run/bpredictpxy.py
@@ -3,17 +3,13 @@ from smapxy import check_index_status
 
 def predict_bnk_sentiment():
     ha_nse_action, nse_power, Day_Change, Open_Change = get_bnk_action()
-    nsma = check_index_status('^NSEBANK')
-    Bearish = "Bearish"
-    Bullish = "Bullish"
-
-    if ha_nse_action == Bearish and nsma == "down":
-        mktpredict = "FALL"
-    elif ha_nse_action == Bullish and nsma == "up":
-        mktpredict = "RISE"
+    sma = check_index_status('^NSEBANK')
+    
+    if Open_Change < 0 and sma == "down":
+        bmktpredict = "FALL"
+    elif Open_Change > 0 and sma == "up":
+        bmktpredict = "RISE"
     else:
-        mktpredict = "SIDE"
+        bmktpredict = "SIDE"
 
-    return mktpredict
-
-
+    return bmktpredict
