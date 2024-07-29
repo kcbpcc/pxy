e386378307fe042531686468f06e96a2c36a0126
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-29 18:58:18 +0800

    Update predictpxy.py

--- sys/exe/run/predictpxy.py
+++ sys/exe/run/predictpxy.py
@@ -4,18 +4,12 @@ from smapxy import check_index_status
 def predict_market_sentiment():
     ha_nse_action, nse_power, Day_Change, Open_Change = get_nse_action()
     nsma = check_index_status('^NSEI')
-    Bearish = "Bearish"
-    Bullish = "Bullish"
 
-    if ha_nse_action == Bearish and nsma == "down":
+    if Open_Change < 0 and nsma == "down":
         mktpredict = "FALL"
-    elif ha_nse_action == Bullish and nsma == "up":
+    elif Open_Change > 0 and nsma == "up":
         mktpredict = "RISE"
     else:
         mktpredict = "SIDE"
 
     return mktpredict
-
-# Call the function to predict market sentiment and print the result
-predicted_sentiment = predict_market_sentiment()
-#print(predicted_sentiment)
