25a1aeabae6e35b7590ac64712842e922ff834be
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-01 05:46:17 +0800

    Update cntrlcoptpxy.py

--- sys/exe/run/cntrlcoptpxy.py
+++ sys/exe/run/cntrlcoptpxy.py
@@ -14,7 +14,26 @@ from predictpxy import predict_market_sentiment
 from bpredictpxy import predict_bnk_sentiment
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 
+# Common configurations
+bot_token = 'YOUR_BOT_TOKEN'
+user_usernames = ('YOUR_USER_ID',)
+
 # Common functions
+def initialize_broker():
+    try:
+        sys.stdout = open('output.txt', 'w')
+        broker = get_kite()
+        return broker
+    except Exception as e:
+        remove_token(dir_path)
+        print(traceback.format_exc())
+        logging.error(f"{str(e)} unable to get holdings")
+        sys.exit(1)
+    finally:
+        if sys.stdout != sys.__stdout__:
+            sys.stdout.close()
+            sys.stdout = sys.__stdout__
+
 def calculate_totals(combined_df):
     if not combined_df.empty:
         extras_df = combined_df[(combined_df['exchange'] == 'NFO') & (combined_df['sell_quantity'] > 0)].copy()
@@ -23,7 +42,7 @@ def calculate_totals(combined_df):
         total_opt_pnl = 0
     return total_opt_pnl
 
-def send_telegram_message(bot_token, user_usernames, message):
+def send_telegram_message(message):
     try:
         for username in user_usernames:
             url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
@@ -52,10 +71,10 @@ def place_order(broker, tradingsymbol, quantity, transaction_type, order_type, p
         print(f"Error placing order: {e}")
         return None
 
-def exit_options(exe_opt_df, broker, bot_token, user_usernames):
+def exit_options(exe_opt_df, broker):
     total_opt_pnl = calculate_totals(exe_opt_df)
     try:
-        for index, row in exe_opt_df.iterrows():
+        for _, row in exe_opt_df.iterrows():
             total_pl_percentage = row['PL%']
             tgtoptsmadepth = row['tgtoptsmadepth']
             
@@ -71,7 +90,7 @@ def exit_options(exe_opt_df, broker, bot_token, user_usernames):
                     f"Total Booked:💰 {total_opt_pnl} 📣"
                 )
                 print(message)
-                send_telegram_message(bot_token, user_usernames, message)
+                send_telegram_message(message)
     except Exception as e:
         print(f"Error placing exit order: {e}")
 
@@ -82,3 +101,33 @@ def format_row(row, widths):
     tgtoptsmadepth = f"{row['tgtoptsmadepth']:.1f}".rjust(int(widths['tgtoptsmadepth']))
     pnl = f"{int(row['PnL'])}".rjust(int(widths['PnL']))
     return f"{symbol}{qty}{pl_pct}{tgtoptsmadepth}{pnl}"
+
+def prepare_data_and_apply_depth(symbol, df, sma, vix):
+    df['key'] = df['key'].str.replace('NFO:', '')
+    df['PL%'] = (df['PnL'] / df['Invested']) * 100
+    df['PL%'] = df['PL%'].fillna(0)
+    df['strike'] = df['key'].str.replace(r'(PE|CE)$', '', regex=True)
+
+    def compute_tgtoptsma(row):
+        if (sma == "up" and "CE" in row['key']) or (sma == "down" and "PE" in row['key']):
+            return 7
+        else:
+            return 5
+
+    df['tgtoptsma'] = df.apply(compute_tgtoptsma, axis=1)
+    cedepth, pedepth = calculate_consecutive_candles(symbol)
+
+    def compute_depth(row):
+        try:
+            if "CE" in row['key'] and cedepth > 1:
+                return max(row['tgtoptsma'], (vix + 9 - cedepth))
+            elif "PE" in row['key'] and pedepth > 1:
+                return max(row['tgtoptsma'], (vix + 9 - pedepth))
+            else:
+                return 5
+        except Exception as e:
+            return 5
+
+    df['tgtoptsmadepth'] = df.apply(compute_depth, axis=1)
+    return df
+
