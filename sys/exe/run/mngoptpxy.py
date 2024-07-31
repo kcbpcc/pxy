a00e657d861e8b0903a4e54daa5121cc79ca4128
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-31 11:13:19 +0800

    Update mngoptpxy.py

--- sys/exe/run/mngoptpxy.py
+++ sys/exe/run/mngoptpxy.py
@@ -2,39 +2,39 @@ import sys
 import traceback
 import pandas as pd
 import requests
-import logging
-from datetime import datetime, timedelta
+from datetime import datetime
 import calendar
+import argparse
 from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
 from cmbddfpxy import process_data
-from smapxy import check_index_status
-from utcpxy import peak_time
-from depthpxy import calculate_consecutive_candles
-from mktpxy import get_market_check
-from predictpxy import predict_market_sentiment
-from bpredictpxy import predict_bnk_sentiment
 from expdaypxy import get_last_weekday_of_current_month
-from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
-import argparse
+from clorpxy import (
+    SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, 
+    BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
+)
+from mktpxy import get_market_check
+from bftpxy import get_bnk_action
+from nftpxy import get_nse_action  # Ensure correct import
+
+# Argument parsing
 parser = argparse.ArgumentParser(description="Process some commands.")
 parser.add_argument('command', nargs='?', choices=['l', 's'], default='s',
                     help="Command to run the program with. Defaults to 's' if not provided.")
 args = parser.parse_args()
 
 # Define function to get last weekday dates
-last_wednesday_str = get_last_weekday_of_current_month(calendar.WEDNESDAY)
-last_thursday_str = get_last_weekday_of_current_month(calendar.THURSDAY)
+def get_last_weekday_dates():
+    last_wednesday_str = get_last_weekday_of_current_month(calendar.WEDNESDAY)
+    last_thursday_str = get_last_weekday_of_current_month(calendar.THURSDAY)
+    current_year = datetime.now().year
 
-# Define the current year
-current_year = datetime.now().year
+    def parse_date(date_str):
+        return datetime.strptime(f"{date_str}-{current_year}", '%d-%b-%Y')
 
-# Convert string dates to datetime objects with current year
-def parse_date(date_str):
-    return datetime.strptime(f"{date_str}-{current_year}", '%d-%b-%Y')
+    return parse_date(last_wednesday_str), parse_date(last_thursday_str)
 
-last_wednesday = parse_date(last_wednesday_str)
-last_thursday = parse_date(last_thursday_str)
+last_wednesday, last_thursday = get_last_weekday_dates()
 
 def business_days_diff(start_date, end_date):
     """Calculate business days between two dates."""
@@ -44,10 +44,10 @@ def business_days_diff(start_date, end_date):
 
 def send_telegram_message(message):
     bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'
-    user_usernames = ('-4282665161',)
+    user_usernames = ['-4282665161']
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     try:
         for username in user_usernames:
-            url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
             payload = {'chat_id': username, 'text': message}
             response = requests.post(url, data=payload)
             if response.status_code != 200:
@@ -59,6 +59,10 @@ def send_telegram_message(message):
 
 def place_order(tradingsymbol, quantity, transaction_type, order_type, product, broker):
     try:
+        if quantity <= 0:
+            print(f"Skipping order for {tradingsymbol} due to non-positive quantity.")
+            return None
+        
         print(f"Placing order for {tradingsymbol} with quantity {quantity}")
         order_id = broker.order_place(
             tradingsymbol=tradingsymbol,
@@ -80,13 +84,13 @@ def exit_options(blnc_opt_df, broker):
             print("Target column is missing in the DataFrame.")
             return
 
+        blnc_opt_df = blnc_opt_df[blnc_opt_df['qty'] > 0]  # Ensure no zero qty rows
+
         for index, row in blnc_opt_df.iterrows():
             total_pl_percentage = row['PL%']
             tgtoptsmadepth = row['Target']
             
-            #print(f"Checking conditions for {row['key']}: PL% = {total_pl_percentage}, Target = {tgtoptsmadepth}")
             if total_pl_percentage > tgtoptsmadepth:
-                #print(f"Conditions met for {row['key']}, placing order")
                 place_order(row['key'], row['qty'], 'SELL', 'MARKET', 'NRML', broker)
                 message = (
                     f"🛬🛬🛬 😧😧😧 EXIT order placed {row['key']} successfully.\n"
@@ -96,80 +100,135 @@ def exit_options(blnc_opt_df, broker):
                 )
                 print(message)
                 send_telegram_message(message)
-            else:
-                pass
-                #print(f"Conditions not met for {row['key']}, skipping order")
     except Exception as e:
-        print(f"Error placing exit order: {e}")
+        print(f"Error placing EXIT order: {e}")
 
-try:
-    sys.stdout = open('output.txt', 'w')
-    broker = get_kite()
-except Exception as e:
-    remove_token(dir_path)
-    print(traceback.format_exc())
-    logging.error(f"{str(e)} unable to get holdings")
-    sys.exit(1)
-finally:
-    if sys.stdout != sys.__stdout__:
-        sys.stdout.close()
-        sys.stdout = sys.__stdout__
+# Main logic
+def main():
+    try:
+        sys.stdout = open('output.txt', 'w')
+        broker = get_kite()
+    except Exception as e:
+        remove_token(dir_path)
+        print(traceback.format_exc())
+        logging.error(f"{str(e)} unable to get holdings")
+        sys.exit(1)
+    finally:
+        if sys.stdout != sys.__stdout__:
+            sys.stdout.close()
+            sys.stdout = sys.__stdout__
 
-combined_df = process_data()
-blnc_opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
-blnc_opt_df['key'] = blnc_opt_df['key'].str.replace('NFO:', '') 
-blnc_opt_df['PL%'] = (blnc_opt_df['PnL'] / blnc_opt_df['Invested']) * 100
-blnc_opt_df['PL%'] = blnc_opt_df['PL%'].fillna(0)
+    combined_df = process_data()
+    if combined_df.empty:
+        print("Combined DataFrame is empty.")
+        return
 
-blnc_opt_df['strike'] = blnc_opt_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
+    blnc_opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
+    blnc_opt_df['key'] = blnc_opt_df['key'].str.replace('NFO:', '', regex=False)
+    blnc_opt_df['PL%'] = (blnc_opt_df['PnL'] / blnc_opt_df['Invested']) * 100
+    blnc_opt_df['PL%'] = blnc_opt_df['PL%'].fillna(0)
 
-# Get the current month's abbreviation
-current_month_abbr = datetime.now().strftime('%b').upper()  # e.g., 'JAN', 'FEB', 'MAR'
+    blnc_opt_df['strike'] = blnc_opt_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
 
-# Select only the key, qty, tradingsymbol, Invested, value, and PL% columns and filter by current month's abbreviation
-selected_df = blnc_opt_df[['key', 'qty', 'Invested', 'value', 'PL%', 'PnL']]
-selected_df.columns = ['key', 'qty', 'Invested', 'value', 'PL%', 'PnL']  # Rename columns for clarity
-filtered_df = selected_df[selected_df['key'].str.contains(current_month_abbr)].copy()
+    # Get the current month's abbreviation
+    current_month_abbr = datetime.now().strftime('%b').upper()
 
-# Add date column based on trading symbol
-def add_date(row):
-    if row['key'].startswith('BANKNIFTY'):
-        return last_wednesday  # Return full date
-    elif row['key'].startswith('NIFTY'):
-        return last_thursday  # Return full date
-    else:
-        return None
+    blnc_opt_df = blnc_opt_df[['key', 'qty', 'Invested', 'value', 'PL%', 'PnL']]
+    blnc_opt_df = blnc_opt_df[blnc_opt_df['qty'] > 0]
+    blnc_opt_df = blnc_opt_df[blnc_opt_df['key'].str.contains(current_month_abbr)].copy()
+    blnc_opt_df = blnc_opt_df.dropna(how='all')
 
-filtered_df.loc[:, 'Date'] = filtered_df.apply(add_date, axis=1)
+    def add_date(row):
+        if row['key'].startswith('BANKNIFTY'):
+            return last_wednesday
+        elif row['key'].startswith('NIFTY'):
+            return last_thursday
+        else:
+            return None
 
-# Add 'Today' column with the current date
-filtered_df.loc[:, 'Today'] = datetime.now()
+    blnc_opt_df['Date'] = blnc_opt_df.apply(add_date, axis=1)
+    blnc_opt_df['Today'] = datetime.now()
+    blnc_opt_df['Diff'] = blnc_opt_df.apply(lambda row: business_days_diff(row['Date'], row['Today']), axis=1)
 
-# Add 'Diff' column showing the difference in working days between 'Date' and 'Today'
-filtered_df.loc[:, 'Diff'] = filtered_df.apply(lambda row: business_days_diff(row['Date'], row['Today']), axis=1)
+    blnc_opt_df['Date'] = blnc_opt_df['Date'].dt.day
+    blnc_opt_df['Today'] = blnc_opt_df['Today'].dt.day
 
-# Extract day for 'Date' and 'Today'
-filtered_df.loc[:, 'Date'] = filtered_df['Date'].dt.day
-filtered_df.loc[:, 'Today'] = filtered_df['Today'].dt.day
+    blnc_opt_df['Target'] = blnc_opt_df['Diff'].apply(lambda x: (100 - (x * 9)) * -1 if x < 10 else 107)
+    blnc_opt_df = blnc_opt_df[blnc_opt_df['qty'] > 0]
 
-# Add 'Target' column with the specified condition
-filtered_df.loc[:, 'Target'] = filtered_df['Diff'].apply(lambda x: (100 - (x * 9)) * -1 if x < 10 else 105)
+    final_df = blnc_opt_df[blnc_opt_df['Target'] < 0][['key', 'qty', 'Invested', 'value', 'PL%', 'PnL', 'Date', 'Today', 'Diff', 'Target']]
+    row_count = final_df.shape[0]
+    sum_invested = final_df['Invested'].sum()
+    print("━" * 42)
+    print(f"{UNDERLINE}{RED}🤔..🤔..Recovering {str(row_count).zfill(2)} opts worth {str(sum_invested).zfill(7)}🤔{RESET}")
 
-# Reorder columns as requested
-final_df = filtered_df[filtered_df['qty'] > 0][['key', 'qty', 'Invested', 'value', 'PL%', 'PnL', 'Date', 'Today', 'Diff', 'Target']]
-print(final_df)
-row_count = final_df.shape[0]
-sum_invested = final_df['Invested'].sum()
-print("━" * 42)
-print(f"🤔..🤔..Recovering {YELLOW}{str(row_count).zfill(2)}{RESET} opts worth {YELLOW}{str(sum_invested).zfill(7)}{RESET}🤔")
-filtered_df['PL%'] = filtered_df['PL%'].astype(int)
-final_prnt_df = filtered_df[['key', 'qty', 'PL%', 'Target', 'PnL']].copy()
+    blnc_ex_prnt_df = (
+        blnc_opt_df.query('Target < 0')
+        .assign(PL_percent=blnc_opt_df['PL%'].astype(int))
+        .rename(columns={'PL_percent': 'PL%'})
+        .assign(key=lambda x: x['key'].str.replace('BANKNIFTY', 'B').str.replace('NIFTY', 'N'))
+        .fillna({'qty': 0, 'PL%': 0, 'Target': 0, 'PnL': 0})
+        .astype({'qty': 'int', 'PL%': 'int', 'Target': 'int', 'PnL': 'int'})
+        [['key', 'qty', 'PL%', 'Target', 'PnL']]
+    )
 
-if args.command == 'l':
-    final_prnt_str = final_prnt_df.to_string(index=False, header=False)
-    right_aligned_output = '\n'.join([line.rjust(42) for line in final_prnt_str.split('\n')])
-    print(right_aligned_output)
+    if args.command == 'l':
+        final_prnt_str = blnc_ex_prnt_df.to_string(index=False, header=False)
+        right_aligned_output = '\n'.join([line.rjust(42) for line in final_prnt_str.split('\n')])
+        print(f"{GREY}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━{RESET}")
+        print(right_aligned_output)
 
+    # Market checks
+    bbnkonemincandlesequance, bmktpxy = get_market_check('^NSEBANK')
+    nonemincandlesequance, mktpxy = get_market_check('^NSEI')
 
-# Call the function to exit options
-exit_options(final_df, broker)
+    ha_bnk_action, bnk_power, bDay_Change, bOpen_Change = get_bnk_action()
+    ha_nse_action, nse_power, Day_Change, Open_Change = get_nse_action()
+
+    # Function to place buy orders based on PL%
+    def place_buy_orders_based_on_pl(df, broker):
+        try:
+            for index, row in df.iterrows():
+                if row['PL%'] < -50:
+                    qty = 0
+                    can_average = False
+
+                    if row['key'].startswith('BANKNIFTY'):
+                        current_qty = row['qty']
+                        if current_qty < 30 and current_qty + 15 <= 45:
+                            qty = 15
+                            if 'PE' in row['key']:
+                                can_average = bnk_power > 0.85 and bmktpxy == 'Sell'
+                            elif 'CE' in row['key']:
+                                can_average = bnk_power < 0.15 and bmktpxy == 'Buy'
+                    elif row['key'].startswith('NIFTY'):
+                        current_qty = row['qty']
+                        if current_qty < 50 and current_qty + 25 <= 75:
+                            qty = 25
+                            if 'PE' in row['key']:
+                                can_average = nse_power > 0.85 and mktpxy == 'Sell'
+                            elif 'CE' in row['key']:
+                                can_average = nse_power < 0.15 and mktpxy == 'Buy'
+                    else:
+                        continue
+
+                    if can_average and qty > 0:
+                        print(f"Placing BUY order for {row['key']} with quantity {qty}")
+                        order_id = place_order(row['key'], qty, 'BUY', 'MARKET', 'NRML', broker)
+
+                        if order_id:
+                            message = (
+                                f"🚀🚀🚀 🤑🤑🤑 BUY order placed {row['key']} successfully.\n"
+                                f"PL%: {round(row['PL%'], 2)}%\n"
+                                f"Quantity: {qty}\n"
+                            )
+                            print(message)
+                            send_telegram_message(message)
+        except Exception as e:
+            print(f"Error placing BUY order: {e}")
+
+    # Call the function to place buy orders
+    place_buy_orders_based_on_pl(final_df, broker)
+
+if __name__ == "__main__":
+    main()
