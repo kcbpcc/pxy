6a4ddb316576e04b4978ebea43e830b434978701
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-31 21:36:58 +0800

    Update mngoptpxy.py

--- sys/exe/run/mngoptpxy.py
+++ sys/exe/run/mngoptpxy.py
@@ -152,7 +152,8 @@ final_df = blnc_opt_df[blnc_opt_df['Target'] < 0][['key', 'qty', 'Invested', 'va
 row_count = final_df.shape[0]
 sum_invested = final_df['Invested'].sum()
 
-print(f"{GREY}🤔..🤔..Recovering {str(row_count).zfill(2)} opts worth {str(sum_invested).zfill(7)}🤔{RESET}")
+#print(f"{GREY}🤔..🤔..Recovering {str(row_count).zfill(2)} opts worth {str(sum_invested).zfill(7)}🤔{RESET}")
+print("━" * 42)
 width = 42
 line1 = f"B:{last_wednesday_str}"
 line2 = f"N:{last_thursday_str}"
@@ -178,7 +179,6 @@ if args.command == 'l':
     final_prnt_str = blnc_ex_prnt_df.to_string(index=False, header=False)
     right_aligned_output = '\n'.join([line.rjust(42) for line in final_prnt_str.split('\n')])
     print(right_aligned_output)
-    print("━" * 42)
 
 # Call the function to exit options
 exit_options(final_df, broker)
