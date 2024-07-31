a8ebcaba29603a30c62d19f8aa0b43258d51646b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-07-31 10:55:38 +0800

    Update mngoptpxy.py

--- sys/exe/run/mngoptpxy.py
+++ sys/exe/run/mngoptpxy.py
@@ -153,7 +153,17 @@ filtered_df.loc[:, 'Date'] = filtered_df['Date'].dt.day
 filtered_df.loc[:, 'Today'] = filtered_df['Today'].dt.day
 
 # Add 'Target' column with the specified condition
-filtered_df.loc[:, 'Target'] = filtered_df['Diff'].apply(lambda x: 99 if x == 0 else (100 - (x * 9)) * -1 if x < 10 else 105)
+def calculate_target(x):
+    print(f"x: {x}")  # Print the value of x
+    if x == 0:
+        return 99
+    elif x < 10:
+        return (100 - (x * 9)) * -1
+    else:
+        return 105
+
+filtered_df.loc[:, 'Target'] = filtered_df['Diff'].apply(calculate_target)
+
 
 # Reorder columns as requested
 final_df = filtered_df[filtered_df['qty'] > 0][['key', 'qty', 'Invested', 'value', 'PL%', 'PnL', 'Date', 'Today', 'Diff', 'Target']]
