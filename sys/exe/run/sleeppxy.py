0978629454bab54479e98a2a48b072467158ee33
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-01 20:45:59 +0800

    Update sleeppxy.py

--- sys/exe/run/sleeppxy.py
+++ sys/exe/run/sleeppxy.py
@@ -7,13 +7,13 @@ def progress_bar(duration, mktpxy):
     for _ in range(int(duration)):
         time.sleep(1)
         if mktpxy == 'Buy':
-            console.print('[green]PXY®[/]👆', end='')  # Up arrow + handshake
+            console.print('[green]🏛 PXY®[/]👆', end='')  # Up arrow + handshake
         elif mktpxy == 'Sell':
-            console.print('[red]PXY®[/]👇', end='')  # Down arrow + handshake
+            console.print('[red]🏛 PXY®[/]👇', end='')  # Down arrow + handshake
         elif mktpxy == 'Bull':
-            console.print('[green]PXY®[/]🟢', end='')  # Right arrow + handshake
+            console.print('[green]🏛 PXY®[/]🟢', end='')  # Right arrow + handshake
         elif mktpxy == 'Bear':
-            console.print('[red]PXY®[/]🔴', end='')  # Left arrow + handshake
+            console.print('[red]🏛 PXY®[/]🔴', end='')  # Left arrow + handshake
         else:
-            console.print('[yellow]PXY®[/]🤝', end='')  # Neutral with handshake
+            console.print('[yellow]🏛 PXY®[/]🤝', end='')  # Neutral with handshake
     console.print()
