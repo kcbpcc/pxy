b18b165daccee577feb0f8df3f5189d212e0d6dc
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-02 13:25:19 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -105,6 +105,7 @@ while True:
         print(f"Error handling index status: {e}")
         nsma, bsma = None, None
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
+    print(" " * 42)
     print((BRIGHT_GREEN + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if ha_nse_action == 'Bullish' else BRIGHT_RED + "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42) if ha_nse_action == 'Bearish' else "🏛 PXY® PreciseXceleratedYield Pvt Ltd™ 🏛".center(42)) + RESET)
     subprocess.run(['python3', 'tistpxy.py']) 
     subprocess.run(['python3', 'acvalchrtpxy.py']) if peak == 'PEAKSTART' else None
