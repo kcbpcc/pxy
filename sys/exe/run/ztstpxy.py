f4f9646fc663aee513af9783a27cb114e2e46a87
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-02 09:15:06 +0800

    Update ztstpxy.py

--- sys/exe/run/ztstpxy.py
+++ sys/exe/run/ztstpxy.py
@@ -1,225 +1,80 @@
-import sys
 import traceback
-import pandas as pd
-import requests
+import sys
 import logging
-from datetime import datetime
-import calendar
+from kiteconnect import KiteConnect
 from login_get_kite import get_kite, remove_token
 from cnstpxy import dir_path
-from cmbddfpxy import process_data
-from smapxy import check_index_status
-from utcpxy import peak_time
-from depthpxy import calculate_consecutive_candles
-from mktpxy import get_market_check
 from predictpxy import predict_market_sentiment
-from bpredictpxy import predict_bnk_sentiment
-from expdaypxy import get_last_weekday_of_current_month
+
+# Import color constants
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
-import argparse
 
-# Command-line argument parsing
-parser = argparse.ArgumentParser(description="Process some commands.")
-parser.add_argument('command', nargs='?', choices=['l', 's'], default='s',
-                    help="Command to run the program with. Defaults to 's' if not provided.")
-args = parser.parse_args()
+# Initialize constants
+mktpredict = predict_market_sentiment()
 
-# Define function to get last weekday dates
-last_wednesday_str = get_last_weekday_of_current_month(calendar.WEDNESDAY)
-last_thursday_str = get_last_weekday_of_current_month(calendar.THURSDAY)
+def construct_symbols(expiry_year, expiry_month, option_type, strike_price):
+    symbols = []
+    if option_type == "CE":
+        symbols.append(f"NIFTY{expiry_year}{expiry_month}{strike_price}CE")
+        symbols.append(f"NIFTY{expiry_year}{expiry_month}{strike_price + 100}CE")
+        symbols.append(f"NIFTY{expiry_year}{expiry_month}{strike_price + 200}CE")
+    elif option_type == "PE":
+        symbols.append(f"NIFTY{expiry_year}{expiry_month}{strike_price}PE")
+        symbols.append(f"NIFTY{expiry_year}{expiry_month}{strike_price - 100}PE")
+        symbols.append(f"NIFTY{expiry_year}{expiry_month}{strike_price - 200}PE")
+    return symbols
 
-# Define the current year
-current_year = datetime.now().year
+def get_option_prices(symbols, kite):
+    prices = {}
+    for symbol in symbols:
+        try:
+            # Fetch the LTP (Last Traded Price) for the symbol
+            ltp = kite.ltp(f"NFO:{symbol}")["NFO:{symbol}"]["last_price"]
+            prices[symbol] = ltp
+        except Exception as e:
+            print(f"Error fetching price for {symbol}: {e}")
+            prices[symbol] = float('inf')  # Set to infinity if price fetch fails
+    return prices
 
-# Convert string dates to datetime objects with current year
-def parse_date(date_str):
-    return datetime.strptime(f"{date_str}-{current_year}", '%d-%b-%Y')
+def find_cheapest_symbol(prices):
+    # Find the symbol with the lowest price
+    cheapest_symbol = min(prices, key=prices.get)
+    return cheapest_symbol
 
-last_wednesday = parse_date(last_wednesday_str)
-last_thursday = parse_date(last_thursday_str)
-
-def business_days_diff(start_date, end_date):
-    """Calculate business days between two dates."""
-    if start_date > end_date:
-        start_date, end_date = end_date, start_date
-    return len(pd.bdate_range(start_date, end_date))
-
-def send_telegram_message(message):
-    bot_token = '7141714085:AAHlyEzszCy9N-L6wO1zSAkRwGdl0VTQCFI'
-    user_usernames = ('-4282665161',)
+def main():
     try:
-        for username in user_usernames:
-            url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-            payload = {'chat_id': username, 'text': message}
-            response = requests.post(url, data=payload)
-            if response.status_code != 200:
-                print(f"Failed to send Telegram message. Status code: {response.status_code}")
-            else:
-                print("Telegram message sent successfully.")
-    except Exception as e:
-        print(f"Error sending Telegram message: {e}")
+        # Redirect sys.stdout to 'output.txt'
+        with open('output.txt', 'w') as file:
+            sys.stdout = file
 
-def place_order(tradingsymbol, quantity, transaction_type, order_type, product, broker):
-    try:
-        print(f"Placing order for {tradingsymbol} with quantity {quantity}")
-        order_id = broker.order_place(
-            tradingsymbol=tradingsymbol,
-            quantity=quantity,
-            exchange='NFO',
-            transaction_type=transaction_type,
-            order_type=order_type,
-            product=product
-        )
-        print(f"Order placed successfully. Order ID: {order_id}")
-        return order_id
-    except Exception as e:
-        print(f"Error placing order: {e}")
-        return None
+            try:
+                # Log in to Kite Connect
+                kite = get_kite()
+            except Exception as e:
+                # If login fails, remove token and log the error
+                remove_token(dir_path)
+                print(traceback.format_exc())
+                logging.error(f"{str(e)} unable to log in to Kite Connect")
+                sys.exit(1)
 
-def exit_options(blnc_opt_df, broker):
-    try:
-        if 'Target' not in blnc_opt_df.columns:
-            print("Target column is missing in the DataFrame.")
-            return
+            # Define your parameters
+            expiry_year = "24"
+            expiry_month = "08"
+            option_type = "CE"
+            strike_price = 25000  # Updated strike price
 
-        for index, row in blnc_opt_df.iterrows():
-            total_pl_percentage = row['PL%']
-            tgtoptsmadepth = row['Target']
-            
-            if total_pl_percentage > tgtoptsmadepth:
-                place_order(row['key'], row['qty'], 'SELL', 'MARKET', 'NRML', broker)
-                message = (
-                    f"🛬🛬🛬 😧😧😧 EXIT order placed {row['key']} successfully.\n"
-                    f"🎯 Target PL%: {round(tgtoptsmadepth, 4)}%\n"
-                    f"🏆 Reached PL%: {round(total_pl_percentage, 2)}%\n"
-                    f"💰 Booked Profit: {row['PnL']}\n"
-                )
-                print(message)
-                send_telegram_message(message)
-    except Exception as e:
-        print(f"Error placing exit order: {e}")
+            # Construct symbols and get their prices
+            symbols = construct_symbols(expiry_year, expiry_month, option_type, strike_price)
+            prices = get_option_prices(symbols, kite)
 
-try:
-    sys.stdout = open('output.txt', 'w')
-    broker = get_kite()
-except Exception as e:
-    remove_token(dir_path)
-    print(traceback.format_exc())
-    logging.error(f"{str(e)} unable to get holdings")
-    sys.exit(1)
-finally:
-    if sys.stdout != sys.__stdout__:
-        sys.stdout.close()
+            # Find the cheapest symbol
+            cheapest_symbol = find_cheapest_symbol(prices)
+            print(f"The cheapest symbol is: {cheapest_symbol}")
+
+    finally:
+        # Reset sys.stdout to its default value
         sys.stdout = sys.__stdout__
 
-combined_df = process_data()
-blnc_opt_df = combined_df[combined_df['key'].str.contains('NFO:', case=False)].copy()
-blnc_opt_df['key'] = blnc_opt_df['key'].str.replace('NFO:', '') 
-blnc_opt_df['PL%'] = (blnc_opt_df['PnL'] / blnc_opt_df['Invested']) * 100
-blnc_opt_df['PL%'] = blnc_opt_df['PL%'].fillna(0)
-
-blnc_opt_df['strike'] = blnc_opt_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
-
-# Get the current month's abbreviation
-current_month_abbr = datetime.now().strftime('%b').upper()
-
-# Select only the key, qty, tradingsymbol, Invested, value, and PL% columns and filter by current month's abbreviation
-selected_df = blnc_opt_df[['key', 'qty', 'Invested', 'value', 'PL%', 'PnL']]
-selected_df.columns = ['key', 'qty', 'Invested', 'value', 'PL%', 'PnL']  # Rename columns for clarity
-filtered_df = selected_df[selected_df['key'].str.contains(current_month_abbr)].copy()
-
-# Add date column based on trading symbol
-def add_date(row):
-    if row['key'].startswith('BANKNIFTY'):
-        return last_wednesday
-    elif row['key'].startswith('NIFTY'):
-        return last_thursday
-    else:
-        return None
-
-filtered_df['Date'] = filtered_df.apply(add_date, axis=1)
-
-# Add 'Today' column with the current date
-filtered_df['Today'] = datetime.now()
-
-# Add 'Diff' column showing the difference in working days between 'Date' and 'Today'
-filtered_df['Diff'] = filtered_df.apply(lambda row: business_days_diff(row['Date'], row['Today']), axis=1)
-
-# Extract day for 'Date' and 'Today'
-filtered_df['Date'] = filtered_df['Date'].dt.day
-filtered_df['Today'] = filtered_df['Today'].dt.day
-
-# Add 'Target' column with the specified condition
-filtered_df['Target'] = filtered_df['Diff'].apply(lambda x: (100 - (x * 9)) * -1 if x < 10 else 105)
-
-# Reorder columns as requested
-final_df = filtered_df[filtered_df['qty'] > 0][['key', 'qty', 'Invested', 'value', 'PL%', 'PnL', 'Date', 'Today', 'Diff', 'Target']]
-row_count = final_df.shape[0]
-sum_invested = final_df['Invested'].sum()
-print("━" * 42)
-print(f"🤔..🤔..Recovering {YELLOW}{str(row_count).zfill(2)}{RESET} opts worth {YELLOW}{str(sum_invested).zfill(7)}{RESET}🤔")
-filtered_df['PL%'] = filtered_df['PL%'].astype(int)
-final_prnt_df = filtered_df[['key', 'qty', 'PL%', 'Target', 'PnL']].copy()
-
-if args.command == 'l':
-    final_prnt_str = final_prnt_df.to_string(index=False, header=False)
-    right_aligned_output = '\n'.join([line.rjust(42) for line in final_prnt_str.split('\n')])
-    print(right_aligned_output)
-
-# Import market check and action functions
-bbnkonemincandlesequance, bmktpxy = get_market_check('^NSEBANK')
-nonemincandlesequance, mktpxy = get_market_check('^NSEI')
-
-from bftpxy import get_bnk_action
-ha_bnk_action, bnk_power, bDay_Change, bOpen_Change = get_bnk_action()
-
-from nftpxy import ha_nse_action, nse_power, Day_Change, Open_Change
-
-# Additional logic to place buy orders for BANKNIFTY and NIFTY based on 'PL%' < -50
-def place_buy_orders_based_on_pl(df, broker):
-    try:
-        for index, row in df.iterrows():
-            if row['PL%'] < -50:
-                qty = 0
-                can_average = False
-
-                if row['key'].startswith('BANKNIFTY'):
-                    current_qty = row['qty']
-                    if current_qty < 30 and current_qty + 15 <= 45:
-                        qty = 15
-                        if 'PE' in row['key']:
-                            can_average = bnk_power > 0.85 and bmktpxy == 'Sell'
-                        elif 'CE' in row['key']:
-                            can_average = bnk_power < 0.15 and bmktpxy == 'Buy'
-                elif row['key'].startswith('NIFTY'):
-                    current_qty = row['qty']
-                    if current_qty < 50 and current_qty + 25 <= 75:
-                        qty = 25
-                        if 'PE' in row['key']:
-                            can_average = nse_power > 0.85 and mktpxy == 'Sell'
-                        elif 'CE' in row['key']:
-                            can_average = nse_power < 0.15 and mktpxy == 'Buy'
-                else:
-                    continue
-
-                if can_average:
-                    print(f"Placing BUY order for {row['key']} with quantity {qty}")
-                    order_id = place_order(row['key'], qty, 'BUY', 'MARKET', 'NRML', broker)
-
-                    if order_id:
-                        message = (
-                            f"🚀🚀🚀 🤑🤑🤑 BUY order placed {row['key']} successfully.\n"
-                            f"PL%: {round(row['PL%'], 2)}%\n"
-                            f"Quantity: {qty}\n"
-                        )
-                        print(message)
-                        send_telegram_message(message)
-                else:
-                    pass
-                    #print(f"Skipping BUY order for {row['key']} due to bnk_power, market conditions, or qty limit.")
-    except Exception as e:
-        print(f"Error placing BUY order: {e}")
-
-# Call the function to place buy orders
-place_buy_orders_based_on_pl(final_df, broker)
+if __name__ == "__main__":
+    main()
 
