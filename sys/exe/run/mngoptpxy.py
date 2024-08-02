b185aefd7aa3efaa0b0ee276ca93caa21857b0a5
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-02 15:04:15 +0800

    Update mngoptpxy.py

--- sys/exe/run/mngoptpxy.py
+++ sys/exe/run/mngoptpxy.py
@@ -11,8 +11,6 @@ from expdaypxy import get_last_weekday_of_current_month
 import argparse
 from clorpxy import SILVER, UNDERLINE, RED, GREEN, YELLOW, RESET, BRIGHT_YELLOW, BRIGHT_RED, BRIGHT_GREEN, BOLD, GREY
 from nftpxy import ha_nse_action, nse_power, Day_Change, Open_Change  
-from bftpxy import get_bnk_action
-bha_nse_action, bnse_power, bDay_Change, bOpen_Change = get_bnk_action()
 from mktpxy import get_market_check
 onemincandlesequance, mktpxy = get_market_check('^NSEI')
 bnkonemincandlesequance, bmktpxy = get_market_check('^NSEBANK')
@@ -111,9 +109,9 @@ def avg_options(df, broker):
                 if current_qty < 30 and current_qty + 15 <= 45:
                     qty = 15
                     if 'PE' in row['key']:
-                        can_average = bnse_power > 0.85 and bmktpxy == 'Sell'
+                        can_average = bnk_power > 0.85 and bmktpxy == 'Sell'
                     elif 'CE' in row['key']:
-                        can_average = bnse_power < 0.15 and bmktpxy == 'Buy'
+                        can_average = bnk_power < 0.15 and bmktpxy == 'Buy'
 
             elif row['key'].startswith('NIFTY'):
                 current_qty = row['qty']
@@ -215,15 +213,14 @@ print(f"{SILVER}{combined_lines:^{width}}{RESET}")
 # Ensure DataFrames are not empty before processing
 if not ext_df.empty:
     ext_options(ext_df, broker)
-    print(ext_df)
+    #print(ext_df)
 else:
     pass
     #print("No options to exit.")
 if not avg_df.empty:
     avg_options(avg_df, broker)
-    print(avg_df)
+    #print(avg_df)
 else:
     pass
     #print("No options to average.")
-
 
