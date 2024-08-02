b4d17a4611c4504a7d0b2cd3e75ed4a7ff2e2b5b
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-02 23:05:55 +0800

    Update buyboptpxy.py

--- sys/exe/run/buyboptpxy.py
+++ sys/exe/run/buyboptpxy.py
@@ -100,56 +100,56 @@ async def main():
 
 
 
-if bmktpredict == "SIDE":
-    if mktpxy == "Buy":
-        if CE_position_exists:
-            print(f"{CE_symbol} exists")
-        else:
-            print(f"{CE_symbol} does not exist")
-            # Process orders only if CE_position_exists is False
-            await process_orders(broker, available_cash, CE_position_exists, False, CE_symbol, None, count_CE, count_PE, mktpxy)
-
-    elif mktpxy == "Sell":
-        if PE_position_exists:
-            print(f"{PE_symbol} exists")
-        else:
-            print(f"{PE_symbol} does not exist")
-            # Process orders only if PE_position_exists is False
-            await process_orders(broker, available_cash, False, PE_position_exists, None, PE_symbol, count_CE, count_PE, mktpxy)
-
-elif bmktpredict == "RISE":
-    if mktpxy == "Buy":
-        if CE_position_exists:
-            print(f"{CE_symbol} exists")
-        else:
-            print(f"{CE_symbol} does not exist")
-            # Process orders only if CE_position_exists is False
-            await process_orders(broker, available_cash, CE_position_exists, False, CE_symbol, None, count_CE, count_PE, mktpxy)
-
-    elif mktpxy == "Sell":
-        if nse_power > 0.85:
-            if PE_position_exists:
-                print(f"{PE_symbol} exists and nse_power > 0.85")
-            else:
-                print(f"{PE_symbol} does not exist and nse_power > 0.85")
-                # Process orders only if PE_position_exists is False
-                await process_orders(broker, available_cash, False, PE_position_exists, None, PE_symbol, count_CE, count_PE, mktpxy)
-
-elif bmktpredict == "FALL":
-    if mktpxy == "Buy":
-        if not CE_position_exists:
-            if nse_power < 0.15:
-                print(f"{CE_symbol} does not exist and nse_power < 0.15")
-                # Process orders only if CE_position_exists is False
-                await process_orders(broker, available_cash, CE_position_exists, False, CE_symbol, None, count_CE, count_PE, mktpxy)
-
-    elif mktpxy == "Sell":
-        if PE_position_exists:
-            print(f"{PE_symbol} exists")
-        else:
-            print(f"{PE_symbol} does not exist")
-            # Process orders only if PE_position_exists is False
-            await process_orders(broker, available_cash, False, PE_position_exists, None, PE_symbol, count_CE, count_PE, mktpxy)
+        if bmktpredict == "SIDE":
+            if mktpxy == "Buy":
+                if CE_position_exists:
+                    print(f"{CE_symbol} exists")
+                else:
+                    print(f"{CE_symbol} does not exist")
+                    # Process orders only if CE_position_exists is False
+                    await process_orders(broker, available_cash, CE_position_exists, False, CE_symbol, None, count_CE, count_PE, mktpxy)
+        
+            elif mktpxy == "Sell":
+                if PE_position_exists:
+                    print(f"{PE_symbol} exists")
+                else:
+                    print(f"{PE_symbol} does not exist")
+                    # Process orders only if PE_position_exists is False
+                    await process_orders(broker, available_cash, False, PE_position_exists, None, PE_symbol, count_CE, count_PE, mktpxy)
+        
+        elif bmktpredict == "RISE":
+            if mktpxy == "Buy":
+                if CE_position_exists:
+                    print(f"{CE_symbol} exists")
+                else:
+                    print(f"{CE_symbol} does not exist")
+                    # Process orders only if CE_position_exists is False
+                    await process_orders(broker, available_cash, CE_position_exists, False, CE_symbol, None, count_CE, count_PE, mktpxy)
+        
+            elif mktpxy == "Sell":
+                if nse_power > 0.85:
+                    if PE_position_exists:
+                        print(f"{PE_symbol} exists and nse_power > 0.85")
+                    else:
+                        print(f"{PE_symbol} does not exist and nse_power > 0.85")
+                        # Process orders only if PE_position_exists is False
+                        await process_orders(broker, available_cash, False, PE_position_exists, None, PE_symbol, count_CE, count_PE, mktpxy)
+        
+        elif bmktpredict == "FALL":
+            if mktpxy == "Buy":
+                if not CE_position_exists:
+                    if nse_power < 0.15:
+                        print(f"{CE_symbol} does not exist and nse_power < 0.15")
+                        # Process orders only if CE_position_exists is False
+                        await process_orders(broker, available_cash, CE_position_exists, False, CE_symbol, None, count_CE, count_PE, mktpxy)
+        
+            elif mktpxy == "Sell":
+                if PE_position_exists:
+                    print(f"{PE_symbol} exists")
+                else:
+                    print(f"{PE_symbol} does not exist")
+                    # Process orders only if PE_position_exists is False
+                    await process_orders(broker, available_cash, False, PE_position_exists, None, PE_symbol, count_CE, count_PE, mktpxy)
 
 
 
