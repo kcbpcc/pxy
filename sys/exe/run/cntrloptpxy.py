b5a1a161c612bc5a819d103dfc7585efb01913f8
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-03 09:39:04 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -85,11 +85,11 @@ def exit_options(exe_opt_df, broker):
                 place_order(row['key'], row['qty'], 'SELL', 'MARKET', 'NRML', broker)
                 message = (
                     f"🛬🛬🛬 🎯🎯 EXIT {row['key']} 🎯\n"
-                    f"🎯 Target PL%: {round(tgtoptsmadepth, 4)}%\n"
-                    f"🏆 Reached PL%: {round(total_pl_percentage, 2)}%\n"
-                    f"📉 Sell Price: {row['ltp']}\n"
-                    f"📈 Buy Price: {row['avg']}\n"
-                    f"          💰 Booked Profit: {row['PnL']}📣"
+                    f"   🎯 Target PL%: {round(tgtoptsmadepth, 4)}%\n"
+                    f"   🏆 Reached PL%: {round(total_pl_percentage, 2)}%\n"
+                    f"   📉 Sell Price: {row['ltp']}\n"
+                    f"   📈 Buy Price: {row['avg']}\n"
+                    f"           💰 Booked Profit: {row['PnL']}📣"
                 )
                 print(message)
                 send_telegram_message(message)
