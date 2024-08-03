a7924af8cb48570c76c059e7b4663402ff26c618
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-03 09:39:39 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -89,7 +89,7 @@ def exit_options(exe_opt_df, broker):
                     f"   🏆 Reached PL%: {round(total_pl_percentage, 2)}%\n"
                     f"   📉 Sell Price: {row['ltp']}\n"
                     f"   📈 Buy Price: {row['avg']}\n"
-                    f"           💰 Booked Profit: {row['PnL']}📣"
+                    f"               💰 Booked Profit: {row['PnL']}📣"
                 )
                 print(message)
                 send_telegram_message(message)
