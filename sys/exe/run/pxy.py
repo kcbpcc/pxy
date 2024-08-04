12d920cf1db1646f94802a7804954b1ace993734
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-04 22:31:25 +0800

    Update pxy.py

--- sys/exe/run/pxy.py
+++ sys/exe/run/pxy.py
@@ -196,8 +196,7 @@ while True:
     ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################    ############################################"PXY® PreciseXceleratedYield Pvt Ltd™############################################ 
     print("━" * 42)
     if mktpredict in ['FALL', 'SIDE']:
-        print(f"{'💥💥  જ⁀➴ CNC Action - NIFTY got down  જ⁀➴':>38}{RESET}")
-        print(f"{GREY}{'✅ ✅ No Action - NIFTY on RISE  🆙 🆙':>38}{RESET}")
+        print(f"{'જ⁀➴  જ⁀➴ CNC Action - NIFTY got down  💥💥':>38}{RESET}")
         subprocess.run(['python3', 'cntrlcncpxy.py'])
     else:
         print(f"{'✅ ✅ No Action - NIFTY on RISE  🆙 🆙':>38}{RESET}")
