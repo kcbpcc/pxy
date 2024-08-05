fff5f03a74029941303efeecf86d836b0c59e9b0
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-05 14:33:01 +0800

    Update mngoptpxy.py

--- sys/exe/run/mngoptpxy.py
+++ sys/exe/run/mngoptpxy.py
@@ -197,7 +197,7 @@ print(f"{header_line:^{width}}")
 
 
 avg_nifty_df = blnc_opt_df[
-    (blnc_opt_df['PL%'] < -33) &
+    (blnc_opt_df['PL%'] < -66) &
     (blnc_opt_df['key'].str.startswith('NIFTY'))
 ]
 
