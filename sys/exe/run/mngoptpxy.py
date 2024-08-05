9624b3509c649c83c0366ff56fa71e3a32e19270
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-05 14:38:06 +0800

    Update mngoptpxy.py

--- sys/exe/run/mngoptpxy.py
+++ sys/exe/run/mngoptpxy.py
@@ -201,6 +201,6 @@ avg_nifty_df = blnc_opt_df[
     (blnc_opt_df['key'].str.startswith('NIFTY'))
 ]
 
-print(avg_nifty_df)
+print(avg_nifty_df[['key', 'qty', 'PL%']])
 
 avg_options(avg_nifty_df , broker)
