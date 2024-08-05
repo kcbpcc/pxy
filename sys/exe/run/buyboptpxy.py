33b1a03789cc5a297e969ff4b121772d60029bb7
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-05 18:41:32 +0800

    Update buyboptpxy.py

--- sys/exe/run/buyboptpxy.py
+++ sys/exe/run/buyboptpxy.py
@@ -138,7 +138,7 @@ async def main():
                                 print(f"{PE_symbol} not there, let's Buy")
                                 await process_orders(broker, available_cash, False, PE_position_exists, None, PE_symbol, count_CE, count_PE, mktpxy)
                         else:
-                            print(f"bnk_power:{bnk_power} is not low enough, let's skip")
+                            print(f"bnk_power:{bnk_power} is not low enough, skipping")
                 
                 elif bmktpredict == "FALL":
                     if mktpxy == "Buy":
@@ -149,7 +149,7 @@ async def main():
                                 print(f"{CE_symbol} not there, let's Buy")
                                 await process_orders(broker, available_cash, CE_position_exists, False, CE_symbol, None, count_CE, count_PE, mktpxy)
                         else:
-                            print(f"bnk_power:{bnk_power} is not low enough, let's skip")
+                            print(f"bnk_power:{bnk_power} is not low enough, skipping")
                     elif mktpxy == "Sell":
                         if PE_position_exists:
                             print(f"{PE_symbol} is there, let's skip")
