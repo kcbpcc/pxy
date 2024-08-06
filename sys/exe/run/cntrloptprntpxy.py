0571517c56e52d219b17fe43ecb0719007cf964e
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-06 22:57:45 +0800

    Update cntrloptprntpxy.py

--- sys/exe/run/cntrloptprntpxy.py
+++ sys/exe/run/cntrloptprntpxy.py
@@ -181,7 +181,7 @@ output_lines.append(left_aligned_format.format(f"F&O-tCap:{BRIGHT_YELLOW}{round(
 output_lines.append(
     left_aligned_format.format(
         f"{'A/C-tCap'.zfill(7)}:{BRIGHT_YELLOW}{str(round(CnC_tCap_rounded + (total_invested_all / 100000), 2)).zfill(5)}"
-        f"{BRIGHT_RED if mktpredict == 'FALL' else GREY if mktpredict == 'SIDE' else BRIGHT_GREEN}     {BOLD}PXY{RESET}"
+        f"{BRIGHT_RED if mktpredict == 'FALL' else GREY if mktpredict == 'SIDE' else BRIGHT_GREEN}    {BOLD}PXY{RESET}"
     ) +
     right_aligned_format.format(
         f"{BRIGHT_GREEN if nmktpxy in ['Bull'] else (BRIGHT_RED if nmktpxy in ['Bear'] else GREY)}"
