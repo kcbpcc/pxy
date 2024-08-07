5b881e35b514b0c2cf9a7346f0fd78703cc3843d
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-07 13:56:23 +0800

    Update mktpxy.py

--- sys/exe/run/mktpxy.py
+++ sys/exe/run/mktpxy.py
@@ -17,7 +17,7 @@ END_TIME = 245
 
 def fetch_data(symbol):
     # Fetch real-time data for the specified interval and symbol
-    data = yf.Ticker(symbol).history(period="5d", interval="1m")
+    data = yf.Ticker(symbol).history(period="5d", interval="3m")
     return data
 
 def calculate_heikin_ashi_colors(data):
