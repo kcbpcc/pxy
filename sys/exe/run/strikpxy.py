3fb0b360c271e88e5a4de164944d1a3db6d4ba2e
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-08 19:50:21 +0800

    Update strikpxy.py

--- sys/exe/run/strikpxy.py
+++ sys/exe/run/strikpxy.py
@@ -1,4 +1,3 @@
-delta = -100
 import yfinance as yf
 import warnings
 from datetime import datetime, timedelta
@@ -19,23 +18,27 @@ def get_day_open(symbol):
 def round_to_nearest_100(price):
     return round(price / 100) * 100
 
-def round_to_nearest_200(price):
-    return round(price / 200) * 200
-
-def round_to_nearest_100_or_50(price):
-    return round(price / 50) * 50 if price % 100 < 50 else round(price / 100) * 100
-
 today = datetime.now()
-days_left_until_thursday = 4 #(3 - today.weekday()) % 7  # Thursday is weekday 3
+days_left_until_thursday = 4  # 4 days left until Thursday
 #print("Days remaining until Thursday:", days_left_until_thursday)    
 
 def get_prices():
-    BCE_Strike = get_day_open('^NSEBANK'))
+    BCE_Strike = round_to_nearest_100(get_day_open('^NSEBANK'))
     CE_Strike = round_to_nearest_100(get_current_price('^NSEI'))
     PE_Strike = round_to_nearest_100(get_current_price('^NSEI'))
-    BPE_Strike = get_day_open('^NSEBANK'))
-                                    
+    BPE_Strike = round_to_nearest_100(get_day_open('^NSEBANK'))
+    
     return BCE_Strike, CE_Strike, PE_Strike, BPE_Strike
-    #print("CE_Strike:", CE_Strike, "PE_Strike:", BPE_Strike)
-#print(get_prices())
+
+def print_prices():
+    BCE_Strike, CE_Strike, PE_Strike, BPE_Strike = get_prices()
+    
+    print(f"BCE_Strike: {BCE_Strike}")
+    print(f"CE_Strike: {CE_Strike}")
+    print(f"PE_Strike: {PE_Strike}")
+    print(f"BPE_Strike: {BPE_Strike}")
+
+# Run the function to print prices
+print_prices()
+
 
