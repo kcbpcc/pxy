667c012c7b46df05768b544871fac62b96c02a21
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-09 07:29:40 +0800

    Update worldpxy.py

--- sys/exe/run/worldpxy.py
+++ sys/exe/run/worldpxy.py
@@ -1,10 +1,10 @@
-#PXY® 
-#print("━" * 42)
 import yfinance as yf
 from rich.console import Console
 import warnings
+
 # Suppress warnings
 warnings.filterwarnings("ignore")
+
 # Function to determine sentiment based on closing prices
 def calculate_sentiment(today_close, yesterday_close):
     if today_close is not None and yesterday_close is not None:
@@ -16,6 +16,7 @@ def calculate_sentiment(today_close, yesterday_close):
             return "Neutral"
     else:
         return "Data Unavailable"
+
 # Dictionary of major stock exchanges with weights based on their significance
 exchanges = {
     "^DJI": {"name": "D&J", "weight": 0.35},
@@ -53,19 +54,12 @@ for name, price_today in closing_prices_today.items():
         price_yesterday = closing_prices_yesterday[name]
         sentiment = calculate_sentiment(price_today, price_yesterday)
         sentiment_style = "green" if sentiment == "Bullish" else "red" if sentiment == "Bearish" else "default"
-        index_info += f"[{sentiment_style}]{name}[/{sentiment_style}]|"
+        
+        # Convert GIFT price to an integer
+        if name == "GIFT":
+            price_today = int(price_today)
+        
+        index_info += f"[{sentiment_style}]{name}[/{sentiment_style}] {price_today}|"
 
-price_today = None
-
-for exchange, name_weight in exchanges.items():
-    ticker = yf.Ticker(exchange)
-    if exchange == "^NSEI":  # Check if the exchange is NIFTY
-        price_today_float = ticker.history(period="1d")['Close'].iloc[-1]  # Fetch today's last trade price
-        # Extract the last four digits of the integer part
-        price_today = int(str(int(price_today_float))[-4:])
-        break  # Exit loop once NIFTY price is found
-
-# Concatenate index_info and price_today into a single string
-output = index_info
-# Print the concatenated string using console.print()
-console.print(output)
+# Concatenate index_info into a single string and print
+console.print(index_info)
