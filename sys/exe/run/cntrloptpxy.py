6ce153a18a794ac0414ebf844aa9c1208f3e0894
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-09 23:45:32 +0800

    Update cntrloptpxy.py

--- sys/exe/run/cntrloptpxy.py
+++ sys/exe/run/cntrloptpxy.py
@@ -193,10 +193,10 @@ next_month_abbr = calendar.month_abbr[next_month].upper()  # e.g., 'SEP'
 # Assuming `opt_df` is already defined earlier in the code
 print_df = exe_opt_df.copy()
 print_df = print_df[print_df['PL%'] > 0]
-print_df['CP'] = print_df['key'].apply(lambda x: '🟠' if x.endswith('PE') else ('🟢' if x.endswith('CE') else None))
+print_df['CP'] = print_df['key'].apply(lambda x: 'PE' if x.endswith('PE') else ('CE' if x.endswith('CE') else None))
 print_df['group'] = print_df['key'].str.extract(r'^(B|N)', expand=False)
 print_df['key'] = print_df['key'].str.replace('BANKNIFTY24', 'B').str.replace('NIFTY24', 'N')
-#print_df['strike'] = print_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
+print_df['strike'] = print_df['key'].str.replace(r'(PE|CE)$', '', regex=True)
 print_df['PL%'] = print_df['PL%'].round(2)
 print_df['tgtoptsmadepth'] = print_df['tgtoptsmadepth'].round(2)
 # Set 'MN' based on the month abbreviation in 'key'
