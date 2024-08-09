5ba1675bd54ee94e194593c0965eb1657e7ea180
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-09 19:43:18 +0800

    Update lncepepxy.py

--- sys/exe/run/lncepepxy.py
+++ sys/exe/run/lncepepxy.py
@@ -33,9 +33,9 @@ def format_investments(total_invested_pe, total_invested_ce):
     
     # Determine the direction of the arrow emoji
     if pe_width > ce_width:
-        arrow = "🏃🏻‍♂️‍➡️"  # Pointing to the left
+        arrow = "🏃🏻"  # Pointing to the left
     else:
-        arrow = "🏃🏻"  # Pointing to the right
+        arrow = "🏃🏻‍♂️‍➡️"  # Pointing to the right
     
     # Combine the bars with the divider and emoji
     formatted_output = pe_bar + arrow   +  "⚽"+ ce_bar
