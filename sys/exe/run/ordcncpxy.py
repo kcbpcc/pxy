e22c3bb9ddb6aeee827f5f5e927b28e74540de55
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-11 07:28:31 +0800

    Update ordcncpxy.py

--- sys/exe/run/ordcncpxy.py
+++ sys/exe/run/ordcncpxy.py
@@ -49,7 +49,7 @@ def place_buy_order(random_symbol):
         # Determine purchase limit
         purchase_limit = 0
         if random_symbol in holdings_symbols and random_symbol not in orders_symbols and random_symbol not in positions_symbols:
-            purchase_limit = 1000
+            purchase_limit = 0
         elif random_symbol not in holdings_symbols and random_symbol not in orders_symbols and random_symbol not in positions_symbols:
             purchase_limit = 2000
 
