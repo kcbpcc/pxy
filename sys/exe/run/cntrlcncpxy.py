2fd2b1aa8ab1ee8d218119a9754fcefdd6528372
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-10 08:39:40 +0800

    Update cntrlcncpxy.py

--- sys/exe/run/cntrlcncpxy.py
+++ sys/exe/run/cntrlcncpxy.py
@@ -326,6 +326,7 @@ try:
                         row['Invested'] < 50000 and
                         available_cash > 1000 and
                         nse_power < 0.15 and
+                        peak == 'PEAKEND' and
                         row['dPL%'] < -1
                     ):
                         try:
