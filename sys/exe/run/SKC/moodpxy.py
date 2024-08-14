17d92e08cc52092ec9c33791663d62fb6ddce584
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-15 04:31:27 +0530

    Update moodpxy.py

--- sys/exe/run/SKC/moodpxy.py
+++ sys/exe/run/SKC/moodpxy.py
@@ -2,4 +2,6 @@ from smapxy import calculate_sma50
 sma_trend = calculate_sma50(data)
 
 from daypxy import calculate_day_metrics
+data = fetch_data(^NSEI)
+metrics = calculate_day_metrics(data)
 metrics = calculate_day_metrics(data)
