d2d7dfe9db87907fd580820b4fc70836fbacdd24
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-14 21:12:34 +0530

    Update ztestskc.py

--- sys/exe/run/SKC/ztestskc.py
+++ sys/exe/run/SKC/ztestskc.py
@@ -144,6 +144,6 @@ macd_trend = calculate_macd(data)
 # Print statements with alignment
 print(f"{'Power:' + f'{stock_power:.2f}':<{width_left}}{'Day%:' + f'{day_change:.2f}':>{width_right}}")
 print(f"{'Open%:' + f'{open_change:.2f}':<{width_left}}{'Depth:' + str(depth_value):>{width_right}}")
-print(f"{'MACD:' + macd_trend:<{width_left}}{'Act:' + ha_action:>{width_right}}")
+print(f"{'MACD:' + macd_trend:<{width_left}}{'' + ha_action:>{width_right}}")
 print(f"{'OnSMA:' + sma:<{width_left}}{'Sign:' + market_signal:>{width_right}}")
 
