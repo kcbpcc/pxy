943da71f4bb42c26ee9b549083797018e4c4fcf6
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-15 05:43:38 +0530

    Update pxy.py

--- sys/exe/run/SKC/pxy.py
+++ sys/exe/run/SKC/pxy.py
@@ -1,11 +1,1 @@
-from candlepxy import get_nifty50_data,dayprinter
-def main():
-    nifty50_ohlc = get_nifty50_data()
-    if not nifty50_ohlc.empty:
-        today_data = nifty50_ohlc.iloc[-1][OHLC_COLUMNS]
-        dayprinter(*today_data)
 
-if __name__ == "__main__":
-    main()
-
-deinit()  # Reset Colorama settings
