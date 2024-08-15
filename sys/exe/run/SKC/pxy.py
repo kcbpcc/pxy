d7cb8807d28e27f2fc09a0da52418ed1e691a1c8
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-15 05:39:30 +0530

    Create pxy.py

---
+++ sys/exe/run/SKC/pxy.py
@@ -0,0 +1,10 @@
+def main():
+    nifty50_ohlc = get_nifty50_data()
+    if not nifty50_ohlc.empty:
+        today_data = nifty50_ohlc.iloc[-1][OHLC_COLUMNS]
+        dayprinter(*today_data)
+
+if __name__ == "__main__":
+    main()
+
+deinit()  # Reset Colorama settings
