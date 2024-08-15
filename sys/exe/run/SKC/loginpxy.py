e45fe036a73cfe4465eba6b3b491a8ac97175449
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-15 07:27:12 +0530

    Create loginpxy.py

---
+++ sys/exe/run/SKC/loginpxy.py
@@ -0,0 +1,24 @@
+# common_utils.py
+
+import sys
+import traceback
+import logging
+from toolkit.currency import round_to_paise
+from login_get_kite import get_kite, remove_token
+from cnstpxy import dir_path
+
+def get_login():
+    try:
+        original_stdout = sys.stdout
+        with open('output.txt', 'w') as file:
+            sys.stdout = file
+            try:
+                broker = get_kite()
+                return broker
+            except Exception as e:
+                remove_token(dir_path)
+                print(traceback.format_exc())
+                logging.error(f"{str(e)} unable to get holdings")
+                sys.exit(1)
+    finally:
+        sys.stdout = original_stdout
