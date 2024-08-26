4b9b8c8c347ff66500f37bdf7300518b70e8a7b2
PXY <148702748+kcbpcc@users.noreply.github.com>
2024-08-26 06:55:23 +0530

    Update cntrloptprntpxy.py

--- sys/exe/run/cntrloptprntpxy.py
+++ sys/exe/run/cntrloptprntpxy.py
@@ -142,7 +142,7 @@ if peak == "PREPEAK":
     process_acvalue(acvalue)
 
 acvalue = retrieve_acvalue()
-
+cashout = 300000
 #print(" " * 42)
 column_width = 30
 left_aligned_format = "{:<" + str(column_width) + "}"
@@ -193,7 +193,7 @@ output_lines.append(
     right_aligned_format.format(
         f"{BRIGHT_GREEN if nmktpxy in ['Bull'] else (BRIGHT_RED if nmktpxy in ['Bear'] else GREY)}"
         f"{BOLD}®{RESET}{BRIGHT_YELLOW} {arrow_map.get(nmktpxy, '')}{RESET}    "
-        f"{'A/C-tPnL'.zfill(5)}:{BRIGHT_GREEN}{str(round(CnC_tCap_rounded + (total_invested_all / 100000) + cashround - 17.8, 2)).zfill(5)}{RESET}"
+        f"{'A/C-tPnL'.zfill(5)}:{BRIGHT_GREEN}{str(round(CnC_tCap_rounded + (total_invested_all + cashout/ 100000) + cashround - 17.8, 2)).zfill(5)}{RESET}"
     )
 )
 
