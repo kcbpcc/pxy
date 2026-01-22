ce3ad60b68d125dc061f4f99f09f2f3017341f1c
PXY <148702748+kcbpcc@users.noreply.github.com>
2026-01-22 22:11:19 +0530

    Create settings.py

---
+++ sh/py/settings.py
@@ -0,0 +1,19 @@
+# ==============================
+# FYERS APP CONFIG
+# ==============================
+
+APP_ID = "YOUR_APP_ID-100"      # must end with -100
+SECRET_KEY = "YOUR_SECRET_KEY"
+REDIRECT_URI = "https://your.redirect.url/"
+
+# ==============================
+# MODE
+# ==============================
+
+PAPER_TRADING = True
+
+# ==============================
+# FILE PATHS
+# ==============================
+
+TOKEN_FILE = "fyers_token.json"
